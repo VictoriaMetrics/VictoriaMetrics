00383652060a7a6b6c61f09445ecd1bca1e3ed2d
Aliaksandr Valialkin <valyala@gmail.com>
2020-01-02 23:43:29 +0200

    docs: refer to standalone MetricsQL package

--- lib/metricsql/doc.go
+++ lib/metricsql/doc.go
@@ -1,6 +1,6 @@
 // Package metricsql implements MetricsQL parser.
 //
-// This parser can parse PromQL. Additionally it can parse MetricsQL extensions.
+// This parser can parse PromQL. Additionally it can parse all the MetricsQL extensions.
 // See https://github.com/VictoriaMetrics/VictoriaMetrics/wiki/ExtendedPromQL for details about MetricsQL extensions.
 //
 // Usage:
@@ -10,6 +10,6 @@
 //        // parse error
 //    }
 //    // Now expr contains parsed MetricsQL as `*Expr` structs.
-//    // See metricsql.Parse examples for more details.
+//    // See Parse examples for more details.
 //
 package metricsql
