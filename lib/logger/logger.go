VictoriaMetrics/VictoriaMetrics

commit f6baee6efe865d665947429435bd6973beb591ab

lib/httpserver: log the caller of httpserver.Errorf
Previously log message contained `httpserver.Errorf`, not it contains the caller of `httpserver.Errorf`, which is more useful.

lib/logger/logger.go
@@ -68,6 +68,11 @@ func Errorf(format string, args ...interface{}) {
 	logLevel("ERROR", format, args...)
 }
 
+// ErrorfSkipframes logs error message and skips the given number of frames for the caller.
+func ErrorfSkipframes(skipframes int, format string, args ...interface{}) {
+	logLevelSkipframes(skipframes, "ERROR", format, args...)
+}
+
 // Fatalf logs fatal message and terminates the app.
 func Fatalf(format string, args ...interface{}) {
 	logLevel("FATAL", format, args...)
@@ -79,19 +84,15 @@ func Panicf(format string, args ...interface{}) {
 }
 
 func logLevel(level, format string, args ...interface{}) {
+	logLevelSkipframes(0, level, format, args...)
+}
+
+func logLevelSkipframes(skipframes int, level, format string, args ...interface{}) {
 	if shouldSkipLog(level) {
 		return
 	}
-
-	// rate limit ERROR log messages
-	if level == "ERROR" {
-		if n := atomic.AddUint64(&errorsLogged, 1); n > 10 {
-			return
-		}
-	}
-
 	msg := fmt.Sprintf(format, args...)
-	logMessage(level, msg, 3)
+	logMessage(level, msg, 3+skipframes)
 }
 
 func errorsLoggedCleaner() {
@@ -107,13 +108,18 @@ type logWriter struct {
 }
 
 func (lw *logWriter) Write(p []byte) (int, error) {
-	if !shouldSkipLog("ERROR") {
-		logMessage("ERROR", string(p), 4)
-	}
+	logLevelSkipframes(2, "ERROR", "%s", p)
 	return len(p), nil
 }
 
 func logMessage(level, msg string, skipframes int) {
+	// rate limit ERROR log messages
+	if level == "ERROR" {
+		if n := atomic.AddUint64(&errorsLogged, 1); n > 10 {
+			return
+		}
+	}
+
 	timestamp := time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
 	levelLowercase := strings.ToLower(level)
 	_, file, line, ok := runtime.Caller(skipframes)

lib/httpserver/httpserver.go
@@ -418,7 +418,7 @@ var (
 // Errorf writes formatted error message to w and to logger.
 func Errorf(w http.ResponseWriter, format string, args ...interface{}) {
 	errStr := fmt.Sprintf(format, args...)
-	logger.Errorf("%s", errStr)
+	logger.ErrorfSkipframes(1, "%s", errStr)
 
 	// Extract statusCode from args
 	statusCode := http.StatusBadRequest

commit 6caa9bb51bc214b66b23ef0f8737036d84f127ab

lib/logger: fix improperly set skipframes for all the logging functions
The bug has been introduced in the previous commit f6baee6efe865d665947429435bd6973beb591ab

lib/logger/logger.go
@@ -84,7 +84,7 @@ func Panicf(format string, args ...interface{}) {
 }
 
 func logLevel(level, format string, args ...interface{}) {
-	logLevelSkipframes(0, level, format, args...)
+	logLevelSkipframes(1, level, format, args...)
 }
 
 func logLevelSkipframes(skipframes int, level, format string, args ...interface{}) {