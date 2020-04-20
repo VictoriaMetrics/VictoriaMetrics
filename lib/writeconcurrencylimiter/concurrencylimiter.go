2dc5593b757478e06fff0a722840aefe3066b1a8
Aliaksandr Valialkin <valyala@gmail.com>
2020-04-20 21:02:54 +0300

    lib/writeconcurrencylimiter: improve docs for -maxConcurrentInserts command-line flag

--- lib/writeconcurrencylimiter/concurrencylimiter.go
+++ lib/writeconcurrencylimiter/concurrencylimiter.go
@@ -13,8 +13,9 @@ import (
 )
 
 var (
-	maxConcurrentInserts = flag.Int("maxConcurrentInserts", runtime.GOMAXPROCS(-1)*4, "The maximum number of concurrent inserts; see also -insert.maxQueueDuration")
-	maxQueueDuration     = flag.Duration("insert.maxQueueDuration", time.Minute, "The maximum duration for waiting in the queue for insert requests due to -maxConcurrentInserts")
+	maxConcurrentInserts = flag.Int("maxConcurrentInserts", runtime.GOMAXPROCS(-1)*4, "The maximum number of concurrent inserts. Default value should work for most cases, "+
+		"since it minimizes the overhead for concurrent inserts. This option is tigthly coupled with -insert.maxQueueDuration")
+	maxQueueDuration = flag.Duration("insert.maxQueueDuration", time.Minute, "The maximum duration for waiting in the queue for insert requests due to -maxConcurrentInserts")
 )
 
 // ch is the channel for limiting concurrent calls to Do.
