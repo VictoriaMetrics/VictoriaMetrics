408e7b95d699fe586c87f7cb1f614ceaba4373e2
肖贝贝 <xbsura@gmail.com>
2020-04-29 03:34:25 +0800

    Update scraper.go
    add bson

--- lib/promscrape/scraper.go
+++ lib/promscrape/scraper.go
@@ -14,6 +14,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/prompbmarshal"
 	"github.com/VictoriaMetrics/metrics"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var (
