515655f52615faa76605d2c889f188af9cb6fe0f
xinyulong <xinyulong@kuaishou.com>
2020-05-15 11:01:01 +0800

    Add Group Interval Update

--- app/vmalert/group_test.go
+++ app/vmalert/group_test.go
@@ -82,6 +82,7 @@ func TestUpdateWith(t *testing.T) {
 			sort.Slice(g.Rules, func(i, j int) bool {
 				return g.Rules[i].Name < g.Rules[j].Name
 			})
+
 			for i, r := range g.Rules {
 				got, want := r, tc.newRules[i]
 				if got.Name != want.Name {
