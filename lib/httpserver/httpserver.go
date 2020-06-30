81e3d4305f060cc2ec8559251ff9739f9dec1fdc
Aliaksandr Valialkin <valyala@gmail.com>
2020-07-01 00:53:43 +0300

    lib/httpserver: add Unwrap method to ErrorWithStatusCode, so `As` and `Is` functions in standard `errors` package may properly unwrap the error inside ErrorWithStatusCode

--- lib/httpserver/httpserver.go
+++ lib/httpserver/httpserver.go
@@ -455,6 +455,13 @@ type ErrorWithStatusCode struct {
 	StatusCode int
 }
 
+// Unwrap returns e.Err.
+//
+// This is used by standard errors package. See https://golang.org/pkg/errors
+func (e *ErrorWithStatusCode) Unwrap() error {
+	return e.Err
+}
+
 // Error implements error interface.
 func (e *ErrorWithStatusCode) Error() string {
 	return e.Err.Error()
