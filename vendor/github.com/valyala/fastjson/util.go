76064ba9e70bcfbad61288e274219ec79064f7e5
Aliaksandr Valialkin <valyala@gmail.com>
2020-08-05 11:53:41 +0300

    Perform conversion from string to []byte according to rule #6 at https://golang.org/pkg/unsafe/#Pointer

--- vendor/github.com/valyala/fastjson/util.go
+++ vendor/github.com/valyala/fastjson/util.go
@@ -9,13 +9,13 @@ func b2s(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
-func s2b(s string) []byte {
+func s2b(s string) (b []byte) {
 	strh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	var sh reflect.SliceHeader
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	sh.Data = strh.Data
 	sh.Len = strh.Len
 	sh.Cap = strh.Len
-	return *(*[]byte)(unsafe.Pointer(&sh))
+	return b
 }
 
 const maxStartEndStringLen = 80
