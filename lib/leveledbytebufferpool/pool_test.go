b4119bb51e8b2cf09579e5036d30b801cda275e6
Aliaksandr Valialkin <valyala@gmail.com>
2020-08-13 23:12:22 +0300

    lib/promscrape: reduce memory usage when scraping big number of targets
    Thanks to @dxtrzhang for the original idea at https://github.com/VictoriaMetrics/VictoriaMetrics/pull/688
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/689

---
+++ lib/leveledbytebufferpool/pool_test.go
@@ -0,0 +1,36 @@
+package leveledbytebufferpool
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGetPutConcurrent(t *testing.T) {
+	const concurrency = 10
+	doneCh := make(chan struct{}, concurrency)
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			for capacity := -1; capacity < 100; capacity++ {
+				bb := Get(capacity)
+				if len(bb.B) > 0 {
+					panic(fmt.Errorf("len(bb.B) must be zero; got %d", len(bb.B)))
+				}
+				if capacity < 0 {
+					capacity = 0
+				}
+				bb.B = append(bb.B, make([]byte, capacity)...)
+				Put(bb)
+			}
+			doneCh <- struct{}{}
+		}()
+	}
+	tc := time.After(10 * time.Second)
+	for i := 0; i < concurrency; i++ {
+		select {
+		case <-tc:
+			t.Fatalf("timeout")
+		case <-doneCh:
+		}
+	}
+}
