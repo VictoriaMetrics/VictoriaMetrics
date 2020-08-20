08b76cb26fc05a918c55ea4eb41b3e01706406f5
Roman Khavronenko <hagen1778@gmail.com>
2020-08-20 22:36:38 +0100

    vmalert: update `-rule` flag description to enforce quotes using (#709)
    Description for `-rule` flag uses as example specific chars like asterisks
    which could be interpreted wrong by different shells. To avoid this, description
    now contains quoted flag values.
    
    See also #708

--- app/vmalert/main.go
+++ app/vmalert/main.go
@@ -29,8 +29,8 @@ var (
 	rulePath = flagutil.NewArray("rule", `Path to the file with alert rules. 
 Supports patterns. Flag can be specified multiple times. 
 Examples:
- -rule /path/to/file. Path to a single file with alerting rules
- -rule dir/*.yaml -rule /*.yaml. Relative path to all .yaml files in "dir" folder, 
+ -rule="/path/to/file". Path to a single file with alerting rules
+ -rule="dir/*.yaml" -rule="/*.yaml". Relative path to all .yaml files in "dir" folder, 
 absolute path to all .yaml files in root.
 Rule files may contain %{ENV_VAR} placeholders, which are substituted by the corresponding env vars.`)
 
