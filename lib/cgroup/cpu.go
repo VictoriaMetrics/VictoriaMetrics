5c429658539b51cd6e8c4d3354ae0f14c9d4aa5f
Aliaksandr Valialkin <valyala@gmail.com>
2020-09-22 23:26:44 +0300

    lib/cgroup: attempt to obtain available CPU cores via /sys/devices/system/cpu/online
    See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/685#issuecomment-674423728

--- lib/cgroup/cpu.go
+++ lib/cgroup/cpu.go
@@ -1,8 +1,11 @@
 package cgroup
 
 import (
+	"io/ioutil"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 )
@@ -40,9 +43,57 @@ func getCPUQuota() float64 {
 	if err != nil {
 		return 0
 	}
+	if quotaUS <= 0 {
+		// The quota isn't set. This may be the case in multilevel containers.
+		// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/685#issuecomment-674423728
+		return getOnlineCPUCount()
+	}
 	periodUS, err := readInt64("/sys/fs/cgroup/cpu/cpu.cfs_period_us", "cat /sys/fs/cgroup/cpu$(cat /proc/self/cgroup | grep cpu, | cut -d: -f3)/cpu.cfs_period_us")
 	if err != nil {
 		return 0
 	}
 	return float64(quotaUS) / float64(periodUS)
+}
+
+func getOnlineCPUCount() float64 {
+	// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/685#issuecomment-674423728
+	data, err := ioutil.ReadFile("/sys/devices/system/cpu/online")
+	if err != nil {
+		return -1
+	}
+	n := float64(countCPUs(string(data)))
+	if n <= 0 {
+		return -1
+	}
+	// Add a half of CPU core, since it looks like actual cores is usually bigger than online cores.
+	// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/685#issuecomment-674423728
+	return n + 0.5
+}
+
+func countCPUs(data string) int {
+	data = strings.TrimSpace(data)
+	n := 0
+	for _, s := range strings.Split(data, ",") {
+		n++
+		if !strings.Contains(s, "-") {
+			if _, err := strconv.Atoi(s); err != nil {
+				return -1
+			}
+			continue
+		}
+		bounds := strings.Split(s, "-")
+		if len(bounds) != 2 {
+			return -1
+		}
+		start, err := strconv.Atoi(bounds[0])
+		if err != nil {
+			return -1
+		}
+		end, err := strconv.Atoi(bounds[1])
+		if err != nil {
+			return -1
+		}
+		n += end - start
+	}
+	return n
 }
