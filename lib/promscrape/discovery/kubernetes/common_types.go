92bc1afcee907d58192b780c943c4af408739590
Aliaksandr Valialkin <valyala@gmail.com>
2020-11-07 13:02:12 +0200

    lib/promscrape/discovery/kubernetes: reduce memory usage for labels when discovering big number of scrape targets by using string concatenation instead of fmt.Sprintf
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/825

--- lib/promscrape/discovery/kubernetes/common_types.go
+++ lib/promscrape/discovery/kubernetes/common_types.go
@@ -1,7 +1,6 @@
 package kubernetes
 
 import (
-	"fmt"
 	"net/url"
 	"strings"
 
@@ -23,13 +22,13 @@ type ObjectMeta struct {
 func (om *ObjectMeta) registerLabelsAndAnnotations(prefix string, m map[string]string) {
 	for _, lb := range om.Labels {
 		ln := discoveryutils.SanitizeLabelName(lb.Name)
-		m[fmt.Sprintf("%s_label_%s", prefix, ln)] = lb.Value
-		m[fmt.Sprintf("%s_labelpresent_%s", prefix, ln)] = "true"
+		m[prefix + "_label_" + ln] = lb.Value
+		m[prefix + "_labelpresent_" + ln] = "true"
 	}
 	for _, a := range om.Annotations {
 		an := discoveryutils.SanitizeLabelName(a.Name)
-		m[fmt.Sprintf("%s_annotation_%s", prefix, an)] = a.Value
-		m[fmt.Sprintf("%s_annotationpresent_%s", prefix, an)] = "true"
+		m[prefix + "_annotation_" + an] = a.Value
+		m[prefix + "_annotationpresent_" + an] = "true"
 	}
 }
 
