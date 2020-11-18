a3c8815776dfc7368ee23b9f5cb540153b5213f7
S.F <dohnuts@users.noreply.github.com>
2020-11-18 10:55:10 -0500

    Perl logger code review and fix restart
    On restart the logger can read an empty line, ignore it.
    
    Thanks Andrew for code review.

--- ports/OpenBSD/VictoriaMetrics/pkg/vmlogger.pl
+++ ports/OpenBSD/VictoriaMetrics/pkg/vmlogger.pl
@@ -1,18 +1,22 @@
 #!/usr/bin/perl
+# no Preamble to keep it light
 use Sys::Syslog qw(:standard :macros);
 
 openlog($ARGV[0], "pid", "daemon");
+my %lvl_map = (
+    'panic' => LOG_EMERG,
+    'fatal' => LOG_CRIT,
+    'error' => LOG_ERR,
+    'warn'  => LOG_WARNING,
+    'info'  => LOG_INFO,
+);
 
 while (my $l = <STDIN>) {
-  my @d = split /\t/, $l;
-  # go level : "INFO", "WARN", "ERROR", "FATAL", "PANIC":
-  my $lvl = $d[0];
-  $lvl = LOG_EMERG if ($lvl eq 'panic');
-  $lvl = 'crit' if ($lvl eq 'fatal');
-  $lvl = 'err' if ($lvl eq 'error');
-  $lvl = 'warning' if ($lvl eq 'warn');
-  chomp $d[2];
-  syslog( $lvl, $d[2] );
+  my ($lvl, undef, $message) = split /\t/, $_, 3;
+  next unless $message;
+  $lvl = $lvl_map{ lc $lvl } || 'warn';
+  chomp $message;
+  syslog( $lvl, $message );
 }
 
 closelog();
