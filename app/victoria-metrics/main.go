4523b552224a9708a5471ad2a8d6d54f329c7850
f41gh7 <nik@victoriametrics.com>
2020-12-15 13:19:11 +0300

    adds custom apiPathLinks for victoria-metrics / api help

--- app/victoria-metrics/main.go
+++ app/victoria-metrics/main.go
@@ -32,6 +32,8 @@ var (
 		"Unknown config entries are allowed in -promscrape.config by default. This can be changed with -promscrape.config.strictParse")
 )
 
+var customAPIPathList [][]string // custom api help links [["/api","doc"]] without http.pathPrefix.
+
 func main() {
 	// Write flags and help message to stdout, since it is easier to grep or pipe.
 	flag.CommandLine.SetOutput(os.Stdout)
@@ -112,6 +114,10 @@ func writeAPIHelp(w io.Writer, pathList [][]string) {
 	for _, p := range pathList {
 		p, doc := p[0], p[1]
 		p = path.Join(pathPrefix, p)
+		fmt.Fprintf(w, "<a href='%s'>%q</a> - %s<br/>", p, p, doc)
+	}
+	for _, p := range customAPIPathList {
+		p, doc := p[0], p[1]
 		fmt.Fprintf(w, "<a href='%s'>%q</a> - %s<br/>", p, p, doc)
 	}
 }
