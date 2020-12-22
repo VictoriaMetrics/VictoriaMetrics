13e1d838ac25d70659a0f14e1388dca07dc7defd
f41gh7 <nik@victoriametrics.com>
2020-12-22 13:44:50 +0300

    fixes panic https://github.com/VictoriaMetrics/helm-charts/issues/89

--- app/vmselect/prometheus/prometheus.go
+++ app/vmselect/prometheus/prometheus.go
@@ -501,8 +501,12 @@ var deleteDuration = metrics.NewSummary(`vm_request_duration_seconds{path="/api/
 
 func resetRollupResultCaches() {
 	if len(*selectNodes) == 0 {
-		logger.Panicf("BUG: missing -selectNode flag")
+		logger.Warnf("missing -selectNode flag, cache reset request wont be propagated to the other select nodes." +
+			"you can fix it by adding flag with all vmselect node addresses from your cluster," +
+			" fox example: -selectNode select-addr-1:8481,select-addr-2:8481")
 	}
+	// reset local cache.
+	promql.ResetRollupResultCache()
 	for _, selectNode := range *selectNodes {
 		callURL := fmt.Sprintf("http://%s/internal/resetRollupResultCache", selectNode)
 		resp, err := httpClient.Get(callURL)
