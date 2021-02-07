c5d8197547ee3a0ef5c8b6e56da27f82e125c3cf
f41gh7 <nik@victoriametrics.com>
2021-02-07 23:29:04 +0300

    refactored cgroups limits, adds tests, remove os.Exec

--- lib/cgroup/cpu_test.go
+++ lib/cgroup/cpu_test.go
@@ -22,3 +22,35 @@ func TestCountCPUs(t *testing.T) {
 	f("0-3", 4)
 	f("0-6", 7)
 }
+
+func TestGetCPUStatQuota(t *testing.T) {
+	f := func(sysPath, cgroupPath string, want int64, wantErr bool) {
+		t.Helper()
+		got, err := getCPUStat(sysPath, cgroupPath, "cpu.cfs_quota_us")
+		if (err != nil && !wantErr) || (err == nil && wantErr) {
+			t.Fatalf("unxpected error value: %v, want err: %v", err, wantErr)
+		}
+		if got != want {
+			t.Fatalf("unxpected result, got: %d, want %d", got, want)
+		}
+	}
+	f("testdata/", "testdata/self/cgroup", -1, false)
+	f("testdata/cgroup", "testdata/self/cgroup", 10, false)
+	f("testdata/", "testdata/missing_folder", 0, true)
+}
+
+func TestGetCPUStatPeriod(t *testing.T) {
+	f := func(sysPath, cgroupPath string, want int64, wantErr bool) {
+		t.Helper()
+		got, err := getCPUStat(sysPath, cgroupPath, "cpu.cfs_period_us")
+		if (err != nil && !wantErr) || (err == nil && wantErr) {
+			t.Fatalf("unxpected error value: %v, want err: %v", err, wantErr)
+		}
+		if got != want {
+			t.Fatalf("unxpected result, got: %d, want %d", got, want)
+		}
+	}
+	f("testdata/", "testdata/self/cgroup", 100000, false)
+	f("testdata/cgroup", "testdata/self/cgroup", 500000, false)
+	f("testdata/", "testdata/missing_folder", 0, true)
+}
