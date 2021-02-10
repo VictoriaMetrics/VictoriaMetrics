5d5f0b0627e9719fa214649b58b282dbfefef79e
Aliaksandr Valialkin <valyala@gmail.com>
2021-02-10 17:55:33 +0200

    lib/storage: load metadata before loading indexdb, since indexdb depends on the metadata

--- lib/storage/storage.go
+++ lib/storage/storage.go
@@ -167,10 +167,17 @@ func OpenStorage(path string, retentionMsecs int64) (*Storage, error) {
 
 	s.prefetchedMetricIDs.Store(&uint64set.Set{})
 
+	// Load metadata
+	metadataDir := path + "/metadata"
+	isEmptyDB := !fs.IsPathExist(path + "/indexdb")
+	if err := fs.MkdirAllIfNotExist(metadataDir); err != nil {
+		return nil, fmt.Errorf("cannot create %q: %w", metadataDir, err)
+	}
+	s.minTimestampForCompositeIndex = mustGetMinTimestampForCompositeIndex(metadataDir, isEmptyDB)
+
 	// Load indexdb
 	idbPath := path + "/indexdb"
 	idbSnapshotsPath := idbPath + "/snapshots"
-	isEmptyDB := !fs.IsPathExist(idbPath)
 	if err := fs.MkdirAllIfNotExist(idbSnapshotsPath); err != nil {
 		return nil, fmt.Errorf("cannot create %q: %w", idbSnapshotsPath, err)
 	}
@@ -189,13 +196,6 @@ func OpenStorage(path string, retentionMsecs int64) (*Storage, error) {
 		return nil, fmt.Errorf("cannot open table at %q: %w", tablePath, err)
 	}
 	s.tb = tb
-
-	// Load metadata
-	metadataDir := path + "/metadata"
-	if err := fs.MkdirAllIfNotExist(metadataDir); err != nil {
-		return nil, fmt.Errorf("cannot create %q: %w", metadataDir, err)
-	}
-	s.minTimestampForCompositeIndex = mustGetMinTimestampForCompositeIndex(metadataDir, isEmptyDB)
 
 	s.startCurrHourMetricIDsUpdater()
 	s.startNextDayMetricIDsUpdater()
