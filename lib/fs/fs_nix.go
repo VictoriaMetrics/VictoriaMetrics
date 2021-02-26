7f1302688f6896c078b2e79705d0f936330fe544
Aliaksandr Valialkin <valyala@gmail.com>
2021-02-27 01:01:47 +0200

    lib/fs: follow-up after f3a03c416408bf7d81dee438a10566428962c5ee

--- lib/fs/fs_nix.go
+++ lib/fs/fs_nix.go
@@ -3,26 +3,8 @@
 package fs
 
 import (
-	"os"
-
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 	"golang.org/x/sys/unix"
 )
-
-func mustGetFreeSpace(path string) uint64 {
-	d, err := os.Open(path)
-	if err != nil {
-		logger.Panicf("FATAL: cannot determine free disk space on %q: %s", path, err)
-	}
-	defer MustClose(d)
-
-	fd := d.Fd()
-	var stat unix.Statfs_t
-	if err := unix.Fstatfs(int(fd), &stat); err != nil {
-		logger.Panicf("FATAL: cannot determine free disk space on %q: %s", path, err)
-	}
-	return freeSpace(stat)
-}
 
 func freeSpace(stat unix.Statfs_t) uint64 {
 	return uint64(stat.Bavail) * uint64(stat.Bsize)
