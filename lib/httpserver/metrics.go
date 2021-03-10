bc5c4add89c82c557f7d0daedbef099bdd4658c5
Aliaksandr Valialkin <valyala@gmail.com>
2021-03-10 12:01:18 +0200

    lib/httpserver: export `vm_available_memory_bytes` and `vm_available_cpu_cores` metrics
    These metrics are useful for tracking the available memory and CPU cores for VictoriaMetrics apps.

--- lib/httpserver/metrics.go
+++ lib/httpserver/metrics.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/buildinfo"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/cgroup"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/flagutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/memory"
 	"github.com/VictoriaMetrics/metrics"
@@ -48,6 +49,8 @@ func writePrometheusMetrics(w io.Writer) {
 	fmt.Fprintf(w, "vm_app_version{version=%q, short_version=%q} 1\n", buildinfo.Version,
 		versionRe.FindString(buildinfo.Version))
 	fmt.Fprintf(w, "vm_allowed_memory_bytes %d\n", memory.Allowed())
+	fmt.Fprintf(w, "vm_available_memory_bytes %d\n", memory.Allowed()+memory.Remaining())
+	fmt.Fprintf(w, "vm_available_cpu_cores %d\n", cgroup.AvailableCPUs())
 
 	// Export start time and uptime in seconds
 	fmt.Fprintf(w, "vm_app_start_timestamp %d\n", startTime.Unix())
