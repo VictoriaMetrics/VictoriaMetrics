45dabfac1b58e395fb9f36b3ff00150179ea5a12
Aliaksandr Valialkin <valyala@gmail.com>
2021-03-17 15:09:40 +0200

    lib/storage: faster move heavy filters to the end of list

--- lib/storage/index_db.go
+++ lib/storage/index_db.go
@@ -2864,7 +2864,7 @@ func (is *indexSearch) getMetricIDsForDateAndFilters(date uint64, tfs *TagFilter
 		if err != nil {
 			if errors.Is(err, errTooManyLoops) {
 				// The tf took too many loops compared to the next filter. Postpone applying this filter.
-				storeLoopsCount(&tfw, loopsCount+1)
+				storeLoopsCount(&tfw, 2*loopsCount)
 				tfwsRemaining = append(tfwsRemaining, tfw)
 				continue
 			}
@@ -2961,7 +2961,7 @@ func (is *indexSearch) getMetricIDsForDateAndFilters(date uint64, tfs *TagFilter
 		if err != nil {
 			if errors.Is(err, errTooManyLoops) {
 				// Postpone tf, since it took more loops than the next filter may need.
-				storeFilterLoopsCount(&tfw, filterLoopsCount+1)
+				storeFilterLoopsCount(&tfw, 2*filterLoopsCount)
 				tfsPostponed = append(tfsPostponed, tf)
 				continue
 			}
