6a81a89b3dd11befd320dbffd8cbc56dbf5099f0
Nikolay <nik@victoriametrics.com>
2021-04-20 10:51:03 +0300

    adds query params support for vmauth urlPrefix (#1226)
    * adds query params support for vmauth urlPrefix
    
    * Update app/vmauth/example_config.yml
    
    * Update app/vmauth/example_config.yml
    
    Co-authored-by: Aliaksandr Valialkin <valyala@gmail.com>

--- app/vmauth/target_url.go
+++ app/vmauth/target_url.go
@@ -7,6 +7,32 @@ import (
 	"strings"
 )
 
+func mergeURLs(uiURL string, requestURI *url.URL) (string, error) {
+	prefixURL, err := url.Parse(uiURL)
+	if err != nil {
+		return "", fmt.Errorf("BUG - cannot parse userInfo url: %q, err: %w", uiURL, err)
+	}
+	prefixURL.Path += requestURI.Path
+	requestParams := requestURI.Query()
+	// fast path
+	if len(requestParams) == 0 {
+		return prefixURL.String(), nil
+	}
+	// merge query parameters from requests.
+	userInfoParams := prefixURL.Query()
+	for k, v := range requestParams {
+		// skip clashed query params from original request
+		if exist := userInfoParams.Get(k); len(exist) > 0 {
+			continue
+		}
+		for i := range v {
+			userInfoParams.Add(k, v[i])
+		}
+	}
+	prefixURL.RawQuery = userInfoParams.Encode()
+	return prefixURL.String(), nil
+}
+
 func createTargetURL(ui *UserInfo, uOrig *url.URL) (string, error) {
 	u, err := url.Parse(uOrig.String())
 	if err != nil {
@@ -20,12 +46,12 @@ func createTargetURL(ui *UserInfo, uOrig *url.URL) (string, error) {
 	for _, e := range ui.URLMap {
 		for _, sp := range e.SrcPaths {
 			if sp.match(u.Path) {
-				return e.URLPrefix + u.RequestURI(), nil
+				return mergeURLs(e.URLPrefix, u)
 			}
 		}
 	}
 	if len(ui.URLPrefix) > 0 {
-		return ui.URLPrefix + u.RequestURI(), nil
+		return mergeURLs(ui.URLPrefix, u)
 	}
 	return "", fmt.Errorf("missing route for %q", u)
 }
