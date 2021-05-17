8764b0ae213df273c362d51342134ce2fe38a6f7
Aliaksandr Valialkin <valyala@gmail.com>
2021-05-17 23:45:20 +0300

    lib/promscrape/discovery/kubernetes: key ScrapeWork objects by urlWatcher instead of namespace
    This makes the code less fragile if urlWatcher would depend on additional to namepsace properties.
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1170
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1240

--- lib/promscrape/discovery/kubernetes/api_watcher_test.go
+++ lib/promscrape/discovery/kubernetes/api_watcher_test.go
@@ -11,54 +11,51 @@ import (
 )
 
 func TestGetAPIPathsWithNamespaces(t *testing.T) {
-	f := func(role string, namespaces []string, selectors []Selector, expectedPaths, expectedNamespaces []string) {
+	f := func(role string, namespaces []string, selectors []Selector, expectedPaths []string) {
 		t.Helper()
-		paths, resultNamespaces := getAPIPathsWithNamespaces(role, namespaces, selectors)
+		paths := getAPIPathsWithNamespaces(role, namespaces, selectors)
 		if !reflect.DeepEqual(paths, expectedPaths) {
 			t.Fatalf("unexpected paths; got\n%q\nwant\n%q", paths, expectedPaths)
-		}
-		if !reflect.DeepEqual(resultNamespaces, expectedNamespaces) {
-			t.Fatalf("unexpected namespaces; got\n%q\nwant\n%q", resultNamespaces, expectedNamespaces)
 		}
 	}
 
 	// role=node
-	f("node", nil, nil, []string{"/api/v1/nodes"}, []string{""})
-	f("node", []string{"foo", "bar"}, nil, []string{"/api/v1/nodes"}, []string{""})
+	f("node", nil, nil, []string{"/api/v1/nodes"})
+	f("node", []string{"foo", "bar"}, nil, []string{"/api/v1/nodes"})
 	f("node", nil, []Selector{
 		{
 			Role:  "pod",
 			Label: "foo",
 			Field: "bar",
 		},
-	}, []string{"/api/v1/nodes"}, []string{""})
+	}, []string{"/api/v1/nodes"})
 	f("node", nil, []Selector{
 		{
 			Role:  "node",
 			Label: "foo",
 			Field: "bar",
 		},
-	}, []string{"/api/v1/nodes?labelSelector=foo&fieldSelector=bar"}, []string{""})
+	}, []string{"/api/v1/nodes?labelSelector=foo&fieldSelector=bar"})
 	f("node", []string{"x", "y"}, []Selector{
 		{
 			Role:  "node",
 			Label: "foo",
 			Field: "bar",
 		},
-	}, []string{"/api/v1/nodes?labelSelector=foo&fieldSelector=bar"}, []string{""})
+	}, []string{"/api/v1/nodes?labelSelector=foo&fieldSelector=bar"})
 
 	// role=pod
-	f("pod", nil, nil, []string{"/api/v1/pods"}, []string{""})
+	f("pod", nil, nil, []string{"/api/v1/pods"})
 	f("pod", []string{"foo", "bar"}, nil, []string{
 		"/api/v1/namespaces/foo/pods",
 		"/api/v1/namespaces/bar/pods",
-	}, []string{"foo", "bar"})
+	})
 	f("pod", nil, []Selector{
 		{
 			Role:  "node",
 			Label: "foo",
 		},
-	}, []string{"/api/v1/pods"}, []string{""})
+	}, []string{"/api/v1/pods"})
 	f("pod", nil, []Selector{
 		{
 			Role:  "pod",
@@ -69,7 +66,7 @@ func TestGetAPIPathsWithNamespaces(t *testing.T) {
 			Label: "x",
 			Field: "y",
 		},
-	}, []string{"/api/v1/pods?labelSelector=foo%2Cx&fieldSelector=y"}, []string{""})
+	}, []string{"/api/v1/pods?labelSelector=foo%2Cx&fieldSelector=y"})
 	f("pod", []string{"x", "y"}, []Selector{
 		{
 			Role:  "pod",
@@ -83,14 +80,14 @@ func TestGetAPIPathsWithNamespaces(t *testing.T) {
 	}, []string{
 		"/api/v1/namespaces/x/pods?labelSelector=foo%2Cx&fieldSelector=y",
 		"/api/v1/namespaces/y/pods?labelSelector=foo%2Cx&fieldSelector=y",
-	}, []string{"x", "y"})
+	})
 
 	// role=service
-	f("service", nil, nil, []string{"/api/v1/services"}, []string{""})
+	f("service", nil, nil, []string{"/api/v1/services"})
 	f("service", []string{"x", "y"}, nil, []string{
 		"/api/v1/namespaces/x/services",
 		"/api/v1/namespaces/y/services",
-	}, []string{"x", "y"})
+	})
 	f("service", nil, []Selector{
 		{
 			Role:  "node",
@@ -100,7 +97,7 @@ func TestGetAPIPathsWithNamespaces(t *testing.T) {
 			Role:  "service",
 			Field: "bar",
 		},
-	}, []string{"/api/v1/services?fieldSelector=bar"}, []string{""})
+	}, []string{"/api/v1/services?fieldSelector=bar"})
 	f("service", []string{"x", "y"}, []Selector{
 		{
 			Role:  "service",
@@ -109,14 +106,14 @@ func TestGetAPIPathsWithNamespaces(t *testing.T) {
 	}, []string{
 		"/api/v1/namespaces/x/services?labelSelector=abc%3Dde",
 		"/api/v1/namespaces/y/services?labelSelector=abc%3Dde",
-	}, []string{"x", "y"})
+	})
 
 	// role=endpoints
-	f("endpoints", nil, nil, []string{"/api/v1/endpoints"}, []string{""})
+	f("endpoints", nil, nil, []string{"/api/v1/endpoints"})
 	f("endpoints", []string{"x", "y"}, nil, []string{
 		"/api/v1/namespaces/x/endpoints",
 		"/api/v1/namespaces/y/endpoints",
-	}, []string{"x", "y"})
+	})
 	f("endpoints", []string{"x", "y"}, []Selector{
 		{
 			Role:  "endpoints",
@@ -129,10 +126,10 @@ func TestGetAPIPathsWithNamespaces(t *testing.T) {
 	}, []string{
 		"/api/v1/namespaces/x/endpoints?labelSelector=bbb",
 		"/api/v1/namespaces/y/endpoints?labelSelector=bbb",
-	}, []string{"x", "y"})
+	})
 
 	// role=endpointslices
-	f("endpointslices", nil, nil, []string{"/apis/discovery.k8s.io/v1beta1/endpointslices"}, []string{""})
+	f("endpointslices", nil, nil, []string{"/apis/discovery.k8s.io/v1beta1/endpointslices"})
 	f("endpointslices", []string{"x", "y"}, []Selector{
 		{
 			Role:  "endpointslices",
@@ -142,10 +139,10 @@ func TestGetAPIPathsWithNamespaces(t *testing.T) {
 	}, []string{
 		"/apis/discovery.k8s.io/v1beta1/namespaces/x/endpointslices?labelSelector=label&fieldSelector=field",
 		"/apis/discovery.k8s.io/v1beta1/namespaces/y/endpointslices?labelSelector=label&fieldSelector=field",
-	}, []string{"x", "y"})
+	})
 
 	// role=ingress
-	f("ingress", nil, nil, []string{"/apis/networking.k8s.io/v1beta1/ingresses"}, []string{""})
+	f("ingress", nil, nil, []string{"/apis/networking.k8s.io/v1beta1/ingresses"})
 	f("ingress", []string{"x", "y"}, []Selector{
 		{
 			Role:  "node",
@@ -166,7 +163,7 @@ func TestGetAPIPathsWithNamespaces(t *testing.T) {
 	}, []string{
 		"/apis/networking.k8s.io/v1beta1/namespaces/x/ingresses?labelSelector=cde%2Cbaaa&fieldSelector=abc",
 		"/apis/networking.k8s.io/v1beta1/namespaces/y/ingresses?labelSelector=cde%2Cbaaa&fieldSelector=abc",
-	}, []string{"x", "y"})
+	})
 }
 
 func TestParseBookmark(t *testing.T) {
