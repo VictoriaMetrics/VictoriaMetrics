4adf6c9766fabec9f764e76456ac96c54cc7b48c
Aliaksandr Valialkin <valyala@gmail.com>
2021-06-22 13:40:33 +0300

    lib/promscrape/discovery/http: follow up after e307bbb29a6b862b998a43a8ad8913b3ccb2c6e7

--- lib/promscrape/discovery/http/http_test.go
+++ lib/promscrape/discovery/http/http_test.go
@@ -23,27 +23,29 @@ func Test_addHTTPTargetLabels(t *testing.T) {
 				src: []httpGroupTarget{
 					{
 						Targets: []string{"127.0.0.1:9100", "127.0.0.2:91001"},
-						Labels:  map[string]string{"__meta__kubernetes_pod": "pod-1", "__meta_consul_dc": "dc-2"},
+						Labels:  map[string]string{"__meta_kubernetes_pod": "pod-1", "__meta_consul_dc": "dc-2"},
 					},
 				},
 			},
 			want: [][]prompbmarshal.Label{
 				discoveryutils.GetSortedLabels(map[string]string{
-					"__address__":            "127.0.0.1:9100",
-					"__meta__kubernetes_pod": "pod-1",
-					"__meta_consul_dc":       "dc-2",
+					"__address__":           "127.0.0.1:9100",
+					"__meta_kubernetes_pod": "pod-1",
+					"__meta_consul_dc":      "dc-2",
+					"__meta_url":            "http://foo.bar/baz?aaa=bb",
 				}),
 				discoveryutils.GetSortedLabels(map[string]string{
-					"__address__":            "127.0.0.2:91001",
-					"__meta__kubernetes_pod": "pod-1",
-					"__meta_consul_dc":       "dc-2",
+					"__address__":           "127.0.0.2:91001",
+					"__meta_kubernetes_pod": "pod-1",
+					"__meta_consul_dc":      "dc-2",
+					"__meta_url":            "http://foo.bar/baz?aaa=bb",
 				}),
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := addHTTPTargetLabels(tt.args.src)
+			got := addHTTPTargetLabels(tt.args.src, "http://foo.bar/baz?aaa=bb")
 			var sortedLabelss [][]prompbmarshal.Label
 			for _, labels := range got {
 				sortedLabelss = append(sortedLabelss, discoveryutils.GetSortedLabels(labels))
