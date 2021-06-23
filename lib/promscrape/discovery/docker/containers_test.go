e4182509ee54c3abf5a8bc9524d8721e4f2f7f2b
Megrez Lu <lujiajing1126@gmail.com>
2021-06-23 19:31:20 +0800

    add test

---
+++ lib/promscrape/discovery/docker/containers_test.go
@@ -0,0 +1,222 @@
+package docker
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseContainers(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []container
+		wantErr bool
+	}{
+		{
+			name: "parse two containers",
+			args: args{
+				data: []byte(`[
+  {
+    "Id": "90bc3b31aa13da5c0b11af2e228d54b38428a84e25d4e249ae9e9c95e51a0700",
+    "Names": [
+      "/crow-server"
+    ],
+    "ImageID": "sha256:11045371758ccf9468d807d53d1e1faa100c8ebabe87296bc107d52bdf983378",
+    "Created": 1624440429,
+    "Ports": [
+      {
+        "IP": "0.0.0.0",
+        "PrivatePort": 8080,
+        "PublicPort": 18081,
+        "Type": "tcp"
+      }
+    ],
+    "Labels": {
+      "com.docker.compose.config-hash": "c9f0bd5bb31921f94cff367d819a30a0cc08d4399080897a6c5cd74b983156ec",
+      "com.docker.compose.container-number": "1",
+      "com.docker.compose.oneoff": "False",
+      "com.docker.compose.project": "crowserver",
+      "com.docker.compose.service": "crow-server",
+      "com.docker.compose.version": "1.11.2"
+    },
+    "State": "running",
+    "Status": "Up 2 hours",
+    "HostConfig": {
+      "NetworkMode": "bridge"
+    },
+    "NetworkSettings": {
+      "Networks": {
+        "bridge": {
+          "IPAMConfig": null,
+          "Links": null,
+          "Aliases": null,
+          "NetworkID": "1dd8d1a8bef59943345c7231d7ce8268333ff5a8c5b3c94881e6b4742b447634",
+          "EndpointID": "2bcd751c98578ad1ce3d6798077a3e110535f3dcb0b0735cc84bd1e03905d907",
+          "Gateway": "172.17.0.1",
+          "IPAddress": "172.17.0.2",
+          "IPPrefixLen": 16,
+          "IPv6Gateway": "",
+          "GlobalIPv6Address": "",
+          "GlobalIPv6PrefixLen": 0,
+          "DriverOpts": null
+        }
+      }
+    }
+  },
+  {
+    "Id": "0e0f72a6eb7d9fb443f0426a66f7b8dd7d3283ab7e3a308b2bed584ac03a33dc",
+    "Names": [
+      "/crow-web"
+    ],
+    "ImageID": "sha256:36e04dd2f67950179ab62a4ebd1b8b741232263fa1f210496a53335d5820b3af",
+    "Created": 1618302442,
+    "Ports": [
+      {
+        "IP": "0.0.0.0",
+        "PrivatePort": 8080,
+        "PublicPort": 18082,
+        "Type": "tcp"
+      }
+    ],
+    "Labels": {
+      "com.docker.compose.config-hash": "d99ebd0fde8512366c2d78c367e95ddc74528bb60b7cf0c991c9f4835981e00e",
+      "com.docker.compose.container-number": "1",
+      "com.docker.compose.oneoff": "False",
+      "com.docker.compose.project": "crowweb",
+      "com.docker.compose.service": "crow-web",
+      "com.docker.compose.version": "1.11.2"
+    },
+    "State": "running",
+    "Status": "Up 2 months",
+    "HostConfig": {
+      "NetworkMode": "bridge"
+    },
+    "NetworkSettings": {
+      "Networks": {
+        "bridge": {
+          "IPAMConfig": null,
+          "Links": null,
+          "Aliases": null,
+          "NetworkID": "1dd8d1a8bef59943345c7231d7ce8268333ff5a8c5b3c94881e6b4742b447634",
+          "EndpointID": "78d751dfa31923d73e001e5dc5751ad6f9f5ffeb88056fc950f0a952d7f24302",
+          "Gateway": "172.17.0.1",
+          "IPAddress": "172.17.0.3",
+          "IPPrefixLen": 16,
+          "IPv6Gateway": "",
+          "GlobalIPv6Address": "",
+          "GlobalIPv6PrefixLen": 0,
+          "DriverOpts": null
+        }
+      }
+    }
+  }
+]`),
+			},
+			want: []container{
+				{
+					Id:    "90bc3b31aa13da5c0b11af2e228d54b38428a84e25d4e249ae9e9c95e51a0700",
+					Names: []string{"/crow-server"},
+					Labels: map[string]string{
+						"com.docker.compose.config-hash":      "c9f0bd5bb31921f94cff367d819a30a0cc08d4399080897a6c5cd74b983156ec",
+						"com.docker.compose.container-number": "1",
+						"com.docker.compose.oneoff":           "False",
+						"com.docker.compose.project":          "crowserver",
+						"com.docker.compose.service":          "crow-server",
+						"com.docker.compose.version":          "1.11.2",
+					},
+					Ports: []struct {
+						IP          string
+						PrivatePort int
+						PublicPort  int
+						Type        string
+					}{{
+						IP:          "0.0.0.0",
+						PrivatePort: 8080,
+						PublicPort:  18081,
+						Type:        "tcp",
+					}},
+					HostConfig: struct {
+						NetworkMode string
+					}{
+						NetworkMode: "bridge",
+					},
+					NetworkSettings: struct {
+						Networks map[string]struct {
+							IPAddress string
+							NetworkID string
+						}
+					}{
+						Networks: map[string]struct {
+							IPAddress string
+							NetworkID string
+						}{
+							"bridge": {
+								IPAddress: "172.17.0.2",
+								NetworkID: "1dd8d1a8bef59943345c7231d7ce8268333ff5a8c5b3c94881e6b4742b447634",
+							},
+						},
+					},
+				},
+				{
+					Id:    "0e0f72a6eb7d9fb443f0426a66f7b8dd7d3283ab7e3a308b2bed584ac03a33dc",
+					Names: []string{"/crow-web"},
+					Labels: map[string]string{
+						"com.docker.compose.config-hash":      "d99ebd0fde8512366c2d78c367e95ddc74528bb60b7cf0c991c9f4835981e00e",
+						"com.docker.compose.container-number": "1",
+						"com.docker.compose.oneoff":           "False",
+						"com.docker.compose.project":          "crowweb",
+						"com.docker.compose.service":          "crow-web",
+						"com.docker.compose.version":          "1.11.2",
+					},
+					Ports: []struct {
+						IP          string
+						PrivatePort int
+						PublicPort  int
+						Type        string
+					}{{
+						IP:          "0.0.0.0",
+						PrivatePort: 8080,
+						PublicPort:  18082,
+						Type:        "tcp",
+					}},
+					HostConfig: struct {
+						NetworkMode string
+					}{
+						NetworkMode: "bridge",
+					},
+					NetworkSettings: struct {
+						Networks map[string]struct {
+							IPAddress string
+							NetworkID string
+						}
+					}{
+						Networks: map[string]struct {
+							IPAddress string
+							NetworkID string
+						}{
+							"bridge": {
+								IPAddress: "172.17.0.3",
+								NetworkID: "1dd8d1a8bef59943345c7231d7ce8268333ff5a8c5b3c94881e6b4742b447634",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseContainers(tt.args.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseContainers() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseNetworks() \ngot  %v, \nwant %v", got, tt.want)
+			}
+		})
+	}
+}
