f6bb13089856ccfd77609662c6116a73723f6b3c
Aliaksandr Valialkin <valyala@gmail.com>
2021-07-02 15:02:24 +0300

    app/{vmselect,vmstorage}: clarify the description for `-dedup.minScrapeInterval` command-line flag
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1426

--- app/victoria-metrics/main.go
+++ app/victoria-metrics/main.go
@@ -24,9 +24,8 @@ import (
 
 var (
 	httpListenAddr    = flag.String("httpListenAddr", ":8428", "TCP address to listen for http connections")
-	minScrapeInterval = flag.Duration("dedup.minScrapeInterval", 0, "Remove superflouos samples from time series if they are located closer to each other than this duration. "+
-		"This may be useful for reducing overhead when multiple identically configured Prometheus instances write data to the same VictoriaMetrics. "+
-		"Deduplication is disabled if the -dedup.minScrapeInterval is 0")
+	minScrapeInterval = flag.Duration("dedup.minScrapeInterval", 0, "Leave only the first sample in every time series per each discrete interval "+
+		"equal to -dedup.minScrapeInterval > 0. See https://docs.victoriametrics.com/#deduplication for details")
 	dryRun = flag.Bool("dryRun", false, "Whether to check only -promscrape.config and then exit. "+
 		"Unknown config entries are allowed in -promscrape.config by default. This can be changed with -promscrape.config.strictParse")
 )
