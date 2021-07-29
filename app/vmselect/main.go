ef3c3bc5e15630ea1874a6869c6428cacda90506
f41gh7 <nik@victoriametrics.com>
2021-07-29 06:23:09 +0300

    adds /rules and /alerts api for grafana

--- app/vmselect/main.go
+++ app/vmselect/main.go
@@ -381,14 +381,14 @@ func RequestHandler(w http.ResponseWriter, r *http.Request) bool {
 			return true
 		}
 		return true
-	case "/api/v1/rules":
+	case "/rules", "/api/v1/rules":
 		// Return dumb placeholder for https://prometheus.io/docs/prometheus/latest/querying/api/#rules
 		rulesRequests.Inc()
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		fmt.Fprintf(w, "%s", `{"status":"success","data":{"groups":[]}}`)
 		return true
-	case "/api/v1/alerts":
-		// Return dumb placehloder for https://prometheus.io/docs/prometheus/latest/querying/api/#alerts
+	case "/alerts", "/api/v1/alerts":
+		// Return dumb placeholder for https://prometheus.io/docs/prometheus/latest/querying/api/#alerts
 		alertsRequests.Inc()
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		fmt.Fprintf(w, "%s", `{"status":"success","data":{"alerts":[]}}`)
