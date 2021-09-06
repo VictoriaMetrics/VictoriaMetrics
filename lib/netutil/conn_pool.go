9548e52e97345af82b932718486591493e962c37
mxlxm <minzilu@me.com>
2021-09-06 13:24:27 +0800

    make errcheck happy

--- lib/netutil/conn_pool.go
+++ lib/netutil/conn_pool.go
@@ -71,7 +71,12 @@ func (cp *ConnPool) Get() (*handshake.BufferedConn, error) {
 //
 // Do not put broken and closed connections to the pool!
 func (cp *ConnPool) Put(bc *handshake.BufferedConn) {
-	bc.SetDeadline(time.Time{})
+	if err := bc.SetDeadline(time.Time{}); err != nil {
+		// Close the connection instead of returning it to the pool,
+		// since it may be broken.
+		_ = bc.Close()
+		return
+	}
 	cp.mu.Lock()
 	cp.conns = append(cp.conns, bc)
 	cp.mu.Unlock()
