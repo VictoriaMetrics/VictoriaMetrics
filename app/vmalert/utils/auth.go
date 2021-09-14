fcff27793d49190bcc1db645c7a083d3f755c49d
Roman Khavronenko <hagen1778@gmail.com>
2021-09-14 16:26:29 +0300

    vmalert: make lint happy

--- app/vmalert/utils/auth.go
+++ app/vmalert/utils/auth.go
@@ -4,6 +4,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promauth"
 )
 
+// AuthConfig returns promauth.Config based on the given params
 func AuthConfig(baUser, baPass, baFile, bearerToken, bearerTokenFile string) (*promauth.Config, error) {
 	var baCfg *promauth.BasicAuthConfig
 	if baUser != "" || baPass != "" || baFile != "" {
