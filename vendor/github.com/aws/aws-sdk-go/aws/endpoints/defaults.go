26cd0d36b4764d730e87fb5c48667ee2b4aa02fe
Aliaksandr Valialkin <valyala@gmail.com>
2021-09-15 18:22:59 +0300

    vendor: `make vendor-update`

--- vendor/github.com/aws/aws-sdk-go/aws/endpoints/defaults.go
+++ vendor/github.com/aws/aws-sdk-go/aws/endpoints/defaults.go
@@ -4799,6 +4799,20 @@ var awsPartition = partition{
 				"us-east-1": endpoint{},
 			},
 		},
+		"models-v2-lex": service{
+
+			Endpoints: endpoints{
+				"ap-northeast-1": endpoint{},
+				"ap-southeast-1": endpoint{},
+				"ap-southeast-2": endpoint{},
+				"ca-central-1":   endpoint{},
+				"eu-central-1":   endpoint{},
+				"eu-west-1":      endpoint{},
+				"eu-west-2":      endpoint{},
+				"us-east-1":      endpoint{},
+				"us-west-2":      endpoint{},
+			},
+		},
 		"models.lex": service{
 			Defaults: endpoint{
 				CredentialScope: credentialScope{
@@ -5870,6 +5884,20 @@ var awsPartition = partition{
 				"us-east-1":      endpoint{},
 				"us-east-2":      endpoint{},
 				"us-west-1":      endpoint{},
+				"us-west-2":      endpoint{},
+			},
+		},
+		"runtime-v2-lex": service{
+
+			Endpoints: endpoints{
+				"ap-northeast-1": endpoint{},
+				"ap-southeast-1": endpoint{},
+				"ap-southeast-2": endpoint{},
+				"ca-central-1":   endpoint{},
+				"eu-central-1":   endpoint{},
+				"eu-west-1":      endpoint{},
+				"eu-west-2":      endpoint{},
+				"us-east-1":      endpoint{},
 				"us-west-2":      endpoint{},
 			},
 		},
--- vendor/golang.org/x/sys/unix/syscall_linux_amd64.go
+++ vendor/golang.org/x/sys/unix/syscall_linux_amd64.go
@@ -8,7 +8,6 @@
 package unix
 
 //sys	dup2(oldfd int, newfd int) (err error)
-//sysnb	EpollCreate(size int) (fd int, err error)
 //sys	EpollWait(epfd int, events []EpollEvent, msec int) (n int, err error)
 //sys	Fadvise(fd int, offset int64, length int64, advice int) (err error) = SYS_FADVISE64
 //sys	Fchown(fd int, uid int, gid int) (err error)
