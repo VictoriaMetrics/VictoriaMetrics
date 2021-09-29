6d9f1d42272d604873b44dc1deb13d9b577b6f72
Aliaksandr Valialkin <valyala@victoriametrics.com>
2021-09-29 21:17:25 +0300

    app/vminsert: document that -relabelConfig is reloaded on SIGHUP signal

--- app/vminsert/relabel/relabel.go
+++ app/vminsert/relabel/relabel.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	relabelConfig = flag.String("relabelConfig", "", "Optional path to a file with relabeling rules, which are applied to all the ingested metrics. "+
-		"See https://docs.victoriametrics.com/#relabeling for details")
+		"See https://docs.victoriametrics.com/#relabeling for details. The config is reloaded on SIGHUP signal")
 	relabelDebug = flag.Bool("relabelDebug", false, "Whether to log metrics before and after relabeling with -relabelConfig. If the -relabelDebug is enabled, "+
 		"then the metrics aren't sent to storage. This is useful for debugging the relabeling configs")
 )
