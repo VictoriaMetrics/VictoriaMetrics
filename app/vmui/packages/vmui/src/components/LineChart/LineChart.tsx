bd4f9259623ab102f6ecc3fa89e5d9943bde8cf5
Yury Moladau <yurymolodov@gmail.com>
2021-10-05 21:08:16 +0300

    feat: add zoom/pan for graph

--- app/vmui/packages/vmui/src/components/LineChart/LineChart.tsx
+++ app/vmui/packages/vmui/src/components/LineChart/LineChart.tsx
@@ -1,7 +1,7 @@
 import React, {FC, useEffect, useMemo, useRef, useState} from "react";
 import {getNameForMetric} from "../../utils/metric";
 import "chartjs-adapter-date-fns";
-import {useAppState} from "../../state/common/StateContext";
+import {useAppDispatch, useAppState} from "../../state/common/StateContext";
 import {GraphViewProps} from "../Home/Views/GraphView";
 import uPlot, {AlignedData as uPlotData, Options as uPlotOptions, Series as uPlotSeries} from "uplot";
 import UplotReact from "uplot-react";
@@ -11,6 +11,7 @@ import "./legend.css";
 
 const LineChart: FC<GraphViewProps> = ({data = []}) => {
 
+  const dispatch = useAppDispatch();
   const {time: {period}} = useAppState();
   const [dataChart, setDataChart] = useState<uPlotData>();
   const [series, setSeries] = useState<uPlotSeries[]>([]);
@@ -31,7 +32,7 @@ const LineChart: FC<GraphViewProps> = ({data = []}) => {
   };
 
   const times = useMemo(() => {
-    const allTimes = data.map(d => d.values.map(v => v[0])).flat().filter(t => t >= scale.min && t <= scale.max);
+    const allTimes = data.map(d => d.values.map(v => v[0])).flat(); //.filter(t => t >= scale.min && t <= scale.max);
     const start = Math.min(...allTimes);
     const end = Math.max(...allTimes);
     const output = [];
@@ -55,50 +56,84 @@ const LineChart: FC<GraphViewProps> = ({data = []}) => {
     setDataChart([times, ...values]);
   }, [data]);
 
-  const wheelZoomPlugin = () => {
-    const factor = 0.75;
+  const onReadyChart = (u: uPlot) => {
+    const factor = 0.85;
 
-    return {
-      hooks: {
-        ready: (u: uPlot) => {
-          const over = u.over;
-          const rect = over.getBoundingClientRect();
+    // wheel drag pan
+    u.over.addEventListener("mousedown", e => {
+      if (e.button !== 0) return;
+      e.preventDefault();
+      const left0 = e.clientX;
+      const scXMin0 = u.scales.x.min || 1;
+      const scXMax0 = u.scales.x.max || 1;
+      const xUnitsPerPx = u.posToVal(1, "x") - u.posToVal(0, "x");
 
-          // wheel scroll zoom
-          over.addEventListener("wheel", e => {
-            e.preventDefault();
-            const {left = 0} = u.cursor;
-            const leftPct = left/rect.width;
-            const xVal = u.posToVal(left, "x");
-            const oxRange = (u.scales.x.max || 0) - (u.scales.x.min || 0);
-            const nxRange = e.deltaY < 0 ? oxRange * factor : oxRange / factor;
-            const min = xVal - leftPct * nxRange;
-            const max = min + nxRange;
-            // dispatch({type: "SET_PERIOD", payload: {from: new Date(min * 1000), to: new Date(max * 1000)}});
-            // setScale({min, max});
-            u.batch(() => {
-              u.setScale("x", {min, max});
-            });
-          });
-        }
-      }
-    };
+      const onmove = (e: MouseEvent) => {
+        e.preventDefault();
+        const dx = xUnitsPerPx * (e.clientX - left0);
+        const min = scXMin0 - dx;
+        const max = scXMax0 - dx;
+        u.setScale("x", {min, max});
+        setScale({min, max});
+      };
+
+      const onup = () => {
+        document.removeEventListener("mousemove", onmove);
+        document.removeEventListener("mouseup", onup);
+      };
+
+      document.addEventListener("mousemove", onmove);
+      document.addEventListener("mouseup", onup);
+    });
+
+    // wheel scroll zoom
+    u.over.addEventListener("wheel", e => {
+      if (!e.ctrlKey) return;
+      e.preventDefault();
+      const {width} = u.over.getBoundingClientRect();
+      const {left = width/2} = u.cursor;
+      const leftPct = left/width;
+      const xVal = u.posToVal(left, "x");
+      const oxRange = (u.scales.x.max || 0) - (u.scales.x.min || 0);
+      const nxRange = e.deltaY < 0 ? oxRange * factor : oxRange / factor;
+      const min = xVal - leftPct * nxRange;
+      const max = min + nxRange;
+      u.batch(() => {
+        u.setScale("x", {min, max});
+        setScale({min, max});
+      });
+    });
   };
+
+  useEffect(() => {setScale({min: period.start, max: period.end});}, [period]);
+
+  useEffect(() => {
+    // TODO add check zoom in
+    const duration = (period.end - period.start)/2;
+    if (scale.max > period.end + duration || scale.min < period.start - duration) {
+      dispatch({type: "SET_PERIOD", payload: {from: new Date(scale.min * 1000), to: new Date(scale.max * 1000)}});
+    }
+  }, [scale]);
 
   const options: uPlotOptions = {
     width: refContainer.current ? refContainer.current.offsetWidth : 400,
     height: 500,
     series: series,
-    plugins: [wheelZoomPlugin()],
-    cursor: { drag: { x: false, y: false }},
+    plugins: [{
+      hooks: {
+        ready: onReadyChart
+      }
+    }],
+    cursor: {drag: {x: false, y: false}},
     axes: [
-      { space: 80},
+      {space: 80},
       {
         show: true,
         font: "10px Arial",
         values: (self, ticks) => ticks.map(n => n > 1000 ? numeral(n).format("0.0a") : n)
       }
     ],
+    scales: {x: {range: () => [scale.min, scale.max]}}
   };
 
   return <div ref={refContainer}>
