c0e58ade45f18448e680bdc3e914ae6089b66547
Alexander Rickardsson <alxric@aiven.io>
2021-10-18 09:20:26 +0200

    vmalert: Redact passwords from error messages (#1713)

--- app/vmalert/datasource/vm_graphite_api.go
+++ app/vmalert/datasource/vm_graphite_api.go
@@ -38,7 +38,7 @@ func (r graphiteResponse) metrics() []Metric {
 func parseGraphiteResponse(req *http.Request, resp *http.Response) ([]Metric, error) {
 	r := &graphiteResponse{}
 	if err := json.NewDecoder(resp.Body).Decode(r); err != nil {
-		return nil, fmt.Errorf("error parsing graphite metrics for %s: %w", req.URL, err)
+		return nil, fmt.Errorf("error parsing graphite metrics for %s: %w", req.URL.Redacted(), err)
 	}
 	return r.metrics(), nil
 }
