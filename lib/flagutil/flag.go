8991c8b58990528c786fa9e084fd5dc11f4d04e0
Aliaksandr Valialkin <valyala@victoriametrics.com>
2021-10-20 00:51:06 +0300

    lib/flagutil: do not expose sensitive info (passwords, keys and urls) at /flags page

--- lib/flagutil/flag.go
+++ lib/flagutil/flag.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"strings"
 )
 
 // WriteFlags writes all the explicitly set flags to w.
 func WriteFlags(w io.Writer) {
 	flag.Visit(func(f *flag.Flag) {
+		lname := strings.ToLower(f.Name)
 		value := f.Value.String()
+		if IsSecretFlag(lname) {
+			value = "secret"
+		}
 		fmt.Fprintf(w, "-%s=%q\n", f.Name, value)
 	})
 }
