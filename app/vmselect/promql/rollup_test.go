1952ab99aa7e461d664b2a286e557f64366490fe
Aliaksandr Valialkin <valyala@victoriametrics.com>
2021-10-27 15:06:32 +0300

    all: fix build issues and tests for Apple M1
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1653

--- app/vmselect/promql/rollup_test.go
+++ app/vmselect/promql/rollup_test.go
@@ -1169,8 +1169,9 @@ func testRowsEqual(t *testing.T, values []float64, timestamps []int64, valuesExp
 		}
 		// Compare values with the reduced precision because of different precision errors
 		// on different OS/architectures. See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1738
-		if math.Abs(v-vExpected) > 1e-13 {
-			t.Fatalf("unexpected value at values[%d]; got %f; want %f\nvalues=\n%v\nvaluesExpected=\n%v",
+		// and https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1653
+		if math.Abs(v-vExpected)/math.Abs(vExpected) > 1e-13 {
+			t.Fatalf("unexpected value at values[%d]; got %v; want %v\nvalues=\n%v\nvaluesExpected=\n%v",
 				i, v, vExpected, values, valuesExpected)
 		}
 	}
