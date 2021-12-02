VictoriaMetrics/VictoriaMetrics

commit 7275ebf91a0c847a388c7d21656097d99a16414b

app/vmstorage: export vm_cache_size_max_bytes metrics for determining capacity of various caches
The vm_cache_size_max_bytes metric can be used for determining caches which reach their capacity via the following query:

   vm_cache_size_bytes / vm_cache_size_max_bytes > 0.9

lib/workingsetcache/cache.go
@@ -21,9 +21,6 @@ const (
 //
 // The cache evicts inactive entries after the given expireDuration.
 // Recently accessed entries survive expireDuration.
-//
-// Comparing to fastcache, this cache minimizes the required RAM size
-// to values smaller than maxBytes.
 type Cache struct {
 	curr atomic.Value
 	prev atomic.Value
@@ -35,9 +32,6 @@ type Cache struct {
 	// it is better to use only curr cache with doubled size.
 	// After the process of switching, this flag will be set to whole.
 	mode uint32
-
-	// The maximum cache size in bytes.
-	maxBytes int
 
 	// mu serializes access to curr, prev and mode
 	// in expirationWatcher and cacheSizeWatcher.
@@ -65,7 +59,7 @@ func Load(filePath string, maxBytes int, expireDuration time.Duration) *Cache {
 		// Try loading it again with maxBytes / 2 size.
 		curr := fastcache.New(maxBytes / 2)
 		prev := fastcache.LoadFromFileOrNew(filePath, maxBytes/2)
-		c := newCacheInternal(curr, prev, maxBytes, split)
+		c := newCacheInternal(curr, prev, split)
 		c.runWatchers(expireDuration)
 		return c
 	}
@@ -74,24 +68,23 @@ func Load(filePath string, maxBytes int, expireDuration time.Duration) *Cache {
 	// Set its' mode to `whole`.
 	// There is no need in runWatchers call.
 	prev := fastcache.New(1024)
-	return newCacheInternal(curr, prev, maxBytes, whole)
+	return newCacheInternal(curr, prev, whole)
 }
 
-// New creates new cache with the given maxBytes capcity and the given expireDuration
+// New creates new cache with the given maxBytes capacity and the given expireDuration
 // for inactive entries.
 //
 // Stop must be called on the returned cache when it is no longer needed.
 func New(maxBytes int, expireDuration time.Duration) *Cache {
 	curr := fastcache.New(maxBytes / 2)
 	prev := fastcache.New(1024)
-	c := newCacheInternal(curr, prev, maxBytes, split)
+	c := newCacheInternal(curr, prev, split)
 	c.runWatchers(expireDuration)
 	return c
 }
 
-func newCacheInternal(curr, prev *fastcache.Cache, maxBytes, mode int) *Cache {
+func newCacheInternal(curr, prev *fastcache.Cache, mode int) *Cache {
 	var c Cache
-	c.maxBytes = maxBytes
 	c.curr.Store(curr)
 	c.prev.Store(prev)
 	c.stopCh = make(chan struct{})
@@ -128,13 +121,15 @@ func (c *Cache) expirationWatcher(expireDuration time.Duration) {
 			c.mu.Unlock()
 			return
 		}
-		// Expire prev cache and create fresh curr cache with c.maxBytes/2 capacity.
-		// Do not reuse prev cache, since it can have too big capacity.
+		// Expire prev cache and create fresh curr cache with the same capacity.
+		// Do not reuse prev cache, since it can occupy too big amounts of memory.
 		prev := c.prev.Load().(*fastcache.Cache)
 		prev.Reset()
 		curr := c.curr.Load().(*fastcache.Cache)
+		var cs fastcache.Stats
+		curr.UpdateStats(&cs)
 		c.prev.Store(curr)
-		curr = fastcache.New(c.maxBytes / 2)
+		curr = fastcache.New(int(cs.MaxBytesSize))
 		c.curr.Store(curr)
 		c.mu.Unlock()
 	}
@@ -144,6 +139,7 @@ func (c *Cache) cacheSizeWatcher() {
 	t := time.NewTicker(time.Minute)
 	defer t.Stop()
 
+	var maxBytesSize uint64
 	for {
 		select {
 		case <-c.stopCh:
@@ -153,12 +149,13 @@ func (c *Cache) cacheSizeWatcher() {
 		var cs fastcache.Stats
 		curr := c.curr.Load().(*fastcache.Cache)
 		curr.UpdateStats(&cs)
-		if cs.BytesSize >= uint64(0.95*float64(c.maxBytes)/2) {
+		if cs.BytesSize >= uint64(0.9*float64(cs.MaxBytesSize)) {
+			maxBytesSize = cs.MaxBytesSize
 			break
 		}
 	}
 
-	// curr cache size exceeds 50% of its capacity. It is better
+	// curr cache size exceeds 90% of its capacity. It is better
 	// to double the size of curr cache and stop using prev cache,
 	// since this will result in higher summary cache capacity.
 	//
@@ -166,7 +163,7 @@ func (c *Cache) cacheSizeWatcher() {
 	// 1) switch to mode=switching
 	// 2) move curr cache to prev
 	// 3) create curr with the double size
-	// 4) wait until curr size exceeds c.maxBytes/2, i.e. it is populated with new data
+	// 4) wait until curr size exceeds maxBytesSize, i.e. it is populated with new data
 	// 5) switch to mode=whole
 	// 6) drop prev
 
@@ -176,7 +173,7 @@ func (c *Cache) cacheSizeWatcher() {
 	prev.Reset()
 	curr := c.curr.Load().(*fastcache.Cache)
 	c.prev.Store(curr)
-	c.curr.Store(fastcache.New(c.maxBytes))
+	c.curr.Store(fastcache.New(int(maxBytesSize * 2)))
 	c.mu.Unlock()
 
 	for {
@@ -188,7 +185,7 @@ func (c *Cache) cacheSizeWatcher() {
 		var cs fastcache.Stats
 		curr := c.curr.Load().(*fastcache.Cache)
 		curr.UpdateStats(&cs)
-		if cs.BytesSize >= uint64(c.maxBytes)/2 {
+		if cs.BytesSize >= maxBytesSize {
 			break
 		}
 	}
@@ -245,6 +242,7 @@ func (c *Cache) UpdateStats(fcs *fastcache.Stats) {
 	fcs.Corruptions += cs.Corruptions
 	fcs.EntriesCount += cs.EntriesCount
 	fcs.BytesSize += cs.BytesSize
+	fcs.MaxBytesSize += cs.MaxBytesSize
 
 	fcs.GetCalls += atomic.LoadUint64(&c.cs.GetCalls)
 	fcs.SetCalls += atomic.LoadUint64(&c.cs.SetCalls)
@@ -255,6 +253,7 @@ func (c *Cache) UpdateStats(fcs *fastcache.Stats) {
 	prev.UpdateStats(&cs)
 	fcs.EntriesCount += cs.EntriesCount
 	fcs.BytesSize += cs.BytesSize
+	fcs.MaxBytesSize += cs.MaxBytesSize
 }
 
 // Get appends the found value for the given key to dst and returns the result.

app/vmstorage/main.go
@@ -713,6 +713,31 @@ func registerStorageMetrics() {
 		return float64(m().PrefetchedMetricIDsSizeBytes)
 	})
 
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="storage/tsid"}`, func() float64 {
+		return float64(m().TSIDCacheSizeMaxBytes)
+	})
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="storage/metricIDs"}`, func() float64 {
+		return float64(m().MetricIDCacheSizeMaxBytes)
+	})
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="storage/metricName"}`, func() float64 {
+		return float64(m().MetricNameCacheSizeMaxBytes)
+	})
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="storage/bigIndexBlocks"}`, func() float64 {
+		return float64(tm().BigIndexBlocksCacheSizeMaxBytes)
+	})
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="storage/smallIndexBlocks"}`, func() float64 {
+		return float64(tm().SmallIndexBlocksCacheSizeMaxBytes)
+	})
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="indexdb/dataBlocks"}`, func() float64 {
+		return float64(idbm().DataBlocksCacheSizeMaxBytes)
+	})
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="indexdb/indexBlocks"}`, func() float64 {
+		return float64(idbm().IndexBlocksCacheSizeMaxBytes)
+	})
+	metrics.NewGauge(`vm_cache_size_max_bytes{type="indexdb/tagFilters"}`, func() float64 {
+		return float64(idbm().TagFiltersCacheSizeMaxBytes)
+	})
+
 	metrics.NewGauge(`vm_cache_requests_total{type="storage/tsid"}`, func() float64 {
 		return float64(m().TSIDCacheRequests)
 	})

go.mod
@@ -2,7 +2,7 @@ module github.com/VictoriaMetrics/VictoriaMetrics
 
 require (
 	cloud.google.com/go/storage v1.18.2
-	github.com/VictoriaMetrics/fastcache v1.7.0
+	github.com/VictoriaMetrics/fastcache v1.8.0
 
 	// Do not use the original github.com/valyala/fasthttp because of issues
 	// like https://github.com/valyala/fasthttp/commit/996610f021ff45fdc98c2ce7884d5fa4e7f9199b

go.sum
@@ -101,8 +101,8 @@ github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578/go.mod h1:uGdko
 github.com/SAP/go-hdb v0.14.1/go.mod h1:7fdQLVC2lER3urZLjZCm0AuMQfApof92n3aylBPEkMo=
 github.com/Shopify/sarama v1.19.0/go.mod h1:FVkBWblsNy7DGZRfXLU0O9RCGt5g3g3yEuWXgklEdEo=
 github.com/Shopify/toxiproxy v2.1.4+incompatible/go.mod h1:OXgGpZ6Cli1/URJOF1DMxUHB2q5Ap20/P/eIdh4G0pI=
-github.com/VictoriaMetrics/fastcache v1.7.0 h1:E6GibaGI685TafrI7E/QqZPkMsOzRw+3gpICQx08ISg=
-github.com/VictoriaMetrics/fastcache v1.7.0/go.mod h1:n7Sl+ioh/HlWeYHLSIBIE8TcZFHg/+xgvomWSS5xuEE=
+github.com/VictoriaMetrics/fastcache v1.8.0 h1:ybZqS7kRy8YVzYsI09GLzQhs7iqS6cOEH2avtknD1SU=
+github.com/VictoriaMetrics/fastcache v1.8.0/go.mod h1:n7Sl+ioh/HlWeYHLSIBIE8TcZFHg/+xgvomWSS5xuEE=
 github.com/VictoriaMetrics/fasthttp v1.1.0 h1:3crd4YWHsMwu60GUXRH6OstowiFvqrwS4a/ueoLdLL0=
 github.com/VictoriaMetrics/fasthttp v1.1.0/go.mod h1:/7DMcogqd+aaD3G3Hg5kFgoFwlR2uydjiWvoLp5ZTqQ=
 github.com/VictoriaMetrics/metrics v1.12.2/go.mod h1:Z1tSfPfngDn12bTfZSCqArT3OPY3u88J12hSoOhuiRE=

lib/mergeset/part.go
@@ -296,6 +296,15 @@ func (idxbc *indexBlockCache) SizeBytes() uint64 {
 	return uint64(n)
 }
 
+func (idxbc *indexBlockCache) SizeMaxBytes() uint64 {
+	avgBlockSize := float64(64 * 1024)
+	blocksCount := idxbc.Len()
+	if blocksCount > 0 {
+		avgBlockSize = float64(idxbc.SizeBytes()) / float64(blocksCount)
+	}
+	return uint64(avgBlockSize * float64(getMaxCachedIndexBlocksPerPart()))
+}
+
 func (idxbc *indexBlockCache) Requests() uint64 {
 	return atomic.LoadUint64(&idxbc.requests)
 }
@@ -461,6 +470,15 @@ func (ibc *inmemoryBlockCache) SizeBytes() uint64 {
 	}
 	ibc.mu.RUnlock()
 	return uint64(n)
+}
+
+func (ibc *inmemoryBlockCache) SizeMaxBytes() uint64 {
+	avgBlockSize := float64(128 * 1024)
+	blocksCount := ibc.Len()
+	if blocksCount > 0 {
+		avgBlockSize = float64(ibc.SizeBytes()) / float64(blocksCount)
+	}
+	return uint64(avgBlockSize * float64(getMaxCachedInmemoryBlocksPerPart()))
 }
 
 func (ibc *inmemoryBlockCache) Requests() uint64 {

lib/mergeset/table.go
@@ -410,15 +410,17 @@ type TableMetrics struct {
 	ItemsCount  uint64
 	SizeBytes   uint64
 
-	DataBlocksCacheSize      uint64
-	DataBlocksCacheSizeBytes uint64
-	DataBlocksCacheRequests  uint64
-	DataBlocksCacheMisses    uint64
+	DataBlocksCacheSize         uint64
+	DataBlocksCacheSizeBytes    uint64
+	DataBlocksCacheSizeMaxBytes uint64
+	DataBlocksCacheRequests     uint64
+	DataBlocksCacheMisses       uint64
 
-	IndexBlocksCacheSize      uint64
-	IndexBlocksCacheSizeBytes uint64
-	IndexBlocksCacheRequests  uint64
-	IndexBlocksCacheMisses    uint64
+	IndexBlocksCacheSize         uint64
+	IndexBlocksCacheSizeBytes    uint64
+	IndexBlocksCacheSizeMaxBytes uint64
+	IndexBlocksCacheRequests     uint64
+	IndexBlocksCacheMisses       uint64
 
 	PartsRefCount uint64
 }
@@ -443,11 +445,13 @@ func (tb *Table) UpdateMetrics(m *TableMetrics) {
 
 		m.DataBlocksCacheSize += p.ibCache.Len()
 		m.DataBlocksCacheSizeBytes += p.ibCache.SizeBytes()
+		m.DataBlocksCacheSizeMaxBytes += p.ibCache.SizeMaxBytes()
 		m.DataBlocksCacheRequests += p.ibCache.Requests()
 		m.DataBlocksCacheMisses += p.ibCache.Misses()
 
 		m.IndexBlocksCacheSize += p.idxbCache.Len()
 		m.IndexBlocksCacheSizeBytes += p.idxbCache.SizeBytes()
+		m.IndexBlocksCacheSizeMaxBytes += p.idxbCache.SizeMaxBytes()
 		m.IndexBlocksCacheRequests += p.idxbCache.Requests()
 		m.IndexBlocksCacheMisses += p.idxbCache.Misses()
 

lib/storage/index_db.go
@@ -130,10 +130,11 @@ const noDeadline = 1<<64 - 1
 
 // IndexDBMetrics contains essential metrics for indexDB.
 type IndexDBMetrics struct {
-	TagFiltersCacheSize      uint64
-	TagFiltersCacheSizeBytes uint64
-	TagFiltersCacheRequests  uint64
-	TagFiltersCacheMisses    uint64
+	TagFiltersCacheSize         uint64
+	TagFiltersCacheSizeBytes    uint64
+	TagFiltersCacheSizeMaxBytes uint64
+	TagFiltersCacheRequests     uint64
+	TagFiltersCacheMisses       uint64
 
 	DeletedMetricsCount uint64
 
@@ -173,6 +174,7 @@ func (db *indexDB) UpdateMetrics(m *IndexDBMetrics) {
 	db.tagFiltersCache.UpdateStats(&cs)
 	m.TagFiltersCacheSize += cs.EntriesCount
 	m.TagFiltersCacheSizeBytes += cs.BytesSize
+	m.TagFiltersCacheSizeMaxBytes += cs.MaxBytesSize
 	m.TagFiltersCacheRequests += cs.GetCalls
 	m.TagFiltersCacheMisses += cs.Misses
 

lib/storage/part.go
@@ -280,3 +280,12 @@ func (ibc *indexBlockCache) SizeBytes() uint64 {
 	ibc.mu.Unlock()
 	return uint64(n)
 }
+
+func (ibc *indexBlockCache) SizeMaxBytes() uint64 {
+	avgBlockSize := float64(64 * 1024)
+	blocksCount := ibc.Len()
+	if blocksCount > 0 {
+		avgBlockSize = float64(ibc.SizeBytes()) / float64(blocksCount)
+	}
+	return uint64(avgBlockSize * float64(getMaxCachedIndexBlocksPerPart()))
+}

commit da402fbdfa53b51adf1281016573bd7b49af235c

lib/workingsetcache: fix `unaligned 64-bit atomic operation` panic on 32-bit architectures
The panic has been introduced in 7275ebf91a0c847a388c7d21656097d99a16414b

lib/workingsetcache/cache.go
@@ -25,6 +25,9 @@ type Cache struct {
 	curr atomic.Value
 	prev atomic.Value
 
+	// cs holds cache stats
+	cs fastcache.Stats
+
 	// mode indicates whether to use only curr and skip prev.
 	//
 	// This flag is set to switching if curr is filled for more than 50% space.
@@ -39,9 +42,6 @@ type Cache struct {
 
 	wg     sync.WaitGroup
 	stopCh chan struct{}
-
-	// cs holds cache stats
-	cs fastcache.Stats
 }
 
 // Load loads the cache from filePath and limits its size to maxBytes