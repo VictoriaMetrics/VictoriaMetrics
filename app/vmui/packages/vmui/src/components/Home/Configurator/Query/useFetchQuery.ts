8bdc45ba005b1557d42b74e8f5f1bcbc4e997a59
Yury Molodov <yurymolodov@gmail.com>
2022-01-18 22:42:56 +0300

    fix: remove buffer period (#2078)
    * fix: remove buffer period
    
    * app/vmselect/vmui: `make vmui-update`
    
    * docs/CHANGELOG.md: document the implemented feature
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/2064
    
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- app/vmui/packages/vmui/src/components/Home/Configurator/Query/useFetchQuery.ts
+++ app/vmui/packages/vmui/src/components/Home/Configurator/Query/useFetchQuery.ts
@@ -1,12 +1,13 @@
-import {useEffect, useMemo, useState} from "preact/compat";
+import {useEffect, useMemo, useCallback, useState} from "preact/compat";
 import {getQueryOptions, getQueryRangeUrl, getQueryUrl} from "../../../../api/query-range";
 import {useAppState} from "../../../../state/common/StateContext";
 import {InstantMetricResult, MetricBase, MetricResult} from "../../../../api/types";
 import {isValidHttpUrl} from "../../../../utils/url";
 import {useAuthState} from "../../../../state/auth/AuthStateContext";
-import {ErrorTypes, TimeParams} from "../../../../types";
+import {ErrorTypes} from "../../../../types";
 import {useGraphState} from "../../../../state/graph/GraphStateContext";
 import {getAppModeEnable, getAppModeParams} from "../../../../utils/app-mode";
+import throttle from "lodash.throttle";
 
 const appModeEnable = getAppModeEnable();
 const {serverURL: appServerUrl} = getAppModeParams();
@@ -19,7 +20,7 @@ export const useFetchQuery = (): {
   error?: ErrorTypes | string,
   queryOptions: string[],
 } => {
-  const {query, displayType, serverUrl, time: {period}, queryControls: {nocache, autoRefresh}} = useAppState();
+  const {query, displayType, serverUrl, time: {period}, queryControls: {nocache}} = useAppState();
 
   const {basicData, bearerData, authMethod} = useAuthState();
   const {customStep} = useGraphState();
@@ -29,7 +30,7 @@ export const useFetchQuery = (): {
   const [graphData, setGraphData] = useState<MetricResult[]>();
   const [liveData, setLiveData] = useState<InstantMetricResult[]>();
   const [error, setError] = useState<ErrorTypes | string>();
-  const [prevPeriod, setPrevPeriod] = useState<TimeParams>();
+  const [fetchQueue, setFetchQueue] = useState<AbortController[]>([]);
 
   useEffect(() => {
     if (error) {
@@ -38,19 +39,11 @@ export const useFetchQuery = (): {
     }
   }, [error]);
 
-  const needUpdateData = useMemo(() => {
-    if (!prevPeriod || autoRefresh) return true;
-    const duration = (prevPeriod.end - prevPeriod.start) / 3;
-    const factorLimit = duration / (period.end - period.start) >= 0.7;
-    const maxLimit = period.end > (prevPeriod.end + duration);
-    const minLimit = period.start < (prevPeriod.start - duration);
-    return factorLimit || maxLimit || minLimit;
-  }, [period]);
-
-  const fetchData = async () => {
+  const fetchData = async (fetchUrl: string[] | undefined) => {
     if (!fetchUrl?.length) return;
+    const controller = new AbortController();
+    setFetchQueue([...fetchQueue, controller]);
     setIsLoading(true);
-    setPrevPeriod(period);
 
     const headers = new Headers();
     if (authMethod === "BASIC_AUTH") {
@@ -61,7 +54,7 @@ export const useFetchQuery = (): {
     }
 
     try {
-      const responses = await Promise.all(fetchUrl.map(url => fetch(url, {headers})));
+      const responses = await Promise.all(fetchUrl.map(url => fetch(url, {headers, signal: controller.signal})));
       const tempData = [];
       let counter = 1;
       for await (const response of responses) {
@@ -79,11 +72,15 @@ export const useFetchQuery = (): {
       }
       displayType === "chart" ? setGraphData(tempData) : setLiveData(tempData);
     } catch (e) {
-      if (e instanceof Error) setError(`${e.name}: ${e.message}`);
+      if (e instanceof Error && e.name !== "AbortError") {
+        setError(`${e.name}: ${e.message}`);
+      }
     }
 
     setIsLoading(false);
   };
+
+  const throttledFetchData = useCallback(throttle(fetchData, 1000), []);
 
   const fetchOptions = async () => {
     if (!serverUrl) return;
@@ -108,11 +105,9 @@ export const useFetchQuery = (): {
     } else if (query.every(q => !q.trim())) {
       setError(ErrorTypes.validQuery);
     } else if (isValidHttpUrl(server)) {
-      const duration = (period.end - period.start) / 2;
-      const bufferPeriod = {...period, start: period.start - duration, end: period.end + duration};
-      if (customStep.enable) bufferPeriod.step = customStep.value;
+      if (customStep.enable) period.step = customStep.value;
       return query.filter(q => q.trim()).map(q => displayType === "chart"
-        ? getQueryRangeUrl(server, q, bufferPeriod, nocache)
+        ? getQueryRangeUrl(server, q, period, nocache)
         : getQueryUrl(server, q, period));
     } else {
       setError(ErrorTypes.validServer);
@@ -124,18 +119,17 @@ export const useFetchQuery = (): {
     fetchOptions();
   }, [serverUrl]);
 
-  useEffect(() => {
-    setPrevPeriod(undefined);
-  }, [query]);
-
   // TODO: this should depend on query as well, but need to decide when to do the request. Doing it on each query change - looks to be a bad idea. Probably can be done on blur
   useEffect(() => {
-    fetchData();
-  }, [serverUrl, displayType, customStep]);
+    throttledFetchData(fetchUrl);
+  }, [fetchUrl]);
 
   useEffect(() => {
-    if (needUpdateData) fetchData();
-  }, [period]);
+    const fetchPast = fetchQueue.slice(0, -1);
+    if (!fetchPast.length) return;
+    fetchPast.map(f => f.abort());
+    setFetchQueue(fetchQueue.filter(f => !f.signal.aborted));
+  }, [fetchQueue]);
 
   return { fetchUrl, isLoading, graphData, liveData, error, queryOptions: queryOptions };
 };
