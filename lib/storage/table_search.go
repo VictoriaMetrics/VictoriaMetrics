a1083d0531a0f40c399bfa5e50262c513d4d858b
匠心零度 <97019752@qq.com>
2022-01-28 18:10:47 +0800

    optimized code  (#2103)
    * optimized code ,because only the first error,so no need var errors []error
    
    * optimized code ,because only the first error,so no need var errors []error
    
    Co-authored-by: lirenzuo <lirenzuo@shein.com>

--- lib/storage/table_search.go
+++ lib/storage/table_search.go
@@ -93,22 +93,18 @@ func (ts *tableSearch) Init(tb *table, tsids []TSID, tr TimeRange) {
 	}
 
 	// Initialize the ptsHeap.
-	var errors []error
 	ts.ptsHeap = ts.ptsHeap[:0]
 	for i := range ts.ptsPool {
 		pts := &ts.ptsPool[i]
 		if !pts.NextBlock() {
 			if err := pts.Error(); err != nil {
-				errors = append(errors, err)
+				// Return only the first error, since it has no sense in returning all errors.
+				ts.err = fmt.Errorf("cannot initialize table search: %w", err)
+				return
 			}
 			continue
 		}
 		ts.ptsHeap = append(ts.ptsHeap, pts)
-	}
-	if len(errors) > 0 {
-		// Return only the first error, since it has no sense in returning all errors.
-		ts.err = fmt.Errorf("cannot initialize table search: %w", errors[0])
-		return
 	}
 	if len(ts.ptsHeap) == 0 {
 		ts.err = io.EOF
