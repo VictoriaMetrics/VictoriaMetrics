b80d493c3b1b11bd2f1f49da048099b940aadbd6
artifactori <artifactori62@gmail.com>
2022-02-11 22:34:36 +0700

    vmagent: add test for marshalling gce sdconfig with ZoneYAML

---
+++ lib/promscrape/discovery/gce/gce_test.go
@@ -0,0 +1,27 @@
+package gce
+
+import (
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+func TestMarshallingSDConfigWithZoneYAML(t *testing.T) {
+	sdConfig := SDConfig{
+		Project: "test-project",
+		Zone: ZoneYAML{
+			zones: []string{"zone-a", "zone-b"},
+		},
+	}
+
+	data, err := yaml.Marshal(sdConfig)
+	if err != nil {
+		t.Fatalf("unexpected non-nil error")
+	}
+
+	strData := string(data)
+	expected := "project: test-project\nzone:\n- zone-a\n- zone-b\n"
+	if strData != expected {
+		t.Fatalf("unexpected marshal:\ngot \n%vwant\n%v", strData, expected)
+	}
+}
