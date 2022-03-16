9101e0b5d5a067ac23bc9fe616fb9a685619550b
John Duncan <jduncan0000@gmail.com>
2022-03-15 21:29:19 -0500

    add test for positive empty match

--- lib/storage/index_db_test.go
+++ lib/storage/index_db_test.go
@@ -1181,6 +1181,19 @@ func TestMatchTagFilters(t *testing.T) {
 	if !ok {
 		t.Fatalf("Should match")
 	}
+	
+	// Positive empty match by non-existing tag
+	tfs.Reset()
+	if err := tfs.Add([]byte("non-existing-tag"), []byte("foobar|"), false , true); err != nil {
+		t.Fatalf("cannot add regexp, positive filter: %s", err)
+	}
+	ok, err = matchTagFilters(&mn, toTFPointers(tfs.tfs), &bb)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if !ok {
+		t.Fatalf("Should match")
+	}
 
 	// Negative match by non-existing tag
 	tfs.Reset()
