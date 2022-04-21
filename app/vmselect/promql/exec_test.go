VictoriaMetrics/VictoriaMetrics

commit d335cc886c62480934cf237efbce54400bda1ae0

app/vmselect/promql: fix `duplicate time series` error on joins against time series filtered by values
This should prevent from `duplicate time series` errors when executing the following query:

kube_pod_container_resource_requests{resource="cpu"} * on (namespace,pod) group_left() (kube_pod_status_phase{phase=~"Pending|Running"}==1)

where `kube_pod_status_phase{phase=~"Pending|Running"}==1` filters out diplicate time series

app/vmselect/promql/binary_op.go
@@ -82,14 +82,35 @@ func newBinaryOpArithFunc(af func(left, right float64) float64) binaryOpFunc {
 
 func newBinaryOpFunc(bf func(left, right float64, isBool bool) float64) binaryOpFunc {
 	return func(bfa *binaryOpFuncArg) ([]*timeseries, error) {
-		isBool := bfa.be.Bool
-		left, right, dst, err := adjustBinaryOpTags(bfa.be, bfa.left, bfa.right)
+		left := bfa.left
+		right := bfa.right
+		switch bfa.be.Op {
+		case "ifnot":
+			left = removeEmptySeries(left)
+			// Do not remove empty series on the right side,
+			// so the left-side series could be matched against them.
+		case "default":
+			// Do not remove empty series on the left side,
+			// so they could be replaced with the corresponding series on the right side.
+			right = removeEmptySeries(right)
+			if len(right) == 0 {
+				return left, nil
+			}
+		default:
+			left = removeEmptySeries(left)
+			right = removeEmptySeries(right)
+		}
+		if len(left) == 0 || len(right) == 0 {
+			return nil, nil
+		}
+		left, right, dst, err := adjustBinaryOpTags(bfa.be, left, right)
 		if err != nil {
 			return nil, err
 		}
 		if len(left) != len(right) || len(left) != len(dst) {
 			logger.Panicf("BUG: len(left) must match len(right) and len(dst); got %d vs %d vs %d", len(left), len(right), len(dst))
 		}
+		isBool := bfa.be.Bool
 		for i, tsLeft := range left {
 			leftValues := tsLeft.Values
 			rightValues := right[i].Values
@@ -206,7 +227,11 @@ func ensureSingleTimeseries(side string, be *metricsql.BinaryOpExpr, tss []*time
 
 func groupJoin(singleTimeseriesSide string, be *metricsql.BinaryOpExpr, rvsLeft, rvsRight, tssLeft, tssRight []*timeseries) ([]*timeseries, []*timeseries, error) {
 	joinTags := be.JoinModifier.Args
-	var m map[string]*timeseries
+	type tsPair struct {
+		left  *timeseries
+		right *timeseries
+	}
+	m := make(map[string]*tsPair)
 	for _, tsLeft := range tssLeft {
 		resetMetricGroupIfRequired(be, tsLeft)
 		if len(tssRight) == 1 {
@@ -219,12 +244,8 @@ func groupJoin(singleTimeseriesSide string, be *metricsql.BinaryOpExpr, rvsLeft,
 
 		// Hard case - right part contains multiple matching time series.
 		// Verify it doesn't result in duplicate MetricName values after adding missing tags.
-		if m == nil {
-			m = make(map[string]*timeseries, len(tssRight))
-		} else {
-			for k := range m {
-				delete(m, k)
-			}
+		for k := range m {
+			delete(m, k)
 		}
 		bb := bbPool.Get()
 		for _, tsRight := range tssRight {
@@ -232,20 +253,29 @@ func groupJoin(singleTimeseriesSide string, be *metricsql.BinaryOpExpr, rvsLeft,
 			tsCopy.CopyFromShallowTimestamps(tsLeft)
 			tsCopy.MetricName.SetTags(joinTags, &tsRight.MetricName)
 			bb.B = marshalMetricTagsSorted(bb.B[:0], &tsCopy.MetricName)
-			if tsExisting := m[string(bb.B)]; tsExisting != nil {
-				// Try merging tsExisting with tsRight if they don't overlap.
-				if mergeNonOverlappingTimeseries(tsExisting, tsRight) {
-					continue
+			pair, ok := m[string(bb.B)]
+			if !ok {
+				m[string(bb.B)] = &tsPair{
+					left:  &tsCopy,
+					right: tsRight,
 				}
+				continue
+			}
+			// Try merging pair.right with tsRight if they don't overlap.
+			var tmp timeseries
+			tmp.CopyFromShallowTimestamps(pair.right)
+			if !mergeNonOverlappingTimeseries(&tmp, tsRight) {
 				return nil, nil, fmt.Errorf("duplicate time series on the %s side of `%s %s %s`: %s and %s",
 					singleTimeseriesSide, be.Op, be.GroupModifier.AppendString(nil), be.JoinModifier.AppendString(nil),
-					stringMetricTags(&tsExisting.MetricName), stringMetricTags(&tsRight.MetricName))
+					stringMetricTags(&tmp.MetricName), stringMetricTags(&tsRight.MetricName))
 			}
-			m[string(bb.B)] = tsRight
-			rvsLeft = append(rvsLeft, &tsCopy)
-			rvsRight = append(rvsRight, tsRight)
+			pair.right = &tmp
 		}
 		bbPool.Put(bb)
+		for _, pair := range m {
+			rvsLeft = append(rvsLeft, pair.left)
+			rvsRight = append(rvsRight, pair.right)
+		}
 	}
 	return rvsLeft, rvsRight, nil
 }

app/vmselect/promql/exec_test.go
@@ -2675,6 +2675,17 @@ func TestExecSuccess(t *testing.T) {
 		resultExpected := []netstorage.Result{r}
 		f(q, resultExpected)
 	})
+	t.Run(`scalar default NaN`, func(t *testing.T) {
+		t.Parallel()
+		q := `time() > 1400 default (time() < -100)`
+		r := netstorage.Result{
+			MetricName: metricNameExpected,
+			Values:     []float64{nan, nan, nan, 1600, 1800, 2000},
+			Timestamps: timestampsExpected,
+		}
+		resultExpected := []netstorage.Result{r}
+		f(q, resultExpected)
+	})
 	t.Run(`vector default scalar`, func(t *testing.T) {
 		t.Parallel()
 		q := `sort_desc(union(

commit d1a9fac8942bb80c74404c43149bb49f7cf7dfdc

app/vmselect/promql: fix comparison to nan
The comparison to nan has been broken in d335cc886c62480934cf237efbce54400bda1ae0

Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/150

app/vmselect/promql/binary_op.go
@@ -84,18 +84,22 @@ func newBinaryOpFunc(bf func(left, right float64, isBool bool) float64) binaryOp
 	return func(bfa *binaryOpFuncArg) ([]*timeseries, error) {
 		left := bfa.left
 		right := bfa.right
-		switch bfa.be.Op {
-		case "ifnot":
+		op := bfa.be.Op
+		switch true {
+		case op == "ifnot":
 			left = removeEmptySeries(left)
 			// Do not remove empty series on the right side,
 			// so the left-side series could be matched against them.
-		case "default":
+		case op == "default":
 			// Do not remove empty series on the left and the right side,
-			// since this may result in missing result:
+			// since this may lead to missing result:
 			// - if empty time series are removed on the left side,
 			// then they won't be substituted by time series from the right side.
 			// - if empty time series are removed on the right side,
 			// then this may result in missing time series from the left side.
+		case metricsql.IsBinaryOpCmp(op):
+			// Do not remove empty series for comparison operations,
+			// since this may lead to missing result.
 		default:
 			left = removeEmptySeries(left)
 			right = removeEmptySeries(right)

app/vmselect/promql/exec_test.go
@@ -2625,6 +2625,23 @@ func TestExecSuccess(t *testing.T) {
 		resultExpected := []netstorage.Result{}
 		f(q, resultExpected)
 	})
+	t.Run(`compare_to_nan_right`, func(t *testing.T) {
+		t.Parallel()
+		q := `1 != nan`
+		r := netstorage.Result{
+			MetricName: metricNameExpected,
+			Values: []float64{1, 1, 1, 1, 1, 1},
+			Timestamps: timestampsExpected,
+		}
+		resultExpected := []netstorage.Result{r}
+		f(q, resultExpected)
+	})
+	t.Run(`compare_to_nan_left`, func(t *testing.T) {
+		t.Parallel()
+		q := `nan != 1`
+		resultExpected := []netstorage.Result{}
+		f(q, resultExpected)
+	})
 	t.Run(`-1 < 2`, func(t *testing.T) {
 		t.Parallel()
 		q := `-1 < 2`