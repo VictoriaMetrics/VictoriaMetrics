190c8b463c206d04d731c1317ca25a833ae2482d
Aliaksandr Valialkin <valyala@victoriametrics.com>
2022-05-02 15:01:50 +0300

    lib/netutil: close connections in ConnPool if they are idle for more than 30 seconds
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/2508

--- lib/netutil/conn_pool.go
+++ lib/netutil/conn_pool.go
@@ -5,7 +5,9 @@ import (
 	"sync"
 	"time"
 
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fasttime"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/handshake"
+	"github.com/VictoriaMetrics/metrics"
 )
 
 // ConnPool is a connection pool with ZSTD-compressed connections.
@@ -17,7 +19,12 @@ type ConnPool struct {
 	handshakeFunc    handshake.Func
 	compressionLevel int
 
-	conns []*handshake.BufferedConn
+	conns []connWithTimestamp
+}
+
+type connWithTimestamp struct {
+	bc             *handshake.BufferedConn
+	lastActiveTime uint64
 }
 
 // NewConnPool creates a new connection pool for the given addr.
@@ -26,13 +33,23 @@ type ConnPool struct {
 // handshakeFunc is used for handshaking after the connection establishing.
 // The compression is disabled if compressionLevel <= 0.
 func NewConnPool(name, addr string, handshakeFunc handshake.Func, compressionLevel int) *ConnPool {
-	return &ConnPool{
+	cp := &ConnPool{
 		d: NewTCPDialer(name, addr),
 
 		name:             name,
 		handshakeFunc:    handshakeFunc,
 		compressionLevel: compressionLevel,
 	}
+	_ = metrics.NewGauge(fmt.Sprintf(`vm_tcpdialer_conns_idle{name=%q, addr=%q}`, name, addr), func() float64 {
+		cp.mu.Lock()
+		n := len(cp.conns)
+		cp.mu.Unlock()
+		return float64(n)
+	})
+	connPoolsMu.Lock()
+	connPools = append(connPools, cp)
+	connPoolsMu.Unlock()
+	return cp
 }
 
 // Addr returns the address where connections are established.
@@ -45,8 +62,9 @@ func (cp *ConnPool) Get() (*handshake.BufferedConn, error) {
 	var bc *handshake.BufferedConn
 	cp.mu.Lock()
 	if len(cp.conns) > 0 {
-		bc = cp.conns[len(cp.conns)-1]
-		cp.conns[len(cp.conns)-1] = nil
+		c := cp.conns[len(cp.conns)-1]
+		bc = c.bc
+		c.bc = nil
 		cp.conns = cp.conns[:len(cp.conns)-1]
 	}
 	cp.mu.Unlock()
@@ -78,6 +96,43 @@ func (cp *ConnPool) Put(bc *handshake.BufferedConn) {
 		return
 	}
 	cp.mu.Lock()
-	cp.conns = append(cp.conns, bc)
+	cp.conns = append(cp.conns, connWithTimestamp{
+		bc:             bc,
+		lastActiveTime: fasttime.UnixTimestamp(),
+	})
 	cp.mu.Unlock()
 }
+
+func (cp *ConnPool) closeIdleConns() {
+	// Close connections, which were idle for more than 30 seconds.
+	deadline := fasttime.UnixTimestamp() - 30
+	var activeConns []connWithTimestamp
+	cp.mu.Lock()
+	conns := cp.conns
+	for _, c := range conns {
+		if c.lastActiveTime > deadline {
+			activeConns = append(activeConns, c)
+		} else {
+			_ = c.bc.Close()
+			c.bc = nil
+		}
+	}
+	cp.conns = activeConns
+	cp.mu.Unlock()
+}
+
+func init() {
+	go func() {
+		for {
+			time.Sleep(17 * time.Second)
+			connPoolsMu.Lock()
+			for _, cp := range connPools {
+				cp.closeIdleConns()
+			}
+			connPoolsMu.Unlock()
+		}
+	}()
+}
+
+var connPoolsMu sync.Mutex
+var connPools []*ConnPool
