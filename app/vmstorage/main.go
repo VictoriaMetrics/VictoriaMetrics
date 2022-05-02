a43683640294616c4967346682471c1c1213667c
Aliaksandr Valialkin <valyala@victoriametrics.com>
2022-05-02 10:12:05 +0300

    lib/flagutil: re-use Duration.Set() call in NewDuration

--- app/vmstorage/main.go
+++ app/vmstorage/main.go
@@ -24,7 +24,7 @@ import (
 )
 
 var (
-	retentionPeriod   = flagutil.NewDuration("retentionPeriod", 1, "Data with timestamps outside the retentionPeriod is automatically deleted")
+	retentionPeriod   = flagutil.NewDuration("retentionPeriod", "1", "Data with timestamps outside the retentionPeriod is automatically deleted")
 	httpListenAddr    = flag.String("httpListenAddr", ":8482", "Address to listen for http connections")
 	storageDataPath   = flag.String("storageDataPath", "vmstorage-data", "Path to storage data")
 	vminsertAddr      = flag.String("vminsertAddr", ":8400", "TCP address to accept connections from vminsert services")
