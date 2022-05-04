7dd9f3b98e668e5086f2f0eb92b25971f11c5be3
Dmytro Kozlov <kozlovdmitriyy@gmail.com>
2022-05-04 22:12:03 +0300

    {vmbackup, vmbackup/snapshot}: fixed problem with snapshot backup in another snapshot folder (#2535)
    * {vmbackup, vmbackup/snapshot}: validate snapshot name
    
    * vmbackup/snapshot: added another checks
    
    * backup/actions: added check that we ignore backup_complete.ignore file
    
    * vmbackup: moved snapshot to lib directory
    
    * lib/snapshot: added functions description
    
    * lib/snapshot: fixed typo
    
    * vmbackup: code cleanup
    
    * wip
    
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- lib/snapshot/snapshot.go
+++ lib/snapshot/snapshot.go
@@ -7,9 +7,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"regexp"
+	"strings"
+	"sync/atomic"
+	"time"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 )
+
+var snapshotNameRegexp = regexp.MustCompile(`^[0-9]{14}-[0-9A-Fa-f]+$`)
 
 type snapshot struct {
 	Status   string `json:"status"`
@@ -17,8 +23,7 @@ type snapshot struct {
 	Msg      string `json:"msg"`
 }
 
-// Create creates a snapshot and the provided api endpoint and returns
-// the snapshot name
+// Create creates a snapshot via the provided api endpoint and returns the snapshot name
 func Create(createSnapshotURL string) (string, error) {
 	logger.Infof("Creating snapshot")
 	u, err := url.Parse(createSnapshotURL)
@@ -53,7 +58,7 @@ func Create(createSnapshotURL string) (string, error) {
 	}
 }
 
-// Delete deletes a snapshot and the provided api endpoint returns any failure
+// Delete deletes a snapshot via the provided api endpoint
 func Delete(deleteSnapshotURL string, snapshotName string) error {
 	logger.Infof("Deleting snapshot %s", snapshotName)
 	formData := url.Values{
@@ -90,3 +95,37 @@ func Delete(deleteSnapshotURL string, snapshotName string) error {
 		return fmt.Errorf("Unkown status: %v", snap.Status)
 	}
 }
+
+// Validate validates the snapshotName
+func Validate(snapshotName string) error {
+	_, err := Time(snapshotName)
+	return err
+}
+
+// Time returns snapshot creation time from the given snapshotName
+func Time(snapshotName string) (time.Time, error) {
+	if !snapshotNameRegexp.MatchString(snapshotName) {
+		return time.Time{}, fmt.Errorf("unexpected snapshot name=%q; it must match %q regexp", snapshotName, snapshotNameRegexp.String())
+	}
+	n := strings.IndexByte(snapshotName, '-')
+	if n < 0 {
+		logger.Panicf("BUG: cannot find `-` in snapshotName=%q", snapshotName)
+	}
+	s := snapshotName[:n]
+	t, err := time.Parse("20060102150405", s)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("unexpected timestamp=%q in snapshot name: %w; it must match YYYYMMDDhhmmss pattern", s, err)
+	}
+	return t, nil
+}
+
+// NewName returns new name for new snapshot
+func NewName() string {
+	return fmt.Sprintf("%s-%08X", time.Now().UTC().Format("20060102150405"), nextSnapshotIdx())
+}
+
+func nextSnapshotIdx() uint64 {
+	return atomic.AddUint64(&snapshotIdx, 1)
+}
+
+var snapshotIdx = uint64(time.Now().UnixNano())
