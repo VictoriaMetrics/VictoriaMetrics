fcf4190d0ba80d15dbc0c33ccf3f3ce00283958b
Yury Molodov <yurymolodov@gmail.com>
2022-05-17 14:13:15 +0300

    vmui: add option to customize url params for individual pages (#2582)

--- app/vmui/packages/vmui/src/App.tsx
+++ app/vmui/packages/vmui/src/App.tsx
@@ -19,29 +19,29 @@ import DashboardsLayout from "./components/PredefinedPanels/DashboardsLayout";
 const App: FC = () => {
 
   return <>
-    <CssBaseline /> {/* CSS Baseline: kind of normalize.css made by materialUI team - can be scoped */}
-    <LocalizationProvider dateAdapter={DayjsUtils}> {/* Allows datepicker to work with DayJS */}
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={THEME}>  {/* Material UI theme customization */}
-          <StateProvider> {/* Serialized into query string, common app settings */}
-            <AuthStateProvider> {/* Auth related info - optionally persisted to Local Storage */}
-              <GraphStateProvider> {/* Graph settings */}
-                <SnackbarProvider> {/* Display various snackbars */}
-                  <HashRouter>
+    <HashRouter>
+      <CssBaseline /> {/* CSS Baseline: kind of normalize.css made by materialUI team - can be scoped */}
+      <LocalizationProvider dateAdapter={DayjsUtils}> {/* Allows datepicker to work with DayJS */}
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={THEME}>  {/* Material UI theme customization */}
+            <StateProvider> {/* Serialized into query string, common app settings */}
+              <AuthStateProvider> {/* Auth related info - optionally persisted to Local Storage */}
+                <GraphStateProvider> {/* Graph settings */}
+                  <SnackbarProvider> {/* Display various snackbars */}
                     <Routes>
                       <Route path={"/"} element={<HomeLayout/>}>
                         <Route path={router.home} element={<CustomPanel/>}/>
                         <Route path={router.dashboards} element={<DashboardsLayout/>}/>
                       </Route>
                     </Routes>
-                  </HashRouter>
-                </SnackbarProvider>
-              </GraphStateProvider>
-            </AuthStateProvider>
-          </StateProvider>
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </LocalizationProvider>
+                  </SnackbarProvider>
+                </GraphStateProvider>
+              </AuthStateProvider>
+            </StateProvider>
+          </ThemeProvider>
+        </StyledEngineProvider>
+      </LocalizationProvider>
+    </HashRouter>
   </>;
 };
 
