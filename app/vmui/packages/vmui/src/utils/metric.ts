879670418fd0c4b21a2d7e9106b8c321da075e3d
Yury Molodov <yurymolodov@gmail.com>
2022-06-13 09:43:37 +0300

    vmui: enhancements (#2638) (#2717)
    * feat: make datepicker to be set to last 30 min by default
    
    * fix: correct spinner while loading data
    
    * feat: change legend style
    
    * app/vmselect: `make vmui-update`
    
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- app/vmui/packages/vmui/src/utils/metric.ts
+++ app/vmui/packages/vmui/src/utils/metric.ts
@@ -2,10 +2,10 @@ import {MetricBase} from "../api/types";
 
 export const getNameForMetric = (result: MetricBase, alias?: string): string => {
   const { __name__, ...freeFormFields } = result.metric;
-  const name = alias || __name__ || `Query ${result.group} result`;
+  const name = alias || __name__ || "";
 
   if (Object.keys(result.metric).length === 0) {
-    return name; // a bit better than just {} for case of aggregation functions
+    return name || `Result ${result.group}`; // a bit better than just {} for case of aggregation functions
   }
 
   return `${name} {${Object.entries(freeFormFields).map(e => `${e[0]}: ${e[1]}`).join(", ")}}`;
