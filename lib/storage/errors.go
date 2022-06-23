de4da044b74c1398e0d7f82c8beb71360bbf5fa5
hagen1778 <roman@victoriametrics.com>
2022-06-23 10:21:54 +0200

    lib/storage: linter fix
    Signed-off-by: hagen1778 <roman@victoriametrics.com>

--- lib/storage/errors.go
+++ lib/storage/errors.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 )
 
+// FlagSeriesLimit contains a name of the flag name placeholder
+// for search limits reaching errors
 const FlagSeriesLimit = "-search.max*"
 
+// ErrSeriesLimit is a generic error for cases when search limit is reached
 var ErrSeriesLimit = fmt.Errorf("either narrow down the search or increase %s command-line "+
 	"flag values at vmselect; see https://docs.victoriametrics.com/#resource-usage-limits", FlagSeriesLimit)
