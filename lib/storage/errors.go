1ea725bbece1172966cd94ead77cd71eba848750
hagen1778 <roman@victoriametrics.com>
2022-06-23 08:40:58 +0200

    lib/storage: linter fix
    Signed-off-by: hagen1778 <roman@victoriametrics.com>

--- lib/storage/errors.go
+++ lib/storage/errors.go
@@ -1,11 +1,10 @@
 package storage
 
 import (
-	"errors"
 	"fmt"
 )
 
 const FlagSeriesLimit = "-search.max*"
 
-var ErrSeriesLimit = errors.New(fmt.Sprintf("either narrow down the search or increase %s command-line "+
-	"flag values at vmselect; see https://docs.victoriametrics.com/#resource-usage-limits", FlagSeriesLimit))
+var ErrSeriesLimit = fmt.Errorf("either narrow down the search or increase %s command-line "+
+	"flag values at vmselect; see https://docs.victoriametrics.com/#resource-usage-limits", FlagSeriesLimit)
