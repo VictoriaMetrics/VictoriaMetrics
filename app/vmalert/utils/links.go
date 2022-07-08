338fd115d91e6e85171a4cd599d84a20a12ce94e
Aliaksandr Valialkin <valyala@victoriametrics.com>
2022-07-08 13:25:23 +0300

    app/vmalert/utils/links.go: document Prefix function, which has been added in b29fafa86b3cfc3a6caf14b7f30a55e49cbcb339

--- app/vmalert/utils/links.go
+++ app/vmalert/utils/links.go
@@ -4,6 +4,7 @@ import "strings"
 
 const prefix = "/vmalert/"
 
+// Prefix returns "/vmalert/" prefix if it is missing in the path.
 func Prefix(path string) string {
 	if strings.HasPrefix(path, prefix) {
 		return ""
