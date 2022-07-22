1a86cc84fc13e3ea1b4737abba5de47c8bcdc2c3
dmitryk-dk <kozlovdmitriyy@gmail.com>
2022-07-22 17:08:43 +0300

    vmselect/promql: cleanup

--- app/vmselect/promql/transform.go
+++ app/vmselect/promql/transform.go
@@ -3,7 +3,6 @@ package promql
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"math"
 	"math/rand"
 	"regexp"
@@ -565,7 +564,6 @@ func vmrangeBucketsToLE(tss []*timeseries) []*timeseries {
 			}
 			xsPrev = xs
 		}
-		log.Printf("xsPrev.end => %#v", xsPrev)
 		if !math.IsInf(xsPrev.end, 1) {
 			xssNew = append(xssNew, x{
 				endStr: "+Inf",
