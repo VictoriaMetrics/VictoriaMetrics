44739095005017cb272e6a0119893f98318e10a2
dmitryk-dk <kozlovdmitriyy@gmail.com>
2022-08-03 11:27:40 +0300

    lib/auth: simplify failure tests

--- lib/auth/auth_test.go
+++ lib/auth/auth_test.go
@@ -27,37 +27,33 @@ func TestNewTokenSuccess(t *testing.T) {
 }
 
 func TestNewTokenFailure(t *testing.T) {
-	f := func(name string, token string, want string) {
+	f := func(name string, token string) {
 		t.Helper()
 		t.Run(name, func(t *testing.T) {
 			newToken, err := NewToken(token)
 			if err == nil {
 				t.Fatalf("expecting non-nil error")
 			}
-			var got string
 			if newToken != nil {
-				got = fmt.Sprintf("%d:%d", newToken.AccountID, newToken.ProjectID)
-			}
-			if got != want {
-				t.Errorf("NewToken() got = %v, want %v", newToken, want)
+				t.Fatalf("expecting nil token")
 			}
 		})
 	}
-	f("empty token", "", "")
-	f("empty accountID and projectID", ":", "")
-	f("accountID and projectID not int values", "a:b", "")
-	f("missed projectID", "1:", "")
-	f("missed accountID", ":2", "")
-	f("large int value for accountID", "9223372036854775808:1", "")
-	f("large int value for projectID", "2:9223372036854775808", "")
-	f("both large int values incorrect", "9223372036854775809:9223372036854775808", "")
-	f("large uint32 values incorrect", "4294967297:4294967295", "")
-	f("negative accountID", "-100:100", "")
-	f("negative projectID", "100:-100", "")
-	f("negative accountID and projectID", "-100:-100", "")
-	f("accountID is string", "abcd:2", "")
-	f("projectID is string", "2:abcd", "")
-	f("empty many parts in the token", "::", "")
-	f("many string parts in the token", "a:b:c", "")
-	f("many int parts in the token", "1:2:3", "")
+	f("empty token", "")
+	f("empty accountID and projectID", ":")
+	f("accountID and projectID not int values", "a:b")
+	f("missed projectID", "1:")
+	f("missed accountID", ":2")
+	f("large int value for accountID", "9223372036854775808:1")
+	f("large int value for projectID", "2:9223372036854775808")
+	f("both large int values incorrect", "9223372036854775809:9223372036854775808")
+	f("large uint32 values incorrect", "4294967297:4294967295")
+	f("negative accountID", "-100:100")
+	f("negative projectID", "100:-100")
+	f("negative accountID and projectID", "-100:-100")
+	f("accountID is string", "abcd:2")
+	f("projectID is string", "2:abcd")
+	f("empty many parts in the token", "::")
+	f("many string parts in the token", "a:b:c")
+	f("many int parts in the token", "1:2:3")
 }
