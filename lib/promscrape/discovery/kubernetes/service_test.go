c4fcd9f1c5a9b4ac93e49c4b0e869f90d02a892f
Aliaksandr Valialkin <valyala@victoriametrics.com>
2022-08-15 01:05:12 +0300

    lib/promscrape/discovery/kubernetes: add `__meta_kubernetes_service_port_number` label to `role: service` in the same way as Prometheus 2.38 does
    See https://github.com/prometheus/prometheus/pull/11002

--- lib/promscrape/discovery/kubernetes/service_test.go
+++ lib/promscrape/discovery/kubernetes/service_test.go
@@ -107,6 +107,7 @@ func TestParseServiceListSuccess(t *testing.T) {
 			"__meta_kubernetes_service_name":          "kube-dns",
 			"__meta_kubernetes_service_type":          "ClusterIP",
 			"__meta_kubernetes_service_port_name":     "dns",
+			"__meta_kubernetes_service_port_number":   "53",
 			"__meta_kubernetes_service_port_protocol": "UDP",
 			"__meta_kubernetes_service_cluster_ip":    "10.96.0.10",
 
@@ -130,6 +131,7 @@ func TestParseServiceListSuccess(t *testing.T) {
 			"__meta_kubernetes_service_name":          "kube-dns",
 			"__meta_kubernetes_service_type":          "ClusterIP",
 			"__meta_kubernetes_service_port_name":     "dns-tcp",
+			"__meta_kubernetes_service_port_number":   "53",
 			"__meta_kubernetes_service_port_protocol": "TCP",
 			"__meta_kubernetes_service_cluster_ip":    "10.96.0.10",
 
@@ -153,6 +155,7 @@ func TestParseServiceListSuccess(t *testing.T) {
 			"__meta_kubernetes_service_name":          "kube-dns",
 			"__meta_kubernetes_service_type":          "ClusterIP",
 			"__meta_kubernetes_service_port_name":     "metrics",
+			"__meta_kubernetes_service_port_number":   "9153",
 			"__meta_kubernetes_service_port_protocol": "TCP",
 			"__meta_kubernetes_service_cluster_ip":    "10.96.0.10",
 
