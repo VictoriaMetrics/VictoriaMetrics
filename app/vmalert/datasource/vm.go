a887c1bc07129a1b4f7c8016b955bb2297acf5ef
Roman Khavronenko <roman@victoriametrics.com>
2022-09-13 15:25:43 +0200

    vmalert: add `debug` mode for alerting rules (#3055)
    * vmalert: add `debug` mode for alerting rules
    
    Debug information includes alerts state changes and requests
    sent to the datasource. Debug can be enabled only on rule's
    level. It might be useful for debugging unexpected
    behaviour of alerting rule.
    
    https://github.com/VictoriaMetrics/VictoriaMetrics/issues/3025
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>
    
    * vmalert: review fixes
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>
    
    * Update app/vmalert/alerting.go
    
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>
    
    * vmalert: go fmt
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- app/vmalert/datasource/vm.go
+++ app/vmalert/datasource/vm.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promauth"
 )
 
@@ -39,6 +40,10 @@ type VMStorage struct {
 	evaluationInterval time.Duration
 	extraParams        url.Values
 	extraHeaders       []keyValue
+
+	// whether to print additional log messages
+	// for each sent request
+	debug bool
 }
 
 type keyValue struct {
@@ -64,6 +69,7 @@ func (s *VMStorage) ApplyParams(params QuerierParams) *VMStorage {
 	s.dataSourceType = toDatasourceType(params.DataSourceType)
 	s.evaluationInterval = params.EvaluationInterval
 	s.extraParams = params.QueryParams
+	s.debug = params.Debug
 	if params.Headers != nil {
 		for key, value := range params.Headers {
 			kv := keyValue{key: key, value: value}
@@ -151,6 +157,9 @@ func (s *VMStorage) QueryRange(ctx context.Context, query string, start, end tim
 }
 
 func (s *VMStorage) do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	if s.debug {
+		logger.Infof("DEBUG datasource request: executing %s request with params %q", req.Method, req.URL.RawQuery)
+	}
 	resp, err := s.c.Do(req.WithContext(ctx))
 	if err != nil {
 		return nil, fmt.Errorf("error getting response from %s: %w", req.URL.Redacted(), err)
