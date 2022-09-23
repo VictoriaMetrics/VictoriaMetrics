7115b8610a6e47602153852beec57fb9017cfdb0
Roman Khavronenko <roman@victoriametrics.com>
2022-09-23 10:22:53 +0200

    vmalert: set default value for `datasource.queryStep` to `5m` (#3149)
    Change default value for command-line flag `datasource.queryStep` from `0s` to `5m`.
    Param `step` is added by vmalert to every rule evaluation request sent to datasource.
    Before this change, `step` was equal to group's evaluation interval by default.
    Param `step` for instant queries defines how far VM can look back for the last written data point.
    The change supposed to improve reliability of the rules evaluation when evaluation interval
    is lower than scraping interval.
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>

--- app/vmalert/datasource/init.go
+++ app/vmalert/datasource/init.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/app/vmalert/utils"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/flagutil"
@@ -38,9 +39,9 @@ var (
 	oauth2Scopes           = flag.String("datasource.oauth2.scopes", "", "Optional OAuth2 scopes to use for -datasource.url. Scopes must be delimited by ';'")
 
 	lookBack  = flag.Duration("datasource.lookback", 0, `Lookback defines how far into the past to look when evaluating queries. For example, if the datasource.lookback=5m then param "time" with value now()-5m will be added to every query.`)
-	queryStep = flag.Duration("datasource.queryStep", 0, "queryStep defines how far a value can fallback to when evaluating queries. "+
+	queryStep = flag.Duration("datasource.queryStep", 5*time.Minute, "queryStep defines how far a value can fallback to when evaluating queries. "+
 		"For example, if datasource.queryStep=15s then param \"step\" with value \"15s\" will be added to every query."+
-		"If queryStep isn't specified, rule's evaluationInterval will be used instead.")
+		"If queryStep is set to 0, rule's evaluationInterval will be used instead.")
 	queryTimeAlignment = flag.Bool("datasource.queryTimeAlignment", true, `Whether to align "time" parameter with evaluation interval.`+
 		"Alignment supposed to produce deterministic results despite of number of vmalert replicas or time they were started. See more details here https://github.com/VictoriaMetrics/VictoriaMetrics/pull/1257")
 	maxIdleConnections = flag.Int("datasource.maxIdleConnections", 100, `Defines the number of idle (keep-alive connections) to each configured datasource. Consider setting this value equal to the value: groups_total * group.concurrency. Too low a value may result in a high number of sockets in TIME_WAIT state.`)
