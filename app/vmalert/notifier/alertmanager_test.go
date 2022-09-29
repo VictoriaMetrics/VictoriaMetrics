00e61189888b2a858e0b17b013ca3da6998efe5c
lihaowei <haoweili35@gmail.com>
2022-09-29 23:27:10 +0800

    fix issue#3053
    Signed-off-by: lihaowei <haoweili35@gmail.com>

--- app/vmalert/notifier/alertmanager_test.go
+++ app/vmalert/notifier/alertmanager_test.go
@@ -67,9 +67,6 @@ func TestAlertManager_Send(t *testing.T) {
 			if a[0].GeneratorURL != "0/0" {
 				t.Errorf("expected 0/0 as generatorURL got %s", a[0].GeneratorURL)
 			}
-			if a[0].Labels["alertname"] != "alert0" {
-				t.Errorf("expected alert0 as alert name got %s", a[0].Labels["alertname"])
-			}
 			if a[0].StartsAt.IsZero() {
 				t.Errorf("expected non-zero start time")
 			}
