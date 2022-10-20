VictoriaMetrics/VictoriaMetrics

commit 68f3a025897ed8fb6170f41bbd5d879016b075a8

lib/{storage,mergeset}: do not hold per-shard lock in fast path when adding per-shard items to the flush list

lib/mergeset/table.go
@@ -204,7 +204,7 @@ func (ris *rawItemsShard) addItems(tb *Table, items [][]byte) error {
 			ibs[i] = nil
 		}
 		ris.ibs = ibs[:0]
-		ris.lastFlushTime = fasttime.UnixTimestamp()
+		atomic.StoreUint64(&ris.lastFlushTime, fasttime.UnixTimestamp())
 	}
 	ris.mu.Unlock()
 
@@ -632,19 +632,18 @@ func (ris *rawItemsShard) appendBlocksToFlush(dst []*inmemoryBlock, tb *Table, i
 	if flushSeconds <= 0 {
 		flushSeconds = 1
 	}
-
-	ris.mu.Lock()
-	if isFinal || currentTime-ris.lastFlushTime > uint64(flushSeconds) {
+	lastFlushTime := atomic.LoadUint64(&ris.lastFlushTime)
+	if isFinal || currentTime-lastFlushTime > uint64(flushSeconds) {
+		ris.mu.Lock()
 		ibs := ris.ibs
 		dst = append(dst, ibs...)
 		for i := range ibs {
 			ibs[i] = nil
 		}
 		ris.ibs = ibs[:0]
-		ris.lastFlushTime = currentTime
+		atomic.StoreUint64(&ris.lastFlushTime, currentTime)
+		ris.mu.Unlock()
 	}
-	ris.mu.Unlock()
-
 	return dst
 }
 

lib/storage/partition.go
@@ -477,7 +477,7 @@ func (rrs *rawRowsShard) addRows(pt *partition, rows []rawRow) {
 		rowsToFlush = append(rowsToFlush, rrs.rows...)
 		rowsToFlush = append(rowsToFlush, rows...)
 		rrs.rows = rrs.rows[:0]
-		rrs.lastFlushTime = fasttime.UnixTimestamp()
+		atomic.StoreUint64(&rrs.lastFlushTime, fasttime.UnixTimestamp())
 	}
 	rrs.mu.Unlock()
 
@@ -726,14 +726,14 @@ func (rrs *rawRowsShard) appendRawRowsToFlush(dst []rawRow, pt *partition, isFin
 	if flushSeconds <= 0 {
 		flushSeconds = 1
 	}
-
-	rrs.mu.Lock()
-	if isFinal || currentTime-rrs.lastFlushTime > uint64(flushSeconds) {
+	lastFlushTime := atomic.LoadUint64(&rrs.lastFlushTime)
+	if isFinal || currentTime-lastFlushTime > uint64(flushSeconds) {
+		rrs.mu.Lock()
 		dst = append(dst, rrs.rows...)
 		rrs.rows = rrs.rows[:0]
+		atomic.StoreUint64(&rrs.lastFlushTime, currentTime)
+		rrs.mu.Unlock()
 	}
-	rrs.mu.Unlock()
-
 	return dst
 }
 

commit 6855de311c7d33a3024f68dfda51ae4acfa02cd3

lib/{mergeset,storage}: avoid `unaligned 64-bit atomic operation` panic on 32-bit platforms
The panic has been introduced in 68f3a025897ed8fb6170f41bbd5d879016b075a8

While at it, add padding to shard structs in order to avoid false sharing on mordern CPUs

This should improve scalability on systems with many CPU cores

lib/mergeset/table.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"unsafe"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/cgroup"
@@ -158,10 +159,20 @@ func (riss *rawItemsShards) Len() int {
 	return n
 }
 
-type rawItemsShard struct {
-	mu            sync.Mutex
-	ibs           []*inmemoryBlock
+type rawItemsShardNopad struct {
+	// Put lastFlushTime to the top in order to avoid unaligned memory access on 32-bit architectures
 	lastFlushTime uint64
+
+	mu  sync.Mutex
+	ibs []*inmemoryBlock
+}
+
+type rawItemsShard struct {
+	rawItemsShardNopad
+
+	// The padding prevents false sharing on widespread platforms with
+	// 128 mod (cache line size) = 0 .
+	_ [128 - unsafe.Sizeof(rawItemsShardNopad{})%128]byte
 }
 
 func (ris *rawItemsShard) Len() int {

lib/storage/partition.go
@@ -445,10 +445,20 @@ func (rrss *rawRowsShards) Len() int {
 	return n
 }
 
-type rawRowsShard struct {
-	mu            sync.Mutex
-	rows          []rawRow
+type rawRowsShardNopad struct {
+	// Put lastFlushTime to the top in order to avoid unaligned memory access on 32-bit architectures
 	lastFlushTime uint64
+
+	mu   sync.Mutex
+	rows []rawRow
+}
+
+type rawRowsShard struct {
+	rawRowsShardNopad
+
+	// The padding prevents false sharing on widespread platforms with
+	// 128 mod (cache line size) = 0 .
+	_ [128 - unsafe.Sizeof(rawRowsShardNopad{})%128]byte
 }
 
 func (rrs *rawRowsShard) Len() int {