ade0839ebd769740574872b7ebfb8b5a108c302c
Yury Moladau <yurymolodov@gmail.com>
2022-12-21 00:10:38 +0100

    fix: add encodeURIComponent to query

--- app/vmui/packages/vmui/src/api/explore-metrics.ts
+++ app/vmui/packages/vmui/src/api/explore-metrics.ts
@@ -4,7 +4,7 @@ export const getJobsUrl = (server: string, period: TimeParams): string =>
   `${server}/api/v1/label/job/values?start=${period.start}&end=${period.end}`;
 
 export const getInstancesUrl = (server: string, period: TimeParams, job: string): string =>
-  `${server}/api/v1/label/instance/values?match[]={job="${job}"}&start=${period.start}&end=${period.end}`;
+  `${server}/api/v1/label/instance/values?match[]={job="${encodeURIComponent(job)}"}&start=${period.start}&end=${period.end}`;
 
 export const getNamesUrl = (server: string, job: string, instance: string): string => {
   const match = Object.entries({ job, instance })
@@ -12,5 +12,5 @@ export const getNamesUrl = (server: string, job: string, instance: string): stri
     .map(([key, val]) => `${key}="${val}"`)
     .join(",");
 
-  return `${server}/api/v1/label/__name__/values?match[]={${match}}`;
+  return `${server}/api/v1/label/__name__/values?match[]={${encodeURIComponent(match)}}`;
 };
