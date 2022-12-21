3300546eab2aad3e54313f3ca8770431ea24f3f8
Aliaksandr Valialkin <valyala@victoriametrics.com>
2022-12-21 12:57:28 -0800

    lib/bytesutil: make sure that the cleanup code is performed only by a single goroutine out of many concurrently running goroutines
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/3466

--- lib/bytesutil/fast_string_matcher_test.go
+++ lib/bytesutil/fast_string_matcher_test.go
@@ -23,3 +23,27 @@ func TestFastStringMatcher(t *testing.T) {
 	f("a_b-C", false)
 	f("foobar", true)
 }
+
+func TestNeedCleanup(t *testing.T) {
+	f := func(lastCleanupTime, currentTime uint64, resultExpected bool) {
+		t.Helper()
+		lct := lastCleanupTime
+		result := needCleanup(&lct, currentTime)
+		if result != resultExpected {
+			t.Fatalf("unexpected result for needCleanup(%d, %d); got %v; want %v", lastCleanupTime, currentTime, result, resultExpected)
+		}
+		if result {
+			if lct != currentTime {
+				t.Fatalf("unexpected value for lct; got %d; want currentTime=%d", lct, currentTime)
+			}
+		} else {
+			if lct != lastCleanupTime {
+				t.Fatalf("unexpected value for lct; got %d; want lastCleanupTime=%d", lct, lastCleanupTime)
+			}
+		}
+	}
+	f(0, 0, false)
+	f(0, 61, false)
+	f(0, 62, true)
+	f(10, 100, true)
+}
