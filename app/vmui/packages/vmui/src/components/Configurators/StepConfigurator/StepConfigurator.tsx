6f21435d2d3e7045775fad2f7818a734ed4a61ca
Yury Molodov <yurymolodov@gmail.com>
2022-12-29 01:00:51 +0100

    vmui: fix step field (#3561)
    * feat: use a unit next to the step value
    
    * app/vmselect/vmui: `make vmui-update`
    
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- app/vmui/packages/vmui/src/components/Configurators/StepConfigurator/StepConfigurator.tsx
+++ app/vmui/packages/vmui/src/components/Configurators/StepConfigurator/StepConfigurator.tsx
@@ -1,14 +1,15 @@
-import React, { FC, useCallback, useEffect, useState } from "preact/compat";
-import debounce from "lodash.debounce";
+import React, { FC, useEffect, useState } from "preact/compat";
 import { RestartIcon } from "../../Main/Icons";
 import TextField from "../../Main/TextField/TextField";
 import Button from "../../Main/Button/Button";
 import Tooltip from "../../Main/Tooltip/Tooltip";
+import { ErrorTypes } from "../../../types";
+import { supportedDurations } from "../../../utils/time";
 
 interface StepConfiguratorProps {
-  defaultStep?: number,
-  value?: number,
-  setStep: (step: number) => void,
+  defaultStep?: string,
+  value?: string,
+  setStep: (step: string) => void,
 }
 
 const StepConfigurator: FC<StepConfiguratorProps> = ({ value, defaultStep, setStep }) => {
@@ -16,40 +17,46 @@ const StepConfigurator: FC<StepConfiguratorProps> = ({ value, defaultStep, setSt
   const [customStep, setCustomStep] = useState(value || defaultStep);
   const [error, setError] = useState("");
 
-  const handleApply = (step: number) => setStep(step || 1);
-  const debouncedHandleApply = useCallback(debounce(handleApply, 500), []);
-
-  const onChangeStep = (val: string) => {
-    const value = +val;
-    if (!value) return;
-    handleSetStep(value);
+  const handleApply = (value?: string) => {
+    const step = value || customStep || defaultStep || "1s";
+    const durations = step.match(/[a-zA-Z]+/g) || [];
+    setStep(!durations.length ? `${step}s` : step);
   };
 
-  const handleSetStep = (value: number) => {
-    if (value > 0) {
-      setCustomStep(value);
-      debouncedHandleApply(value);
+  const handleChangeStep = (value: string) => {
+    const numbers = value.match(/[-+]?([0-9]*\.[0-9]+|[0-9]+)/g) || [];
+    const durations = value.match(/[a-zA-Z]+/g) || [];
+    const isValidNumbers = numbers.length && numbers.every(num => parseFloat(num) > 0);
+    const isValidDuration = durations.every(d => supportedDurations.find(dur => dur.short === d));
+    const isValidStep = isValidNumbers && isValidDuration;
+
+    setCustomStep(value);
+
+    if (isValidStep) {
       setError("");
     } else {
-      setError("step is out of allowed range");
+      setError(ErrorTypes.validStep);
     }
   };
 
   const handleReset = () => {
-    handleSetStep(defaultStep || 1);
+    const value = defaultStep || "1s";
+    handleChangeStep(value);
+    handleApply(value);
   };
 
   useEffect(() => {
-    if (value) handleSetStep(value);
+    if (value) handleChangeStep(value);
   }, [value]);
 
   return (
     <TextField
-      label="Step value of seconds"
-      type="number"
+      label="Step value"
       value={customStep}
       error={error}
-      onChange={onChangeStep}
+      onChange={handleChangeStep}
+      onEnter={handleApply}
+      onBlur={handleApply}
       endIcon={(
         <Tooltip title="Reset step to default">
           <Button
