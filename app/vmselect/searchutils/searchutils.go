f27bb192130ee22e8b37e73d83ba84c3c8e13734
Aliaksandr Valialkin <valyala@victoriametrics.com>
2022-12-28 19:41:51 -0800

    app/vmselect/searchutils: accept partial RFC3339 values at `time`, `start` and `end` query args
    This simplifies manual usage of the APIs. For example, the following query
    would return the results over the 2022 year.
    
      /api/v1/query_range?start=2022&end=2023&step=1d&query=...
    
    This is equivalent to:
    
      /api/v1/query_range?start=2022-01-01T00:00:00Z&end=2023-01-01T00:00:00Z&step=1d&query=...

--- app/vmselect/searchutils/searchutils.go
+++ app/vmselect/searchutils/searchutils.go
@@ -50,30 +50,18 @@ func GetTime(r *http.Request, argKey string, defaultMs int64) (int64, error) {
 	if len(argValue) == 0 {
 		return roundToSeconds(defaultMs), nil
 	}
-	secs, err := strconv.ParseFloat(argValue, 64)
+	// Handle Prometheus'-provided minTime and maxTime.
+	// See https://github.com/prometheus/client_golang/issues/614
+	switch argValue {
+	case prometheusMinTimeFormatted:
+		return minTimeMsecs, nil
+	case prometheusMaxTimeFormatted:
+		return maxTimeMsecs, nil
+	}
+	// Parse argValue
+	secs, err := parseTime(argValue)
 	if err != nil {
-		// Try parsing string format
-		t, err := time.Parse(time.RFC3339, argValue)
-		if err != nil {
-			// Handle Prometheus'-provided minTime and maxTime.
-			// See https://github.com/prometheus/client_golang/issues/614
-			switch argValue {
-			case prometheusMinTimeFormatted:
-				return minTimeMsecs, nil
-			case prometheusMaxTimeFormatted:
-				return maxTimeMsecs, nil
-			}
-			// Try parsing duration relative to the current time
-			d, err1 := promutils.ParseDuration(argValue)
-			if err1 != nil {
-				return 0, fmt.Errorf("cannot parse %q=%q: %w", argKey, argValue, err)
-			}
-			if d > 0 {
-				d = -d
-			}
-			t = time.Now().Add(d)
-		}
-		secs = float64(t.UnixNano()) / 1e9
+		return 0, fmt.Errorf("cannot parse %s=%s: %w", argKey, argValue, err)
 	}
 	msecs := int64(secs * 1e3)
 	if msecs < minTimeMsecs {
@@ -83,6 +71,78 @@ func GetTime(r *http.Request, argKey string, defaultMs int64) (int64, error) {
 		msecs = maxTimeMsecs
 	}
 	return msecs, nil
+}
+
+func parseTime(s string) (float64, error) {
+	if len(s) > 0 && (s[len(s)-1] != 'Z' && s[len(s)-1] > '9' || s[0] == '-') {
+		// Parse duration relative to the current time
+		d, err := promutils.ParseDuration(s)
+		if err != nil {
+			return 0, err
+		}
+		if d > 0 {
+			d = -d
+		}
+		t := time.Now().Add(d)
+		return float64(t.UnixNano()) / 1e9, nil
+	}
+	if len(s) == 4 {
+		// Parse YYYY
+		t, err := time.Parse("2006", s)
+		if err != nil {
+			return 0, err
+		}
+		return float64(t.UnixNano()) / 1e9, nil
+	}
+	if !strings.Contains(s, "-") {
+		// Parse the timestamp in milliseconds
+		return strconv.ParseFloat(s, 64)
+	}
+	if len(s) == 7 {
+		// Parse YYYY-MM
+		t, err := time.Parse("2006-01", s)
+		if err != nil {
+			return 0, err
+		}
+		return float64(t.UnixNano()) / 1e9, nil
+	}
+	if len(s) == 10 {
+		// Parse YYYY-MM-DD
+		t, err := time.Parse("2006-01-02", s)
+		if err != nil {
+			return 0, err
+		}
+		return float64(t.UnixNano()) / 1e9, nil
+	}
+	if len(s) == 13 {
+		// Parse YYYY-MM-DDTHH
+		t, err := time.Parse("2006-01-02T15", s)
+		if err != nil {
+			return 0, err
+		}
+		return float64(t.UnixNano()) / 1e9, nil
+	}
+	if len(s) == 16 {
+		// Parse YYYY-MM-DDTHH:MM
+		t, err := time.Parse("2006-01-02T15:04", s)
+		if err != nil {
+			return 0, err
+		}
+		return float64(t.UnixNano()) / 1e9, nil
+	}
+	if len(s) == 19 {
+		// Parse YYYY-MM-DDTHH:MM:SS
+		t, err := time.Parse("2006-01-02T15:04:05", s)
+		if err != nil {
+			return 0, err
+		}
+		return float64(t.UnixNano()) / 1e9, nil
+	}
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return 0, err
+	}
+	return float64(t.UnixNano()) / 1e9, nil
 }
 
 var (
