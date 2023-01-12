6992f071bb40e1cefc6c18b7df848261084cdadf
Yury Moladau <yurymolodov@gmail.com>
2023-01-12 15:29:23 +0100

    fix: set minimumSignificantDigits to 1

--- app/vmui/packages/vmui/src/utils/uplot/helpers.ts
+++ app/vmui/packages/vmui/src/utils/uplot/helpers.ts
@@ -42,7 +42,7 @@ export const formatPrettyNumber = (n: number | null | undefined, min = 0, max = 
     digits = 20;
   }
   return n.toLocaleString("en-US", {
-    minimumSignificantDigits: digits,
+    minimumSignificantDigits: 1,
     maximumSignificantDigits: digits,
   });
 };
