bb1b87e5afb021a32fd78612b7a1a1c65ddefb98
Aliaksandr Valialkin <valyala@victoriametrics.com>
2023-01-23 08:05:39 -0800

    vendor: `make vendor-update`

--- vendor/cloud.google.com/go/storage/bucket.go
+++ vendor/cloud.google.com/go/storage/bucket.go
@@ -35,6 +35,7 @@ import (
 	raw "google.golang.org/api/storage/v1"
 	dpb "google.golang.org/genproto/googleapis/type/date"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 // BucketHandle provides operations on a Google Cloud Storage bucket.
@@ -1389,12 +1390,12 @@ func (rp *RetentionPolicy) toProtoRetentionPolicy() *storagepb.Bucket_RetentionP
 	}
 	// RetentionPeriod must be greater than 0, so if it is 0, the user left it
 	// unset, and so we should not send it in the request i.e. nil is sent.
-	var period *int64
+	var dur *durationpb.Duration
 	if rp.RetentionPeriod != 0 {
-		period = proto.Int64(int64(rp.RetentionPeriod / time.Second))
+		dur = durationpb.New(rp.RetentionPeriod)
 	}
 	return &storagepb.Bucket_RetentionPolicy{
-		RetentionPeriod: period,
+		RetentionDuration: dur,
 	}
 }
 
@@ -1418,7 +1419,7 @@ func toRetentionPolicyFromProto(rp *storagepb.Bucket_RetentionPolicy) *Retention
 		return nil
 	}
 	return &RetentionPolicy{
-		RetentionPeriod: time.Duration(rp.GetRetentionPeriod()) * time.Second,
+		RetentionPeriod: rp.GetRetentionDuration().AsDuration(),
 		EffectiveTime:   rp.GetEffectiveTime().AsTime(),
 		IsLocked:        rp.GetIsLocked(),
 	}
--- vendor/cloud.google.com/go/storage/grpc_client.go
+++ vendor/cloud.google.com/go/storage/grpc_client.go
@@ -34,7 +34,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
 	fieldmaskpb "google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
@@ -1496,11 +1495,16 @@ func (w *gRPCWriter) startResumableUpload() error {
 	if err != nil {
 		return err
 	}
+	req := &storagepb.StartResumableWriteRequest{
+		WriteObjectSpec:           spec,
+		CommonObjectRequestParams: toProtoCommonObjectRequestParams(w.encryptionKey),
+	}
+	// TODO: Currently the checksums are only sent on the request to initialize
+	// the upload, but in the future, we must also support sending it
+	// on the *last* message of the stream.
+	req.ObjectChecksums = toProtoChecksums(w.sendCRC32C, w.attrs)
 	return run(w.ctx, func() error {
-		upres, err := w.c.raw.StartResumableWrite(w.ctx, &storagepb.StartResumableWriteRequest{
-			WriteObjectSpec:           spec,
-			CommonObjectRequestParams: toProtoCommonObjectRequestParams(w.encryptionKey),
-		})
+		upres, err := w.c.raw.StartResumableWrite(w.ctx, req)
 		w.upid = upres.GetUploadId()
 		return err
 	}, w.settings.retry, w.settings.idempotent, setRetryHeaderGRPC(w.ctx))
@@ -1585,25 +1589,13 @@ func (w *gRPCWriter) uploadBuffer(recvd int, start int64, doneReading bool) (*st
 					WriteObjectSpec: spec,
 				}
 				req.CommonObjectRequestParams = toProtoCommonObjectRequestParams(w.encryptionKey)
+				// For a non-resumable upload, checksums must be sent in this message.
+				// TODO: Currently the checksums are only sent on the first message
+				// of the stream, but in the future, we must also support sending it
+				// on the *last* message of the stream (instead of the first).
+				req.ObjectChecksums = toProtoChecksums(w.sendCRC32C, w.attrs)
 			}
 
-			// TODO: Currently the checksums are only sent on the first message
-			// of the stream, but in the future, we must also support sending it
-			// on the *last* message of the stream (instead of the first).
-			if w.sendCRC32C {
-				req.ObjectChecksums = &storagepb.ObjectChecksums{
-					Crc32C: proto.Uint32(w.attrs.CRC32C),
-				}
-			}
-			if len(w.attrs.MD5) != 0 {
-				if cs := req.GetObjectChecksums(); cs == nil {
-					req.ObjectChecksums = &storagepb.ObjectChecksums{
-						Md5Hash: w.attrs.MD5,
-					}
-				} else {
-					cs.Md5Hash = w.attrs.MD5
-				}
-			}
 		}
 
 		err = w.stream.Send(req)
