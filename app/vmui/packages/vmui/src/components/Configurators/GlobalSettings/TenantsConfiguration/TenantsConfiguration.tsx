85c217ff40b31a102438dd465f5eaf9b36482506
Yury Moladau <yurymolodov@gmail.com>
2023-01-23 17:55:04 +0100

    fix: hide tenantID selector by condition

--- app/vmui/packages/vmui/src/components/Configurators/GlobalSettings/TenantsConfiguration/TenantsConfiguration.tsx
+++ app/vmui/packages/vmui/src/components/Configurators/GlobalSettings/TenantsConfiguration/TenantsConfiguration.tsx
@@ -1,102 +1,118 @@
-import React, { FC, useState, useEffect, useRef, useMemo } from "preact/compat";
+import React, { FC, useState, useRef, useEffect, useMemo } from "preact/compat";
 import { useAppDispatch, useAppState } from "../../../../state/common/StateContext";
 import { useTimeDispatch } from "../../../../state/time/TimeStateContext";
-import { InfoIcon } from "../../../Main/Icons";
-import TextField from "../../../Main/TextField/TextField";
+import { ArrowDownIcon, StorageIcons } from "../../../Main/Icons";
 import Button from "../../../Main/Button/Button";
-import Tooltip from "../../../Main/Tooltip/Tooltip";
 import { useFetchAccountIds } from "./hooks/useFetchAccountIds";
-import Autocomplete from "../../../Main/Autocomplete/Autocomplete";
 import "./style.scss";
 import { replaceTenantId } from "../../../../utils/default-server-url";
+import classNames from "classnames";
+import Popper from "../../../Main/Popper/Popper";
+import { getAppModeEnable } from "../../../../utils/app-mode";
+import Tooltip from "../../../Main/Tooltip/Tooltip";
 
 const TenantsConfiguration: FC = () => {
+  const appModeEnable = getAppModeEnable();
+
   const { tenantId: tenantIdState, serverUrl } = useAppState();
   const dispatch = useAppDispatch();
   const timeDispatch = useTimeDispatch();
   const { accountIds } = useFetchAccountIds();
 
-  const [openAutocomplete, setOpenAutocomplete] = useState(false);
-  const autocompleteAnchorEl = useRef<HTMLDivElement>(null);
-  const [tenantId, setTenantId] = useState(tenantIdState || "0");
+  const [openOptions, setOpenOptions] = useState(false);
+  const optionsButtonRef = useRef<HTMLDivElement>(null);
 
-  const autocompleteValue = useMemo(() => {
-    return !openAutocomplete ? "" : "(.+)";
-  }, [tenantId, openAutocomplete]);
+  const getTenantIdFromUrl = (url: string) => {
+    const regexp = /(\/select\/)(\d+|\d.+)(\/)(.+)/;
+    return (url.match(regexp) || [])[2];
+  };
 
-  const handleApply = (value?: string) => {
-    const tenant = value || tenantId;
+  const showTenantSelector = useMemo(() => {
+    const id = getTenantIdFromUrl(serverUrl);
+    return accountIds.length > 1 && id;
+  }, [accountIds, serverUrl]);
+
+  const toggleOpenOptions = () => {
+    setOpenOptions(prev => !prev);
+  };
+
+  const handleCloseOptions = () => {
+    setOpenOptions(false);
+  };
+
+  const createHandlerChange = (value: string) => () => {
+    const tenant = value;
     dispatch({ type: "SET_TENANT_ID", payload: tenant });
     if (serverUrl) {
       const updateServerUrl = replaceTenantId(serverUrl, tenant);
       if (updateServerUrl === serverUrl) return;
+      console.log("SET_SERVER", updateServerUrl);
       dispatch({ type: "SET_SERVER", payload: updateServerUrl });
       timeDispatch({ type: "RUN_QUERY" });
     }
-
-    if (document.activeElement instanceof HTMLInputElement) {
-      document.activeElement.blur();
-    }
-  };
-
-  const handleChange = (value: string) => {
-    setTenantId(value);
-  };
-
-  const handleFocus = () => {
-    setOpenAutocomplete(true);
+    handleCloseOptions();
   };
 
   useEffect(() => {
-    if (tenantId === tenantIdState) return;
-    setTenantId(tenantIdState);
-  }, [tenantIdState]);
-
-  useEffect(() => {
-    const regexp = /(\/select\/)(\d+|\d.+)(\/)(.+)/;
-    const id = (serverUrl.match(regexp) || [])[2];
+    const id = getTenantIdFromUrl(serverUrl);
 
     if (tenantIdState && tenantIdState !== id) {
-      handleApply(tenantIdState);
+      createHandlerChange(tenantIdState)();
     } else {
-      handleApply(id);
+      createHandlerChange(id)();
     }
   }, [serverUrl]);
 
+  if (!showTenantSelector) return null;
+
   return (
-    <div
-      className="vm-tenant-input"
-      ref={autocompleteAnchorEl}
-    >
-      <TextField
-        label="Tenant ID"
-        value={tenantId}
-        onChange={handleChange}
-        onEnter={handleApply}
-        onFocus={handleFocus}
-        onBlur={handleApply}
-        endIcon={(
-          <Tooltip title="Define tenant id if you need request to another storage">
-            <Button
-              variant={"text"}
-              size={"small"}
-              startIcon={<InfoIcon/>}
-            />
-          </Tooltip>
-        )}
-      />
-      {!!accountIds.length && (
-        <Autocomplete
-          value={autocompleteValue}
-          options={accountIds}
-          anchor={autocompleteAnchorEl}
-          maxWords={10}
-          minLength={0}
-          fullWidth
-          onSelect={handleApply}
-          onOpenAutocomplete={setOpenAutocomplete}
-        />
-      )}
+    <div className="vm-tenant-input">
+      <Tooltip title="Define Tenant ID if you need request to another storage">
+        <div ref={optionsButtonRef}>
+          <Button
+            className={appModeEnable ? "" : "vm-header-button"}
+            variant="contained"
+            color="primary"
+            fullWidth
+            startIcon={<StorageIcons/>}
+            endIcon={(
+              <div
+                className={classNames({
+                  "vm-execution-controls-buttons__arrow": true,
+                  "vm-execution-controls-buttons__arrow_open": openOptions,
+                })}
+              >
+                <ArrowDownIcon/>
+              </div>
+            )}
+            onClick={toggleOpenOptions}
+          >
+            {tenantIdState}
+          </Button>
+        </div>
+      </Tooltip>
+      <Popper
+        open={openOptions}
+        placement="bottom-left"
+        onClose={handleCloseOptions}
+        buttonRef={optionsButtonRef}
+        fullWidth
+      >
+        <div className="vm-list">
+          {accountIds.map(id => (
+            <div
+              className={classNames({
+                "vm-list-item": true,
+                "vm-list-item_active": id === tenantIdState
+              })}
+              key={id}
+              onClick={createHandlerChange(id)}
+            >
+              {id}
+            </div>
+          ))}
+        </div>
+      </Popper>
     </div>
   );
 };
