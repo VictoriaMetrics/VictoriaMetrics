0c8fac12468b664db55ca1b9950ab021d6d42251
Yury Moladau <yurymolodov@gmail.com>
2023-01-24 13:19:21 +0100

    feat: add multilevel menu (#3678)

--- app/vmui/packages/vmui/src/components/Layout/Header/Header.tsx
+++ app/vmui/packages/vmui/src/components/Layout/Header/Header.tsx
@@ -1,27 +1,24 @@
-import React, { FC, useMemo, useState } from "preact/compat";
+import React, { FC, useMemo } from "preact/compat";
 import { ExecutionControls } from "../../Configurators/TimeRangeSettings/ExecutionControls/ExecutionControls";
 import { setQueryStringWithoutPageReload } from "../../../utils/query-string";
 import { TimeSelector } from "../../Configurators/TimeRangeSettings/TimeSelector/TimeSelector";
 import GlobalSettings from "../../Configurators/GlobalSettings/GlobalSettings";
 import { useLocation, useNavigate } from "react-router-dom";
 import router, { RouterOptions, routerOptions } from "../../../router";
-import { useEffect } from "react";
 import ShortcutKeys from "../../Main/ShortcutKeys/ShortcutKeys";
 import { getAppModeEnable, getAppModeParams } from "../../../utils/app-mode";
 import CardinalityDatePicker from "../../Configurators/CardinalityDatePicker/CardinalityDatePicker";
 import { LogoFullIcon } from "../../Main/Icons";
 import { getCssVariable } from "../../../utils/theme";
-import Tabs from "../../Main/Tabs/Tabs";
 import "./style.scss";
 import classNames from "classnames";
-import { useDashboardsState } from "../../../state/dashboards/DashboardsStateContext";
 import StepConfigurator from "../../Configurators/StepConfigurator/StepConfigurator";
 import { useAppState } from "../../../state/common/StateContext";
+import HeaderNav from "./HeaderNav/HeaderNav";
 
 const Header: FC = () => {
   const { darkTheme } = useAppState();
   const appModeEnable = getAppModeEnable();
-  const { dashboardsSettings } = useDashboardsState();
 
   const primaryColor = useMemo(() => {
     const variable = darkTheme ? "color-background-block" : "color-primary";
@@ -39,53 +36,17 @@ const Header: FC = () => {
 
   const navigate = useNavigate();
   const { search, pathname } = useLocation();
-  const routes = useMemo(() => ([
-    {
-      label: routerOptions[router.home].title,
-      value: router.home,
-    },
-    {
-      label: routerOptions[router.metrics].title,
-      value: router.metrics,
-    },
-    {
-      label: routerOptions[router.cardinality].title,
-      value: router.cardinality,
-    },
-    {
-      label: routerOptions[router.topQueries].title,
-      value: router.topQueries,
-    },
-    {
-      label: routerOptions[router.trace].title,
-      value: router.trace,
-    },
-    {
-      label: routerOptions[router.dashboards].title,
-      value: router.dashboards,
-      hide: appModeEnable || !dashboardsSettings.length
-    }
-  ]), [appModeEnable, dashboardsSettings]);
 
-  const [activeMenu, setActiveMenu] = useState(pathname);
 
   const headerSetup = useMemo(() => {
     return ((routerOptions[pathname] || {}) as RouterOptions).header || {};
   }, [pathname]);
 
   const onClickLogo = () => {
-    navigateHandler(router.home);
+    navigate({ pathname: router.home, search: search });
     setQueryStringWithoutPageReload({});
     window.location.reload();
   };
-
-  const navigateHandler = (pathname: string) => {
-    navigate({ pathname, search: search });
-  };
-
-  useEffect(() => {
-    setActiveMenu(pathname);
-  }, [pathname]);
 
   return <header
     className={classNames({
@@ -104,14 +65,10 @@ const Header: FC = () => {
         <LogoFullIcon/>
       </div>
     )}
-    <div className="vm-header-nav">
-      <Tabs
-        isNavLink
-        activeItem={activeMenu}
-        items={routes.filter(r => !r.hide)}
-        color={color}
-      />
-    </div>
+    <HeaderNav
+      color={color}
+      background={background}
+    />
     <div className="vm-header__settings">
       {headerSetup?.stepControl && <StepConfigurator/>}
       {headerSetup?.timeSelector && <TimeSelector/>}
