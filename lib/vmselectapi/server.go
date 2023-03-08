d9d6b9fd580fe4d97310030c7542519b8e32c068
f41gh7 <nik@victoriametrics.com>
2023-03-08 18:23:49 +0100

    lib/vmselectapi: fixes regression for disable compression setting after vmselect api refactoring it wasn't possible to disable response cache. This patch restores correct behavior for rpc.disableCompression flag

--- lib/vmselectapi/server.go
+++ lib/vmselectapi/server.go
@@ -114,9 +114,10 @@ func NewServer(addr string, api API, limits Limits, disableResponseCompression b
 		return float64(len(concurrencyLimitCh))
 	})
 	s := &Server{
-		api:    api,
-		limits: limits,
-		ln:     ln,
+		api:                        api,
+		limits:                     limits,
+		disableResponseCompression: disableResponseCompression,
+		ln:                         ln,
 
 		concurrencyLimitCh: concurrencyLimitCh,
 
