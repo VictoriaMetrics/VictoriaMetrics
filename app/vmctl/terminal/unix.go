ddf36e43ad695768c84f3a348042e5c5e3c39152
dmitryk-dk <kozlovdmitriyy@gmail.com>
2023-03-21 00:08:08 +0200

    app/vmctl: follow up after aed59b90298a6b5e44ffea4543a6aa3b9e402e65

--- app/vmctl/terminal/unix.go
+++ app/vmctl/terminal/unix.go
@@ -6,3 +6,8 @@ package terminal
 import "golang.org/x/sys/unix"
 
 const ioctlReadTermios = unix.TCGETS
+
+func isTerminal(fd int) bool {
+	_, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
+	return err == nil
+}
