4ba237ec14967b6f809b9d4b9fdf4f0b7236b971
Dmytro Kozlov <kozlovdmitriyy@gmail.com>
2023-03-21 16:53:53 +0200

    app/vmctl: follow up after aed59b90298a6b5e44ffea4543a6aa3b9e402e65 (#3983)

--- app/vmctl/terminal/terminal.go
+++ app/vmctl/terminal/terminal.go
@@ -1,14 +1,6 @@
-//go:build darwin || linux || solaris
-// +build darwin linux solaris
-
 package terminal
-
-import (
-	"golang.org/x/sys/unix"
-)
 
 // IsTerminal returns true if the file descriptor is terminal
 func IsTerminal(fd int) bool {
-	_, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
-	return err == nil
+	return isTerminal(fd)
 }
