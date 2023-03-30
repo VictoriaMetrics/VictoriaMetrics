4ab4694cc2c5278cbcee078edc86272732a4667e
Yury Moladau <yurymolodov@gmail.com>
2023-03-30 12:38:28 +0200

    fix: update link to instant query

--- app/vmui/packages/vmui/src/pages/CustomPanel/InstantQueryTip/InstantQueryTip.tsx
+++ app/vmui/packages/vmui/src/pages/CustomPanel/InstantQueryTip/InstantQueryTip.tsx
@@ -9,18 +9,17 @@ const last_over_time = <Hyperlink
 
 const instant_query = <Hyperlink
   text="instant query"
-  href="https://docs.victoriametrics.com/MetricsQL.html#last_over_time"
+  href="https://docs.victoriametrics.com/keyConcepts.html#instant-query"
   underlined
 />;
 
 const InstantQueryTip: FC = () => (
   <div>
     <p>
-      This tab use {instant_query} that tries to locate a data sample is equal to <b>5m</b>.
-      We have intentionally done this to ensure compatibility with Prometheus.
+      This tab shows {instant_query} results for the last 5 minutes ending at the selected time range.
     </p>
     <p>
-      You can change the <b>time range</b> or use {last_over_time} .
+      Please wrap the query into {last_over_time} if you need results over arbitrary lookbehind interval.
     </p>
   </div>
 );
