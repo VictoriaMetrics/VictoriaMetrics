74eea53dee348f0fc434832e16e76f5b45f208d8
Yury Molodov <yurymolodov@gmail.com>
2023-04-06 07:13:57 +0200

    vmui: implement heatmap improvements (#4078)
    * fix: disabled limits for histogram
    
    * fix: add sorted buckets by upper bound
    
    * refactor: move line chart components to folder
    
    * feat: implement heatmap improvements (https://github.com/VictoriaMetrics/VictoriaMetrics/issues/3384#issuecomment-1484023162)
    
    * app/vmselect/vmui: `make vmui-update`
    
    ---------
    
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- app/vmui/packages/vmui/src/utils/metric.ts
+++ app/vmui/packages/vmui/src/utils/metric.ts
@@ -31,14 +31,13 @@ export const promValueToNumber = (s: string): number => {
 
 export const isHistogramData = (result: MetricBase[]) => {
   if (result.length < 2) return false;
-  const histogramNames = ["le", "vmrange"];
+  const histogramLabels = ["le", "vmrange"];
 
-  return result.every(r => {
-    const keys = Object.keys(r.metric);
-    const labels = Object.keys(r.metric).filter(n => !histogramNames.includes(n));
-    const byName = keys.length > labels.length;
-    const byLabels = labels.every(l => r.metric[l] === result[0].metric[l]);
-
-    return byName && byLabels;
+  const firstLabels = Object.keys(result[0].metric).filter(n => !histogramLabels.includes(n));
+  const isHistogram = result.every(r => {
+    const labels = Object.keys(r.metric).filter(n => !histogramLabels.includes(n));
+    return firstLabels.length === labels.length && labels.every(l => r.metric[l] === result[0].metric[l]);
   });
+
+  return isHistogram && result.every(r => histogramLabels.some(l => l in r.metric));
 };
