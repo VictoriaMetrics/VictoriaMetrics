ceed8b7c754d016497b9fdd98b88b5a3189e0a45
Yury Moladau <yurymolodov@gmail.com>
2023-07-30 16:55:24 +0200

    feat: display partial response warning (#4721)

---
+++ app/vmui/packages/vmui/src/components/Configurators/QueryEditor/warningText.ts
@@ -0,0 +1,7 @@
+export const seriesFetchedWarning = `No match! 
+This query hasn't selected any time series from database.
+Either the requested metrics are missing in the database,
+or there is a typo in series selector.`;
+
+export const partialWarning = `The results you are viewing are marked as PARTIAL.
+This is due to one or more vmstorage nodes currently being unavailable.`;
