191170a6ba783b684815d2fe634895625f66ebc5
Tamara <toma.vashchuk@gmail.com>
2023-07-31 18:11:30 -0700

    Add sync/await to prettifyQuery function

--- app/vmui/packages/vmui/src/pages/CustomPanel/QueryConfigurator/QueryConfigurator.tsx
+++ app/vmui/packages/vmui/src/pages/CustomPanel/QueryConfigurator/QueryConfigurator.tsx
@@ -123,27 +123,43 @@ const QueryConfigurator: FC<QueryConfiguratorProps> = ({
     handleToggleHideQuery(e, i);
   };
 
-  const handlePrettifyQuery = (el: HTMLButtonElement, i: number) => {
-    const { serverUrl } = useAppState();
+  const { serverUrl } = useAppState();
+
+  const handlePrettifyQuery = async (el: HTMLButtonElement, i: number) => {
 
     const oldQuery = encodeURIComponent(stateQuery[i]);
-    fetch(`${serverUrl}/prettify-query?query=${oldQuery}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data["status"] == "success") {
-          const newQueryErrors = [...queryErrors];
-          newQueryErrors[i] = "";
-          setQueryErrors(newQueryErrors);
+    let response: Response;
+    try {
+      response = await fetch(`${serverUrl}/prettify-query?query=${oldQuery}`);
+    } catch (e) {
+      const newQueryErrors = [...queryErrors];
+      newQueryErrors[i] = `${e}`;
+      setQueryErrors(newQueryErrors);
+      return;
+    }
 
-          const newStateQuery = [...stateQuery];
-          newStateQuery[i] = data["query"];
-          setStateQuery(newStateQuery);
-        } else {
-          const newQueryErrors = [...queryErrors];
-          newQueryErrors[i] = data["msg"];
-          setQueryErrors(newQueryErrors);
-        }
-      });
+    if (response.status != 200) {
+      const newQueryErrors = [...queryErrors];
+      newQueryErrors[i] = "Error requesting /prettify-query, status: " + response.status;
+      setQueryErrors(newQueryErrors);
+    }
+
+    const data = await response.json();
+
+    if (data["status"] == "success") {
+      const newQueryErrors = [...queryErrors];
+      newQueryErrors[i] = "";
+      setQueryErrors(newQueryErrors);
+
+      const newStateQuery = [...stateQuery];
+      newStateQuery[i] = data["query"];
+      setStateQuery(newStateQuery);
+    } else {
+      const newQueryErrors = [...queryErrors];
+      newQueryErrors[i] = data["msg"];
+      setQueryErrors(newQueryErrors);
+    }
+
   };
 
   const createHandlerPrettifyQuery = (i: number) => {
@@ -210,6 +226,7 @@ const QueryConfigurator: FC<QueryConfiguratorProps> = ({
                 color={"gray"}
                 startIcon={<Prettify/>}
                 onClick={createHandlerPrettifyQuery(i)}
+                className="prettify"
               />
             </div>
           </Tooltip>
