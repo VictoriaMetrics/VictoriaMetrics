f20bb496daf7fd300429191644a00f2108e49eea
Yury Molodov <yurymolodov@gmail.com>
2023-08-01 16:15:44 +0200

    Update app/vmui/packages/vmui/src/components/Configurators/QueryEditor/warningText.ts
    Co-authored-by: Roman Khavronenko <roman@victoriametrics.com>

--- app/vmui/packages/vmui/src/components/Configurators/QueryEditor/warningText.ts
+++ app/vmui/packages/vmui/src/components/Configurators/QueryEditor/warningText.ts
@@ -3,5 +3,5 @@ This query hasn't selected any time series from database.
 Either the requested metrics are missing in the database,
 or there is a typo in series selector.`;
 
-export const partialWarning = `The results you are viewing are marked as PARTIAL.
+export const partialWarning = `The shown results are marked as PARTIAL.
 This is due to one or more vmstorage nodes currently being unavailable.`;
