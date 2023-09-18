9e50ea6cced8104ecea61220549dbeff80d9612a
Aliaksandr Valialkin <valyala@victoriametrics.com>
2023-09-18 11:59:46 +0200

    app/vlinsert/insertutils: cosmetic changes after 8d3e574c31568553f03bbed06224601c47467eb4

--- app/vlinsert/insertutils/flags.go
+++ app/vlinsert/insertutils/flags.go
@@ -11,5 +11,5 @@ var (
 	MaxLineSizeBytes = flagutil.NewBytes("insert.maxLineSizeBytes", 256*1024, "The maximum size of a single line, which can be read by /insert/* handlers")
 
 	// MaxFieldsPerLine is the maximum number of fields per line for /insert/* handlers
-	MaxFieldsPerLine = flag.Int("insert.maxFieldsPerLine", 1000, "The maximum number of fields per line, which can be read by /insert/* handlers")
+	MaxFieldsPerLine = flag.Int("insert.maxFieldsPerLine", 1000, "The maximum number of log fields per line, which can be read by /insert/* handlers")
 )
