70a638accc9cb4c5bde2459c9624ac65f86c9ede
Ilya Trefilov <ilya.trefilov@outlook.com>
2023-10-13 15:41:51 +0300

    lib/storage: do not create tsid if metric contains stale marker(#5069)

--- lib/storage/storage.go
+++ lib/storage/storage.go
@@ -1716,6 +1716,7 @@ func (s *Storage) add(rows []rawRow, dstMrs []*MetricRow, mrs []MetricRow, preci
 
 	// Return only the first error, since it has no sense in returning all errors.
 	var firstWarn error
+	var isStaleNan bool
 	j := 0
 	for i := range mrs {
 		mr := &mrs[i]
@@ -1725,6 +1726,7 @@ func (s *Storage) add(rows []rawRow, dstMrs []*MetricRow, mrs []MetricRow, preci
 				// doesn't know how to work with them.
 				continue
 			}
+			isStaleNan = true
 		}
 		if mr.Timestamp < minTimestamp {
 			// Skip rows with too small timestamps outside the retention.
@@ -1835,6 +1837,13 @@ func (s *Storage) add(rows []rawRow, dstMrs []*MetricRow, mrs []MetricRow, preci
 			r.TSID = genTSID.TSID
 			prevTSID = genTSID.TSID
 			prevMetricNameRaw = mr.MetricNameRaw
+			continue
+		}
+
+		// If TSID was not found in cache and in indexdb, it's deleted. If metric contains stale
+		// marker, do not create a new TSID.
+		if isStaleNan {
+			j--
 			continue
 		}
 
