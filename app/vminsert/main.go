2c334ed95374762292c8dc2508bce8db56505021
Aliaksandr Valialkin <valyala@victoriametrics.com>
2023-10-16 00:25:23 +0200

    app/{vmagent,vminsert}: follow-up for NewRelic data ingestion protocol support
    This is a follow-up for f60c08a7bdcc87570d1076a3f4520637d3a6ea0f
    
    Changes:
    
    - Make sure all the urls related to NewRelic protocol start from /newrelic . Previously some urls were started from /api/v1/newrelic
    
    - Remove /api/v1 part from NewRelic urls, since it has no sense
    
    - Remove automatic transformation from CamelCase to snake_case for NewRelic labels and metric names,
      since it may complicate the transition from NewRelic to VictoriaMetrics. Preserve all the metric names and label names,
      so users could query metrics and labels by the same names which are used in NewRelic.
      The automatic transformation from CamelCase to snake_case can be added later as a special action for relabeling rules if needed.
    
    - Properly update per-tenant data ingestion stats at app/vmagent/newrelic/request_handler.go . Previously it was always zero.
    
    - Fix NewRelic urls in vmagent when multitenant data ingestion is enabled. Previously they were mistakenly started from `/`.
    
    - Document NewRelic data ingestion url at docs/Cluster-VictoriaMetrics.md
    
    - Remove superflouos memory allocations at lib/protoparser/newrelic
    
    - Improve tests at lib/protoparser/newrelic/*
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/3520
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/4712

--- app/vminsert/main.go
+++ app/vminsert/main.go
@@ -222,19 +222,19 @@ func RequestHandler(w http.ResponseWriter, r *http.Request) bool {
 		}
 		w.WriteHeader(http.StatusOK)
 		return true
-	case "/newrelic/api/v1":
+	case "/newrelic":
 		newrelicCheckRequest.Inc()
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(202)
 		fmt.Fprintf(w, `{"status":"ok"}`)
 		return true
-	case "/newrelic/api/v1/inventory/deltas":
+	case "/newrelic/inventory/deltas":
 		newrelicInventoryRequests.Inc()
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(202)
 		fmt.Fprintf(w, `{"payload":{"version": 1, "state": {}, "reset": "false"}}`)
 		return true
-	case "/newrelic/api/v1/infra/v2/metrics/events/bulk":
+	case "/newrelic/infra/v2/metrics/events/bulk":
 		newrelicWriteRequests.Inc()
 		if err := newrelic.InsertHandlerForHTTP(r); err != nil {
 			newrelicWriteErrors.Inc()
@@ -382,11 +382,11 @@ var (
 	opentelemetryPushRequests = metrics.NewCounter(`vm_http_requests_total{path="/opentelemetry/api/v1/push", protocol="opentelemetry"}`)
 	opentelemetryPushErrors   = metrics.NewCounter(`vm_http_request_errors_total{path="/opentelemetry/api/v1/push", protocol="opentelemetry"}`)
 
-	newrelicWriteRequests = metrics.NewCounter(`vm_http_requests_total{path="/newrelic/api/v1/infra/v2/metrics/events/bulk", protocol="newrelic"}`)
-	newrelicWriteErrors   = metrics.NewCounter(`vm_http_request_errors_total{path="/newrelic/api/v1/infra/v2/metrics/events/bulk", protocol="newrelic"}`)
+	newrelicWriteRequests = metrics.NewCounter(`vm_http_requests_total{path="/newrelic/infra/v2/metrics/events/bulk", protocol="newrelic"}`)
+	newrelicWriteErrors   = metrics.NewCounter(`vm_http_request_errors_total{path="/newrelic/infra/v2/metrics/events/bulk", protocol="newrelic"}`)
 
-	newrelicInventoryRequests = metrics.NewCounter(`vm_http_requests_total{path="/newrelic/api/v1/inventory/deltas", protocol="newrelic"}`)
-	newrelicCheckRequest      = metrics.NewCounter(`vm_http_requests_total{path="/newrelic/api/v1", protocol="newrelic"}`)
+	newrelicInventoryRequests = metrics.NewCounter(`vm_http_requests_total{path="/newrelic/inventory/deltas", protocol="newrelic"}`)
+	newrelicCheckRequest      = metrics.NewCounter(`vm_http_requests_total{path="/newrelic", protocol="newrelic"}`)
 
 	promscrapeTargetsRequests          = metrics.NewCounter(`vm_http_requests_total{path="/targets"}`)
 	promscrapeServiceDiscoveryRequests = metrics.NewCounter(`vm_http_requests_total{path="/service-discovery"}`)
