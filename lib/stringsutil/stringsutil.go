d9ecc3f6d779b6efb5ddb688130dc047c37b6bba
Aliaksandr Valialkin <valyala@victoriametrics.com>
2023-11-11 12:30:08 +0100

    lib/logger: add `-loggerMaxArgLen` command-line flag for fine-tuning the maximum length of logged args

---
+++ lib/stringsutil/stringsutil.go
@@ -0,0 +1,16 @@
+package stringsutil
+
+// LimitStringLen limits the length of s with maxLen.
+//
+// If len(s) > maxLen, then s is replaced with "s_prefix..s_suffix",
+// so the total length of the returned string doesn't exceed maxLen.
+func LimitStringLen(s string, maxLen int) string {
+	if len(s) <= maxLen || maxLen <= 4 {
+		return s
+	}
+	n := (maxLen / 2) - 1
+	if n < 0 {
+		n = 0
+	}
+	return s[:n] + ".." + s[len(s)-n:]
+}
