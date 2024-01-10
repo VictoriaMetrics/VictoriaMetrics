efebfa7ba94ce6cee8213724189fc9cb948d6a8a
dmitryk-dk <kozlovdmitriyy@gmail.com>
2024-01-10 16:16:27 +0100

    app/vmctl: fix tests

--- app/vmctl/native/client.go
+++ app/vmctl/native/client.go
@@ -42,7 +42,7 @@ type Response struct {
 // Explore finds metric names by provided filter from api/v1/label/__name__/values
 func (c *Client) Explore(ctx context.Context, f Filter, tenantID string) ([]string, error) {
 	exploreChunk := f.Chunk
-	if f.Chunk == stepper.StepHour || f.Chunk == stepper.StepMinute || f.Chunk == stepper.StepDay {
+	if f.Chunk == stepper.StepHour || f.Chunk == stepper.StepMinute || f.Chunk == stepper.StepDay || f.Chunk == "" {
 		// the minimal step wil be used for metrics explore process
 		exploreChunk = stepper.StepWeek
 	}
