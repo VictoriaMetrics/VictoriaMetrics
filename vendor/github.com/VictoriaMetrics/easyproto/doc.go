0597718435155499db2f74ae0d2001b11a2c5de5
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-01-14 21:06:01 +0200

    lib/protoparser/datadogv2: add support for reading protobuf-encoded requests at /api/v2/series endpoint
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/4451
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/5094

---
+++ vendor/github.com/VictoriaMetrics/easyproto/doc.go
@@ -0,0 +1,3 @@
+// Package easyproto provides building blocks for marshaling and unmarshaling protobuf v3 messages
+// according to https://protobuf.dev/programming-guides/encoding/ .
+package easyproto
