41bdf087a6aa2e94bde6b5a6dae3149adbc47260
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-01-15 13:37:02 +0200

    lib/pushmetrics: wait until the background goroutines, which push metrics, are stopped at pushmetrics.Stop()
    Previously the was a race condition when the background goroutine still could try collecting metrics
    from already stopped resources after returning from pushmetrics.Stop().
    Now the pushmetrics.Stop() waits until the background goroutine is stopped before returning.
    
    This is a follow-up for https://github.com/VictoriaMetrics/VictoriaMetrics/pull/5549
    and the commit fe2d9f6646a49c347b1ee03a0285971eaf681ed6 .
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/5548

--- app/victoria-metrics/main.go
+++ app/victoria-metrics/main.go
@@ -45,7 +45,6 @@ func main() {
 	envflag.Parse()
 	buildinfo.Init()
 	logger.Init()
-	pushmetrics.Init()
 
 	if promscrape.IsDryRun() {
 		*dryRun = true
@@ -65,13 +64,16 @@ func main() {
 	vmstorage.Init(promql.ResetRollupResultCacheIfNeeded)
 	vmselect.Init()
 	vminsert.Init()
+
 	startSelfScraper()
 
 	go httpserver.Serve(*httpListenAddr, *useProxyProtocol, requestHandler)
 	logger.Infof("started VictoriaMetrics in %.3f seconds", time.Since(startTime).Seconds())
 
+	pushmetrics.Init()
 	sig := procutil.WaitForSigterm()
 	logger.Infof("received signal %s", sig)
+	pushmetrics.Stop()
 
 	stopSelfScraper()
 
@@ -80,9 +82,8 @@ func main() {
 	if err := httpserver.Stop(*httpListenAddr); err != nil {
 		logger.Fatalf("cannot stop the webservice: %s", err)
 	}
-	pushmetrics.Stop()
-	vminsert.Stop()
 	logger.Infof("successfully shut down the webservice in %.3f seconds", time.Since(startTime).Seconds())
+	vminsert.Stop()
 
 	vmstorage.Stop()
 	vmselect.Stop()
