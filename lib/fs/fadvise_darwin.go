91b6b32c3ad1a84a56166968238cb9fa278eb946
Zongyang <xiaozongyang@kanyun.com>
2024-01-16 08:19:36 +0800

    FIX bottomk doesn't return any data when there are no time range overlap between timeseries (#5509)
    * FIX sort order in bottomk
    
    * Add lessWithNaNsReversed for bottomk
    
    * Add ut for TopK
    
    * Move lt from loop
    
    * FIX lint
    
    * FIX lint
    
    * FIX lint
    
    * Mod log format
    
    ---------
    
    Co-authored-by: xiaozongyang <xiaozngyang@kanyun.com>
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- lib/fs/fadvise_darwin.go
+++ lib/fs/fadvise_darwin.go
@@ -4,7 +4,7 @@ import (
 	"os"
 )
 
-func fadviseSequentialRead(f *os.File, prefetch bool) error {
+func fadviseSequentialRead(f *os.File, prefetch bool) error { // nolint
 	// TODO: implement this properly
 	return nil
 }
