b3b29099257cdf962c4f70ac1af74fc32afdaf6e
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-01-16 02:55:06 +0200

    app/vmselect/promql: follow-up for ce4f26db02d487cfc8d1605b472f8968e41bc8b3
    - Document the bugfix at docs/CHANGELOG.md
    - Filter out NaN values before sorting as suggested at https://github.com/VictoriaMetrics/VictoriaMetrics/pull/5509#discussion_r1447369218
    - Revert unrelated changes in lib/filestream and lib/fs
    - Use simpler test at app/vmselect/promql/exec_test.go
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/5509
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/5506

--- lib/fs/fadvise_darwin.go
+++ lib/fs/fadvise_darwin.go
@@ -4,7 +4,7 @@ import (
 	"os"
 )
 
-func fadviseSequentialRead(f *os.File, prefetch bool) error { // nolint
+func fadviseSequentialRead(f *os.File, prefetch bool) error {
 	// TODO: implement this properly
 	return nil
 }
