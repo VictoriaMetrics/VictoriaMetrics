12f2c5679b1d66e5ac8bdf059bde603573fa9a7a
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-01-21 05:11:05 +0200

    all: consistently clear prompbmarshal.Label by assigning an empty struct instead of zeroing Name and Value individually

--- lib/promrelabel/relabel.go
+++ lib/promrelabel/relabel.go
@@ -592,9 +592,7 @@ func GetLabelByName(labels []prompbmarshal.Label, name string) *prompbmarshal.La
 // This should help GC cleaning up label.Name and label.Value strings.
 func CleanLabels(labels []prompbmarshal.Label) {
 	for i := range labels {
-		label := &labels[i]
-		label.Name = ""
-		label.Value = ""
+		labels[i] = prompbmarshal.Label{}
 	}
 }
 
