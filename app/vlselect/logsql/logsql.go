99ac8c1e2b84cd4b9885b70b33fbebaf6acc8a94
dmitryk-dk <kozlovdmitriyy@gmail.com>
2024-02-07 14:16:22 +0100

    app/vlselect: add limit for logs query

--- app/vlselect/logsql/logsql.go
+++ app/vlselect/logsql/logsql.go
@@ -2,11 +2,13 @@ package logsql
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/app/vlstorage"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/flagutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/httpserver"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/httputils"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logstorage"
 )
 
@@ -15,6 +17,8 @@ var (
 		"if their summary size doesn't exceed this value; otherwise, query results are streamed in the response without sorting; "+
 		"too big value for this flag may result in high memory usage since the sorting is performed in memory")
 )
+
+const defaultRowsCount = 1000
 
 // ProcessQueryRequest handles /select/logsql/query request
 func ProcessQueryRequest(w http.ResponseWriter, r *http.Request, stopCh <-chan struct{}) {
@@ -31,17 +35,31 @@ func ProcessQueryRequest(w http.ResponseWriter, r *http.Request, stopCh <-chan s
 		httpserver.Errorf(w, r, "cannot parse query [%s]: %s", qStr, err)
 		return
 	}
+	limit, err := httputils.GetInt(r, "limit")
+	if err != nil {
+		httpserver.Errorf(w, r, "cannot parse limit from the request: %s", err)
+		return
+	}
+	if limit <= 0 || limit > defaultRowsCount {
+		limit = defaultRowsCount
+	}
 	w.Header().Set("Content-Type", "application/stream+json; charset=utf-8")
-
 	sw := getSortWriter()
 	sw.Init(w, maxSortBufferSize.IntN())
 	tenantIDs := []logstorage.TenantID{tenantID}
+
+	var mx sync.Mutex
 	vlstorage.RunQuery(tenantIDs, q, stopCh, func(columns []logstorage.BlockColumn) {
 		if len(columns) == 0 {
 			return
 		}
 		rowsCount := len(columns[0].Values)
-
+		mx.Lock()
+		if rowsCount > limit {
+			rowsCount = limit
+		}
+		limit = limit - rowsCount
+		mx.Unlock()
 		bb := blockResultPool.Get()
 		for rowIdx := 0; rowIdx < rowsCount; rowIdx++ {
 			WriteJSONRow(bb, columns, rowIdx)
