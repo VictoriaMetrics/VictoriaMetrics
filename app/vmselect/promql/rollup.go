726751a3112d6f1fe0366c1641d5d025b435ce92
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-02-08 02:30:55 +0200

    app/vmselect/promql: properly handle precision errors in rollup functions
    changes(), increases_over_time() and resets() shouldn't take into account
    value changes, which may occur because of precision errors.
    
    The maximum guaranteed precision for raw samples stored in VictoriaMetrics is 12 decimal digits.
    So do not count relative changes for values if they are smaller than 1e-12 comparing to the value.
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/767

--- app/vmselect/promql/rollup.go
+++ app/vmselect/promql/rollup.go
@@ -1893,6 +1893,10 @@ func rollupChangesPrometheus(rfa *rollupFuncArg) float64 {
 	n := 0
 	for _, v := range values[1:] {
 		if v != prevValue {
+			if math.Abs(v-prevValue) < 1e-12*math.Abs(v) {
+				// This may be precision error. See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/767#issuecomment-1650932203
+				continue
+			}
 			n++
 			prevValue = v
 		}
@@ -1916,6 +1920,10 @@ func rollupChanges(rfa *rollupFuncArg) float64 {
 	}
 	for _, v := range values {
 		if v != prevValue {
+			if math.Abs(v-prevValue) < 1e-12*math.Abs(v) {
+				// This may be precision error. See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/767#issuecomment-1650932203
+				continue
+			}
 			n++
 			prevValue = v
 		}
@@ -1944,6 +1952,10 @@ func rollupIncreases(rfa *rollupFuncArg) float64 {
 	n := 0
 	for _, v := range values {
 		if v > prevValue {
+			if math.Abs(v-prevValue) < 1e-12*math.Abs(v) {
+				// This may be precision error. See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/767#issuecomment-1650932203
+				continue
+			}
 			n++
 		}
 		prevValue = v
@@ -1975,6 +1987,10 @@ func rollupResets(rfa *rollupFuncArg) float64 {
 	n := 0
 	for _, v := range values {
 		if v < prevValue {
+			if math.Abs(v-prevValue) < 1e-12*math.Abs(v) {
+				// This may be precision error. See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/767#issuecomment-1650932203
+				continue
+			}
 			n++
 		}
 		prevValue = v
