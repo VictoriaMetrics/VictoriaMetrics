4dc196eb6861ee5d63d280d70ac75b31987217d9
hagen1778 <roman@victoriametrics.com>
2024-02-09 10:30:17 +0100

    app/vmalert: support filtering for /api/v1/rule like Prometheus does
    Follow-up after https://github.com/VictoriaMetrics/VictoriaMetrics/commit/62e5e2a4c8b6362ad61c2d7e20d0d6ba2aec58f5
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>

--- app/vmalert/web.go
+++ app/vmalert/web.go
@@ -14,6 +14,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/app/vmalert/tpl"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/flagutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/httpserver"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/httputils"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/procutil"
 )
@@ -88,8 +89,8 @@ func (rh *requestHandler) handler(w http.ResponseWriter, r *http.Request) bool {
 		return true
 	case "/vmalert/groups":
 		var data []apiGroup
-		ruleType := r.URL.Query().Get("type")
-		data = rh.groups(ruleType)
+		rf := extractRulesFilter(r)
+		data = rh.groups(rf)
 		WriteListGroups(w, r, data)
 		return true
 	case "/vmalert/notifiers":
@@ -102,8 +103,8 @@ func (rh *requestHandler) handler(w http.ResponseWriter, r *http.Request) bool {
 		// Grafana makes an extra request to `/rules`
 		// handler in addition to `/api/v1/rules` calls in alerts UI,
 		var data []apiGroup
-		ruleType := r.URL.Query().Get("type")
-		data = rh.groups(ruleType)
+		rf := extractRulesFilter(r)
+		data = rh.groups(rf)
 		WriteListGroups(w, r, data)
 		return true
 
@@ -112,8 +113,8 @@ func (rh *requestHandler) handler(w http.ResponseWriter, r *http.Request) bool {
 		var data []byte
 		var err error
 
-		ruleType := r.URL.Query().Get("type")
-		data, err = rh.listGroups(ruleType)
+		rf := extractRulesFilter(r)
+		data, err = rh.listGroups(rf)
 
 		if err != nil {
 			httpserver.Errorf(w, r, "%s", err)
@@ -219,42 +220,97 @@ type listGroupsResponse struct {
 	} `json:"data"`
 }
 
-func (rh *requestHandler) groups(ruleType string) []apiGroup {
+// see https://prometheus.io/docs/prometheus/latest/querying/api/#rules
+type rulesFilter struct {
+	files         []string
+	groupNames    []string
+	ruleNames     []string
+	ruleType      string
+	excludeAlerts bool
+}
+
+func extractRulesFilter(r *http.Request) rulesFilter {
+	rf := rulesFilter{}
+
+	var ruleType string
+	ruleTypeParam := r.URL.Query().Get("type")
+	// for some reason, `type` in filter doesn't match `type` in response,
+	// so we use this matching here
+	if ruleTypeParam == "alert" {
+		ruleType = ruleTypeAlerting
+	} else if ruleTypeParam == "record" {
+		ruleType = ruleTypeRecording
+	}
+	rf.ruleType = ruleType
+
+	rf.excludeAlerts = httputils.GetBool(r, "exclude_alerts")
+	for _, names := range r.Form["rule_name[]"] {
+		rf.ruleNames = append(rf.ruleNames, names)
+	}
+	for _, groups := range r.Form["rule_group[]"] {
+		rf.groupNames = append(rf.groupNames, groups)
+	}
+	for _, files := range r.Form["file[]"] {
+		rf.files = append(rf.files, files)
+	}
+
+	return rf
+}
+
+func (rh *requestHandler) groups(rf rulesFilter) []apiGroup {
 	rh.m.groupsMu.RLock()
 	defer rh.m.groupsMu.RUnlock()
 
-	groups := make([]apiGroup, 0)
-	for _, g := range rh.m.groups {
-		g = g.DeepCopy()
-		var matchedRules []rule.Rule
-		if ruleType == "alert" || ruleType == "record" {
-			for _, r := range g.Rules {
-				if _, ok := r.(*rule.AlertingRule); ok && ruleType == "alert" {
-					matchedRules = append(matchedRules, r)
-				}
-				if _, ok := r.(*rule.RecordingRule); ok && ruleType == "record" {
-					matchedRules = append(matchedRules, r)
-				}
-			}
-			if len(matchedRules) == 0 {
-				continue
-			}
-			g.Rules = matchedRules
+	isInList := func(list []string, needle string) bool {
+		if len(list) < 1 {
+			return true
 		}
-		groups = append(groups, groupToAPI(g))
+		for _, i := range list {
+			if i == needle {
+				return true
+			}
+		}
+		return false
 	}
 
-	// sort list of alerts for deterministic output
+	groups := make([]apiGroup, 0)
+	for _, group := range rh.m.groups {
+		if !isInList(rf.groupNames, group.Name) {
+			continue
+		}
+		if !isInList(rf.files, group.File) {
+			continue
+		}
+
+		g := groupToAPI(group)
+		// the returned list should always be non-nil
+		// https://github.com/VictoriaMetrics/VictoriaMetrics/issues/4221
+		filteredRules := make([]apiRule, 0)
+		for _, r := range g.Rules {
+			if rf.ruleType != "" && rf.ruleType != r.Type {
+				continue
+			}
+			if !isInList(rf.ruleNames, r.Name) {
+				continue
+			}
+			if rf.excludeAlerts {
+				r.Alerts = nil
+			}
+			filteredRules = append(filteredRules, r)
+		}
+		g.Rules = filteredRules
+		groups = append(groups, g)
+	}
+	// sort list of groups for deterministic output
 	sort.Slice(groups, func(i, j int) bool {
 		return groups[i].Name < groups[j].Name
 	})
-
 	return groups
 }
 
-func (rh *requestHandler) listGroups(ruleType string) ([]byte, error) {
+func (rh *requestHandler) listGroups(rf rulesFilter) ([]byte, error) {
 	lr := listGroupsResponse{Status: "success"}
-	lr.Data.Groups = rh.groups(ruleType)
+	lr.Data.Groups = rh.groups(rf)
 	b, err := json.Marshal(lr)
 	if err != nil {
 		return nil, &httpserver.ErrorWithStatusCode{
