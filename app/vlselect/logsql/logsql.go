2d674f98d4a736ec78b3bae989c881cb595d46c7
Dmytro Kozlov <kozlovdmitriyy@gmail.com>
2024-02-18 21:58:47 +0100

    Enable the `limit` query param for the `/select/logsql/query` (#5778)
    * app/vlselect: add limit for logs query
    
    * app/vlselect: CHANGELOG.md
    
    * app/vlselect: stop search process if limit is reached, update logic, remove default limit
    
    * app/vlselect: fix tests
    
    * app/vlselect: fix filter tests
    
    * app/vlselect: fix tests

--- app/vlselect/logsql/logsql.go
+++ app/vlselect/logsql/logsql.go
@@ -2,11 +2,13 @@ package logsql
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/app/vlstorage"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/flagutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/httpserver"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/httputils"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logstorage"
 )
 
@@ -31,23 +33,36 @@ func ProcessQueryRequest(w http.ResponseWriter, r *http.Request, stopCh <-chan s
 		httpserver.Errorf(w, r, "cannot parse query [%s]: %s", qStr, err)
 		return
 	}
+	limit, err := httputils.GetInt(r, "limit")
+	if err != nil {
+		httpserver.Errorf(w, r, "cannot parse limit from the request: %s", err)
+		return
+	}
 	w.Header().Set("Content-Type", "application/stream+json; charset=utf-8")
-
 	sw := getSortWriter()
 	sw.Init(w, maxSortBufferSize.IntN())
 	tenantIDs := []logstorage.TenantID{tenantID}
-	vlstorage.RunQuery(tenantIDs, q, stopCh, func(columns []logstorage.BlockColumn) {
+
+	var mx sync.Mutex
+	vlstorage.RunQuery(tenantIDs, q, stopCh, func(columns []logstorage.BlockColumn) bool {
 		if len(columns) == 0 {
-			return
+			return true
 		}
 		rowsCount := len(columns[0].Values)
-
+		mx.Lock()
+		if rowsCount > limit {
+			rowsCount = limit
+		}
+		limit = limit - rowsCount
+		mx.Unlock()
 		bb := blockResultPool.Get()
 		for rowIdx := 0; rowIdx < rowsCount; rowIdx++ {
 			WriteJSONRow(bb, columns, rowIdx)
 		}
 		sw.MustWrite(bb.B)
 		blockResultPool.Put(bb)
+
+		return limit == 0
 	})
 	sw.FinalFlush()
 	putSortWriter(sw)
