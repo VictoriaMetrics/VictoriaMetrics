b60dcbe11f5f6a6782ac7efe046ec2dfab07ef53
Victor Amorim dos Santos <victor.amorim@nubank.com.br>
2024-02-20 08:31:41 -0300

    vmalert: add filter by group or rule name to UI (#5791)
    Co-authored-by: Yury Molodov <yurymolodov@gmail.com>

--- app/vmalert/static/js/custom.js
+++ app/vmalert/static/js/custom.js
@@ -1,8 +1,10 @@
 function expandAll() {
+    $(".group-heading").show()
     $('.collapse').addClass('show');
 }
 
 function collapseAll() {
+    $(".group-heading").show()
     $('.collapse').removeClass('show');
 }
 
@@ -13,6 +15,87 @@ function toggleByID(id) {
             el.click();
         }
     }
+}
+
+function debounce(func, delay) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+$('#filter').on("keyup", debounce(filter, 500));
+
+function filter(){
+    $(".rule-table").removeClass('show');
+    $(".rule").show();
+    
+    if($("#filter").val().length === 0){
+        $(".group-heading").show()
+        return
+    }
+
+    $(".group-heading").hide()
+
+    filterRuleByName();
+    filterRuleByLabels();
+    filterGroupsByName();
+}
+
+function filterGroupsByName(){
+    $( ".group-heading" ).each(function() {
+        const groupName = $(this).attr('data-group-name');
+        const filter = $("#filter").val()
+        const hasValue = groupName.indexOf(filter) >= 0
+
+        if (hasValue){
+            const target = $(this).attr("data-bs-target");
+            
+            $(this).show();
+            $(`div[id="${target}"] .rule`).show();
+        }
+    });
+}
+
+function filterRuleByName(){
+    $( ".rule" ).each(function() {
+        const ruleName = $(this).attr("data-rule-name");
+        const filter = $("#filter").val()
+        const hasValue = ruleName.indexOf(filter) >= 0
+
+        if (hasValue){
+            const target = $(this).attr('data-bs-target')
+
+            $(`#rules-${target}`).addClass('show');
+            $(`div[data-bs-target='rules-${target}']`).show();
+            $(this).show();
+        }else{
+            $(this).hide();
+        }
+    });  
+}
+
+function filterRuleByLabels(){
+    $( ".rule" ).each(function() {
+        const filter = $("#filter").val()
+        
+        const matches = $( ".label", this ).filter(function() {
+            const label = $(this).text();
+            const hasValue = label.indexOf(filter) >= 0
+            return hasValue;
+        }).length; 
+
+        if (matches > 0){
+            const target = $(this).attr('data-bs-target')
+
+            $(`#rules-${target}`).addClass('show');
+            $(`div[data-bs-target='rules-${target}']`).show();
+            $(this).show();
+        }
+    }); 
 }
 
 $(document).ready(function () {
