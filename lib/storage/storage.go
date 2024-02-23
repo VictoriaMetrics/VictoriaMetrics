VictoriaMetrics/VictoriaMetrics

commit 431aa16c8de4f36ece730edced4117243f141c80

lib/storage: keep (date, metricID) entries only for the last two dates
Entries for the previous dates is usually not used, so there is little sense in keeping them in memory.

This should reduce the size of storage/date_metricID cache, which can be monitored
via vm_cache_entries{type="storage/date_metricID"} metric.

lib/storage/storage.go
@@ -2352,6 +2352,8 @@ func (dmc *dateMetricIDCache) syncLocked() {
 	// Merge data from byDate into byDateMutable and then atomically replace byDate with the merged data.
 	byDate := dmc.byDate.Load()
 	byDateMutable := dmc.byDateMutable
+	byDateMutable.hotEntry.Store(&byDateMetricIDEntry{})
+
 	for k, e := range byDateMutable.m {
 		v := byDate.get(k.generation, k.date)
 		if v == nil {
@@ -2365,11 +2367,8 @@ func (dmc *dateMetricIDCache) syncLocked() {
 			v: *v,
 		}
 		byDateMutable.m[k] = dme
-		he := byDateMutable.hotEntry.Load()
-		if he.k == k {
-			byDateMutable.hotEntry.Store(dme)
-		}
 	}
+
 	// Copy entries from byDate, which are missing in byDateMutable
 	for k, e := range byDate.m {
 		v := byDateMutable.get(k.generation, k.date)
@@ -2377,6 +2376,24 @@ func (dmc *dateMetricIDCache) syncLocked() {
 			continue
 		}
 		byDateMutable.m[k] = e
+	}
+
+	if len(byDateMutable.m) > 2 {
+		// Keep only entries for the last two dates - these are usually
+		// the current date and the next date.
+		dates := make([]uint64, 0, len(byDateMutable.m))
+		for k := range byDateMutable.m {
+			dates = append(dates, k.date)
+		}
+		sort.Slice(dates, func(i, j int) bool {
+			return dates[i] < dates[j]
+		})
+		maxDate := dates[len(dates)-2]
+		for k := range byDateMutable.m {
+			if k.date < maxDate {
+				delete(byDateMutable.m, k)
+			}
+		}
 	}
 
 	// Atomically replace byDate with byDateMutable

lib/storage/storage_test.go
@@ -98,7 +98,7 @@ func testDateMetricIDCache(c *dateMetricIDCache, concurrent bool) error {
 	m := make(map[dmk]bool)
 	for i := 0; i < 1e5; i++ {
 		generation := uint64(i) % 2
-		date := uint64(i) % 3
+		date := uint64(i) % 2
 		metricID := uint64(i) % 1237
 		if !concurrent && c.Has(generation, date, metricID) {
 			if !m[dmk{generation, date, metricID}] {
@@ -127,7 +127,7 @@ func testDateMetricIDCache(c *dateMetricIDCache, concurrent bool) error {
 	// Verify fast path after sync.
 	for i := 0; i < 1e5; i++ {
 		generation := uint64(i) % 2
-		date := uint64(i) % 3
+		date := uint64(i) % 2
 		metricID := uint64(i) % 123
 		c.Set(generation, date, metricID)
 	}
@@ -136,7 +136,7 @@ func testDateMetricIDCache(c *dateMetricIDCache, concurrent bool) error {
 	c.mu.Unlock()
 	for i := 0; i < 1e5; i++ {
 		generation := uint64(i) % 2
-		date := uint64(i) % 3
+		date := uint64(i) % 2
 		metricID := uint64(i) % 123
 		if !concurrent && !c.Has(generation, date, metricID) {
 			return fmt.Errorf("c.Has(%d, %d, %d) must return true after sync", generation, date, metricID)

commit f79944532b137e7b8cb0386a0534d386fcd37100

lib/storage: do not drop (date, metricID) entries for the date older than 2 days if samples are ingested at this date
Previously the (date, metricID) entries for dates older than the last 2 days were removed.
This could lead to slow check for the (date, metricID) entry in the indexdb during ingesting historical data (aka backfilling).

The issue has been introduced in 431aa16c8de4f36ece730edced4117243f141c80

lib/storage/storage.go
@@ -2342,7 +2342,9 @@ func (dmc *dateMetricIDCache) syncLocked() {
 	byDateMutable := dmc.byDateMutable
 	byDateMutable.hotEntry.Store(&byDateMetricIDEntry{})
 
+	keepDatesMap := make(map[uint64]struct{}, len(byDateMutable.m))
 	for k, e := range byDateMutable.m {
+		keepDatesMap[k.date] = struct{}{}
 		v := byDate.get(k.generation, k.date)
 		if v == nil {
 			// Nothing to merge
@@ -2358,7 +2360,9 @@ func (dmc *dateMetricIDCache) syncLocked() {
 	}
 
 	// Copy entries from byDate, which are missing in byDateMutable
+	allDatesMap := make(map[uint64]struct{}, len(byDate.m))
 	for k, e := range byDate.m {
+		allDatesMap[k.date] = struct{}{}
 		v := byDateMutable.get(k.generation, k.date)
 		if v != nil {
 			continue
@@ -2367,18 +2371,22 @@ func (dmc *dateMetricIDCache) syncLocked() {
 	}
 
 	if len(byDateMutable.m) > 2 {
-		// Keep only entries for the last two dates - these are usually
-		// the current date and the next date.
-		dates := make([]uint64, 0, len(byDateMutable.m))
-		for k := range byDateMutable.m {
-			dates = append(dates, k.date)
+		// Keep only entries for the last two dates from allDatesMap plus all the entries for byDateMutable.
+		dates := make([]uint64, 0, len(allDatesMap))
+		for date := range allDatesMap {
+			dates = append(dates, date)
 		}
 		sort.Slice(dates, func(i, j int) bool {
 			return dates[i] < dates[j]
 		})
-		maxDate := dates[len(dates)-2]
+		if len(dates) > 2 {
+			dates = dates[len(dates)-2:]
+		}
+		for _, date := range dates {
+			keepDatesMap[date] = struct{}{}
+		}
 		for k := range byDateMutable.m {
-			if k.date < maxDate {
+			if _, ok := keepDatesMap[k.date]; !ok {
 				delete(byDateMutable.m, k)
 			}
 		}