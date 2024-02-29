7832d0800e70da5be3e1638349f1312ce090fcbe
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-02-29 14:38:21 +0200

    app/{vminsert,vmagent}: follow-up after 67a55b89a488e47451438ddc73331cafb38c01ea
    - Document the ability to read OpenTelemetry data from Amazon Firehose at docs/CHANGELOG.md
    
    - Simplify parsing Firehose data. There is no need in trying to optimize the parsing with fastjson
      and byte slice tricks, since OpenTelemetry protocol is really slooow because of over-engineering.
      It is better to write clear code for better maintanability in the future.
    
    - Move Firehose parser from /lib/protoparser/firehose to lib/protoparser/opentelemetry/firehose,
      since it is used only by opentelemetry parser.
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/5893

--- app/vminsert/opentelemetry/request_handler.go
+++ app/vminsert/opentelemetry/request_handler.go
@@ -9,7 +9,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/auth"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/prompbmarshal"
 	parserCommon "github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/common"
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/firehose"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/opentelemetry/firehose"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/opentelemetry/stream"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/storage"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/tenantmetrics"
@@ -29,7 +29,7 @@ func InsertHandler(at *auth.Token, req *http.Request) error {
 		return err
 	}
 	isGzipped := req.Header.Get("Content-Encoding") == "gzip"
-	var processBody func(*[]byte) error
+	var processBody func([]byte) ([]byte, error)
 	if req.Header.Get("Content-Type") == "application/json" {
 		if req.Header.Get("X-Amz-Firehose-Protocol-Version") != "" {
 			processBody = firehose.ProcessRequestBody
--- lib/protoparser/opentelemetry/firehose/parser_test.go
+++ lib/protoparser/opentelemetry/firehose/parser_test.go
@@ -6,11 +6,12 @@ import (
 
 func TestProcessRequestBody(t *testing.T) {
 	data := []byte(`{"records": [{"data": "SGVsbG8sIA=="}, {"data": "d29ybGQh"}]}`)
-	err := ProcessRequestBody(&data)
+	result, err := ProcessRequestBody(data)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
-	if string(data) != "Hello, world!" {
-		t.Fatalf("unexpected string: %q", string(data))
+	resultExpected := "Hello, world!"
+	if string(result) != resultExpected {
+		t.Fatalf("unexpected result; got %q; want %q", result, resultExpected)
 	}
 }
