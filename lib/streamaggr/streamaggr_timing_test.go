9773ad200ea5637943d3bc1be5ebb9bddae28ea1
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-03-04 17:37:04 +0200

    lib/streamaggr: use multiple job labels in BenchmarkAggregatorsPush

--- lib/streamaggr/streamaggr_timing_test.go
+++ lib/streamaggr/streamaggr_timing_test.go
@@ -97,8 +97,8 @@ func newBenchAggregators(output string, pushFunc PushFunc) *Aggregators {
 func newBenchSeries(seriesCount int) []prompbmarshal.TimeSeries {
 	a := make([]string, seriesCount)
 	for j := 0; j < seriesCount; j++ {
-		s := fmt.Sprintf(`http_requests_total{path="/foo/%d",job="foo",instance="bar",pod="pod-123232312",namespace="kube-foo-bar",node="node-123-3434-443",`+
-			`some_other_label="foo-bar-baz",environment="prod",label1="value1",label2="value2",label3="value3"} %d`, j, j*1000)
+		s := fmt.Sprintf(`http_requests_total{path="/foo/%d",job="foo_%d",instance="bar",pod="pod-123232312",namespace="kube-foo-bar",node="node-123-3434-443",`+
+			`some_other_label="foo-bar-baz",environment="prod",label1="value1",label2="value2",label3="value3"} %d`, j, j%100, j*1000)
 		a = append(a, s)
 	}
 	metrics := strings.Join(a, "\n")
