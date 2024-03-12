a58a81d80b86282a536c898bf211a1427f296f29
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2024-03-12 23:47:29 +0200

    lib/streamaggr: pick sample with bigger timestamp or value on deduplicator (#5939)
    Apply the same deduplication logic as in https://docs.victoriametrics.com/#deduplication
    This would require more memory for deduplication, since we need to track timestamp
    for each record. However, deduplication should become more consistent.
    
    https://github.com/VictoriaMetrics/VictoriaMetrics/issues/5643
    
    ---------
    
    Co-authored-by: Roman Khavronenko <roman@victoriametrics.com>

--- lib/streamaggr/deduplicator_test.go
+++ lib/streamaggr/deduplicator_test.go
@@ -41,7 +41,7 @@ baz_aaa_aaa_fdd{instance="x",job="aaa",pod="sdfd-dfdfdfs",node="aosijjewrerfd",n
 bar{container="ohohffd",job="aaa",namespace="asdff",pod="sdfd-dfdfdfs"} 34.54
 baz_aaa_aaa_fdd{container="ohohffd",job="aaa",namespace="asdff",pod="sdfd-dfdfdfs"} -2.3
 foo{container="ohohffd",job="aaa",namespace="asdff",pod="sdfd-dfdfdfs"} 894
-x 433
+x 90984
 `
 	if result != resultExpected {
 		t.Fatalf("unexpected result; got\n%s\nwant\n%s", result, resultExpected)
