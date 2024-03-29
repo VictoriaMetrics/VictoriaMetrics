36c1ca9949152668b31572e90aeee9598f7cf9a2
hagen1778 <roman@victoriametrics.com>
2024-03-29 14:07:43 +0100

    app/vmagent: follow-up 166b97b8d03e80830befd6512b872a3c1c28bc15
    * add tests for sharding function
    * update flags description
    * add changelog note
    
    https://github.com/VictoriaMetrics/VictoriaMetrics/commit/166b97b8d03e80830befd6512b872a3c1c28bc15
    Signed-off-by: hagen1778 <roman@victoriametrics.com>
    (cherry picked from commit b6bd9a97a3cf1da0da3e8df36dfcd2be343e21c4)

--- app/vmagent/remotewrite/remotewrite.go
+++ app/vmagent/remotewrite/remotewrite.go
@@ -50,8 +50,8 @@ var (
 		"By default the data is replicated across all the -remoteWrite.url . See https://docs.victoriametrics.com/vmagent.html#sharding-among-remote-storages")
 	shardByURLLabels = flagutil.NewArrayString("remoteWrite.shardByURL.labels", "Optional list of labels, which must be used for sharding outgoing samples "+
 		"among remote storage systems if -remoteWrite.shardByURL command-line flag is set. By default all the labels are used for sharding in order to gain "+
-		"even distribution of series over the specified -remoteWrite.url systems")
-	shardByURLInverseLabels = flag.Bool("remoteWrite.shardByURL.inverseLabels", false, "Inverse the behavior of remoteWrite.shardByURL.labels so that series are sharded using all labels except the ones specified in remoteWrite.shardByURL.labels.")
+		"even distribution of series over the specified -remoteWrite.url systems. See also -remoteWrite.shardByURL.inverseLabels.")
+	shardByURLLabelsInverse = flag.Bool("remoteWrite.shardByURL.inverseLabels", false, "Inverse the behavior of -remoteWrite.shardByURL.labels so that series are sharded using all labels except the ones specified in -remoteWrite.shardByURL.labels.")
 	tmpDataPath             = flag.String("remoteWrite.tmpDataPath", "vmagent-remotewrite-data", "Path to directory for storing pending data, which isn't sent to the configured -remoteWrite.url . "+
 		"See also -remoteWrite.maxDiskUsagePerURL and -remoteWrite.disableOnDiskQueue")
 	keepDanglingQueues = flag.Bool("remoteWrite.keepDanglingQueues", false, "Keep persistent queues contents at -remoteWrite.tmpDataPath in case there are no matching -remoteWrite.url. "+
@@ -538,18 +538,8 @@ func tryPushBlockToRemoteStorages(rwctxs []*remoteWriteCtx, tssBlock []prompbmar
 		// Shard the data among rwctxs
 		tssByURL := make([][]prompbmarshal.TimeSeries, len(rwctxs))
 		tmpLabels := promutils.GetLabels()
-		inverseLabels := *shardByURLInverseLabels
 		for _, ts := range tssBlock {
-			hashLabels := ts.Labels
-			if len(shardByURLLabelsMap) > 0 {
-				hashLabels = tmpLabels.Labels[:0]
-				for _, label := range ts.Labels {
-					_, ok := shardByURLLabelsMap[label.Name]
-					if ok && !inverseLabels || !ok && inverseLabels {
-						hashLabels = append(hashLabels, label)
-					}
-				}
-			}
+			hashLabels := extractShardingLabels(tmpLabels.Labels, ts.Labels, *shardByURLLabelsInverse)
 			h := getLabelsHash(hashLabels)
 			idx := h % uint64(len(tssByURL))
 			tssByURL[idx] = append(tssByURL[idx], ts)
@@ -593,6 +583,20 @@ func tryPushBlockToRemoteStorages(rwctxs []*remoteWriteCtx, tssBlock []prompbmar
 	}
 	wg.Wait()
 	return !anyPushFailed.Load()
+}
+
+func extractShardingLabels(dst, src []prompbmarshal.Label, inverse bool) []prompbmarshal.Label {
+	if len(shardByURLLabelsMap) < 1 {
+		return src
+	}
+	dst = dst[:0]
+	for _, label := range src {
+		_, ok := shardByURLLabelsMap[label.Name]
+		if ok && !inverse || !ok && inverse {
+			dst = append(dst, label)
+		}
+	}
+	return dst
 }
 
 // sortLabelsIfNeeded sorts labels if -sortLabels command-line flag is set.
