04dd7bd8fb1b0ac2ee1c16df5311df7012ad8b18
Dzmitry Lazerka <dlazerka@gmail.com>
2024-04-03 16:53:05 +0200

    Remove "/prometheus" from default serverUrl.

--- app/vmui/packages/vmui/src/utils/default-server-url.ts
+++ app/vmui/packages/vmui/src/utils/default-server-url.ts
@@ -8,7 +8,7 @@ export const getDefaultServer = (tenantId?: string): string => {
   const { serverURL } = getAppModeParams();
   const storageURL = getFromStorage("SERVER_URL") as string;
   const logsURL = window.location.href.replace(/\/(select\/)?(vmui)\/.*/, "");
-  const defaultURL = window.location.href.replace(/\/(?:prometheus\/)?(?:graph|vmui)\/.*/, "/prometheus");
+  const defaultURL = window.location.href.replace(/\/(?:prometheus\/)?(?:graph|vmui)\/.*/, "/");
   const url = serverURL || storageURL || defaultURL;
   if (REACT_APP_TYPE === AppType.logs) return logsURL;
   if (tenantId) return replaceTenantId(url, tenantId);
