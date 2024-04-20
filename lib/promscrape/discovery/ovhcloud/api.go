75d721837c288ca47787d64e38ac3ccf3425689a
Jiekun <jiekun.dev@gmail.com>
2024-04-20 16:06:51 +0800

    feature: [ovh sd] Added support for ovh service discovery
    Signed-off-by: Jiekun <jiekun.dev@gmail.com>

---
+++ lib/promscrape/discovery/ovhcloud/api.go
@@ -0,0 +1,87 @@
+package ovhcloud
+
+import (
+	"fmt"
+	"sync/atomic"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promscrape/discoveryutils"
+)
+
+// Endpoints
+const (
+	OvhEU        = "https://eu.api.ovh.com/1.0"
+	OvhCA        = "https://ca.api.ovh.com/1.0"
+	OvhUS        = "https://api.us.ovhcloud.com/1.0"
+	KimsufiEU    = "https://eu.api.kimsufi.com/1.0"
+	KimsufiCA    = "https://ca.api.kimsufi.com/1.0"
+	SoyoustartEU = "https://eu.api.soyoustart.com/1.0"
+	SoyoustartCA = "https://ca.api.soyoustart.com/1.0"
+)
+
+// Endpoints conveniently maps endpoints names to their URI for external configuration
+var availableEndpoints = map[string]string{
+	"ovh-eu":        OvhEU,
+	"ovh-ca":        OvhCA,
+	"ovh-us":        OvhUS,
+	"kimsufi-eu":    KimsufiEU,
+	"kimsufi-ca":    KimsufiCA,
+	"soyoustart-eu": SoyoustartEU,
+	"soyoustart-ca": SoyoustartCA,
+}
+
+var configMap = discoveryutils.NewConfigMap()
+
+type apiConfig struct {
+	client *discoveryutils.Client
+
+	applicationKey    string `yaml:"application_key"`
+	applicationSecret string `yaml:"application_secret"`
+	consumerKey       string `yaml:"consumer_key"`
+
+	// internal fields, for ovh auth
+	timeDelta atomic.Value
+}
+
+func getAPIConfig(sdc *SDConfig, baseDir string) (*apiConfig, error) {
+	v, err := configMap.Get(sdc, func() (interface{}, error) { return newAPIConfig(sdc, baseDir) })
+	if err != nil {
+		return nil, err
+	}
+	return v.(*apiConfig), nil
+}
+
+func newAPIConfig(sdc *SDConfig, baseDir string) (*apiConfig, error) {
+	if sdc.Endpoint == "" {
+		sdc.Endpoint = "ovh-eu"
+	}
+
+	var (
+		apiServer string
+		ok        bool
+	)
+	if apiServer, ok = availableEndpoints[sdc.Endpoint]; !ok {
+		return nil, fmt.Errorf("unsupported endpoint for ovhcloud sd: %s", sdc.Endpoint)
+	}
+
+	ac, err := sdc.HTTPClientConfig.NewConfig(baseDir)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse auth config: %w", err)
+	}
+	proxyAC, err := sdc.ProxyClientConfig.NewConfig(baseDir)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse proxy auth config: %w", err)
+	}
+
+	client, err := discoveryutils.NewClient(apiServer, ac, sdc.ProxyURL, proxyAC, &sdc.HTTPClientConfig)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create HTTP client for %q: %w", apiServer, err)
+	}
+
+	return &apiConfig{
+		client: client,
+
+		applicationKey:    sdc.ApplicationKey,
+		applicationSecret: sdc.ApplicationSecret.String(),
+		consumerKey:       sdc.ConsumerKey.String(),
+	}, nil
+}
---
+++ lib/promscrape/discovery/ovhcloud/common.go
@@ -0,0 +1,105 @@
+package ovhcloud
+
+import (
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/netip"
+	"strconv"
+	"time"
+)
+
+func getAuthHeaders(cfg *apiConfig, headers http.Header, endpoint, path string) (http.Header, error) {
+	headers = setGeneralHeaders(cfg, headers)
+
+	timeDelta, err := getTimeDelta(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	timestamp := time.Now().Add(-timeDelta).Unix()
+	headers.Set("X-Ovh-Timestamp", strconv.FormatInt(timestamp, 10))
+	headers.Add("X-Ovh-Consumer", cfg.consumerKey)
+
+	h := sha1.New()
+	h.Write([]byte(fmt.Sprintf("%s+%s+%s+%s+%s+%d",
+		cfg.applicationSecret,
+		cfg.consumerKey,
+		"GET",
+		endpoint+path,
+		"", // no body contained in any service discovery request, so it's set to empty by default
+		timestamp,
+	)))
+	headers.Set("X-Ovh-Signature", fmt.Sprintf("$1$%x", h.Sum(nil)))
+	return headers, nil
+}
+
+func setGeneralHeaders(cfg *apiConfig, headers http.Header) http.Header {
+	headers.Set("X-Ovh-Application", cfg.applicationKey)
+	headers.Set("Accept", "application/json")
+	headers.Set("User-Agent", "github.com/VictoriaMetrics/VictoriaMetrics")
+	return headers
+}
+
+func getServerTime(cfg *apiConfig) (*time.Time, error) {
+	resp, err := cfg.client.GetAPIResponseWithReqParams("/auth/time", func(req *http.Request) {
+		req.Header = setGeneralHeaders(cfg, req.Header)
+	})
+	if err != nil {
+		return nil, err
+	}
+	ts, err := strconv.ParseInt(string(resp), 10, 0)
+	if err != nil {
+		return nil, fmt.Errorf("parse ovh response to timestamp failed: %w", err)
+	}
+	serverTime := time.Unix(ts, 0)
+	return &serverTime, nil
+}
+
+func getTimeDelta(cfg *apiConfig) (time.Duration, error) {
+	d, ok := cfg.timeDelta.Load().(time.Duration)
+	if ok {
+		return d, nil
+	}
+
+	ovhTime, err := getServerTime(cfg)
+	if err != nil {
+		return 0, err
+	}
+
+	d = time.Since(*ovhTime)
+	cfg.timeDelta.Store(d)
+
+	return d, nil
+
+}
+
+// ParseIPList parses ip list as they can have different formats.
+func parseIPList(ipList []string) ([]netip.Addr, error) {
+	var ipAddresses []netip.Addr
+	for _, ip := range ipList {
+		ipAddr, err := netip.ParseAddr(ip)
+		if err != nil {
+			ipPrefix, err := netip.ParsePrefix(ip)
+			if err != nil {
+				return nil, fmt.Errorf("could not parse IP addresses: %s", ip)
+			}
+			if ipPrefix.IsValid() {
+				netmask := ipPrefix.Bits()
+				if netmask != 32 {
+					continue
+				}
+				ipAddr = ipPrefix.Addr()
+			}
+		}
+		if ipAddr.IsValid() && !ipAddr.IsUnspecified() {
+			ipAddresses = append(ipAddresses, ipAddr)
+		}
+	}
+
+	if len(ipAddresses) == 0 {
+		return nil, errors.New("could not parse IP addresses from list")
+	}
+	return ipAddresses, nil
+}
