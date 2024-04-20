498fe1cfa523be5bfecaa372293c3cded85e75ab
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-04-20 20:38:20 +0200

    app/vminsert/common: remove obsolete optimization for reducing memory usage for InsertCtx pool
    This optimization is no longer needed according to benchmarks with ingestion rate.
    
    This simplifies the code a bit.

--- app/vminsert/netstorage/insert_ctx_pool.go
+++ app/vminsert/netstorage/insert_ctx_pool.go
@@ -2,23 +2,16 @@ package netstorage
 
 import (
 	"sync"
-
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/cgroup"
 )
 
 // GetInsertCtx returns InsertCtx from the pool.
 //
 // Call PutInsertCtx for returning it to the pool.
 func GetInsertCtx() *InsertCtx {
-	select {
-	case ctx := <-insertCtxPoolCh:
-		return ctx
-	default:
-		if v := insertCtxPool.Get(); v != nil {
-			return v.(*InsertCtx)
-		}
-		return &InsertCtx{}
+	if v := insertCtxPool.Get(); v != nil {
+		return v.(*InsertCtx)
 	}
+	return &InsertCtx{}
 }
 
 // PutInsertCtx returns ctx to the pool.
@@ -26,12 +19,7 @@ func GetInsertCtx() *InsertCtx {
 // ctx cannot be used after the call.
 func PutInsertCtx(ctx *InsertCtx) {
 	ctx.Reset()
-	select {
-	case insertCtxPoolCh <- ctx:
-	default:
-		insertCtxPool.Put(ctx)
-	}
+	insertCtxPool.Put(ctx)
 }
 
 var insertCtxPool sync.Pool
-var insertCtxPoolCh = make(chan *InsertCtx, cgroup.AvailableCPUs())
