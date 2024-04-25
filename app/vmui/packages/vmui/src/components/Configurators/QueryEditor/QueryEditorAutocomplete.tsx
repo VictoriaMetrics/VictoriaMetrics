6193fa3dcfab29af6a2ac7dddebae8877288ebba
Yury Molodov <yurymolodov@gmail.com>
2024-04-25 12:48:49 +0200

    vmui: trigger auto-suggestion at any cursor position (#6155)
    - Implemented auto-suggestion triggers for mid-string cursor positions
    in vmui.
    - Improved the suggestion list positioning to appear directly beneath
    the active text editing area.
    
    https://github.com/VictoriaMetrics/VictoriaMetrics/issues/5864

--- app/vmui/packages/vmui/src/components/Configurators/QueryEditor/QueryEditorAutocomplete.tsx
+++ app/vmui/packages/vmui/src/components/Configurators/QueryEditor/QueryEditorAutocomplete.tsx
@@ -1,7 +1,6 @@
-import React, { FC, Ref, useState, useEffect, useMemo } from "preact/compat";
+import React, { FC, Ref, useState, useEffect, useMemo, useCallback } from "preact/compat";
 import Autocomplete, { AutocompleteOptions } from "../../Main/Autocomplete/Autocomplete";
 import { useFetchQueryOptions } from "../../../hooks/useFetchQueryOptions";
-import { getTextWidth } from "../../../utils/uplot";
 import { escapeRegexp, hasUnclosedQuotes } from "../../../utils/regexp";
 import useGetMetricsQL from "../../../hooks/useGetMetricsQL";
 import { QueryContextType } from "../../../types";
@@ -10,8 +9,9 @@ import { AUTOCOMPLETE_LIMITS } from "../../../constants/queryAutocomplete";
 interface QueryEditorAutocompleteProps {
   value: string;
   anchorEl: Ref<HTMLInputElement>;
-  caretPosition: number[];
-  onSelect: (val: string) => void;
+  caretPosition: [number, number]; // [start, end]
+  hasHelperText: boolean;
+  onSelect: (val: string, caretPosition: number) => void;
   onFoundOptions: (val: AutocompleteOptions[]) => void;
 }
 
@@ -19,16 +19,24 @@ const QueryEditorAutocomplete: FC<QueryEditorAutocompleteProps> = ({
   value,
   anchorEl,
   caretPosition,
+  hasHelperText,
   onSelect,
   onFoundOptions
 }) => {
-  const [leftOffset, setLeftOffset] = useState(0);
+  const [offsetPos, setOffsetPos] = useState({ top: 0, left: 0 });
   const metricsqlFunctions = useGetMetricsQL();
 
+  const values = useMemo(() => {
+    if (caretPosition[0] !== caretPosition[1]) return { beforeCursor: value, afterCursor: "" };
+    const beforeCursor = value.substring(0, caretPosition[0]);
+    const afterCursor = value.substring(caretPosition[1]);
+    return { beforeCursor, afterCursor };
+  }, [value, caretPosition]);
+
   const exprLastPart = useMemo(() => {
-    const parts = value.split("}");
+    const parts = values.beforeCursor.split("}");
     return parts[parts.length - 1];
-  }, [value]);
+  }, [values]);
 
   const metric = useMemo(() => {
     const regexp = /\b[^{}(),\s]+(?={|$)/g;
@@ -43,7 +51,7 @@ const QueryEditorAutocomplete: FC<QueryEditorAutocompleteProps> = ({
   }, [exprLastPart]);
 
   const shouldSuppressAutoSuggestion = (value: string) => {
-    const pattern = /([(),+\-*/^]|\b(?:or|and|unless|default|ifnot|if|group_left|group_right)\b)/;
+    const pattern = /([{(),+\-*/^]|\b(?:or|and|unless|default|ifnot|if|group_left|group_right)\b)/;
     const parts = value.split(/\s+/);
     const partsCount = parts.length;
     const lastPart = parts[partsCount - 1];
@@ -55,7 +63,7 @@ const QueryEditorAutocomplete: FC<QueryEditorAutocompleteProps> = ({
   };
 
   const context = useMemo(() => {
-    if (!value || value.endsWith("}") || shouldSuppressAutoSuggestion(value)) {
+    if (!values.beforeCursor || values.beforeCursor.endsWith("}") || shouldSuppressAutoSuggestion(values.beforeCursor)) {
       return QueryContextType.empty;
     }
 
@@ -63,19 +71,19 @@ const QueryEditorAutocomplete: FC<QueryEditorAutocompleteProps> = ({
     const labelValueRegexp = new RegExp(`(${escapeRegexp(metric)})?{?.+${escapeRegexp(label)}(=|!=|=~|!~)"?([^"]*)$`, "g");
 
     switch (true) {
-      case labelValueRegexp.test(value):
+      case labelValueRegexp.test(values.beforeCursor):
         return QueryContextType.labelValue;
-      case labelRegexp.test(value):
+      case labelRegexp.test(values.beforeCursor):
         return QueryContextType.label;
       default:
         return QueryContextType.metricsql;
     }
-  }, [value, metric, label]);
+  }, [values, metric, label]);
 
   const valueByContext = useMemo(() => {
-    const wordMatch = value.match(/([\w_\-.:/]+(?![},]))$/);
+    const wordMatch = values.beforeCursor.match(/([\w_\-.:/]+(?![},]))$/);
     return wordMatch ? wordMatch[0] : "";
-  }, [value]);
+  }, [values.beforeCursor]);
 
   const { metrics, labels, labelValues, loading } = useFetchQueryOptions({
     valueByContext,
@@ -97,8 +105,10 @@ const QueryEditorAutocomplete: FC<QueryEditorAutocompleteProps> = ({
     }
   }, [context, metrics, labels, labelValues]);
 
-  const handleSelect = (insert: string) => {
+  const handleSelect = useCallback((insert: string) => {
     // Find the start and end of valueByContext in the query string
+    const value = values.beforeCursor;
+    let valueAfterCursor = values.afterCursor;
     const startIndexOfValueByContext = value.lastIndexOf(valueByContext, caretPosition[0]);
     const endIndexOfValueByContext = startIndexOfValueByContext + valueByContext.length;
 
@@ -110,26 +120,59 @@ const QueryEditorAutocomplete: FC<QueryEditorAutocompleteProps> = ({
     if (context === QueryContextType.labelValue) {
       const quote = "\"";
       const needsQuote = /(?:=|!=|=~|!~)$/.test(beforeValueByContext);
+      valueAfterCursor = valueAfterCursor.replace(/^[^\s"|},]*/, "");
       insert = `${needsQuote ? quote : ""}${insert}`;
     }
 
+    if (context === QueryContextType.label) {
+      valueAfterCursor = valueAfterCursor.replace(/^[^\s=!,{}()"|+\-/*^]*/, "");
+    }
+
+    if (context === QueryContextType.metricsql) {
+      valueAfterCursor = valueAfterCursor.replace(/^[^\s[\]{}()"|+\-/*^]*/, "");
+    }
     // Assemble the new value with the inserted text
-    const newVal = `${beforeValueByContext}${insert}${afterValueByContext}`;
-    onSelect(newVal);
-  };
+    const newVal = `${beforeValueByContext}${insert}${afterValueByContext}${valueAfterCursor}`;
+    onSelect(newVal, beforeValueByContext.length + insert.length);
+  }, [values]);
 
   useEffect(() => {
     if (!anchorEl.current) {
-      setLeftOffset(0);
+      setOffsetPos({ top: 0, left: 0 });
       return;
     }
 
-    const style = window.getComputedStyle(anchorEl.current);
+    const element = anchorEl.current.querySelector("textarea") || anchorEl.current;
+    const style = window.getComputedStyle(element);
     const fontSize = `${style.getPropertyValue("font-size")}`;
     const fontFamily = `${style.getPropertyValue("font-family")}`;
-    const offset = getTextWidth(value, `${fontSize} ${fontFamily}`);
-    setLeftOffset(offset);
-  }, [anchorEl, caretPosition]);
+    const lineHeight = parseInt(`${style.getPropertyValue("line-height")}`);
+
+    const span = document.createElement("div");
+    span.style.font = `${fontSize} ${fontFamily}`;
+    span.style.padding = style.getPropertyValue("padding");
+    span.style.lineHeight = `${lineHeight}px`;
+    span.style.width = `${element.offsetWidth}px`;
+    span.style.maxWidth = `${element.offsetWidth}px`;
+    span.style.whiteSpace = style.getPropertyValue("white-space");
+    span.style.overflowWrap = style.getPropertyValue("overflow-wrap");
+
+    const marker = document.createElement("span");
+    span.appendChild(document.createTextNode(values.beforeCursor));
+    span.appendChild(marker);
+    span.appendChild(document.createTextNode(values.afterCursor));
+    document.body.appendChild(span);
+
+    const spanRect = span.getBoundingClientRect();
+    const markerRect = marker.getBoundingClientRect();
+
+    const leftOffset = markerRect.left - spanRect.left;
+    const topOffset = markerRect.bottom - spanRect.bottom - (hasHelperText ? lineHeight : 0);
+    setOffsetPos({ top: topOffset, left: leftOffset });
+
+    span.remove();
+    marker.remove();
+  }, [anchorEl, caretPosition, hasHelperText]);
 
   return (
     <>
@@ -140,7 +183,7 @@ const QueryEditorAutocomplete: FC<QueryEditorAutocompleteProps> = ({
         options={options}
         anchor={anchorEl}
         minLength={0}
-        offset={{ top: 0, left: leftOffset }}
+        offset={offsetPos}
         onSelect={handleSelect}
         onFoundOptions={onFoundOptions}
         maxDisplayResults={{
