f49352ece4f4664e7d89915fab5dcf01b8f4379a
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-04-29 06:42:18 +0200

    wip

--- lib/logstorage/filter.go
+++ lib/logstorage/filter.go
@@ -72,71 +72,6 @@ func (fs *streamFilter) apply(bs *blockSearch, bm *bitmap) {
 	}
 }
 
-// filterRange matches the given range [minValue..maxValue].
-//
-// Example LogsQL: `fieldName:range(minValue, maxValue]`
-type filterRange struct {
-	fieldName string
-	minValue  float64
-	maxValue  float64
-
-	stringRepr string
-}
-
-func (fr *filterRange) String() string {
-	return quoteFieldNameIfNeeded(fr.fieldName) + "range" + fr.stringRepr
-}
-
-func (fr *filterRange) apply(bs *blockSearch, bm *bitmap) {
-	fieldName := fr.fieldName
-	minValue := fr.minValue
-	maxValue := fr.maxValue
-
-	if minValue > maxValue {
-		bm.resetBits()
-		return
-	}
-
-	v := bs.csh.getConstColumnValue(fieldName)
-	if v != "" {
-		if !matchRange(v, minValue, maxValue) {
-			bm.resetBits()
-		}
-		return
-	}
-
-	// Verify whether filter matches other columns
-	ch := bs.csh.getColumnHeader(fieldName)
-	if ch == nil {
-		// Fast path - there are no matching columns.
-		bm.resetBits()
-		return
-	}
-
-	switch ch.valueType {
-	case valueTypeString:
-		matchStringByRange(bs, ch, bm, minValue, maxValue)
-	case valueTypeDict:
-		matchValuesDictByRange(bs, ch, bm, minValue, maxValue)
-	case valueTypeUint8:
-		matchUint8ByRange(bs, ch, bm, minValue, maxValue)
-	case valueTypeUint16:
-		matchUint16ByRange(bs, ch, bm, minValue, maxValue)
-	case valueTypeUint32:
-		matchUint32ByRange(bs, ch, bm, minValue, maxValue)
-	case valueTypeUint64:
-		matchUint64ByRange(bs, ch, bm, minValue, maxValue)
-	case valueTypeFloat64:
-		matchFloat64ByRange(bs, ch, bm, minValue, maxValue)
-	case valueTypeIPv4:
-		bm.resetBits()
-	case valueTypeTimestampISO8601:
-		bm.resetBits()
-	default:
-		logger.Panicf("FATAL: %s: unknown valueType=%d", bs.partPath(), ch.valueType)
-	}
-}
-
 // regexpFilter matches the given regexp
 //
 // Example LogsQL: `fieldName:re("regexp")`
@@ -635,23 +570,6 @@ func matchIPv4ByPhrase(bs *blockSearch, ch *columnHeader, bm *bitmap, phrase str
 	bbPool.Put(bb)
 }
 
-func matchFloat64ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
-	if minValue > math.Float64frombits(ch.maxValue) || maxValue < math.Float64frombits(ch.minValue) {
-		bm.resetBits()
-		return
-	}
-
-	visitValues(bs, ch, bm, func(v string) bool {
-		if len(v) != 8 {
-			logger.Panicf("FATAL: %s: unexpected length for binary representation of floating-point number: got %d; want 8", bs.partPath(), len(v))
-		}
-		b := bytesutil.ToUnsafeBytes(v)
-		n := encoding.UnmarshalUint64(b)
-		f := math.Float64frombits(n)
-		return f >= minValue && f <= maxValue
-	})
-}
-
 func matchFloat64ByRegexp(bs *blockSearch, ch *columnHeader, bm *bitmap, re *regexp.Regexp) {
 	bb := bbPool.Get()
 	visitValues(bs, ch, bm, func(v string) bool {
@@ -715,17 +633,6 @@ func matchFloat64ByPhrase(bs *blockSearch, ch *columnHeader, bm *bitmap, phrase 
 		s := toFloat64StringExt(bs, bb, v)
 		return matchPhrase(s, phrase)
 	})
-	bbPool.Put(bb)
-}
-
-func matchValuesDictByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
-	bb := bbPool.Get()
-	for i, v := range ch.valuesDict.values {
-		if matchRange(v, minValue, maxValue) {
-			bb.B = append(bb.B, byte(i))
-		}
-	}
-	matchEncodedValuesDict(bs, ch, bm, bb.B)
 	bbPool.Put(bb)
 }
 
@@ -811,12 +718,6 @@ func matchEncodedValuesDict(bs *blockSearch, ch *columnHeader, bm *bitmap, encod
 	})
 }
 
-func matchStringByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
-	visitValues(bs, ch, bm, func(v string) bool {
-		return matchRange(v, minValue, maxValue)
-	})
-}
-
 func matchStringByRegexp(bs *blockSearch, ch *columnHeader, bm *bitmap, re *regexp.Regexp) {
 	visitValues(bs, ch, bm, func(v string) bool {
 		return re.MatchString(v)
@@ -867,77 +768,6 @@ func matchMinMaxValueLen(ch *columnHeader, minLen, maxLen uint64) bool {
 	bb.B = strconv.AppendUint(bb.B[:0], ch.maxValue, 10)
 	s = bytesutil.ToUnsafeString(bb.B)
 	return minLen <= uint64(len(s))
-}
-
-func matchUint8ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
-	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
-	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
-		bm.resetBits()
-		return
-	}
-	bb := bbPool.Get()
-	visitValues(bs, ch, bm, func(v string) bool {
-		if len(v) != 1 {
-			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint8 number: got %d; want 1", bs.partPath(), len(v))
-		}
-		n := uint64(v[0])
-		return n >= minValueUint && n <= maxValueUint
-	})
-	bbPool.Put(bb)
-}
-
-func matchUint16ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
-	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
-	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
-		bm.resetBits()
-		return
-	}
-	bb := bbPool.Get()
-	visitValues(bs, ch, bm, func(v string) bool {
-		if len(v) != 2 {
-			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint16 number: got %d; want 2", bs.partPath(), len(v))
-		}
-		b := bytesutil.ToUnsafeBytes(v)
-		n := uint64(encoding.UnmarshalUint16(b))
-		return n >= minValueUint && n <= maxValueUint
-	})
-	bbPool.Put(bb)
-}
-
-func matchUint32ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
-	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
-	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
-		bm.resetBits()
-		return
-	}
-	bb := bbPool.Get()
-	visitValues(bs, ch, bm, func(v string) bool {
-		if len(v) != 4 {
-			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint8 number: got %d; want 4", bs.partPath(), len(v))
-		}
-		b := bytesutil.ToUnsafeBytes(v)
-		n := uint64(encoding.UnmarshalUint32(b))
-		return n >= minValueUint && n <= maxValueUint
-	})
-	bbPool.Put(bb)
-}
-
-func matchUint64ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
-	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
-	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
-		bm.resetBits()
-		return
-	}
-	bb := bbPool.Get()
-	visitValues(bs, ch, bm, func(v string) bool {
-		if len(v) != 8 {
-			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint8 number: got %d; want 8", bs.partPath(), len(v))
-		}
-		b := bytesutil.ToUnsafeBytes(v)
-		n := encoding.UnmarshalUint64(b)
-		return n >= minValueUint && n <= maxValueUint
-	})
-	bbPool.Put(bb)
 }
 
 func matchUint8ByRegexp(bs *blockSearch, ch *columnHeader, bm *bitmap, re *regexp.Regexp) {
@@ -1155,14 +985,6 @@ func matchPrefix(s, prefix string) bool {
 		}
 		return true
 	}
-}
-
-func matchRange(s string, minValue, maxValue float64) bool {
-	f, ok := tryParseFloat64(s)
-	if !ok {
-		return false
-	}
-	return f >= minValue && f <= maxValue
 }
 
 func matchAnyCasePhrase(s, phraseLowercase string) bool {
---
+++ lib/logstorage/filter_range.go
@@ -0,0 +1,187 @@
+package logstorage
+
+import (
+	"math"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/encoding"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
+)
+
+// filterRange matches the given range [minValue..maxValue].
+//
+// Example LogsQL: `fieldName:range(minValue, maxValue]`
+type filterRange struct {
+	fieldName string
+	minValue  float64
+	maxValue  float64
+
+	stringRepr string
+}
+
+func (fr *filterRange) String() string {
+	return quoteFieldNameIfNeeded(fr.fieldName) + "range" + fr.stringRepr
+}
+
+func (fr *filterRange) apply(bs *blockSearch, bm *bitmap) {
+	fieldName := fr.fieldName
+	minValue := fr.minValue
+	maxValue := fr.maxValue
+
+	if minValue > maxValue {
+		bm.resetBits()
+		return
+	}
+
+	v := bs.csh.getConstColumnValue(fieldName)
+	if v != "" {
+		if !matchRange(v, minValue, maxValue) {
+			bm.resetBits()
+		}
+		return
+	}
+
+	// Verify whether filter matches other columns
+	ch := bs.csh.getColumnHeader(fieldName)
+	if ch == nil {
+		// Fast path - there are no matching columns.
+		bm.resetBits()
+		return
+	}
+
+	switch ch.valueType {
+	case valueTypeString:
+		matchStringByRange(bs, ch, bm, minValue, maxValue)
+	case valueTypeDict:
+		matchValuesDictByRange(bs, ch, bm, minValue, maxValue)
+	case valueTypeUint8:
+		matchUint8ByRange(bs, ch, bm, minValue, maxValue)
+	case valueTypeUint16:
+		matchUint16ByRange(bs, ch, bm, minValue, maxValue)
+	case valueTypeUint32:
+		matchUint32ByRange(bs, ch, bm, minValue, maxValue)
+	case valueTypeUint64:
+		matchUint64ByRange(bs, ch, bm, minValue, maxValue)
+	case valueTypeFloat64:
+		matchFloat64ByRange(bs, ch, bm, minValue, maxValue)
+	case valueTypeIPv4:
+		bm.resetBits()
+	case valueTypeTimestampISO8601:
+		bm.resetBits()
+	default:
+		logger.Panicf("FATAL: %s: unknown valueType=%d", bs.partPath(), ch.valueType)
+	}
+}
+
+func matchFloat64ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
+	if minValue > math.Float64frombits(ch.maxValue) || maxValue < math.Float64frombits(ch.minValue) {
+		bm.resetBits()
+		return
+	}
+
+	visitValues(bs, ch, bm, func(v string) bool {
+		if len(v) != 8 {
+			logger.Panicf("FATAL: %s: unexpected length for binary representation of floating-point number: got %d; want 8", bs.partPath(), len(v))
+		}
+		b := bytesutil.ToUnsafeBytes(v)
+		n := encoding.UnmarshalUint64(b)
+		f := math.Float64frombits(n)
+		return f >= minValue && f <= maxValue
+	})
+}
+
+func matchValuesDictByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
+	bb := bbPool.Get()
+	for i, v := range ch.valuesDict.values {
+		if matchRange(v, minValue, maxValue) {
+			bb.B = append(bb.B, byte(i))
+		}
+	}
+	matchEncodedValuesDict(bs, ch, bm, bb.B)
+	bbPool.Put(bb)
+}
+
+func matchStringByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
+	visitValues(bs, ch, bm, func(v string) bool {
+		return matchRange(v, minValue, maxValue)
+	})
+}
+
+func matchUint8ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
+	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
+	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
+		bm.resetBits()
+		return
+	}
+	bb := bbPool.Get()
+	visitValues(bs, ch, bm, func(v string) bool {
+		if len(v) != 1 {
+			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint8 number: got %d; want 1", bs.partPath(), len(v))
+		}
+		n := uint64(v[0])
+		return n >= minValueUint && n <= maxValueUint
+	})
+	bbPool.Put(bb)
+}
+
+func matchUint16ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
+	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
+	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
+		bm.resetBits()
+		return
+	}
+	bb := bbPool.Get()
+	visitValues(bs, ch, bm, func(v string) bool {
+		if len(v) != 2 {
+			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint16 number: got %d; want 2", bs.partPath(), len(v))
+		}
+		b := bytesutil.ToUnsafeBytes(v)
+		n := uint64(encoding.UnmarshalUint16(b))
+		return n >= minValueUint && n <= maxValueUint
+	})
+	bbPool.Put(bb)
+}
+
+func matchUint32ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
+	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
+	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
+		bm.resetBits()
+		return
+	}
+	bb := bbPool.Get()
+	visitValues(bs, ch, bm, func(v string) bool {
+		if len(v) != 4 {
+			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint8 number: got %d; want 4", bs.partPath(), len(v))
+		}
+		b := bytesutil.ToUnsafeBytes(v)
+		n := uint64(encoding.UnmarshalUint32(b))
+		return n >= minValueUint && n <= maxValueUint
+	})
+	bbPool.Put(bb)
+}
+
+func matchUint64ByRange(bs *blockSearch, ch *columnHeader, bm *bitmap, minValue, maxValue float64) {
+	minValueUint, maxValueUint := toUint64Range(minValue, maxValue)
+	if maxValue < 0 || minValueUint > ch.maxValue || maxValueUint < ch.minValue {
+		bm.resetBits()
+		return
+	}
+	bb := bbPool.Get()
+	visitValues(bs, ch, bm, func(v string) bool {
+		if len(v) != 8 {
+			logger.Panicf("FATAL: %s: unexpected length for binary representation of uint8 number: got %d; want 8", bs.partPath(), len(v))
+		}
+		b := bytesutil.ToUnsafeBytes(v)
+		n := encoding.UnmarshalUint64(b)
+		return n >= minValueUint && n <= maxValueUint
+	})
+	bbPool.Put(bb)
+}
+
+func matchRange(s string, minValue, maxValue float64) bool {
+	f, ok := tryParseFloat64(s)
+	if !ok {
+		return false
+	}
+	return f >= minValue && f <= maxValue
+}
