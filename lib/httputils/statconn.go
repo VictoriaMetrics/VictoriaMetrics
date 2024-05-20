a14c01c9311aa25aeb0f204428f5a7b275a1e207
Haley Wang <haley@victoriametrics.com>
2024-05-21 01:39:56 +0800

    make GetStatDialFunc reusable

--- lib/httputils/statconn.go
+++ lib/httputils/statconn.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"sync"
+	"strconv"
+	"strings"
 	"sync/atomic"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/netutil"
 	"github.com/VictoriaMetrics/metrics"
 )
 
-var (
+var connStateMetricsRegistry map[string]connStateMetrics
+
+type connStateMetrics struct {
 	dialsTotal *metrics.Counter
 	dialErrors *metrics.Counter
 	conns      *metrics.Counter
@@ -22,72 +25,121 @@ var (
 	connWriteErrors  *metrics.Counter
 	connBytesRead    *metrics.Counter
 	connBytesWritten *metrics.Counter
-)
+}
 
-var metricSetRegister sync.Once
+func (cms *connStateMetrics) init(metricPrefix string) {
+	cms.dialsTotal = metrics.NewCounter(fmt.Sprintf(`%s_dials_total`, metricPrefix))
+	cms.dialErrors = metrics.NewCounter(fmt.Sprintf(`%s_dial_errors_total`, metricPrefix))
+	cms.conns = metrics.NewCounter(fmt.Sprintf(`%s_conns`, metricPrefix))
+
+	cms.connReadsTotal = metrics.NewCounter(fmt.Sprintf(`%s_conn_reads_total`, metricPrefix))
+	cms.connWritesTotal = metrics.NewCounter(fmt.Sprintf(`%s_conn_writes_total`, metricPrefix))
+	cms.connReadErrors = metrics.NewCounter(fmt.Sprintf(`%s_conn_read_errors_total`, metricPrefix))
+	cms.connWriteErrors = metrics.NewCounter(fmt.Sprintf(`%s_conn_write_errors_total`, metricPrefix))
+	cms.connBytesRead = metrics.NewCounter(fmt.Sprintf(`%s_conn_bytes_read_total`, metricPrefix))
+	cms.connBytesWritten = metrics.NewCounter(fmt.Sprintf(`%s_conn_bytes_written_total`, metricPrefix))
+}
 
 // GetStatDialFunc returns dial function that supports DNS SRV records,
 // and register stats metrics for conns.
+// GetStatDialFunc is not concurrent safe.
 func GetStatDialFunc(metricPrefix string) func(ctx context.Context, network, addr string) (net.Conn, error) {
-	metricSetRegister.Do(func() {
-		dialsTotal = metrics.NewCounter(fmt.Sprintf(`%s_dials_total`, metricPrefix))
-		dialErrors = metrics.NewCounter(fmt.Sprintf(`%s_dial_errors_total`, metricPrefix))
-		conns = metrics.NewCounter(fmt.Sprintf(`%s_conns`, metricPrefix))
-
-		connReadsTotal = metrics.NewCounter(fmt.Sprintf(`%s_conn_reads_total`, metricPrefix))
-		connWritesTotal = metrics.NewCounter(fmt.Sprintf(`%s_conn_writes_total`, metricPrefix))
-		connReadErrors = metrics.NewCounter(fmt.Sprintf(`%s_conn_read_errors_total`, metricPrefix))
-		connWriteErrors = metrics.NewCounter(fmt.Sprintf(`%s_conn_write_errors_total`, metricPrefix))
-		connBytesRead = metrics.NewCounter(fmt.Sprintf(`%s_conn_bytes_read_total`, metricPrefix))
-		connBytesWritten = metrics.NewCounter(fmt.Sprintf(`%s_conn_bytes_written_total`, metricPrefix))
-	})
-	return statDial
-}
-
-func statDial(ctx context.Context, _, addr string) (conn net.Conn, err error) {
-	network := netutil.GetTCPNetwork()
-	conn, err = netutil.DialMaybeSRV(ctx, network, addr)
-	dialsTotal.Inc()
-	if err != nil {
-		dialErrors.Inc()
-		return nil, err
+	if connStateMetricsRegistry == nil {
+		connStateMetricsRegistry = make(map[string]connStateMetrics)
 	}
-	conns.Inc()
-	sc := &statConn{
-		Conn: conn,
+	newSet, ok := connStateMetricsRegistry[metricPrefix]
+	if !ok {
+		newSet.init(metricPrefix)
+		connStateMetricsRegistry[metricPrefix] = newSet
 	}
-	return sc, nil
+
+	return func(ctx context.Context, _, addr string) (net.Conn, error) {
+		network := netutil.GetTCPNetwork()
+		conn, err := netutil.DialMaybeSRV(ctx, network, addr)
+		newSet.dialsTotal.Inc()
+		if err != nil {
+			newSet.dialErrors.Inc()
+			if !netutil.TCP6Enabled() && !isTCPv4Addr(addr) {
+				err = fmt.Errorf("%w; try -enableTCP6 command-line flag if you scrape ipv6 addresses", err)
+			}
+			return nil, err
+		}
+		newSet.conns.Inc()
+		sc := &statConn{
+			Conn:             conn,
+			connStateMetrics: newSet,
+		}
+		return sc, nil
+	}
 }
 
 type statConn struct {
 	closed atomic.Int32
 	net.Conn
+	connStateMetrics
 }
 
 func (sc *statConn) Read(p []byte) (int, error) {
 	n, err := sc.Conn.Read(p)
-	connReadsTotal.Inc()
+	sc.connReadsTotal.Inc()
 	if err != nil {
-		connReadErrors.Inc()
+		sc.connReadErrors.Inc()
 	}
-	connBytesRead.Add(n)
+	sc.connBytesRead.Add(n)
 	return n, err
 }
 
 func (sc *statConn) Write(p []byte) (int, error) {
 	n, err := sc.Conn.Write(p)
-	connWritesTotal.Inc()
+	sc.connWritesTotal.Inc()
 	if err != nil {
-		connWriteErrors.Inc()
+		sc.connWriteErrors.Inc()
 	}
-	connBytesWritten.Add(n)
+	sc.connBytesWritten.Add(n)
 	return n, err
 }
 
 func (sc *statConn) Close() error {
 	err := sc.Conn.Close()
 	if sc.closed.Add(1) == 1 {
-		conns.Dec()
+		sc.conns.Dec()
 	}
 	return err
+}
+
+func isTCPv4Addr(addr string) bool {
+	s := addr
+	for i := 0; i < 3; i++ {
+		n := strings.IndexByte(s, '.')
+		if n < 0 {
+			return false
+		}
+		if !isUint8NumString(s[:n]) {
+			return false
+		}
+		s = s[n+1:]
+	}
+	n := strings.IndexByte(s, ':')
+	if n < 0 {
+		return false
+	}
+	if !isUint8NumString(s[:n]) {
+		return false
+	}
+	s = s[n+1:]
+
+	// Verify TCP port
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return false
+	}
+	return n >= 0 && n < (1<<16)
+}
+
+func isUint8NumString(s string) bool {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return false
+	}
+	return n >= 0 && n < (1<<8)
 }
