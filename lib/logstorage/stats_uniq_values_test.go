d551520e6f30c3ec20f51978f9f5f054439beea4
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-05-21 15:58:41 +0200

    wip

--- lib/logstorage/stats_uniq_values_test.go
+++ lib/logstorage/stats_uniq_values_test.go
@@ -13,6 +13,7 @@ func TestParseStatsUniqValuesSuccess(t *testing.T) {
 	f(`uniq_values(*)`)
 	f(`uniq_values(a)`)
 	f(`uniq_values(a, b)`)
+	f(`uniq_values(a, b) limit 10`)
 }
 
 func TestParseStatsUniqValuesFailure(t *testing.T) {
@@ -24,6 +25,8 @@ func TestParseStatsUniqValuesFailure(t *testing.T) {
 	f(`uniq_values`)
 	f(`uniq_values(a b)`)
 	f(`uniq_values(x) y`)
+	f(`uniq_values(x) limit`)
+	f(`uniq_values(x) limit N`)
 }
 
 func TestStatsUniqValues(t *testing.T) {
@@ -33,6 +36,26 @@ func TestStatsUniqValues(t *testing.T) {
 	}
 
 	f("stats uniq_values(*) as x", [][]Field{
+		{
+			{"_msg", `abc`},
+			{"a", `2`},
+			{"b", `3`},
+		},
+		{
+			{"_msg", `def`},
+			{"a", `1`},
+		},
+		{
+			{"a", `-3`},
+			{"b", `54`},
+		},
+	}, [][]Field{
+		{
+			{"x", `["-3","1","2","3","54","abc","def"]`},
+		},
+	})
+
+	f("stats uniq_values(*) limit 1999 as x", [][]Field{
 		{
 			{"_msg", `abc`},
 			{"a", `2`},
@@ -208,6 +231,36 @@ func TestStatsUniqValues(t *testing.T) {
 	})
 
 	f("stats by (a) uniq_values(*) as x", [][]Field{
+		{
+			{"_msg", `abc`},
+			{"a", `1`},
+			{"b", `3`},
+		},
+		{
+			{"_msg", `def`},
+			{"a", `1`},
+			{"c", "3"},
+		},
+		{
+			{"a", `3`},
+			{"b", `5`},
+		},
+		{
+			{"a", `3`},
+			{"b", `7`},
+		},
+	}, [][]Field{
+		{
+			{"a", "1"},
+			{"x", `["1","3","abc","def"]`},
+		},
+		{
+			{"a", "3"},
+			{"x", `["3","5","7"]`},
+		},
+	})
+
+	f("stats by (a) uniq_values(*) limit 100 as x", [][]Field{
 		{
 			{"_msg", `abc`},
 			{"a", `1`},
