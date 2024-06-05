2a45d48fd60035f86cc3b98a6470b173e1514800
yincong <yincong@baidu.com>
2024-06-05 17:32:57 +0800

    unit test

---
+++ lib/promscrape/targetstatus_test.go
@@ -0,0 +1,44 @@
+package promscrape
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bloomfilter"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/prompbmarshal"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promutils"
+)
+
+const (
+	targetDropReasonTest = targetDropReason("test")
+)
+
+func TestDroppedTargetsRegister(t *testing.T) {
+	var tmpDroppedTargetsMap = &droppedTargets{
+		m:      make(map[uint64]droppedTarget),
+		filter: bloomfilter.NewFilter(4000),
+	}
+
+	randSeed := rand.New(rand.NewSource(time.Now().Unix()))
+	for i := 0; i < 10000; i++ {
+		tmpDroppedTargetsMap.Register(&promutils.Labels{
+			Labels: []prompbmarshal.Label{
+				{
+					Name:  strconv.Itoa(randSeed.Int()),
+					Value: strconv.Itoa(randSeed.Int()),
+				},
+			},
+		}, nil, targetDropReasonTest, nil)
+	}
+
+	if len(tmpDroppedTargetsMap.m) != 1000 {
+		t.Fatalf("expected 1000 targets, got %d", len(tmpDroppedTargetsMap.m))
+	}
+
+	// over 98% coverage rate
+	if tmpDroppedTargetsMap.totalTargets > 9800 {
+		t.Fatalf("%d", tmpDroppedTargetsMap.totalTargets)
+	}
+}
