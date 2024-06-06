a68c2c0f17ced863248496892b97829cdfdb11f0
Yury Molodov <yurymolodov@gmail.com>
2024-06-06 12:14:06 +0200

    vmui/logs: improve log display for group view (#6419)
    ### Describe Your Changes
    
    1) Set the default limit to `50`. 
        #6408
    2) Configure the default search to cover the `last 5 minutes` and
    include all messages (`*`).
         #6405
    3) In the header, display only streams and group by stream.
         #6406
    4) Add log processing, without the fields `msg`, `time`, and `stream`.
    5) When clicking on logs, display a list of all fields.
         #6407
    
    <img width="400" alt="image"
    src="https://github.com/VictoriaMetrics/VictoriaMetrics/assets/29711459/666dcaa3-20fb-4828-b77b-1d849dd9a8ed">
    
    ### Checklist
    
    The following checks are **mandatory**:
    
    - [ ] My change adheres [VictoriaMetrics contributing
    guidelines](https://docs.victoriametrics.com/contributing/).

--- app/vmui/packages/vmui/src/utils/time.ts
+++ app/vmui/packages/vmui/src/utils/time.ts
@@ -3,6 +3,7 @@ import dayjs, { UnitTypeShort } from "dayjs";
 import { getQueryStringValue } from "./query-string";
 import { DATE_ISO_FORMAT } from "../constants/date";
 import timezones from "../constants/timezones";
+import { AppType } from "../types/appType";
 
 const MAX_ITEMS_PER_CHART = window.innerWidth / 4;
 const MAX_ITEMS_PER_HISTOGRAM = window.innerWidth / 40;
@@ -159,10 +160,11 @@ export const dateFromSeconds = (epochTimeInSeconds: number): Date => {
 const getYesterday = () => dayjs().tz().subtract(1, "day").endOf("day").toDate();
 const getToday = () => dayjs().tz().endOf("day").toDate();
 
+const isLogsApp = process.env.REACT_APP_TYPE === AppType.logs;
 export const relativeTimeOptions: RelativeTimeOption[] = [
-  { title: "Last 5 minutes", duration: "5m" },
+  { title: "Last 5 minutes", duration: "5m", isDefault: isLogsApp },
   { title: "Last 15 minutes", duration: "15m" },
-  { title: "Last 30 minutes", duration: "30m", isDefault: true },
+  { title: "Last 30 minutes", duration: "30m", isDefault: !isLogsApp },
   { title: "Last 1 hour", duration: "1h" },
   { title: "Last 3 hours", duration: "3h" },
   { title: "Last 6 hours", duration: "6h" },
