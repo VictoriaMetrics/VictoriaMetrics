cae9a8ab0b77838e8cfa94ef5530688ac883d4c3
Federico Leva <federico.leva@relexsolutions.com>
2024-07-11 16:55:57 +0300

    vmctl: Increase backoff for retries
    Less retries but starting from a longer base and higher backoff factor
    so that any ongoing slow queries have time to finish before we abort.

--- app/vmctl/backoff/backoff.go
+++ app/vmctl/backoff/backoff.go
@@ -11,9 +11,9 @@ import (
 )
 
 const (
-	backoffRetries     = 10
-	backoffFactor      = 1.8
-	backoffMinDuration = time.Second * 2
+	backoffRetries     = 5
+	backoffFactor      = 3
+	backoffMinDuration = time.Second * 30
 )
 
 // retryableFunc describes call back which will repeat on errors
