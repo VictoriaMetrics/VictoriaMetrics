a468a6e98535b6facb0f51b96b860308cd11ee58
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-07-15 23:00:14 +0200

    lib/{httputils,netutil}: move httputils.GetStatDialFunc to netutil.NewStatDialFunc
    - Rename GetStatDialFunc to NewStatDialFunc, since it returns new function with every call
    - NewStatDialFunc isn't related to http in any way, so it must be moved from lib/httputils to lib/netutil
    - Simplify the implementation of NewStatDialFunc by removing sync.Map from there.
    - Use netutil.NewStatDialFunc at app/vmauth and lib/promscrape/discoveryutils
    - Use gauge instead of counter type for *_conns metric
    
    This is a follow-up for d7b5062917c4a6460dcfe061e870e9f9ae0ef82e
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/6299

--- app/vmauth/main.go
+++ app/vmauth/main.go
@@ -450,7 +450,7 @@ func newRoundTripper(caFileOpt, certFileOpt, keyFileOpt, serverNameOpt string, i
 	if tr.MaxIdleConns != 0 && tr.MaxIdleConns < tr.MaxIdleConnsPerHost {
 		tr.MaxIdleConns = tr.MaxIdleConnsPerHost
 	}
-	tr.DialContext = netutil.DialMaybeSRV
+	tr.DialContext = netutil.NewStatDialFunc("vmauth_backend")
 
 	rt := cfg.NewRoundTripper(tr)
 	return rt, nil
