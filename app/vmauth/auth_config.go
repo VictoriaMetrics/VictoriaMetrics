590aeccd7d769cf48a6afbdecae03fbcabd130ac
Aliaksandr Valialkin <valyala@victoriametrics.com>
2024-07-16 10:31:21 +0200

    app/vmauth: follow-up for 3a45bbb4e0f21b8d343d41bcd3cb8dd87956ac05
    - Move the test for SRV discovery into a separate function. This allows verifying round-robin discovery across SRV records.
    - Restore the original netutil.Resolver after the test finishes, so it doesn't interfere with other tests.
    - Move the description of the bugfix into the correct place at docs/CHANGELOG.md - it should be placed under v1.102.0-rc2
      instead of v1.102.0-rc1.
    - Remove unneeded code in URLPrefix.sanitizeAndInitialize(), since it is expected this function is called only once
      for finishing URLPrefix initializiation. In this case URLPrefix.nextDiscoveryDeadline and URLPrefix.n are equal to 0
      according to https://pkg.go.dev/sync/atomic#Uint64
    - Properly fix the bug at URLPrefix.discoverBackendAddrsIfNeeded() - it is expected that hostToAddrs map uses
      the original hostname keys, including 'srv+' prefix, so it shouldn't be removed when looping over up.busOriginal.
      Instead, the 'srv+' prefix must be removed from the hostname only locally before passing the hostname to netutil.Resolver.LookupSRV.
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/6401

--- app/vmauth/auth_config.go
+++ app/vmauth/auth_config.go
@@ -354,11 +354,12 @@ func (up *URLPrefix) discoverBackendAddrsIfNeeded() {
 			// ips for the given host have been already discovered
 			continue
 		}
+
 		var resolvedAddrs []string
 		if strings.HasPrefix(host, "srv+") {
 			// The host has the format 'srv+realhost'. Strip 'srv+' prefix before performing the lookup.
-			host = strings.TrimPrefix(host, "srv+")
-			_, addrs, err := netutil.Resolver.LookupSRV(ctx, "", "", host)
+			srvHost := strings.TrimPrefix(host, "srv+")
+			_, addrs, err := netutil.Resolver.LookupSRV(ctx, "", "", srvHost)
 			if err != nil {
 				logger.Warnf("cannot discover backend SRV records for %s: %s; use it literally", bu, err)
 				resolvedAddrs = []string{host}
@@ -390,7 +391,6 @@ func (up *URLPrefix) discoverBackendAddrsIfNeeded() {
 	var busNew []*backendURL
 	for _, bu := range up.busOriginal {
 		host := bu.Hostname()
-		host = strings.TrimPrefix(host, "srv+")
 		port := bu.Port()
 		for _, addr := range hostToAddrs[host] {
 			buCopy := *bu
@@ -1017,8 +1017,6 @@ func (up *URLPrefix) sanitizeAndInitialize() error {
 		}
 	}
 	up.bus.Store(&bus)
-	up.nextDiscoveryDeadline.Store(0)
-	up.n.Store(0)
 
 	return nil
 }
