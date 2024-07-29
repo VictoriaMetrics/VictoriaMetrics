e80c48c8954ac65c2256b9057887ece2c9a5b010
Artem Fetishev <wwctrsrx@gmail.com>
2024-07-29 17:48:53 +0200

    Add a benchmark that stores high- and no churn rate data with and without per day indexes
    The results of the benchmark:
    
    goos: linux
    goarch: amd64
    pkg: github.com/VictoriaMetrics/VictoriaMetrics/lib/storage
    cpu: 13th Gen Intel(R) Core(TM) i7-1355U
    BenchmarkStorageInsertVariousDataPatterns/HighChurnRate/perDayIndexes-12       3    345354701 ns/op    39.56 data-MiB     28.29 indexdb-MiB     2895574 rows/s
    BenchmarkStorageInsertVariousDataPatterns/HighChurnRate/noPerDayIndexes-12     4    280344824 ns/op    39.56 data-MiB     25.79 indexdb-MiB     3567036 rows/s
    BenchmarkStorageInsertVariousDataPatterns/NoChurnRate/perDayIndexes-12         8    129190377 ns/op    62.29 data-MiB     13.73 indexdb-MiB     7740515 rows/s
    BenchmarkStorageInsertVariousDataPatterns/NoChurnRate/noPerDayIndexes-12      12     92362808 ns/op    72.58 data-MiB    0.3553 indexdb-MiB    10826869 rows/s
    
    It can be seen that the disabling per-day indexes speeds up the data insertion
    and decrease disk usage not only for no-churh-rate data but also for
    high-churn-rate data. However the data size of no-churn-rate date is much
    higher than of high-churn-rate data and becomes even higher if per-day indexes
    are disabled. This is unexpected and is to be investigated.
    
    Signed-off-by: Artem Fetishev <wwctrsrx@gmail.com>

--- lib/storage/storage_timing_test.go
+++ lib/storage/storage_timing_test.go
@@ -2,9 +2,12 @@ package storage
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"sync/atomic"
 	"testing"
+
+	vmfs "github.com/VictoriaMetrics/VictoriaMetrics/lib/fs"
 )
 
 func BenchmarkStorageAddRows(b *testing.B) {
@@ -52,9 +55,103 @@ func benchmarkStorageAddRows(b *testing.B, rowsPerBatch int) {
 	b.StopTimer()
 }
 
-func BenchmarkStorageIndexTuning(b *testing.B) {
-	b.Skip()
+func BenchmarkStorageInsertVariousDataPatterns(b *testing.B) {
+	defer vmfs.MustRemoveAll(b.Name())
 
-	// TODO(rtm0): Implement use cases from documentation (kubernetes, weather,
-	// iot), measure disk space, ingestion and retrieval rate.
+	const (
+		numBatches      = 100
+		numRowsPerBatch = 10000
+		concurrency     = 1
+	)
+
+	highChurnRateData, _ := testGenerateMetricRowBatches(&BatchOptions{
+		numBatches:           numBatches,
+		numRowsPerBatch:      numRowsPerBatch,
+		sameBatchMetricNames: false,
+		sameRowMetricNames:   false,
+		sameBatchDates:       false,
+		sameRowDates:         true,
+	})
+
+	noChurnRateData, _ := testGenerateMetricRowBatches(&BatchOptions{
+		numBatches:           numBatches,
+		numRowsPerBatch:      numRowsPerBatch,
+		sameBatchMetricNames: true,
+		sameRowMetricNames:   false,
+		sameBatchDates:       false,
+		sameRowDates:         true,
+	})
+
+	addRows := func(b *testing.B, disablePerDayIndexes bool, batches [][]MetricRow) {
+		b.Helper()
+
+		var (
+			rowsAddedTotal int
+			dataSize       int64
+			indexSize      int64
+		)
+
+		path := b.Name()
+		s := MustOpenStorage(path, 0, 0, 0, disablePerDayIndexes)
+		defer s.MustClose()
+
+		b.ResetTimer()
+		for range b.N {
+			splitBatches := true
+			testDoConcurrently(s, func(s *Storage, mrs []MetricRow) {
+				s.AddRows(mrs, defaultPrecisionBits)
+			}, concurrency, splitBatches, batches)
+			rowsAddedTotal += numBatches * numRowsPerBatch
+		}
+		b.StopTimer()
+
+		s.DebugFlush()
+		if err := s.ForceMergePartitions(""); err != nil {
+			b.Fatalf("ForceMergePartitions() failed unexpectedly: %v", err)
+		}
+
+		dataSize = benchmarkDirSize(path + "/data")
+		indexSize = benchmarkDirSize(path + "/indexdb")
+		b.ReportMetric(float64(rowsAddedTotal)/float64(b.Elapsed().Seconds()), "rows/s")
+		b.ReportMetric(float64(dataSize)/(1024*1024), "data-MiB")
+		b.ReportMetric(float64(indexSize)/(1024*1024), "indexdb-MiB")
+	}
+
+	b.Run("HighChurnRate/perDayIndexes", func(b *testing.B) {
+		addRows(b, false, highChurnRateData)
+	})
+
+	b.Run("HighChurnRate/noPerDayIndexes", func(b *testing.B) {
+		addRows(b, true, highChurnRateData)
+	})
+
+	b.Run("NoChurnRate/perDayIndexes", func(b *testing.B) {
+		addRows(b, false, noChurnRateData)
+	})
+
+	b.Run("NoChurnRate/noPerDayIndexes", func(b *testing.B) {
+		addRows(b, true, noChurnRateData)
+	})
+}
+
+// benchmarkDirSize calculates the size of a directory.
+func benchmarkDirSize(path string) int64 {
+	var size int64
+	err := fs.WalkDir(os.DirFS(path), ".", func(_ string, d fs.DirEntry, err error) error {
+		if err != nil {
+			panic(err)
+		}
+		if !d.IsDir() {
+			info, err := d.Info()
+			if err != nil {
+				panic(err)
+			}
+			size += info.Size()
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return size
 }
