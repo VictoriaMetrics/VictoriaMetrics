056d1a8192b3b9d3b30ec0dcc25934e02ca14558
hagen1778 <roman@victoriametrics.com>
2024-08-05 14:26:10 +0200

    lib/bytesutil: smooth buffer growth rate
    Before, buffer growth was always x2 of its size, which could lead to excessive
    memory usage when processing big amount of data.
    For example, scraping a target with hundreds of MBs in response could result into
    hih memory spikes in vmagent because buffer has to double its size to fit the response.
    
    The change smoothes out the growth rate, trading higher allocation rate for lower mem usage
    at certain conditions.
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>

--- lib/bytesutil/bytebuffer.go
+++ lib/bytesutil/bytebuffer.go
@@ -65,7 +65,8 @@ func (bb *ByteBuffer) ReadFrom(r io.Reader) (int64, error) {
 	offset := bLen
 	for {
 		if free := len(b) - offset; free < offset {
-			b = slicesutil.SetLength(b, 2*len(b))
+			n := 1.3 * float64(len(b))
+			b = slicesutil.SetLength(b, int(n))
 		}
 		n, err := r.Read(b[offset:])
 		offset += n
