95bafc8caf5b206ecbe1adcb97d6571e5dbb41a9
dufucun <dufuchun@sohu.com>
2024-08-30 16:55:25 +0800

    tests: fix slice init length (#6897)
    ### Describe Your Changes
    
    fix slice init length
    
    ### Checklist
    
    The following checks are **mandatory**:
    
    - [ ] My change adheres [VictoriaMetrics contributing
    guidelines](https://docs.victoriametrics.com/contributing/).
    
    Signed-off-by: dufucun <dufuchun@sohu.com>

--- app/vmalert/rule/group_timing_test.go
+++ app/vmalert/rule/group_timing_test.go
@@ -11,7 +11,7 @@ import (
 func BenchmarkGetStaleSeries(b *testing.B) {
 	ts := time.Now()
 	n := 100
-	payload := make([]prompbmarshal.TimeSeries, n)
+	payload := make([]prompbmarshal.TimeSeries, 0, n)
 	for i := 0; i < n; i++ {
 		s := fmt.Sprintf("%d", i)
 		labels := toPromLabels(b,
