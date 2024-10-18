423df09d7d8985b651e1b763abb627fc342c9176
Yury Molodov <yurymolodov@gmail.com>
2024-10-18 02:30:56 +0200

    vmui/logs: add ability to hide hits chart (#7206)
    ### Describe Your Changes
    
    **Added ability to hide the hits chart**
    
    - Users can now hide or show the hits chart by clicking the "eye" icon
    located in the upper-right corner of the chart.
    - When the chart is hidden, it will stop sending requests to
    `/select/logsql/hits`.
    - Upon displaying the chart again, it will automatically refresh. If a
    relative time range is set, the chart will update according to the time
    period of the logs currently being displayed.
    
    **Hits chart visible:**
    
    ![image](https://github.com/user-attachments/assets/577e877b-6417-4b83-8d84-c55e3d39864a)
    
    **Hits chart hidden:**
    
    ![image](https://github.com/user-attachments/assets/068b1143-d140-4d72-8d65-663900124f32)
    
    Related issue: #7117
    
    ### Checklist
    
    The following checks are **mandatory**:
    
    - [ ] My change adheres [VictoriaMetrics contributing
    guidelines](https://docs.victoriametrics.com/contributing/).
    
    Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

--- app/vmui/packages/vmui/src/pages/ExploreLogs/ExploreLogs.tsx
+++ app/vmui/packages/vmui/src/pages/ExploreLogs/ExploreLogs.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useState } from "preact/compat";
+import React, { FC, useCallback, useEffect, useMemo, useState } from "preact/compat";
 import ExploreLogsBody from "./ExploreLogsBody/ExploreLogsBody";
 import useStateSearchParams from "../../hooks/useStateSearchParams";
 import useSearchParamsFromObject from "../../hooks/useSearchParamsFromObject";
@@ -14,6 +14,7 @@ import ExploreLogsBarChart from "./ExploreLogsBarChart/ExploreLogsBarChart";
 import { useFetchLogHits } from "./hooks/useFetchLogHits";
 import { LOGS_ENTRIES_LIMIT } from "../../constants/logs";
 import { getTimeperiodForDuration, relativeTimeOptions } from "../../utils/time";
+import { useSearchParams } from "react-router-dom";
 
 const storageLimit = Number(getFromStorage("LOGS_LIMIT"));
 const defaultLimit = isNaN(storageLimit) ? LOGS_ENTRIES_LIMIT : storageLimit;
@@ -22,6 +23,8 @@ const ExploreLogs: FC = () => {
   const { serverUrl } = useAppState();
   const { duration, relativeTime, period: periodState } = useTimeState();
   const { setSearchParamsFromKeys } = useSearchParamsFromObject();
+  const [searchParams] = useSearchParams();
+  const hideChart = useMemo(() => searchParams.get("hide_chart"), [searchParams]);
 
   const [limit, setLimit] = useStateSearchParams(defaultLimit, "limit");
   const [query, setQuery] = useStateSearchParams("*", "query");
@@ -49,7 +52,7 @@ const ExploreLogs: FC = () => {
     const newPeriod = getPeriod();
     setPeriod(newPeriod);
     fetchLogs(newPeriod).then((isSuccess) => {
-      isSuccess && fetchLogHits(newPeriod);
+      isSuccess && !hideChart && fetchLogHits(newPeriod);
     }).catch(e => e);
     setSearchParamsFromKeys( {
       query,
@@ -87,6 +90,10 @@ const ExploreLogs: FC = () => {
     handleRunQuery();
     setTmpQuery(query);
   }, [query]);
+
+  useEffect(() => {
+    !hideChart && fetchLogHits(period);
+  }, [hideChart]);
 
   return (
     <div className="vm-explore-logs">
