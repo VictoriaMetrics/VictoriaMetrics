10c42668a1455a0adaa1172ea12179d975fbb7cb
Yury Molodov <yurymolodov@gmail.com>
2024-11-27 13:49:06 +0100

    vmui/logs: optimize memory consumption (#7524)
    ### Describe Your Changes
    
    - **Memory Optimization**: Reduced memory consumption on the "Group" and
    "JSON" tabs by approximately 30%.
    
    - **Table Pagination**: Added pagination to the "Table" view with an
    option to select the number of rows displayed (from 10 to 1000 items per
    page, with a default of 1000). This change significantly reduced memory
    usage by approximately 75%.
    
    Related to #7185
    
    ### Checklist
    
    The following checks are **mandatory**:
    
    - [ ] My change adheres [VictoriaMetrics contributing
    guidelines](https://docs.victoriametrics.com/contributing/).
    
    ---------
    
    Co-authored-by: Roman Khavronenko <roman@victoriametrics.com>

--- app/vmui/packages/vmui/src/pages/ExploreLogs/ExploreLogsBody/TableLogs.tsx
+++ app/vmui/packages/vmui/src/pages/ExploreLogs/ExploreLogsBody/TableLogs.tsx
@@ -1,58 +1,94 @@
-import React, { FC, useMemo } from "preact/compat";
+import React, { FC, useMemo, useRef, useState } from "preact/compat";
 import "./style.scss";
 import Table from "../../../components/Table/Table";
 import { Logs } from "../../../api/types";
+import Pagination from "../../../components/Main/Pagination/Pagination";
+import { useEffect } from "react";
 
 interface TableLogsProps {
   logs: Logs[];
   displayColumns: string[];
   tableCompact: boolean;
   columns: string[];
+  rowsPerPage: number;
 }
 
-const TableLogs: FC<TableLogsProps> = ({ logs, displayColumns, tableCompact, columns }) => {
-  const getColumnClass = (key: string) => {
-    switch (key) {
-      case "_time":
-        return "vm-table-cell_logs-time";
-      case "_vmui_data":
-        return "vm-table-cell_logs vm-table-cell_pre";
-      default:
-        return "vm-table-cell_logs";
-    }
-  };
+const getColumnClass = (key: string) => {
+  switch (key) {
+    case "_time":
+      return "vm-table-cell_logs-time";
+    default:
+      return "vm-table-cell_logs";
+  }
+};
+
+const compactColumns = [{
+  key: "_vmui_data",
+  title: "Data",
+  className: "vm-table-cell_logs vm-table-cell_pre"
+}];
+
+const TableLogs: FC<TableLogsProps> = ({ logs, displayColumns, tableCompact, columns, rowsPerPage }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState(1);
+
+  const rows = useMemo(() => {
+    return logs.map((log) => {
+      const _vmui_data = JSON.stringify(log, null, 2);
+      return { ...log, _vmui_data };
+    }) as Logs[];
+  }, [logs]);
 
   const tableColumns = useMemo(() => {
-    if (tableCompact) {
-      return [{
-        key: "_vmui_data",
-        title: "Data",
-        className: getColumnClass("_vmui_data")
-      }];
-    }
     return columns.map((key) => ({
       key: key as keyof Logs,
       title: key,
       className: getColumnClass(key),
     }));
-  }, [tableCompact, columns]);
+  }, [columns]);
 
 
   const filteredColumns = useMemo(() => {
-    if (tableCompact) return tableColumns;
+    if (tableCompact) return compactColumns;
     if (!displayColumns?.length) return [];
     return tableColumns.filter(c => displayColumns.includes(c.key as string));
   }, [tableColumns, displayColumns, tableCompact]);
 
+  const paginationOffset = useMemo(() => {
+    const startIndex = (page - 1) * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+    return { startIndex, endIndex };
+  }, [page, rowsPerPage]);
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+    if (containerRef.current) {
+      const y = containerRef.current.getBoundingClientRect().top + window.scrollY - 50;
+      window.scrollTo({ top: y });
+    }
+  };
+
+  useEffect(() => {
+    setPage(1);
+  }, [logs, rowsPerPage]);
+
   return (
     <>
-      <Table
-        rows={logs}
-        columns={filteredColumns}
-        defaultOrderBy={"_time"}
-        defaultOrderDir={"desc"}
-        copyToClipboard={"_vmui_data"}
-        paginationOffset={{ startIndex: 0, endIndex: Infinity }}
+      <div ref={containerRef}>
+        <Table
+          rows={rows}
+          columns={filteredColumns}
+          defaultOrderBy={"_time"}
+          defaultOrderDir={"desc"}
+          copyToClipboard={"_vmui_data"}
+          paginationOffset={paginationOffset}
+        />
+      </div>
+      <Pagination
+        currentPage={page}
+        totalItems={rows.length}
+        itemsPerPage={rowsPerPage}
+        onPageChange={handlePageChange}
       />
     </>
   );
