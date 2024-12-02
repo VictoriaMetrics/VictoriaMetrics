6a4e4728a651e72d112e2cd04fdb92399eed6741
Ted Possible <tpossible@victoriametrics.com>
2024-12-02 11:55:00 -0800

    Updated integration tests to check for ratelimiting metric being set and not set correctly

--- apptest/tests/maxingestionrate_test.go
+++ apptest/tests/maxingestionrate_test.go
@@ -5,9 +5,29 @@ import (
 	"testing"
 )
 
-func TestSingleMaxIngestionRate(t *testing.T) {
+func TestSingleMaxIngestionRateIncrementsMetric(t *testing.T) {
 	tc := apptest.NewTestCase(t)
 	defer tc.Stop()
-	sut := tc.MustStartVmsingle("vmsingle", []string{"-maxIngestionRate=10000"})
-	print(sut)
+	sut := tc.MustStartVmsingle("vmsingle", []string{"-maxIngestionRate=5"})
+	sut.PrometheusAPIV1ImportPrometheus(t, docData, apptest.QueryOpts{})
+	max_ingestion_rate := sut.GetMetric(t, "vm_max_ingestion_rate_limit_reached_total")
+	if max_ingestion_rate <= 0 {
+		t.Errorf("Max Ingestion Rate metric not set unexpectedly")
+	} else {
+		t.Logf("MAX INGEST RATE HIT %d times", int(max_ingestion_rate))
+	}
+	sut.ForceFlush(t)
+}
+func TestSingleMaxIngestionRateDoesNotIncrementsMetric(t *testing.T) {
+	tc := apptest.NewTestCase(t)
+	defer tc.Stop()
+	sut := tc.MustStartVmsingle("vmsingle", []string{"-maxIngestionRate=15"})
+	sut.PrometheusAPIV1ImportPrometheus(t, docData, apptest.QueryOpts{})
+	max_ingestion_rate := sut.GetMetric(t, "vm_max_ingestion_rate_limit_reached_total")
+	if max_ingestion_rate > 0 {
+		t.Errorf("Max Ingestion Rate set")
+	} else {
+		t.Logf("MAX INGEST RATE NOT SET as expected")
+	}
+	sut.ForceFlush(t)
 }
