1ebdc6d0b4fe0d5667b03805da4b52e3aea0e34a
Jiekun <jiekun@victoriametrics.com>
2024-12-16 16:34:16 +0800

    fix: [scrape timestamp] Use now() instead of <-ticker.C as the actual scrape timestamp

--- lib/promscrape/scrapework.go
+++ lib/promscrape/scrapework.go
@@ -344,8 +344,8 @@ func (sw *scrapeWork) run(stopCh <-chan struct{}, globalStopCh <-chan struct{}) 
 				sw.seriesLimiter = nil
 			}
 			return
-		case tt := <-ticker.C:
-			t := tt.UnixNano() / 1e6
+		case <-ticker.C:
+			t := time.Now().UnixNano() / 1e6
 			if d := math.Abs(float64(t - timestamp)); d > 0 && d/float64(scrapeInterval.Milliseconds()) > 0.1 {
 				// Too big jitter. Adjust timestamp
 				timestamp = t
