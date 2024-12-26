VictoriaMetrics/VictoriaMetrics

commit 71bb9fc0d0f9f99e4d34cbd1a9839028f0d605b8

app/vminsert: properly apply relabeling at ingestion
Regression was introduced at 564e6ea024112fdfc65121ae889977aee251f668
after implementing:
https://github.com/VictoriaMetrics/VictoriaMetrics/issues/6928

ctx.Labels array could be incorrectly updated and changes to it after
relabeling rules can be lost.
E.g. ctx.Labels passed to WriteDataPoint function as slice copy, but
results of relabeling only changed an actual slice at ctx.Labels.

This commit replaces implicit relabeling call with explicit
`TryPrepareLabels` function.
It also reduces code diffs with cluster version and adds integration tests

 related issue:
https://github.com/VictoriaMetrics/VictoriaMetrics/issues/7865

---------

Signed-off-by: f41gh7 <nik@victoriametrics.com>
Co-authored-by: Roman Khavronenko <roman@victoriametrics.com>

app/vminsert/common/insert_ctx.go
@@ -73,24 +73,15 @@ func (ctx *InsertCtx) TryPrepareLabels(hasRelabeling bool) bool {
 	if timeserieslimits.Enabled() && timeserieslimits.IsExceeding(ctx.Labels) {
 		return false
 	}
-	ctx.SortLabelsIfNeeded()
+	ctx.sortLabelsIfNeeded()
 
 	return true
 }
 
 // WriteDataPoint writes (timestamp, value) with the given prefix and labels into ctx buffer.
-func (ctx *InsertCtx) WriteDataPoint(prefix []byte, hasRelabeling bool, labels []prompbmarshal.Label, timestamp int64, value float64) error {
-	if !ctx.TryPrepareLabels(hasRelabeling) {
-		return nil
-	}
-	metricNameRaw := ctx.marshalMetricNameRaw(prefix, labels)
-	return ctx.addRow(metricNameRaw, timestamp, value)
-}
-
-// WriteDataPointUnchecked writes (timestamp, value) with the given prefix and labels into ctx buffer.
 //
 // caller should invoke TryPrepareLabels before using this function if needed
-func (ctx *InsertCtx) WriteDataPointUnchecked(prefix []byte, labels []prompbmarshal.Label, timestamp int64, value float64) error {
+func (ctx *InsertCtx) WriteDataPoint(prefix []byte, labels []prompbmarshal.Label, timestamp int64, value float64) error {
 	metricNameRaw := ctx.marshalMetricNameRaw(prefix, labels)
 	return ctx.addRow(metricNameRaw, timestamp, value)
 }

app/vminsert/common/sort_labels.go
@@ -12,8 +12,8 @@ var sortLabels = flag.Bool("sortLabels", false, `Whether to sort labels for inco
 	`For example, if m{k1="v1",k2="v2"} may be sent as m{k2="v2",k1="v1"}. `+
 	`Enabled sorting for labels can slow down ingestion performance a bit`)
 
-// SortLabelsIfNeeded sorts labels if -sortLabels command-line flag is set
-func (ctx *InsertCtx) SortLabelsIfNeeded() {
+// sortLabelsIfNeeded sorts labels if -sortLabels command-line flag is set
+func (ctx *InsertCtx) sortLabelsIfNeeded() {
 	if *sortLabels {
 		sort.Sort(&ctx.Labels)
 	}

app/vminsert/influx/request_handler.go
@@ -14,6 +14,7 @@ import (
 	parser "github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/influx"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/influx/stream"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/storage"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/timeserieslimits"
 	"github.com/VictoriaMetrics/metrics"
 )
 
@@ -69,6 +70,7 @@ func insertRows(db string, rows []parser.Row, extraLabels []prompbmarshal.Label)
 	ic.Reset(rowsLen)
 	rowsTotal := 0
 	hasRelabeling := relabel.HasRelabeling()
+	hasLimitsEnabled := timeserieslimits.Enabled()
 	for i := range rows {
 		r := &rows[i]
 		rowsTotal += len(r.Fields)
@@ -108,12 +110,17 @@ func insertRows(db string, rows []parser.Row, extraLabels []prompbmarshal.Label)
 				metricGroup := bytesutil.ToUnsafeString(ctx.metricGroupBuf)
 				ic.Labels = append(ic.Labels[:0], ctx.originLabels...)
 				ic.AddLabel("", metricGroup)
-				if err := ic.WriteDataPoint(nil, true, ic.Labels, r.Timestamp, f.Value); err != nil {
+				if !ic.TryPrepareLabels(true) {
+					continue
+				}
+				if err := ic.WriteDataPoint(nil, ic.Labels, r.Timestamp, f.Value); err != nil {
 					return err
 				}
 			}
 		} else {
-			ic.SortLabelsIfNeeded()
+			if !ic.TryPrepareLabels(false) {
+				continue
+			}
 			ctx.metricNameBuf = storage.MarshalMetricNameRaw(ctx.metricNameBuf[:0], ic.Labels)
 			labelsLen := len(ic.Labels)
 			for j := range r.Fields {
@@ -124,7 +131,14 @@ func insertRows(db string, rows []parser.Row, extraLabels []prompbmarshal.Label)
 				metricGroup := bytesutil.ToUnsafeString(ctx.metricGroupBuf)
 				ic.Labels = ic.Labels[:labelsLen]
 				ic.AddLabel("", metricGroup)
-				if err := ic.WriteDataPoint(ctx.metricNameBuf, false, ic.Labels[len(ic.Labels)-1:], r.Timestamp, f.Value); err != nil {
+				if hasLimitsEnabled {
+					// special case for optimisation above
+					// check only __name__ label value limits
+					if timeserieslimits.IsExceeding(ic.Labels[len(ic.Labels)-1:]) {
+						continue
+					}
+				}
+				if err := ic.WriteDataPoint(ctx.metricNameBuf, ic.Labels[len(ic.Labels)-1:], r.Timestamp, f.Value); err != nil {
 					return err
 				}
 			}

app/vminsert/csvimport/request_handler.go
@@ -46,7 +46,10 @@ func insertRows(rows []parser.Row, extraLabels []prompbmarshal.Label) error {
 			label := &extraLabels[j]
 			ctx.AddLabel(label.Name, label.Value)
 		}
-		if err := ctx.WriteDataPoint(nil, hasRelabeling, ctx.Labels, r.Timestamp, r.Value); err != nil {
+		if !ctx.TryPrepareLabels(hasRelabeling) {
+			continue
+		}
+		if err := ctx.WriteDataPoint(nil, ctx.Labels, r.Timestamp, r.Value); err != nil {
 			return err
 		}
 	}

app/vminsert/graphite/request_handler.go
@@ -36,7 +36,10 @@ func insertRows(rows []parser.Row) error {
 			tag := &r.Tags[j]
 			ctx.AddLabel(tag.Key, tag.Value)
 		}
-		if err := ctx.WriteDataPoint(nil, hasRelabeling, ctx.Labels, r.Timestamp, r.Value); err != nil {
+		if !ctx.TryPrepareLabels(hasRelabeling) {
+			continue
+		}
+		if err := ctx.WriteDataPoint(nil, ctx.Labels, r.Timestamp, r.Value); err != nil {
 			return err
 		}
 	}

app/vminsert/native/request_handler.go
@@ -68,7 +68,7 @@ func insertRows(block *stream.Block, extraLabels []prompbmarshal.Label) error {
 		timestamp := timestamps[j]
 		// TODO: @f41gh7 looks like it's better to use WriteDataPointExt
 		// since metricName never changes inside insertRows call
-		if err := ic.WriteDataPointUnchecked(ctx.metricNameBuf, ic.Labels, timestamp, value); err != nil {
+		if err := ic.WriteDataPoint(ctx.metricNameBuf, ic.Labels, timestamp, value); err != nil {
 			return err
 		}
 	}

app/vminsert/newrelic/request_handler.go
@@ -58,7 +58,10 @@ func insertRows(rows []newrelic.Row, extraLabels []prompbmarshal.Label) error {
 				label := &extraLabels[k]
 				ctx.AddLabel(label.Name, label.Value)
 			}
-			if err := ctx.WriteDataPoint(nil, hasRelabeling, ctx.Labels, r.Timestamp, s.Value); err != nil {
+			if !ctx.TryPrepareLabels(hasRelabeling) {
+				continue
+			}
+			if err := ctx.WriteDataPoint(nil, ctx.Labels, r.Timestamp, s.Value); err != nil {
 				return err
 			}
 		}

app/vminsert/opentsdb/request_handler.go
@@ -36,7 +36,10 @@ func insertRows(rows []parser.Row) error {
 			tag := &r.Tags[j]
 			ctx.AddLabel(tag.Key, tag.Value)
 		}
-		if err := ctx.WriteDataPoint(nil, hasRelabeling, ctx.Labels, r.Timestamp, r.Value); err != nil {
+		if !ctx.TryPrepareLabels(hasRelabeling) {
+			continue
+		}
+		if err := ctx.WriteDataPoint(nil, ctx.Labels, r.Timestamp, r.Value); err != nil {
 			return err
 		}
 	}

commit fc336bbf203ed5fddf1902aebb62961527e4b928

app/vminsert: properly ingest influx metrics
Commit 71bb9fc0d0f9f99e4d34cbd1a9839028f0d605b8 introduced a regression.
If labels are empty and relabeling is not configured, influx ingestion hanlder
performed an earlier exit due to TryPrepareLabels call.
 Due micro-optimisations for this procotol, this check was not valid.
Since it didn't take in account metircName, which added later and skip metrics line.

 This commit removes `TryPrepareLabel` function call from this path and inline it instead.
It properly track empty labels path.

 Adds initial tests implementation for data ingestion protocols.

 Related issue:
https://github.com/VictoriaMetrics/VictoriaMetrics/issues/7933

Signed-off-by: f41gh7 <nik@victoriametrics.com>

app/vminsert/common/insert_ctx.go
@@ -105,7 +105,7 @@ func (ctx *InsertCtx) TryPrepareLabels(hasRelabeling bool) bool {
 	if timeserieslimits.Enabled() && timeserieslimits.IsExceeding(ctx.Labels) {
 		return false
 	}
-	ctx.sortLabelsIfNeeded()
+	ctx.SortLabelsIfNeeded()
 
 	return true
 }

app/vminsert/common/sort_labels.go
@@ -12,8 +12,8 @@ var sortLabels = flag.Bool("sortLabels", false, `Whether to sort labels for inco
 	`For example, if m{k1="v1",k2="v2"} may be sent as m{k2="v2",k1="v1"}. `+
 	`Enabled sorting for labels can slow down ingestion performance a bit`)
 
-// sortLabelsIfNeeded sorts labels if -sortLabels command-line flag is set
-func (ctx *InsertCtx) sortLabelsIfNeeded() {
+// SortLabelsIfNeeded sorts labels if -sortLabels command-line flag is set
+func (ctx *InsertCtx) SortLabelsIfNeeded() {
 	if *sortLabels {
 		sort.Sort(&ctx.Labels)
 	}

app/vminsert/influx/request_handler.go
@@ -118,8 +118,14 @@ func insertRows(db string, rows []parser.Row, extraLabels []prompbmarshal.Label)
 				}
 			}
 		} else {
-			if !ic.TryPrepareLabels(false) {
-				continue
+			// special case for optimisations below
+			// do not call TryPrepareLabels
+			// manually apply sort and limits on demand
+			ic.SortLabelsIfNeeded()
+			if hasLimitsEnabled {
+				if timeserieslimits.IsExceeding(ic.Labels) {
+					continue
+				}
 			}
 			ctx.metricNameBuf = storage.MarshalMetricNameRaw(ctx.metricNameBuf[:0], ic.Labels)
 			labelsLen := len(ic.Labels)
@@ -132,8 +138,6 @@ func insertRows(db string, rows []parser.Row, extraLabels []prompbmarshal.Label)
 				ic.Labels = ic.Labels[:labelsLen]
 				ic.AddLabel("", metricGroup)
 				if hasLimitsEnabled {
-					// special case for optimisation above
-					// check only __name__ label value limits
 					if timeserieslimits.IsExceeding(ic.Labels[len(ic.Labels)-1:]) {
 						continue
 					}

apptest/model.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/url"
 	"slices"
+	"sort"
 	"strconv"
 	"strings"
 	"testing"
@@ -101,6 +102,25 @@ func NewPrometheusAPIV1QueryResponse(t *testing.T, s string) *PrometheusAPIV1Que
 		t.Fatalf("could not unmarshal query response data=\n%s\n: %v", string(s), err)
 	}
 	return res
+}
+
+// Sort performs data.Result sort by metric labels
+func (pqr *PrometheusAPIV1QueryResponse) Sort() {
+	sort.Slice(pqr.Data.Result, func(i, j int) bool {
+		leftS := make([]string, 0, len(pqr.Data.Result[i].Metric))
+		rightS := make([]string, 0, len(pqr.Data.Result[j].Metric))
+		for k, v := range pqr.Data.Result[i].Metric {
+			leftS = append(leftS, fmt.Sprintf("%s=%s", k, v))
+		}
+		for k, v := range pqr.Data.Result[j].Metric {
+			rightS = append(rightS, fmt.Sprintf("%s=%s", k, v))
+
+		}
+		sort.Strings(leftS)
+		sort.Strings(rightS)
+		return strings.Join(leftS, ",") < strings.Join(rightS, ",")
+	})
+
 }
 
 // QueryData holds the query result along with its type.

apptest/vminsert.go
@@ -78,6 +78,19 @@ func (app *Vminsert) ClusternativeListenAddr() string {
 	return app.clusternativeListenAddr
 }
 
+// InfluxWrite is a test helper function that inserts a
+// collection of records in Influx line format by sending a HTTP
+// POST request to /influx/write vmsingle endpoint.
+//
+// See https://docs.victoriametrics.com/url-examples/#influxwrite
+func (app *Vminsert) InfluxWrite(t *testing.T, records []string, opts QueryOpts) {
+	t.Helper()
+
+	url := fmt.Sprintf("http://%s/insert/%s/influx/write", app.httpListenAddr, opts.getTenant())
+	data := []byte(strings.Join(records, "\n"))
+	app.cli.Post(t, url, "text/plain", data, http.StatusNoContent)
+}
+
 // PrometheusAPIV1Write is a test helper function that inserts a
 // collection of records in Prometheus remote-write format by sending a HTTP
 // POST request to /prometheus/api/v1/write vminsert endpoint.

apptest/tests/ingestprotocols_test.go
@@ -1,0 +1,318 @@
+package tests
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	at "github.com/VictoriaMetrics/VictoriaMetrics/apptest"
+	pb "github.com/VictoriaMetrics/VictoriaMetrics/lib/prompbmarshal"
+)
+
+func TestSingleIngestionProtocols(t *testing.T) {
+	os.RemoveAll(t.Name())
+	tc := at.NewTestCase(t)
+	defer tc.Stop()
+	sut := tc.MustStartDefaultVmsingle()
+	type opts struct {
+		query       string
+		wantMetrics []map[string]string
+		wantSamples []*at.Sample
+	}
+	f := func(sut at.PrometheusQuerier, opts *opts) {
+		t.Helper()
+		wantResult := []*at.QueryResult{}
+		for idx, wm := range opts.wantMetrics {
+			wantResult = append(wantResult, &at.QueryResult{
+				Metric:  wm,
+				Samples: []*at.Sample{opts.wantSamples[idx]},
+			})
+
+		}
+		tc.Assert(&at.AssertOptions{
+			Msg: "unexpected /export query response",
+			Got: func() any {
+				got := sut.PrometheusAPIV1Export(t, opts.query, at.QueryOpts{
+					Start: "2024-02-05T08:50:00.700Z",
+					End:   "2024-02-05T09:00:00.700Z",
+				})
+				got.Sort()
+				return got
+			},
+			Want: &at.PrometheusAPIV1QueryResponse{Data: &at.QueryData{Result: wantResult}},
+			CmpOpts: []cmp.Option{
+				cmpopts.IgnoreFields(at.PrometheusAPIV1QueryResponse{}, "Status", "Data.ResultType"),
+			},
+		})
+	}
+
+	// influx line format
+	sut.InfluxWrite(t, []string{
+		`influxline series1=10 1707123456700`,                                        // 2024-02-05T08:57:36.700Z
+		`influxline,label=foo1,label1=value1,label2=value2 series2=40 1707123456800`, // 2024-02-05T08:57:36.800Z
+	}, at.QueryOpts{})
+	sut.ForceFlush(t)
+	f(sut, &opts{
+		query: `{__name__=~"influxline.+"}`,
+		wantMetrics: []map[string]string{
+			{
+				"__name__": "influxline_series1",
+			},
+			{
+				"__name__": "influxline_series2",
+				"label":    "foo1",
+				"label1":   "value1",
+				"label2":   "value2",
+			},
+		},
+		wantSamples: []*at.Sample{
+			{Timestamp: 1707123456700, Value: 10},
+			{Timestamp: 1707123456800, Value: 40},
+		},
+	})
+
+	// prometheus text exposition format
+	sut.PrometheusAPIV1ImportPrometheus(t, []string{
+		`importprometheus_series 10 1707123456700`,                               // 2024-02-05T08:57:36.700Z
+		`importprometheus_series2{label="foo",label1="value1"} 20 1707123456800`, // 2024-02-05T08:57:36.800Z
+	}, at.QueryOpts{})
+	sut.ForceFlush(t)
+	f(sut, &opts{
+		query: `{__name__=~"importprometheus.+"}`,
+		wantMetrics: []map[string]string{
+			{
+				"__name__": "importprometheus_series",
+			},
+			{
+				"__name__": "importprometheus_series2",
+				"label":    "foo",
+				"label1":   "value1",
+			},
+		},
+		wantSamples: []*at.Sample{
+			{Timestamp: 1707123456700, Value: 10},
+			{Timestamp: 1707123456800, Value: 20},
+		},
+	})
+
+	// prometheus remote write format
+	pbData := []pb.TimeSeries{
+		{
+			Labels: []pb.Label{
+				{
+					Name:  "__name__",
+					Value: "prometheusrw_series",
+				},
+			},
+			Samples: []pb.Sample{
+				{
+					Value:     10,
+					Timestamp: 1707123456700, // 2024-02-05T08:57:36.700Z
+
+				},
+			},
+		},
+		{
+			Labels: []pb.Label{
+				{
+					Name:  "__name__",
+					Value: "prometheusrw_series2",
+				},
+				{
+					Name:  "label",
+					Value: "foo2",
+				},
+				{
+					Name:  "label1",
+					Value: "value1",
+				},
+			},
+			Samples: []pb.Sample{
+				{
+					Value:     20,
+					Timestamp: 1707123456800, // 2024-02-05T08:57:36.800Z
+				},
+			},
+		},
+	}
+	sut.PrometheusAPIV1Write(t, pbData, at.QueryOpts{})
+	sut.ForceFlush(t)
+	f(sut, &opts{
+		query: `{__name__=~"prometheusrw.+"}`,
+		wantMetrics: []map[string]string{
+			{
+				"__name__": "prometheusrw_series",
+			},
+			{
+				"__name__": "prometheusrw_series2",
+				"label":    "foo2",
+				"label1":   "value1",
+			},
+		},
+		wantSamples: []*at.Sample{
+			{Timestamp: 1707123456700, Value: 10}, // 2024-02-05T08:57:36.700Z
+			{Timestamp: 1707123456800, Value: 20}, // 2024-02-05T08:57:36.700Z
+		},
+	})
+
+}
+
+func TestClusterIngestionProtocols(t *testing.T) {
+	os.RemoveAll(t.Name())
+	tc := at.NewTestCase(t)
+	defer tc.Stop()
+	vmstorage := tc.MustStartVmstorage("vmstorage", []string{
+		"-storageDataPath=" + tc.Dir() + "/vmstorage",
+		"-retentionPeriod=100y",
+	})
+	vminsert := tc.MustStartVminsert("vminsert", []string{
+		"-storageNode=" + vmstorage.VminsertAddr(),
+	})
+	vmselect := tc.MustStartVmselect("vmselect", []string{
+		"-storageNode=" + vmstorage.VmselectAddr(),
+	})
+
+	type opts struct {
+		query       string
+		wantMetrics []map[string]string
+		wantSamples []*at.Sample
+	}
+	f := func(opts *opts) {
+		t.Helper()
+		wantResult := []*at.QueryResult{}
+		for idx, wm := range opts.wantMetrics {
+			wantResult = append(wantResult, &at.QueryResult{
+				Metric:  wm,
+				Samples: []*at.Sample{opts.wantSamples[idx]},
+			})
+
+		}
+		tc.Assert(&at.AssertOptions{
+			Msg: "unexpected /export query response",
+			Got: func() any {
+				got := vmselect.PrometheusAPIV1Export(t, opts.query, at.QueryOpts{
+					Start: "2024-02-05T08:50:00.700Z",
+					End:   "2024-02-05T09:00:00.700Z",
+				})
+				got.Sort()
+				return got
+			},
+			Want: &at.PrometheusAPIV1QueryResponse{Data: &at.QueryData{Result: wantResult}},
+			CmpOpts: []cmp.Option{
+				cmpopts.IgnoreFields(at.PrometheusAPIV1QueryResponse{}, "Status", "Data.ResultType"),
+			},
+		})
+	}
+
+	// prometheus text exposition format
+	vminsert.PrometheusAPIV1ImportPrometheus(t, []string{
+		`importprometheus_series 10 1707123456700`,                               // 2024-02-05T08:57:36.700Z
+		`importprometheus_series2{label="foo",label1="value1"} 20 1707123456800`, // 2024-02-05T08:57:36.800Z
+	}, at.QueryOpts{})
+	vmstorage.ForceFlush(t)
+	f(&opts{
+		query: `{__name__=~"importprometheus.+"}`,
+		wantMetrics: []map[string]string{
+			{
+				"__name__": "importprometheus_series",
+			},
+			{
+				"__name__": "importprometheus_series2",
+				"label":    "foo",
+				"label1":   "value1",
+			},
+		},
+		wantSamples: []*at.Sample{
+			{Timestamp: 1707123456700, Value: 10},
+			{Timestamp: 1707123456800, Value: 20},
+		},
+	})
+
+	// influx line format
+	vminsert.InfluxWrite(t, []string{
+		`influxline series1=10 1707123456700`,                                        // 2024-02-05T08:57:36.700Z
+		`influxline,label=foo1,label1=value1,label2=value2 series2=40 1707123456800`, // 2024-02-05T08:57:36.800Z
+	}, at.QueryOpts{})
+	vmstorage.ForceFlush(t)
+	f(&opts{
+		query: `{__name__=~"influxline.+"}`,
+		wantMetrics: []map[string]string{
+			{
+				"__name__": "influxline_series1",
+			},
+			{
+				"__name__": "influxline_series2",
+				"label":    "foo1",
+				"label1":   "value1",
+				"label2":   "value2",
+			},
+		},
+		wantSamples: []*at.Sample{
+			{Timestamp: 1707123456700, Value: 10},
+			{Timestamp: 1707123456800, Value: 40},
+		},
+	})
+
+	// prometheus remote write format
+	pbData := []pb.TimeSeries{
+		{
+			Labels: []pb.Label{
+				{
+					Name:  "__name__",
+					Value: "prometheusrw_series",
+				},
+			},
+			Samples: []pb.Sample{
+				{
+					Value:     10,
+					Timestamp: 1707123456700, // 2024-02-05T08:57:36.700Z
+
+				},
+			},
+		},
+		{
+			Labels: []pb.Label{
+				{
+					Name:  "__name__",
+					Value: "prometheusrw_series2",
+				},
+				{
+					Name:  "label",
+					Value: "foo2",
+				},
+				{
+					Name:  "label1",
+					Value: "value1",
+				},
+			},
+			Samples: []pb.Sample{
+				{
+					Value:     20,
+					Timestamp: 1707123456800, // 2024-02-05T08:57:36.800Z
+				},
+			},
+		},
+	}
+	vminsert.PrometheusAPIV1Write(t, pbData, at.QueryOpts{})
+	vmstorage.ForceFlush(t)
+	f(&opts{
+		query: `{__name__=~"prometheusrw.+"}`,
+		wantMetrics: []map[string]string{
+			{
+				"__name__": "prometheusrw_series",
+			},
+			{
+				"__name__": "prometheusrw_series2",
+				"label":    "foo2",
+				"label1":   "value1",
+			},
+		},
+		wantSamples: []*at.Sample{
+			{Timestamp: 1707123456700, Value: 10}, // 2024-02-05T08:57:36.700Z
+			{Timestamp: 1707123456800, Value: 20}, // 2024-02-05T08:57:36.700Z
+		},
+	})
+
+}