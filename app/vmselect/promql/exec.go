795af212d5fd73f20947a73a8e67e613766583c6
f41gh7 <nik@victoriametrics.com>
2025-01-02 17:47:54 +0100

    app/vmselect/promql: improve performance of parseCache on systems with many CPU cores
    Parse cache is a pretty simple implementation of cache. It's just a
    standard map with mutex.
    Map with mutex overall has poor performance, plus when the cache
    overflow occurs, the whole cache locks until 1k elements have been
    deleted (now it's 10% of 10000 max elements in the cache). To avoid this
    bottleneck and improve performance of cache on systems with many CPU
    cores but keep it rather simple, we can implement cache with per bucket
    locks like it's done in fastcache. The logic and API remain the same. So
    now each bucket will have a map with approximately 78 elements (with 128
    buckets), and overflow will occur now for each bucket, and only 7
    elements need to be deleted.
    Because exec_test.go has about 10k lines of code, it's better to move
    the cache into a separate file to add tests and benchmarks for it,
    because now it does not have them.
    
    ```
    goos: windows
    goarch: amd64
    pkg: github.com/VictoriaMetrics/VictoriaMetrics/app/vmselect/promql
    cpu: 11th Gen Intel(R) Core(TM) i9-11900K @ 3.50GHz
    
    Current cache implementation performance on 8 cores:
    BenchmarkCachePutNoOverFlow-8               1932            618372 ns/op             253 B/op          0 allocs/op
    BenchmarkCacheGetNoOverflow-8               6547            211527 ns/op               0 B/op          0 allocs/op
    BenchmarkCachePutGetNoOverflow-8            1873            621718 ns/op             261 B/op          0 allocs/op
    BenchmarkCachePutOverflow-8                 2262            464328 ns/op              32 B/op          0 allocs/op
    BenchmarkCachePutGetOverflow-8              1764            655866 ns/op              38 B/op          0 allocs/op
    
    New cache implementation performance on 8 cores:
    BenchmarkCachePutNoOverFlow-8              10408            111412 ns/op               0 B/op          0 allocs/op
    BenchmarkCacheGetNoOverflow-8              22407             52809 ns/op               0 B/op          0 allocs/op
    BenchmarkCachePutGetNoOverflow-8            6583            168088 ns/op               0 B/op          0 allocs/op
    BenchmarkCachePutOverflow-8                 9822            117212 ns/op               2 B/op          0 allocs/op
    BenchmarkCachePutGetOverflow-8              6481            175952 ns/op               3 B/op          0 allocs/op
    
    Current cache implementation performance on 16 cores:
    BenchmarkCachePutNoOverFlow-16              2331            475307 ns/op             218 B/op          0 allocs/op
    BenchmarkCacheGetNoOverflow-16              6069            196905 ns/op               0 B/op          0 allocs/op
    BenchmarkCachePutGetNoOverflow-16           1870            644236 ns/op             262 B/op          0 allocs/op
    BenchmarkCachePutOverflow-16                2296            509279 ns/op              34 B/op          0 allocs/op
    BenchmarkCachePutGetOverflow-16             1726            671510 ns/op              45 B/op          0 allocs/op
    
    New cache implementation performance on 16 cores:
    BenchmarkCachePutNoOverFlow-16             13549             82413 ns/op               0 B/op          0 allocs/op
    BenchmarkCacheGetNoOverflow-16             30274             38997 ns/op               0 B/op          0 allocs/op
    BenchmarkCachePutGetNoOverflow-16           8512            126239 ns/op               0 B/op          0 allocs/op
    BenchmarkCachePutOverflow-16               13884             88124 ns/op               1 B/op          0 allocs/op
    BenchmarkCachePutGetOverflow-16             7903            131299 ns/op               3 B/op          0 allocs/op
    ```
    From the benchmarks above, we can see that the new implementation is ~5
    times faster than the old one.
    
    ---------
    Co-authored-by: f41gh7 <nik@victoriametrics.com>

--- app/vmselect/promql/exec.go
+++ app/vmselect/promql/exec.go
@@ -6,11 +6,8 @@ import (
 	"math"
 	"sort"
 	"strings"
-	"sync"
-	"sync/atomic"
 	"time"
 
-	"github.com/VictoriaMetrics/metrics"
 	"github.com/VictoriaMetrics/metricsql"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/app/vmselect/netstorage"
@@ -276,7 +273,7 @@ func getReverseCmpOp(op string) string {
 }
 
 func parsePromQLWithCache(q string) (metricsql.Expr, error) {
-	pcv := parseCacheV.Get(q)
+	pcv := parseCacheV.get(q)
 	if pcv == nil {
 		e, err := metricsql.Parse(q)
 		if err == nil {
@@ -290,7 +287,7 @@ func parsePromQLWithCache(q string) (metricsql.Expr, error) {
 			e:   e,
 			err: err,
 		}
-		parseCacheV.Put(q, pcv)
+		parseCacheV.put(q, pcv)
 	}
 	if pcv.err != nil {
 		return nil, pcv.err
@@ -333,81 +330,4 @@ func escapeDots(s string) string {
 		}
 	}
 	return string(result)
-}
-
-var parseCacheV = func() *parseCache {
-	pc := &parseCache{
-		m: make(map[string]*parseCacheValue),
-	}
-	metrics.NewGauge(`vm_cache_requests_total{type="promql/parse"}`, func() float64 {
-		return float64(pc.Requests())
-	})
-	metrics.NewGauge(`vm_cache_misses_total{type="promql/parse"}`, func() float64 {
-		return float64(pc.Misses())
-	})
-	metrics.NewGauge(`vm_cache_entries{type="promql/parse"}`, func() float64 {
-		return float64(pc.Len())
-	})
-	return pc
-}()
-
-const parseCacheMaxLen = 10e3
-
-type parseCacheValue struct {
-	e   metricsql.Expr
-	err error
-}
-
-type parseCache struct {
-	requests atomic.Uint64
-	misses   atomic.Uint64
-
-	m  map[string]*parseCacheValue
-	mu sync.RWMutex
-}
-
-func (pc *parseCache) Requests() uint64 {
-	return pc.requests.Load()
-}
-
-func (pc *parseCache) Misses() uint64 {
-	return pc.misses.Load()
-}
-
-func (pc *parseCache) Len() uint64 {
-	pc.mu.RLock()
-	n := len(pc.m)
-	pc.mu.RUnlock()
-	return uint64(n)
-}
-
-func (pc *parseCache) Get(q string) *parseCacheValue {
-	pc.requests.Add(1)
-
-	pc.mu.RLock()
-	pcv := pc.m[q]
-	pc.mu.RUnlock()
-
-	if pcv == nil {
-		pc.misses.Add(1)
-	}
-	return pcv
-}
-
-func (pc *parseCache) Put(q string, pcv *parseCacheValue) {
-	pc.mu.Lock()
-	overflow := len(pc.m) - parseCacheMaxLen
-	if overflow > 0 {
-		// Remove 10% of items from the cache.
-		overflow = int(float64(len(pc.m)) * 0.1)
-		for k := range pc.m {
-			delete(pc.m, k)
-			overflow--
-			if overflow <= 0 {
-				break
-			}
-		}
-	}
-	pc.m[q] = pcv
-	pc.mu.Unlock()
 }
