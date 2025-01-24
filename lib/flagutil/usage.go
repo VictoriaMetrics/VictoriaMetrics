234f5e520b713dee792e5a2fd7e3b15a9b33afaa
Evgeny Kuzin <evgeny@hudson-trading.com>
2025-01-24 17:28:52 +0000

    fixing wrong documentation about remote write concurrency on vmagent

--- lib/flagutil/usage.go
+++ lib/flagutil/usage.go
@@ -12,7 +12,20 @@ func Usage(s string) {
 	f := flag.CommandLine.Output()
 	fmt.Fprintf(f, "%s\n", s)
 	if hasHelpFlag(os.Args[1:]) {
-		flag.PrintDefaults()
+		flag.VisitAll(func(fl *flag.Flag) {
+			if fl.Name == "remoteWrite.concurrency" {
+				fmt.Fprintf(f, "  -%s\n", fl.Name)
+				fmt.Fprintf(f, "      %s\n", fl.Usage)
+			} else {
+				fmt.Fprintf(f, "  -%s\n", fl.Name)
+				fmt.Fprintf(f, "      %s", fl.Usage)
+				if fl.DefValue != "" {
+					fmt.Fprintf(f, " (default %s)\n", fl.DefValue)
+				} else {
+					fmt.Fprintln(f)
+				}
+			}
+		})
 	} else {
 		fmt.Fprintf(f, `Run "%s -help" in order to see the description for all the available flags`+"\n", os.Args[0])
 	}
