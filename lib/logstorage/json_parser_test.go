d56f9327ecb76f09c189b6cbfdc4921c84c91ac5
Aliaksandr Valialkin <valyala@victoriametrics.com>
2025-02-24 12:55:17 +0100

    lib/logstorage: limit the maximum log field name length, which can be generated by JSONParser.ParseLogMessage
    Make sure that the maximum log field name, which can be generated by JSONParser.ParseLogMessage,
    doesn't exceed the hardcoded limit maxFieldNameSize. Stop flattening of nested JSON objects
    when the resulting field name becomes longer than maxFieldNameSize, and return the nested JSON object
    as a string instead.
    
    This should prevent from parse errors when ingesting deeply nested JSON logs with long field names.

--- lib/logstorage/json_parser_test.go
+++ lib/logstorage/json_parser_test.go
@@ -44,7 +44,7 @@ func TestJSONParserSuccess(t *testing.T) {
 			Value: "bar",
 		},
 	})
-	f(`{"foo":{"bar":{"x":"y","z":["foo"]}},"a":1,"b":true,"c":[1,2],"d":false}`, []Field{
+	f(`{"foo":{"bar":{"x":"y","z":["foo"]}},"a":1,"b":true,"c":[1,2],"d":false,"e":null}`, []Field{
 		{
 			Name:  "foo.bar.x",
 			Value: "y",
@@ -68,6 +68,59 @@ func TestJSONParserSuccess(t *testing.T) {
 		{
 			Name:  "d",
 			Value: "false",
+		},
+	})
+}
+
+func TestJSONParserTooLongFieldName(t *testing.T) {
+	f := func(data string, maxFieldLen int, fieldsExpected []Field) {
+		t.Helper()
+
+		p := GetJSONParser()
+		err := p.parseLogMessage([]byte(data), maxFieldLen)
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		if !reflect.DeepEqual(p.Fields, fieldsExpected) {
+			t.Fatalf("unexpected fields;\ngot\n%s\nwant\n%s", p.Fields, fieldsExpected)
+		}
+		PutJSONParser(p)
+	}
+
+	f(`{"foo":"bar","baz":{"abc":"y"}}`, 7, []Field{
+		{
+			Name:  "foo",
+			Value: "bar",
+		},
+		{
+			Name:  "baz.abc",
+			Value: "y",
+		},
+	})
+	f(`{"foo":"bar","baz":{"abc":"y"}}`, 6, []Field{
+		{
+			Name:  "foo",
+			Value: "bar",
+		},
+		{
+			Name:  "baz",
+			Value: `{"abc":"y"}`,
+		},
+	})
+	f(`{"foo":"bar","baz":{"abc":"y"}}`, 3, []Field{
+		{
+			Name:  "foo",
+			Value: "bar",
+		},
+		{
+			Name:  "baz",
+			Value: `{"abc":"y"}`,
+		},
+	})
+	f(`{"foo":"bar","baz":{"abc":"y"}}`, 2, []Field{
+		{
+			Name:  "_msg",
+			Value: `{"foo":"bar","baz":{"abc":"y"}}`,
 		},
 	})
 }
