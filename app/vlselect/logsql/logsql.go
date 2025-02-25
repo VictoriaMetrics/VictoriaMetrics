VictoriaMetrics/VictoriaMetrics

commit 42c21ff67128d4d2e2c3f2fe7c787c06a863e187

lib/logstorage: always pass the current timestamp to newLexer()
Also always initialize Query.timestamp with the timestamp from the lexer.

This should avoid potential problems with relative timestamps inside inner queries.
For example, the `_time:1h` filter in the following query is correctly executed
relative to the current timestamp:

   foo:in(_time:1h | keep foo)

app/vlselect/logsql/logsql.go
@@ -688,13 +688,13 @@ func ProcessStatsQueryRangeRequest(ctx context.Context, w http.ResponseWriter, r
 	m := make(map[string]*statsSeries)
 	var mLock sync.Mutex
 
+	timestamp := q.GetTimestamp()
 	writeBlock := func(_ uint, timestamps []int64, columns []logstorage.BlockColumn) {
 		clonedColumnNames := make([]string, len(columns))
 		for i, c := range columns {
 			clonedColumnNames[i] = strings.Clone(c.Name)
 		}
 		for i := range timestamps {
-			timestamp := q.GetTimestamp()
 			labels := make([]logstorage.Field, 0, len(byFields))
 			for j, c := range columns {
 				if c.Name == "_time" {

lib/logstorage/parser.go
@@ -54,15 +54,12 @@ func (lex *lexer) restoreState(ls *lexerState) {
 	*lex = ls.lex
 }
 
-// newLexer returns new lexer for the given s.
+// newLexer returns new lexer for the given s at the given timestamp.
+//
+// The timestamp is used for properly parsing relative timestamps such as _time:1d.
 //
 // The lex.token points to the first token in s.
-func newLexer(s string) *lexer {
-	timestamp := time.Now().UnixNano()
-	return newLexerAtTimestamp(s, timestamp)
-}
-
-func newLexerAtTimestamp(s string, timestamp int64) *lexer {
+func newLexer(s string, timestamp int64) *lexer {
 	lex := &lexer{
 		s:                s,
 		sOrig:            s,
@@ -310,7 +307,7 @@ func (q *Query) AddFacetsPipe(limit, maxValuesPerField, maxValueLen int, keepCon
 	if keepConstFields {
 		s += " keep_const_fields"
 	}
-	lex := newLexer(s)
+	lex := newLexer(s, q.timestamp)
 
 	pf, err := parsePipeFacets(lex)
 	if err != nil {
@@ -333,7 +330,7 @@ func (q *Query) AddCountByTimePipe(step, off int64, fields []string) {
 			byFieldsStr += ", " + quoteTokenIfNeeded(f)
 		}
 		s := fmt.Sprintf("stats by (%s) count() hits", byFieldsStr)
-		lex := newLexer(s)
+		lex := newLexer(s, q.timestamp)
 
 		ps, err := parsePipeStats(lex, true)
 		if err != nil {
@@ -353,7 +350,7 @@ func (q *Query) AddCountByTimePipe(step, off int64, fields []string) {
 			sortFieldsStr += ", " + quoteTokenIfNeeded(f)
 		}
 		s := fmt.Sprintf("sort by (%s)", sortFieldsStr)
-		lex := newLexer(s)
+		lex := newLexer(s, q.timestamp)
 		ps, err := parsePipeSort(lex)
 		if err != nil {
 			logger.Panicf("BUG: unexpected error when parsing %q: %s", s, err)
@@ -1004,7 +1001,7 @@ func (q *Query) HasGlobalTimeFilter() bool {
 //
 // E.g. _time:duration filters are adjusted according to the provided timestamp as _time:[timestamp-duration, duration].
 func ParseQueryAtTimestamp(s string, timestamp int64) (*Query, error) {
-	lex := newLexerAtTimestamp(s, timestamp)
+	lex := newLexer(s, timestamp)
 
 	q, err := parseQuery(lex)
 	if err != nil {
@@ -1013,7 +1010,6 @@ func ParseQueryAtTimestamp(s string, timestamp int64) (*Query, error) {
 	if !lex.isEnd() {
 		return nil, fmt.Errorf("unexpected unparsed tail after [%s]; context: [%s]; tail: [%s]", q, lex.context(), lex.s)
 	}
-	q.timestamp = timestamp
 	q.optimize()
 
 	start, end := q.GetFilterTimeRange()
@@ -1065,7 +1061,8 @@ func parseQuery(lex *lexer) (*Query, error) {
 		return nil, fmt.Errorf("%w; context: [%s]", err, lex.context())
 	}
 	q := &Query{
-		f: f,
+		f:         f,
+		timestamp: lex.currentTimestamp,
 	}
 
 	if lex.isKeyword("|") {

lib/logstorage/parser_test.go
@@ -10,7 +10,7 @@ import (
 func TestLexer(t *testing.T) {
 	f := func(s string, tokensExpected []string) {
 		t.Helper()
-		lex := newLexer(s)
+		lex := newLexer(s, 0)
 		for _, tokenExpected := range tokensExpected {
 			if lex.token != tokenExpected {
 				t.Fatalf("unexpected token; got %q; want %q", lex.token, tokenExpected)

lib/logstorage/pipe_copy_test.go
@@ -229,7 +229,7 @@ func expectPipeNeededFields(t *testing.T, s, neededFields, unneededFields, neede
 	nfs := newTestFieldsSet(neededFields)
 	unfs := newTestFieldsSet(unneededFields)
 
-	lex := newLexer(s)
+	lex := newLexer(s, 0)
 	p, err := parsePipe(lex)
 	if err != nil {
 		t.Fatalf("cannot parse %s: %s", s, err)

lib/logstorage/pipe_utils_test.go
@@ -11,7 +11,7 @@ import (
 func expectParsePipeFailure(t *testing.T, pipeStr string) {
 	t.Helper()
 
-	lex := newLexer(pipeStr)
+	lex := newLexer(pipeStr, 0)
 	p, err := parsePipe(lex)
 	if err == nil && lex.isEnd() {
 		t.Fatalf("expecting error when parsing [%s]; parsed result: [%s]", pipeStr, p)
@@ -21,7 +21,7 @@ func expectParsePipeFailure(t *testing.T, pipeStr string) {
 func expectParsePipeSuccess(t *testing.T, pipeStr string) {
 	t.Helper()
 
-	lex := newLexer(pipeStr)
+	lex := newLexer(pipeStr, 0)
 	p, err := parsePipe(lex)
 	if err != nil {
 		t.Fatalf("cannot parse [%s]: %s", pipeStr, err)
@@ -39,7 +39,7 @@ func expectParsePipeSuccess(t *testing.T, pipeStr string) {
 func expectPipeResults(t *testing.T, pipeStr string, rows, rowsExpected [][]Field) {
 	t.Helper()
 
-	lex := newLexer(pipeStr)
+	lex := newLexer(pipeStr, 0)
 	p, err := parsePipe(lex)
 	if err != nil {
 		t.Fatalf("unexpected error when parsing %q: %s", pipeStr, err)

lib/logstorage/stats_avg_test.go
@@ -386,7 +386,7 @@ func TestStatsAvg(t *testing.T) {
 func expectParseStatsFuncFailure(t *testing.T, s string) {
 	t.Helper()
 
-	lex := newLexer(s)
+	lex := newLexer(s, 0)
 	sf, err := parseStatsFunc(lex)
 	if err == nil && lex.isEnd() {
 		t.Fatalf("expecting error when parsing [%s]; parsed result: [%s]", s, sf)
@@ -396,7 +396,7 @@ func expectParseStatsFuncFailure(t *testing.T, s string) {
 func expectParseStatsFuncSuccess(t *testing.T, s string) {
 	t.Helper()
 
-	lex := newLexer(s)
+	lex := newLexer(s, 0)
 	p, err := parseStatsFunc(lex)
 	if err != nil {
 		t.Fatalf("cannot parse [%s]: %s", s, err)

lib/logstorage/storage_search.go
@@ -199,7 +199,7 @@ func (s *Storage) runQuery(ctx context.Context, tenantIDs []TenantID, q *Query, 
 func (s *Storage) GetFieldNames(ctx context.Context, tenantIDs []TenantID, q *Query) ([]ValueWithHits, error) {
 	pipes := append([]pipe{}, q.pipes...)
 	pipeStr := "field_names"
-	lex := newLexer(pipeStr)
+	lex := newLexer(pipeStr, q.timestamp)
 
 	p, err := parsePipeFieldNames(lex)
 	if err != nil {
@@ -297,7 +297,7 @@ func (s *Storage) getFieldValuesNoHits(ctx context.Context, tenantIDs []TenantID
 	pipes := append([]pipe{}, q.pipes...)
 	quotedFieldName := quoteTokenIfNeeded(fieldName)
 	pipeStr := fmt.Sprintf("uniq by (%s)", quotedFieldName)
-	lex := newLexer(pipeStr)
+	lex := newLexer(pipeStr, q.timestamp)
 
 	pu, err := parsePipeUniq(lex)
 	if err != nil {
@@ -362,7 +362,7 @@ func (s *Storage) GetFieldValues(ctx context.Context, tenantIDs []TenantID, q *Q
 	pipes := append([]pipe{}, q.pipes...)
 	quotedFieldName := quoteTokenIfNeeded(fieldName)
 	pipeStr := fmt.Sprintf("field_values %s limit %d", quotedFieldName, limit)
-	lex := newLexer(pipeStr)
+	lex := newLexer(pipeStr, q.timestamp)
 
 	pu, err := parsePipeFieldValues(lex)
 	if err != nil {

lib/logstorage/stream_filter_test.go
@@ -200,7 +200,7 @@ func mustNewTestStreamFilter(s string) *StreamFilter {
 }
 
 func newTestStreamFilter(s string) (*StreamFilter, error) {
-	lex := newLexer(s)
+	lex := newLexer(s, 0)
 	fs, err := parseFilterStream(lex)
 	if err != nil {
 		return nil, err

commit 0de7c8987c2e18269c391a65be6f8b58222d3e59

app/vlselect/logsql: follow-up for 521af4d7a391f77abb40cb0774046d9e5b09a9ef
The ts variable must be initialized to the q.GetTimestamp() for every processed row inside the `for range timestamps` loop.

The original bug has been introduced in the commit 42c21ff67128d4d2e2c3f2fe7c787c06a863e187

Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8312
Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/8385

app/vlselect/logsql/logsql.go
@@ -688,14 +688,16 @@ func ProcessStatsQueryRangeRequest(ctx context.Context, w http.ResponseWriter, r
 	m := make(map[string]*statsSeries)
 	var mLock sync.Mutex
 
-	timestamp := q.GetTimestamp() // do not modify `timestamp` in worker writeBlock
 	writeBlock := func(_ uint, timestamps []int64, columns []logstorage.BlockColumn) {
-		ts := timestamp
 		clonedColumnNames := make([]string, len(columns))
 		for i, c := range columns {
 			clonedColumnNames[i] = strings.Clone(c.Name)
 		}
 		for i := range timestamps {
+			// Do not move q.GetTimestamp() outside writeBlock, since ts
+			// must be initialized to query timestamp for every processed log row.
+			// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8312
+			ts := q.GetTimestamp()
 			labels := make([]logstorage.Field, 0, len(byFields))
 			for j, c := range columns {
 				if c.Name == "_time" {