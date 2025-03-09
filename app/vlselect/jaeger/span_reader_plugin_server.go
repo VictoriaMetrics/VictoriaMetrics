57ac1302debbc52ac0654bdf4d282ca69fc2eecf
Jiekun <jiekun@victoriametrics.com>
2025-03-09 16:34:20 +0800

    poc: [jaeger] skip empty fields for span for get trace

--- app/vlselect/jaeger/span_reader_plugin_server.go
+++ app/vlselect/jaeger/span_reader_plugin_server.go
@@ -46,9 +46,9 @@ func (s *SpanReaderPluginServer) GetTrace(ctx context.Context, traceID model.Tra
 		for i, timestamp := range timestamps {
 			fields := make([]logstorage.Field, len(columns))
 			for j := range columns {
-				f := &fields[j]
-				f.Name = clonedColumnNames[j]
-				f.Value = strings.Clone(columns[j].Values[i])
+				if columns[j].Values[i] != "" {
+					fields = append(fields, logstorage.Field{Name: clonedColumnNames[j], Value: strings.Clone(columns[j].Values[i])})
+				}
 			}
 
 			rowsLock.Lock()
