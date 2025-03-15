d3594920eefd9fccdb6db8a5d01124fc61f1adc9
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2025-03-15 21:37:42 +0200

    lib/promscrape/discovery/kubernetes: support scraping in protobuf format, fixes #8512

--- lib/promscrape/discovery/kubernetes/common_types.go
+++ lib/promscrape/discovery/kubernetes/common_types.go
@@ -1,14 +1,20 @@
 package kubernetes
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/prompbmarshal"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promscrape/discoveryutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promutil"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/slicesutil"
+	"github.com/VictoriaMetrics/easyproto"
 )
 
 // ObjectMeta represents ObjectMeta from k8s API.
 //
-// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#objectmeta-v1-meta
+// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#objectmeta-v1-meta
 type ObjectMeta struct {
 	Name            string
 	Namespace       string
@@ -18,14 +24,171 @@ type ObjectMeta struct {
 	OwnerReferences []OwnerReference
 }
 
+// unmarshalProtobuf unmarshals ObjectMeta according to spec
+//
+// See https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/v1/generated.proto
+func (om *ObjectMeta) unmarshalProtobuf(src []byte) (err error) {
+	// message ObjectMeta {
+	//   optional string name = 1;
+	//   optional string namespace = 3;
+	//   optional string uid = 5;
+	//   map<string, string> labels = 11;
+	//   map<string, string> annotations = 12;
+	//   repeated OwnerReference ownerReferences = 13;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in ObjectMeta: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			name, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Name")
+			}
+			om.Name = strings.Clone(name)
+		case 3:
+			ns, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Namespace")
+			}
+			om.Namespace = strings.Clone(ns)
+		case 5:
+			uid, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read UID")
+			}
+			om.UID = strings.Clone(uid)
+		case 11:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read Labels data")
+			}
+			if om.Labels == nil {
+				om.Labels = &promutil.Labels{}
+			}
+			om.Labels.Labels = slicesutil.SetLength(om.Labels.Labels, len(om.Labels.Labels)+1)
+			l := &om.Labels.Labels[len(om.Labels.Labels)-1]
+			if err := unmarshalLabel(l, data); err != nil {
+				return fmt.Errorf("cannot unmarshal Labels: %w", err)
+			}
+		case 12:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read Annotations data")
+			}
+			if om.Annotations == nil {
+				om.Annotations = &promutil.Labels{}
+			}
+			om.Annotations.Labels = slicesutil.SetLength(om.Annotations.Labels, len(om.Annotations.Labels)+1)
+			l := &om.Annotations.Labels[len(om.Annotations.Labels)-1]
+			if err := unmarshalLabel(l, data); err != nil {
+				return fmt.Errorf("cannot unmarshal Annotations: %w", err)
+			}
+		case 13:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read OwnerReference")
+			}
+			om.OwnerReferences = slicesutil.SetLength(om.OwnerReferences, len(om.OwnerReferences)+1)
+			or := &om.OwnerReferences[len(om.OwnerReferences)-1]
+			if err := or.unmarshalProtobuf(data); err != nil {
+				return fmt.Errorf("failed to unmarshal OwnerReference")
+			}
+		}
+	}
+	return nil
+}
+
+func (om *ObjectMeta) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	mm.AppendString(1, om.Name)
+	mm.AppendString(3, om.Namespace)
+	mm.AppendString(5, om.UID)
+	if om.Labels != nil {
+		for _, l := range om.Labels.Labels {
+			marshalLabel(&l, mm.AppendMessage(11))
+		}
+	}
+	if om.Annotations != nil {
+		for _, l := range om.Annotations.Labels {
+			marshalLabel(&l, mm.AppendMessage(12))
+		}
+	}
+	for _, or := range om.OwnerReferences {
+		or.marshalProtobuf(mm.AppendMessage(13))
+	}
+}
+
 func (om *ObjectMeta) key() string {
 	return om.Namespace + "/" + om.Name
 }
 
+func marshalLabel(l *prompbmarshal.Label, mm *easyproto.MessageMarshaler) {
+	mm.AppendString(1, l.Name)
+	mm.AppendString(2, l.Value)
+}
+
+func unmarshalLabel(l *prompbmarshal.Label, src []byte) (err error) {
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in Label: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			name, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Name")
+			}
+			l.Name = strings.Clone(name)
+		case 2:
+			value, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Value")
+			}
+			l.Value = strings.Clone(value)
+		}
+	}
+	return nil
+}
+
 // ListMeta is a Kubernetes list metadata
-// https://v1-17.docs.kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#listmeta-v1-meta
+//
+// https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#listmeta-v1-meta
 type ListMeta struct {
 	ResourceVersion string
+}
+
+// unmarshalProtobuf unmarshals ListMeta according to spec
+//
+// See https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/v1/generated.proto
+func (r *ListMeta) unmarshalProtobuf(src []byte) (err error) {
+	// message ListMeta {
+	//   optional string resourceVersion = 2;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in ListMeta: %w", err)
+		}
+		switch fc.FieldNum {
+		case 2:
+			resourceVersion, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read ResourceVersion")
+			}
+			r.ResourceVersion = strings.Clone(resourceVersion)
+		}
+	}
+	return nil
+}
+
+func (r *ListMeta) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	mm.AppendString(2, r.ResourceVersion)
 }
 
 func (om *ObjectMeta) registerLabelsAndAnnotations(prefix string, m *promutil.Labels) {
@@ -64,16 +227,90 @@ func appendThreeStrings(dst []byte, a, b, c string) []byte {
 
 // OwnerReference represents OwnerReferense from k8s API.
 //
-// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#ownerreference-v1-meta
+// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#ownerreference-v1-meta
 type OwnerReference struct {
 	Name       string
 	Controller bool
 	Kind       string
 }
 
+// unmarshalProtobuf unmarshals OwnerReference according to spec
+//
+// See https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/v1/generated.proto
+func (r *OwnerReference) unmarshalProtobuf(src []byte) (err error) {
+	// message OwnerReference {
+	//   optional string kind = 1;
+	//   optional string name = 3;
+	//   optional bool controller = 6;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in ObjectMeta: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			kind, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Kind")
+			}
+			r.Kind = strings.Clone(kind)
+		case 3:
+			name, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Name")
+			}
+			r.Name = strings.Clone(name)
+		case 5:
+			controller, ok := fc.Bool()
+			if !ok {
+				return fmt.Errorf("cannot read Controller")
+			}
+			r.Controller = controller
+		}
+	}
+	return nil
+}
+
+func (r *OwnerReference) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	mm.AppendString(1, r.Kind)
+	mm.AppendString(2, r.Name)
+	mm.AppendBool(3, r.Controller)
+}
+
 // DaemonEndpoint represents DaemonEndpoint from k8s API.
 //
-// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#daemonendpoint-v1-core
+// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#daemonendpoint-v1-core
 type DaemonEndpoint struct {
 	Port int
+}
+
+// unmarshalProtobuf unmarshals DaemonEndpoint according to spec
+//
+// See https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/v1/generated.proto
+func (r *DaemonEndpoint) unmarshalProtobuf(src []byte) (err error) {
+	// message DaemonEndpoint {
+	//   optional int32 port = 1;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in DaemonEndpoint: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			port, ok := fc.Int32()
+			if !ok {
+				return fmt.Errorf("cannot read Port")
+			}
+			r.Port = int(port)
+		}
+	}
+	return nil
+}
+
+func (r *DaemonEndpoint) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	mm.AppendInt32(1, int32(r.Port))
 }
--- lib/promscrape/discovery/kubernetes/service.go
+++ lib/promscrape/discovery/kubernetes/service.go
@@ -3,57 +3,161 @@ package kubernetes
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"strconv"
+	"strings"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promscrape/discoveryutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/promutil"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/slicesutil"
+	"github.com/VictoriaMetrics/easyproto"
 )
 
 func (s *Service) key() string {
 	return s.Metadata.key()
 }
 
-func parseServiceList(r io.Reader) (map[string]object, ListMeta, error) {
-	var sl ServiceList
-	d := json.NewDecoder(r)
-	if err := d.Decode(&sl); err != nil {
-		return nil, sl.Metadata, fmt.Errorf("cannot unmarshal ServiceList: %w", err)
+func parseServiceList(data []byte, contentType string) (map[string]object, ListMeta, error) {
+	sl := &ServiceList{}
+	switch contentType {
+	case contentTypeJSON:
+		if err := json.Unmarshal(data, sl); err != nil {
+			return nil, sl.Metadata, fmt.Errorf("cannot unmarshal ServiceList: %w", err)
+		}
+	case contentTypeProtobuf:
+		if err := sl.unmarshalProtobuf(data); err != nil {
+			return nil, sl.Metadata, fmt.Errorf("cannot unmarshal ServiceList: %w", err)
+		}
 	}
 	objectsByKey := make(map[string]object)
 	for _, s := range sl.Items {
-		objectsByKey[s.key()] = s
+		objectsByKey[s.key()] = &s
 	}
 	return objectsByKey, sl.Metadata, nil
 }
 
-func parseService(data []byte) (object, error) {
-	var s Service
-	if err := json.Unmarshal(data, &s); err != nil {
-		return nil, err
+func parseService(data []byte, contentType string) (object, error) {
+	s := &Service{}
+	switch contentType {
+	case contentTypeJSON:
+		if err := json.Unmarshal(data, s); err != nil {
+			return nil, err
+		}
+	case contentTypeProtobuf:
+		if err := s.unmarshalProtobuf(data); err != nil {
+			return nil, err
+		}
 	}
-	return &s, nil
+	return s, nil
 }
 
 // ServiceList is k8s service list.
 //
-// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#servicelist-v1-core
+// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#servicelist-v1-core
 type ServiceList struct {
 	Metadata ListMeta
-	Items    []*Service
+	Items    []Service
+}
+
+// unmarshalProtobuf unmarshals ServiceList according to spec
+//
+// See https://github.com/kubernetes/api/blob/master/core/v1/generated.proto
+func (r *ServiceList) unmarshalProtobuf(src []byte) (err error) {
+	// message ServiceList {
+	//   optional ListMeta metadata = 1;
+	//   repeated Service items = 2;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in ObjectMeta: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read ListMeta")
+			}
+			m := &r.Metadata
+			if err := m.unmarshalProtobuf(data); err != nil {
+				return fmt.Errorf("cannot unmarshal ListMeta: %w", err)
+			}
+		case 2:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read Items")
+			}
+			r.Items = slicesutil.SetLength(r.Items, len(r.Items)+1)
+			s := &r.Items[len(r.Items)-1]
+			if err := s.unmarshalProtobuf(data); err != nil {
+				return fmt.Errorf("cannot unmarshal Items: %w", err)
+			}
+		}
+	}
+	return nil
+}
+
+func (r *ServiceList) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	r.Metadata.marshalProtobuf(mm.AppendMessage(1))
+	for _, item := range r.Items {
+		item.marshalProtobuf(mm.AppendMessage(2))
+	}
 }
 
 // Service is k8s service.
 //
-// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#service-v1-core
+// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#service-v1-core
 type Service struct {
 	Metadata ObjectMeta
 	Spec     ServiceSpec
 }
 
+// unmarshalProtobuf unmarshals Service according to spec
+//
+// See https://github.com/kubernetes/api/blob/master/core/v1/generated.proto
+func (s *Service) unmarshalProtobuf(src []byte) (err error) {
+	// message Service {
+	//   optional ObjectMeta metadata = 1;
+	//   repeated ServiceSpec spec = 2;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in Service: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read ObjectMeta")
+			}
+			m := &s.Metadata
+			if err := m.unmarshalProtobuf(data); err != nil {
+				return fmt.Errorf("cannot unmarshal ObjectMeta: %w", err)
+			}
+		case 2:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read Spec")
+			}
+			spec := &s.Spec
+			if err := spec.unmarshalProtobuf(data); err != nil {
+				return fmt.Errorf("cannot unmarshal Spec: %w", err)
+			}
+		}
+	}
+	return nil
+}
+
+func (s *Service) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	s.Metadata.marshalProtobuf(mm.AppendMessage(1))
+	s.Spec.marshalProtobuf(mm.AppendMessage(2))
+}
+
 // ServiceSpec is k8s service spec.
 //
-// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#servicespec-v1-core
+// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#servicespec-v1-core
 type ServiceSpec struct {
 	ClusterIP    string
 	ExternalName string
@@ -61,13 +165,117 @@ type ServiceSpec struct {
 	Ports        []ServicePort
 }
 
+// unmarshalProtobuf unmarshals ServiceSpec according to spec
+//
+// See https://github.com/kubernetes/api/blob/master/core/v1/generated.proto
+func (r *ServiceSpec) unmarshalProtobuf(src []byte) (err error) {
+	// message ServiceSpec {
+	//   repeated ServicePort ports = 1;
+	//   optional string clusterIP = 3;
+	//   optional string type = 4;
+	//   optional string externalName = 10;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in Service: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			data, ok := fc.MessageData()
+			if !ok {
+				return fmt.Errorf("cannot read Port")
+			}
+			r.Ports = slicesutil.SetLength(r.Ports, len(r.Ports)+1)
+			p := &r.Ports[len(r.Ports)-1]
+			if err := p.unmarshalProtobuf(data); err != nil {
+				return fmt.Errorf("cannot unmarshal Port: %w", err)
+			}
+		case 3:
+			clusterIP, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read ClusterIP")
+			}
+			r.ClusterIP = strings.Clone(clusterIP)
+		case 4:
+			serviceType, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Type")
+			}
+			r.Type = strings.Clone(serviceType)
+		case 10:
+			externalName, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read ExternalName")
+			}
+			r.ExternalName = strings.Clone(externalName)
+		}
+	}
+	return nil
+}
+
+func (r *ServiceSpec) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	for _, p := range r.Ports {
+		p.marshalProtobuf(mm.AppendMessage(1))
+	}
+	mm.AppendString(3, r.ClusterIP)
+	mm.AppendString(4, r.Type)
+	mm.AppendString(10, r.ExternalName)
+}
+
 // ServicePort is k8s service port.
 //
-// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.17/#serviceport-v1-core
+// See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.32/#serviceport-v1-core
 type ServicePort struct {
 	Name     string
 	Protocol string
 	Port     int
+}
+
+// unmarshalProtobuf unmarshals ServicePort according to spec
+//
+// See https://github.com/kubernetes/api/blob/master/core/v1/generated.proto
+func (r *ServicePort) unmarshalProtobuf(src []byte) (err error) {
+	// message ServicePort {
+	//   optional string name = 1;
+	//   optional string protocol = 2;
+	//   optional int32 port = 3;
+	// }
+	var fc easyproto.FieldContext
+	for len(src) > 0 {
+		src, err = fc.NextField(src)
+		if err != nil {
+			return fmt.Errorf("cannot read next field in Service: %w", err)
+		}
+		switch fc.FieldNum {
+		case 1:
+			name, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Name")
+			}
+			r.Name = strings.Clone(name)
+		case 2:
+			protocol, ok := fc.String()
+			if !ok {
+				return fmt.Errorf("cannot read Protocol")
+			}
+			r.Protocol = strings.Clone(protocol)
+		case 3:
+			port, ok := fc.Int32()
+			if !ok {
+				return fmt.Errorf("cannot read Port")
+			}
+			r.Port = int(port)
+		}
+	}
+	return nil
+}
+
+func (r *ServicePort) marshalProtobuf(mm *easyproto.MessageMarshaler) {
+	mm.AppendString(1, r.Name)
+	mm.AppendString(2, r.Protocol)
+	mm.AppendInt32(3, int32(r.Port))
 }
 
 // getTargetLabels returns labels for each port of the given s.
