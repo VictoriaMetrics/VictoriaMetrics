572e48938564b31976f7a1408ba4d6eda5f09d13
emreya <e.yazici1990@gmail.com>
2025-03-17 11:04:22 +0100

    chore: add comment to exporter func SetPartialResponse
    Signed-off-by: emreya <e.yazici1990@gmail.com>

--- app/vmalert/datasource/faker.go
+++ app/vmalert/datasource/faker.go
@@ -22,6 +22,7 @@ func (fq *FakeQuerier) SetErr(err error) {
 	fq.Unlock()
 }
 
+// SetPartialResponse marks query response as partial
 func (fq *FakeQuerier) SetPartialResponse(partial bool) {
 	fq.Lock()
 	fq.isPartial = &partial
