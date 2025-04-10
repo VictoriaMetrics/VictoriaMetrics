b61d8059b8bdaaec9800ef98b04e5fc4185a2846
Aliaksandr Valialkin <valyala@victoriametrics.com>
2025-04-10 13:50:00 +0200

    lib/protoparser: support for `identity` encoding in a generic way inside protoparserutil.GetUncompressedReader
    This should help avoiding future issues when `identity` encoding isn't replaced to `` encoding
    by the caller of protoparserutil.GetUncompressedReader().
    
    This is a follow-up for 303b425fa3c0419f47a0ea5801e28c27eaf91157
    
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/pull/8652
    Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8649

--- lib/protoparser/datadogv1/stream/streamparser.go
+++ lib/protoparser/datadogv1/stream/streamparser.go
@@ -17,11 +17,6 @@ import (
 func Parse(r io.Reader, encoding string, callback func(series []datadogv1.Series) error) error {
 	readCalls.Inc()
 
-	// Datadog extensions sends Content-Encoding: identity, which is not supported by RFC 2616
-	// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8649
-	if encoding == "identity" {
-		encoding = ""
-	}
 	err := protoparserutil.ReadUncompressedData(r, encoding, datadogutil.MaxInsertRequestSize, func(data []byte) error {
 		return parseData(data, callback)
 	})
