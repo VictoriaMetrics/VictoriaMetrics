1c398531647f3bc7668b22d2a76f350c8812e923
Yury Molodov <yurymolodov@gmail.com>
2025-04-24 14:07:29 +0200

    vmui: show read usage in Cardinality Explorer (#8757)
    ### Describe Your Changes
    
    Two new columns were added to the **Cardinality Explorer** table:
    
    `Requests count`
    - Displays how many times a metric was queried since stats collection
    began
    - Highlighted **red** when the value is `0` or missing
    
    `Last request`
    - Shows when the metric was last queried
    - Highlighted:
      - **Red** if older than **30 days**
      - **Yellow** if between **7 and 30 days**
    - Shows exact timestamp on hover in `YYYY-MM-DD HH:mm:ss` format
    
    Related issue: #6145
    
    
    ![image](https://github.com/user-attachments/assets/fcf6f33c-e2d0-45c0-9f20-177cf6fa9dde)
    
    ### Checklist
    
    The following checks are **mandatory**:
    
    - [ ] My change adheres to [VictoriaMetrics contributing
    guidelines](https://docs.victoriametrics.com/contributing/).
    
    ---------
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>
    Co-authored-by: hagen1778 <roman@victoriametrics.com>

--- app/vmui/packages/vmui/src/api/tsdb.ts
+++ app/vmui/packages/vmui/src/api/tsdb.ts
@@ -11,3 +11,19 @@ export const getCardinalityInfo = (server: string, requestsParam: CardinalityReq
   return `${server}/api/v1/status/tsdb?topN=${requestsParam.topN}&date=${requestsParam.date}${match}${focusLabel}`;
 };
 
+interface MetricNamesStatsParams {
+  limit?: number,
+  le?: number, // less than or equal
+  match_pattern?: string, // a regex pattern to match metric names
+}
+
+export const getMetricNamesStats = (server: string, params: MetricNamesStatsParams) => {
+  const searchParams = new URLSearchParams(
+    Object.entries(params).filter(([_, value]) => value != null)
+  ).toString();
+
+  console.log("searchParams", searchParams);
+
+  return `${server}/api/v1/status/metric_names_stats?${searchParams}`;
+};
+
