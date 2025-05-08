VictoriaMetrics/VictoriaMetrics

commit 0e313e53557f01046e3162abb33ef361ccf4a243

lib/httpserver: move ServeWithOpts to Serve
This addresses that todo in the codebase, and updates all callsites to
the new signature.

---------
Signed-off-by: Florian Klink <flokli@flokli.de>

app/vmauth/main.go
@@ -106,10 +106,10 @@ func main() {
 		UseProxyProtocol:     useProxyProtocol,
 		DisableBuiltinRoutes: disableInternalRoutes,
 	}
-	go httpserver.ServeWithOpts(listenAddrs, rh, serveOpts)
+	go httpserver.Serve(listenAddrs, rh, serveOpts)
 
 	if len(*httpInternalListenAddr) > 0 {
-		go httpserver.Serve(*httpInternalListenAddr, nil, internalRequestHandler)
+		go httpserver.Serve(*httpInternalListenAddr, internalRequestHandler, serveOpts)
 	}
 	logger.Infof("started vmauth in %.3f seconds", time.Since(startTime).Seconds())
 

app/victoria-logs/main.go
@@ -46,7 +46,9 @@ func main() {
 	vlselect.Init()
 	vlinsert.Init()
 
-	go httpserver.Serve(listenAddrs, useProxyProtocol, requestHandler)
+	go httpserver.Serve(listenAddrs, requestHandler, httpserver.ServeOptions{
+		UseProxyProtocol: useProxyProtocol,
+	})
 	logger.Infof("started VictoriaLogs in %.3f seconds; see https://docs.victoriametrics.com/victorialogs/", time.Since(startTime).Seconds())
 
 	pushmetrics.Init()

app/victoria-metrics/main.go
@@ -101,7 +101,9 @@ func main() {
 
 	startSelfScraper()
 
-	go httpserver.Serve(listenAddrs, useProxyProtocol, requestHandler)
+	go httpserver.Serve(listenAddrs, requestHandler, httpserver.ServeOptions{
+		UseProxyProtocol: useProxyProtocol,
+	})
 	logger.Infof("started VictoriaMetrics in %.3f seconds", time.Since(startTime).Seconds())
 
 	pushmetrics.Init()

app/vmagent/main.go
@@ -165,7 +165,9 @@ func main() {
 
 	promscrape.Init(remotewrite.PushDropSamplesOnFailure)
 
-	go httpserver.Serve(listenAddrs, useProxyProtocol, requestHandler)
+	go httpserver.Serve(listenAddrs, requestHandler, httpserver.ServeOptions{
+		UseProxyProtocol: useProxyProtocol,
+	})
 	logger.Infof("started vmagent in %.3f seconds", time.Since(startTime).Seconds())
 
 	pushmetrics.Init()

app/vmalert/main.go
@@ -182,7 +182,9 @@ func main() {
 		listenAddrs = []string{":8880"}
 	}
 	rh := &requestHandler{m: manager}
-	go httpserver.Serve(listenAddrs, useProxyProtocol, rh.handler)
+	go httpserver.Serve(listenAddrs, rh.handler, httpserver.ServeOptions{
+		UseProxyProtocol: useProxyProtocol,
+	})
 
 	pushmetrics.Init()
 	sig := procutil.WaitForSigterm()

app/vmbackup/main.go
@@ -97,7 +97,7 @@ func main() {
 	}
 
 	listenAddrs := []string{*httpListenAddr}
-	go httpserver.Serve(listenAddrs, nil, nil)
+	go httpserver.Serve(listenAddrs, nil, httpserver.ServeOptions{})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	go func() {

app/vmrestore/main.go
@@ -40,7 +40,7 @@ func main() {
 	logger.Init()
 
 	listenAddrs := []string{*httpListenAddr}
-	go httpserver.Serve(listenAddrs, nil, nil)
+	go httpserver.Serve(listenAddrs, nil, httpserver.ServeOptions{})
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	go func() {

lib/httpserver/httpserver.go
@@ -102,35 +102,7 @@ type ServeOptions struct {
 // Serve starts an http server on the given addrs with the given optional rh.
 //
 // By default all the responses are transparently compressed, since egress traffic is usually expensive.
-//
-// The compression can be disabled by specifying -http.disableResponseCompression command-line flag.
-//
-// If useProxyProtocol is set to true for the corresponding addr, then the incoming connections are accepted via proxy protocol.
-// See https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt
-func Serve(addrs []string, useProxyProtocol *flagutil.ArrayBool, rh RequestHandler) {
-	if rh == nil {
-		rh = func(_ http.ResponseWriter, _ *http.Request) bool {
-			return false
-		}
-	}
-	opts := ServeOptions{
-		UseProxyProtocol: useProxyProtocol,
-	}
-	for idx, addr := range addrs {
-		if addr == "" {
-			continue
-		}
-		go serve(addr, rh, idx, opts)
-	}
-}
-
-// ServeWithOpts starts an http server on the given addrs with the given optional request handlers.
-//
-// By default all the responses are transparently compressed, since egress traffic is usually expensive.
-//
-// The compression can be disabled by specifying -http.disableResponseCompression command-line flag.
-// TODO: rename to Serve and update Serve usage with new signature
-func ServeWithOpts(addrs []string, rh RequestHandler, opts ServeOptions) {
+func Serve(addrs []string, rh RequestHandler, opts ServeOptions) {
 	if rh == nil {
 		rh = func(_ http.ResponseWriter, _ *http.Request) bool {
 			return false

commit efd6e85f739e2fa867a06dcacd00ad077c6b1949

app/vmauth: expose built-in handlers if `-httpInternalListenAddr` is specified
This functionality was broken in https://github.com/VictoriaMetrics/VictoriaMetrics/commit/0e313e53557f01046e3162abb33ef361ccf4a243

Was caught by integration tests:
```
--- FAIL: TestSingleVMAuthRouterWithInternalAddr (5.00s)
    vmauth_routing_test.go:148: Could not start vmauth: could not extract some or all regexps from stderr: ["pprof handlers are exposed at http://(.*:\\d{1,5})/debug/pprof/"]
```

Signed-off-by: hagen1778 <roman@victoriametrics.com>

app/vmauth/main.go
@@ -96,20 +96,23 @@ func main() {
 	logger.Infof("starting vmauth at %q...", listenAddrs)
 	startTime := time.Now()
 	initAuthConfig()
+
 	disableInternalRoutes := len(*httpInternalListenAddr) > 0
 	rh := requestHandlerWithInternalRoutes
 	if disableInternalRoutes {
 		rh = requestHandler
 	}
 
-	serveOpts := httpserver.ServeOptions{
-		UseProxyProtocol:     useProxyProtocol,
+	go httpserver.Serve(listenAddrs, rh, httpserver.ServeOptions{
+		UseProxyProtocol: useProxyProtocol,
+		// built-in routes will be exposed at *httpInternalListenAddr
 		DisableBuiltinRoutes: disableInternalRoutes,
-	}
-	go httpserver.Serve(listenAddrs, rh, serveOpts)
+	})
 
 	if len(*httpInternalListenAddr) > 0 {
-		go httpserver.Serve(*httpInternalListenAddr, internalRequestHandler, serveOpts)
+		go httpserver.Serve(*httpInternalListenAddr, internalRequestHandler, httpserver.ServeOptions{
+			UseProxyProtocol: useProxyProtocol,
+		})
 	}
 	logger.Infof("started vmauth in %.3f seconds", time.Since(startTime).Seconds())
 