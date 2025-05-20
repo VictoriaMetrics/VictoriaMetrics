f9512cff7725e4a227bd7e284f9ce69627569267
Jiekun <jiekun@victoriametrics.com>
2025-05-20 22:11:04 +0800

    feature: [concurrent dial] update unit test to automate the test for concurrent dial

--- lib/netutil/conn_pool_test.go
+++ lib/netutil/conn_pool_test.go
@@ -2,6 +2,10 @@ package netutil
 
 import (
 	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"sync"
 	"testing"
 	"time"
@@ -61,29 +65,64 @@ func testConnPoolStartStop(t *testing.T, name string, ms *metrics.Set) {
 	}
 }
 
+// TestGetPutDialConnectionPool test the concurrent dial limit of the connection pool.
+// It simulates 256 concurrent goroutine to get new connections, while the concurrent dial limit shouldn't be more than 64.
+// After the first 64 goroutines got connections, the rest 192 goroutines will race for the `select-case`.
+// Only 64 of them have chance to create new connections while the rest will go to the default case and try to obtain
+// used connections.
+//
+// Without extra logging or metrics, it's hard to verify if all the paths are covered. But the coverage report shows it
+// does.
 func TestGetPutDialConnectionPool(t *testing.T) {
-	t.Skip()
-	ms := metrics.NewSet()
-	dialTimeout := 5 * time.Second
-	compressLevel := 1
-	cp := NewConnPool(ms, "test-pool", "127.0.0.1:8401", handshake.VMSelectClient, compressLevel, dialTimeout, 0)
+	mockSvr := newMockServer()
+	addr, _ := url.Parse(mockSvr.URL)
+	cp := NewConnPool(metrics.NewSet(), "test-pool", addr.Host, mockHandshake, 1, 5*time.Second, 0)
 
-	connChan := make(chan *handshake.BufferedConn, 50)
-	wg := sync.WaitGroup{}
-	for i := 0; i < 50; i++ {
-		wg.Add(1)
+	concurrency := 256
+	connChan := make(chan *handshake.BufferedConn, concurrency)
+
+	// concurrent create connections
+	for i := 0; i < concurrency; i++ {
 		go func() {
 			conn, err := cp.Get()
 			if err != nil {
-				t.Errorf("expecting non-nil error after MustStop(), err:%v", err)
+				t.Errorf("get conn from connection pool err:%v", err)
 			}
 			connChan <- conn
-			wg.Done()
 		}()
 	}
 
-	for i := 0; i < 50; i++ {
-		cp.Put(<-connChan)
+	// concurrent return connections to pool.
+	wg := sync.WaitGroup{}
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func() {
+			conn := <-connChan
+			time.Sleep(time.Millisecond * 10)
+			cp.Put(conn)
+			wg.Done()
+		}()
 	}
 	wg.Wait()
+}
+
+// mockServer does nothing. It only acts as a tcp server for connection test.
+type mockServer struct {
+	*httptest.Server
+	responseFunc func() []byte
+}
+
+func newMockServer() *mockServer {
+	var s mockServer
+	s.Server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte(""))
+	}))
+	return &s
+}
+
+func mockHandshake(c net.Conn, compressionLevel int) (*handshake.BufferedConn, error) {
+	bc := &handshake.BufferedConn{
+		Conn: c,
+	}
+	return bc, nil
 }
