756c4f37d233603ab2a9b3344375881041cfb060
dmitryk-dk <kozlovdmitriyy@gmail.com>
2025-05-29 11:09:19 +0200

    apptest/vmctl: migrate vmctl test for the prometheus migration process
    apptest/vmctl: wait until vmctl finish it work
    
    apptest/vmctl: remove unneeded function
    
    apptest/vmctl: fix linter
    
    apptest/vmctl: add vmctl to build for the integration tests
    
    apptest/vmctl: rename file
    
    apptest/vmctl: fix comments
    
    apptest/vmctl: added check of the wait discussed in the comments
    
    apptest/vmctl: compare migrated data with expected response
    
    apptest/vmctl: format expected_response.json
    
    apptest/vmctl: decrease response size
    
    apptest/vmctl: decrease response size
    
    apptest/vmctl: update response
    
    apptest/vmctl: update test response
    
    apptest/vmctl: use unmarshal
    
    apptest/vmctl: update response
    
    apptest/vmctl: update response
    
    apptest/vmctl: update response
    
    apptest/vmctl: just test
    
    apptest/vmctl: update snapshot
    
    apptest/vmctl: cleanup
    
    apptest/vmctl: modify request and response to reduce the amount of data
    
    apptest/vmctl: add cluster tests
    
    apptest/vmctl: update cluster tests

---
+++ apptest/tests/vmctl_prometheus_migration_test.go
@@ -0,0 +1,132 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/apptest"
+)
+
+const (
+	testSnapshot         = "./testdata/prometheus/snapshots/20250602T205846Z-7e03e43cf46dda03"
+	expectedResponseFile = "./testdata/prometheus/expected_response.json"
+)
+
+func TestSingleVmctlPrometheusProtocol(t *testing.T) {
+	os.RemoveAll(t.Name())
+
+	tc := apptest.NewTestCase(t)
+	defer tc.Stop()
+
+	vmsingleDst := tc.MustStartVmsingle("vmsingle", []string{
+		"-storageDataPath=" + tc.Dir() + "/vmsingle",
+		"-retentionPeriod=100y",
+	})
+	vmAddr := fmt.Sprintf("http://%s/", vmsingleDst.HTTPAddr())
+	vmctlFlags := []string{
+		`prometheus`,
+		`--prom-snapshot=` + testSnapshot,
+		`--vm-addr=` + vmAddr,
+		`--disable-progress-bar=true`,
+	}
+
+	testPrometheusProtocol(tc, vmsingleDst, vmctlFlags)
+}
+
+func TestClusterVmctlPrometheusProtocol(t *testing.T) {
+	os.RemoveAll(t.Name())
+
+	tc := apptest.NewTestCase(t)
+	defer tc.Stop()
+
+	vmstorage := tc.MustStartVmstorage("vmstorage", []string{
+		"-storageDataPath=" + tc.Dir() + "/vmstorage",
+		"-retentionPeriod=100y",
+	})
+	vminsert := tc.MustStartVminsert("vminsert", []string{
+		"-storageNode=" + vmstorage.VminsertAddr(),
+	})
+	vmselect := tc.MustStartVmselect("vmselect", []string{
+		"-storageNode=" + vmstorage.VmselectAddr(),
+	})
+
+	cluster := &apptest.Vmcluster{
+		Vminsert:   vminsert,
+		Vmselect:   vmselect,
+		Vmstorages: []*apptest.Vmstorage{vmstorage},
+	}
+	vmAddr := fmt.Sprintf("http://%s/", vminsert.HTTPAddr())
+	vmctlFlags := []string{
+		`prometheus`,
+		`--prom-snapshot=` + testSnapshot,
+		`--vm-addr=` + vmAddr,
+		`--disable-progress-bar=true`,
+		`--vm-account-id=0`,
+	}
+
+	testPrometheusProtocol(tc, cluster, vmctlFlags)
+}
+
+func testPrometheusProtocol(tc *apptest.TestCase, sut apptest.PrometheusWriteQuerier, vmctlFlags []string) {
+	t := tc.T()
+	t.Helper()
+
+	cmpOpt := cmpopts.IgnoreFields(apptest.PrometheusAPIV1QueryResponse{}, "Status", "Data.ResultType")
+
+	// test for empty data request
+	got := sut.PrometheusAPIV1Query(t, `{__name__=~".*"}`, apptest.QueryOpts{
+		Step: "5m",
+		Time: "2025-06-02T17:14:00Z",
+	})
+
+	want := apptest.NewPrometheusAPIV1QueryResponse(t, `{"data":{"result":[]}}`)
+	if diff := cmp.Diff(want, got, cmpOpt); diff != "" {
+		t.Errorf("unexpected response (-want, +got):\n%s", diff)
+	}
+
+	_ = tc.MustStartVmctl("vmctl", vmctlFlags)
+
+	sut.ForceFlush(t)
+
+	// open the expected series response file
+	file, err := os.Open(expectedResponseFile)
+	if err != nil {
+		t.Fatalf("cannot open expected series response file: %s", err)
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatalf("cannot read expected series response file: %s", err)
+	}
+
+	var wantResponse apptest.PrometheusAPIV1QueryResponse
+	if err := json.Unmarshal(bytes, &wantResponse); err != nil {
+		t.Fatalf("cannot unmarshal expected series response file: %s", err)
+	}
+	wantResponse.Sort()
+
+	tc.Assert(&apptest.AssertOptions{
+		// For cluster version, we need to wait longer for the metrics to be stored
+		Retries: 300,
+		Msg:     `unexpected metrics stored on vmsingle via the prometheus protocol`,
+		Got: func() any {
+			expected := sut.PrometheusAPIV1Export(t, `{__name__="vm_log_messages_total", location=~"VictoriaMetrics/lib/ingestserver/opentsdb/server.go:(48|59)"}`, apptest.QueryOpts{
+				Start: "2025-06-02T00:00:00Z",
+				End:   "2025-06-02T23:59:59Z",
+			})
+			expected.Sort()
+			return expected.Data.Result
+		},
+		Want: wantResponse.Data.Result,
+		CmpOpts: []cmp.Option{
+			cmpopts.IgnoreFields(apptest.PrometheusAPIV1QueryResponse{}, "Status", "Data.ResultType"),
+		},
+	})
+}
