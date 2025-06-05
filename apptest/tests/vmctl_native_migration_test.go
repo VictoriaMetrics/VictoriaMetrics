ed87d4177f534e3c282a377fe92e3c495ef67eb5
dmitryk-dk <kozlovdmitriyy@gmail.com>
2025-06-05 17:48:10 +0200

    apptest/vmctl: implement single to single migration test

--- apptest/tests/vmctl_native_migration_test.go
+++ apptest/tests/vmctl_native_migration_test.go
@@ -11,21 +11,42 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/apptest"
 )
 
-func TestVmctlVMSingleToVMSingle(t *testing.T) {
+func TestSingleToSingleVmctlNativeProtocol(t *testing.T) {
 	os.RemoveAll(t.Name())
 
 	tc := apptest.NewTestCase(t)
 	defer tc.Stop()
 
-	cmpOpt := cmpopts.IgnoreFields(apptest.PrometheusAPIV1QueryResponse{}, "Status", "Data.ResultType")
-
-	vmsingleSrc := tc.MustStartVmsingle("vmsingle_src", []string{
-		"-storageDataPath=" + tc.Dir() + "/vmsingle_src",
+	vmsingleSrc := tc.MustStartDefaultVmsingle()
+	// we need a separate vmsingle for the destination to avoid conflicts
+	vmsingleDst := tc.MustStartVmsingle("vmsingle_dst", []string{
+		"-storageDataPath=" + tc.Dir() + "/vmsingle_dst",
 		"-retentionPeriod=100y",
 	})
 
-	// test for empty data request in the source vmsingle
-	got := vmsingleSrc.PrometheusAPIV1Query(t, `{__name__=~".*"}`, apptest.QueryOpts{
+	vmSrcAddr := fmt.Sprintf("http://%s/", vmsingleSrc.HTTPAddr())
+	vmDstAddr := fmt.Sprintf("http://%s/", vmsingleDst.HTTPAddr())
+
+	flags := []string{
+		`vm-native`,
+		`--vm-native-src-addr=` + vmSrcAddr,
+		`--vm-native-dst-addr=` + vmDstAddr,
+		`--vm-native-filter-match={__name__=~".*"}`,
+		`--vm-native-filter-time-start=2025-05-30T16:39:00Z`,
+		`--disable-progress-bar=true`,
+	}
+
+	testNativeProtocol(tc, vmsingleSrc, vmsingleDst, flags)
+}
+
+func testNativeProtocol(tc *apptest.TestCase, srcSut apptest.PrometheusWriteQuerier, dstSut apptest.PrometheusWriteQuerier, vmctlFlags []string) {
+	t := tc.T()
+	t.Helper()
+
+	cmpOpt := cmpopts.IgnoreFields(apptest.PrometheusAPIV1QueryResponse{}, "Status", "Data.ResultType")
+
+	// test for empty data request in the source
+	got := srcSut.PrometheusAPIV1Query(t, `{__name__=~".*"}`, apptest.QueryOpts{
 		Step: "5m",
 		Time: "2025-05-30T12:45:00Z",
 	})
@@ -35,69 +56,55 @@ func TestVmctlVMSingleToVMSingle(t *testing.T) {
 		t.Errorf("unexpected response (-want, +got):\n%s", diff)
 	}
 
-	vmsingleDst := tc.MustStartVmsingle("vmsingle_dst", []string{
-		"-storageDataPath=" + tc.Dir() + "/vmsingle_dst",
-		"-retentionPeriod=100y",
-	})
-
-	// test for empty data request in the destination vmsingle
-	got = vmsingleDst.PrometheusAPIV1Query(t, `{__name__=~".*"}`, apptest.QueryOpts{
-		Step: "5m",
-		Time: "2025-01-18T12:45:00Z",
-	})
-
-	want = apptest.NewPrometheusAPIV1QueryResponse(t, `{"data":{"result":[]}}`)
-	if diff := cmp.Diff(want, got, cmpOpt); diff != "" {
-		t.Errorf("unexpected response (-want, +got):\n%s", diff)
-	}
-
 	// Prepare the source vmsingle with some data
 	// Insert some data.
 	const numSamples = 1000
 	const ingestTimestamp = " 1748623176000" // 2025-05-30T16:39:36Z
+
+	expectedQueryData := apptest.QueryData{
+		ResultType: "matrix",
+		Result:     make([]*apptest.QueryResult, 0, numSamples),
+	}
 	dataSet := make([]string, numSamples)
 	for i := range numSamples {
-		dataSet[i] = fmt.Sprintf("metric_%03d %d %s", i, i, ingestTimestamp)
+		metricsName := fmt.Sprintf("metric_%03d", i)
+		metrics := map[string]string{"__name__": metricsName}
+		sample := &apptest.Sample{Value: float64(i), Timestamp: 1748623176000}
+		expectedQueryData.Result = append(expectedQueryData.Result, &apptest.QueryResult{
+			Metric:  metrics,
+			Samples: []*apptest.Sample{sample},
+		})
+
+		dataSet[i] = fmt.Sprintf("%s %d %s", metricsName, i, ingestTimestamp)
 	}
 
-	vmsingleSrc.PrometheusAPIV1ImportPrometheus(t, dataSet, apptest.QueryOpts{})
-	vmsingleSrc.ForceFlush(t)
+	wantResponse := apptest.PrometheusAPIV1QueryResponse{
+		Status: "success",
+		Data:   &expectedQueryData,
+	}
+
+	wantResponse.Sort()
+
+	srcSut.PrometheusAPIV1ImportPrometheus(t, dataSet, apptest.QueryOpts{})
+	srcSut.ForceFlush(t)
+
+	_ = tc.MustStartVmctl("vmctl", vmctlFlags)
+
+	dstSut.ForceFlush(t)
 
 	tc.Assert(&apptest.AssertOptions{
-		Msg: `unexpected metrics stored on vmsingle source`,
+		Msg: `unexpected metrics stored on vmsingle via the native protocol`,
 		Got: func() any {
-			exported := vmsingleSrc.PrometheusAPIV1Export(t, `{__name__=~".*"}`, apptest.QueryOpts{
+			exported := dstSut.PrometheusAPIV1Export(t, `{__name__=~".*"}`, apptest.QueryOpts{
 				Start: "2025-05-30T16:39:36Z",
 				End:   "2025-05-30T16:39:37Z",
 			})
-			return len(exported.Data.Result)
+			exported.Sort()
+			return exported.Data.Result
 		},
-		Want: numSamples,
-	})
-
-	vmSrcAddr := fmt.Sprintf("http://%s/", vmsingleSrc.HTTPAddr())
-	vmDstAddr := fmt.Sprintf("http://%s/", vmsingleDst.HTTPAddr())
-
-	_ = tc.MustStartVmctl("vmctl", []string{
-		`vm-native`,
-		`--vm-native-src-addr=` + vmSrcAddr,
-		`--vm-native-dst-addr=` + vmDstAddr,
-		`--vm-native-filter-match={__name__=~".*"}`,
-		`--vm-native-filter-time-start=2025-05-30T16:39:00Z`,
-		`--disable-progress-bar=true`,
-	})
-
-	vmsingleDst.ForceFlush(t)
-
-	tc.Assert(&apptest.AssertOptions{
-		Msg: `unexpected metrics stored on vmsingle via the prometheus protocol`,
-		Got: func() any {
-			exported := vmsingleSrc.PrometheusAPIV1Export(t, `{__name__=~".*"}`, apptest.QueryOpts{
-				Start: "2025-05-30T16:39:36Z",
-				End:   "2025-05-30T16:39:37Z",
-			})
-			return len(exported.Data.Result)
+		Want: wantResponse.Data.Result,
+		CmpOpts: []cmp.Option{
+			cmpopts.IgnoreFields(apptest.PrometheusAPIV1QueryResponse{}, "Status", "Data.ResultType"),
 		},
-		Want: numSamples,
 	})
 }
