08da7393194786d7241aed20a1279ca7c8074f0d
Jiekun <jiekun@victoriametrics.com>
2025-06-12 14:51:47 +0800

    feature: [jaeger] make linter happy

--- lib/hashpool/hashpool.go
+++ lib/hashpool/hashpool.go
@@ -11,11 +11,12 @@ var xxhashPool = &sync.Pool{
 	},
 }
 
+// Get return a *xxhash.Digest from hash pool.
 func Get() *xxhash.Digest {
 	return xxhashPool.Get().(*xxhash.Digest)
 }
 
+// Put a *xxhash.Digest back to the hash pool.
 func Put(x *xxhash.Digest) {
 	xxhashPool.Put(x)
-	return
 }
