9e60fc8fc80c39fb439704e5b4a8d39814245ff2
Artur Minchukou <aminchukov@victoriametrics.com>
2025-06-20 19:26:28 +0400

    app/vmui/logs: move compact mode of VictoriaLogs table to separate tab (#8745)
    ### Describe Your Changes
    
    Related issue: #7047 and #8438
    
    - removed compact mode of VictoriaLogs table
    - added sorting by field to JSON tab 
    
    ![image](https://github.com/user-attachments/assets/c02bbbed-cf2c-41e3-86fe-97bc205654a5)
    - added sorting of field names to JSON tab
    
    
    ### Checklist
    
    The following checks are **mandatory**:
    
    - [ ] My change adheres to [VictoriaMetrics contributing
    guidelines](https://docs.victoriametrics.com/contributing/).

--- app/vmui/packages/vmui/src/pages/ExploreLogs/ExploreLogsBody/views/JsonView/JsonView.tsx
+++ app/vmui/packages/vmui/src/pages/ExploreLogs/ExploreLogsBody/views/JsonView/JsonView.tsx
@@ -1,21 +1,67 @@
-import React, { FC } from "preact/compat";
+import React, { FC, useMemo, useCallback, createPortal } from "preact/compat";
 import DownloadLogsButton from "../../../DownloadLogsButton/DownloadLogsButton";
-import { createPortal } from "preact/compat";
 import JsonViewComponent from "../../../../../components/Views/JsonView/JsonView";
 import { ViewProps } from "../../types";
 import EmptyLogs from "../components/EmptyLogs/EmptyLogs";
-import { useCallback } from "react";
+import JsonViewSettings from "./JsonViewSettings/JsonViewSettings";
+import { useSearchParams } from "react-router-dom";
+import orderBy from "lodash.orderBy";
+import "./style.scss";
+import { Logs } from "../../../../../api/types";
+import { SortDirection } from "./types";
 
 const MemoizedJsonView = React.memo(JsonViewComponent);
 
+const jsonQuerySortParam = "json_sort";
+const fieldSortQueryParamName = "json_field_sort";
+
 const JsonView: FC<ViewProps> = ({ data, settingsRef }) => {
   const getLogs = useCallback(() => data, [data]);
+
+  const [searchParams] = useSearchParams();
+  const sortParam = searchParams.get(jsonQuerySortParam);
+  const fieldSortParam = searchParams.get(fieldSortQueryParamName) as SortDirection;
+
+  const [sortField, sortDirection] = useMemo(() => {
+    const [sortField, sortDirection] = sortParam?.split(":").map(decodeURIComponent) || [];
+    return [sortField, sortDirection as "asc" | "desc" | undefined];
+  }, [sortParam]);
+
+  const fields = useMemo(() => {
+    const keys = new Set(data.flatMap(Object.keys));
+    return Array.from(keys);
+  }, [data]);
+
+  const orderedFieldsData = useMemo(() => {
+    if (!fieldSortParam) return data;
+    const orderedFields = fields.toSorted((a, b) => fieldSortParam === "asc" ? a.localeCompare(b): b.localeCompare(a));
+    return data.map((item) => {
+      return orderedFields.reduce((acc, field) => {
+        if (item[field]) acc[field] = item[field];
+        return acc;
+      }, {} as Logs);
+    });
+  }, [fields, fieldSortParam, data]);
+
+  const sortedData = useMemo(() => {
+    if (!sortField || !sortDirection) return orderedFieldsData;
+    return orderBy(orderedFieldsData, [sortField], [sortDirection]);
+  }, [orderedFieldsData, sortField, sortDirection]);
 
   const renderSettings = () => {
     if (!settingsRef.current) return null;
 
     return createPortal(
-      data.length > 0 && <DownloadLogsButton getLogs={getLogs} />,
+      data.length > 0 && (
+        <div className="vm-json-view__settings-container">
+          <DownloadLogsButton getLogs={getLogs} />
+          <JsonViewSettings
+            fields={fields}
+            sortQueryParamName={jsonQuerySortParam}
+            fieldSortQueryParamName={fieldSortQueryParamName}
+          />
+        </div>
+      ),
       settingsRef.current
     );
   };
@@ -25,9 +71,11 @@ const JsonView: FC<ViewProps> = ({ data, settingsRef }) => {
   return (
     <>
       {renderSettings()}
-      <MemoizedJsonView data={data} />
+      <MemoizedJsonView
+        data={sortedData}
+      />
     </>
   );
 };
 
-export default JsonView; 
+export default JsonView;
