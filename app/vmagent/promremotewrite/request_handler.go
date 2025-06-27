78858b2ec89a8c42b61503cfb222882ff3df846f
Haley Wang <haley@victoriametrics.com>
2025-06-27 15:11:33 +0800

    vmagent: support receiving metric metadata from prometheus remote write v1 and OTLP
    metadata supports multi-tenant
    
    otel endpoint supports metadata
    
    fix OTLP to Prometheus type
    
    1

--- app/vmagent/promremotewrite/request_handler.go
+++ app/vmagent/promremotewrite/request_handler.go
@@ -16,6 +16,7 @@ import (
 
 var (
 	rowsInserted       = metrics.NewCounter(`vmagent_rows_inserted_total{type="promremotewrite"}`)
+	metadataInserted   = metrics.NewCounter(`vmagent_metadata_inserted_total{type="promremotewrite"}`)
 	rowsTenantInserted = tenantmetrics.NewCounterMap(`vmagent_tenant_inserted_rows_total{type="promremotewrite"}`)
 	rowsPerInsert      = metrics.NewHistogram(`vmagent_rows_per_insert{type="promremotewrite"}`)
 )
@@ -27,17 +28,18 @@ func InsertHandler(at *auth.Token, req *http.Request) error {
 		return err
 	}
 	isVMRemoteWrite := req.Header.Get("Content-Encoding") == "zstd"
-	return stream.Parse(req.Body, isVMRemoteWrite, func(tss []prompb.TimeSeries, mss []prompb.MetricMetadata) error {
-		return insertRows(at, tss, mss, extraLabels)
+	return stream.Parse(req.Body, isVMRemoteWrite, func(tss []prompb.TimeSeries, mms []prompb.MetricMetadata) error {
+		return insertRows(at, tss, mms, extraLabels)
 	})
 }
 
-func insertRows(at *auth.Token, timeseries []prompb.TimeSeries, metricsMeta []prompb.MetricMetadata, extraLabels []prompbmarshal.Label) error {
+func insertRows(at *auth.Token, timeseries []prompb.TimeSeries, mms []prompb.MetricMetadata, extraLabels []prompbmarshal.Label) error {
 	ctx := common.GetPushCtx()
 	defer common.PutPushCtx(ctx)
 
 	rowsTotal := 0
 	tssDst := ctx.WriteRequest.Timeseries[:0]
+	mmsDst := ctx.WriteRequest.Metadata[:0]
 	labels := ctx.Labels[:0]
 	samples := ctx.Samples[:0]
 	for i := range timeseries {
@@ -65,22 +67,27 @@ func insertRows(at *auth.Token, timeseries []prompb.TimeSeries, metricsMeta []pr
 			Samples: samples[samplesLen:],
 		})
 	}
-	ctx.WriteRequest.Timeseries = tssDst
-	ctx.Labels = labels
-	ctx.Samples = samples
+	var accountID, projectID uint32
+	if at != nil {
+		accountID = at.AccountID
+		projectID = at.ProjectID
+	}
+	for i := range mms {
+		mm := &mms[i]
+		mmsDst = append(mmsDst, prompbmarshal.MetricMetadata{
+			MetricFamilyName: mm.MetricFamilyName,
+			Help:             mm.Help,
+			Type:             mm.Type,
+			Unit:             mm.Unit,
 
-	metadata := ctx.WriteRequest.Metadata[:0]
-	for i := range metricsMeta {
-		meta := &metricsMeta[i]
-		metadata = append(metadata, prompbmarshal.MetricMetadata{
-			MetricFamilyName: meta.MetricFamilyName,
-			Type:             meta.Type,
-			Help:             meta.Help,
-			Unit:             meta.Unit,
+			AccountID: accountID,
+			ProjectID: projectID,
 		})
 	}
-	ctx.WriteRequest.Metadata = metadata
-
+	ctx.WriteRequest.Timeseries = tssDst
+	ctx.WriteRequest.Metadata = mmsDst
+	ctx.Labels = labels
+	ctx.Samples = samples
 	if !remotewrite.TryPush(at, &ctx.WriteRequest) {
 		return remotewrite.ErrQueueFullHTTPRetry
 	}
@@ -88,6 +95,7 @@ func insertRows(at *auth.Token, timeseries []prompb.TimeSeries, metricsMeta []pr
 	if at != nil {
 		rowsTenantInserted.Get(at).Add(rowsTotal)
 	}
+	metadataInserted.Add(len(mmsDst))
 	rowsPerInsert.Update(float64(rowsTotal))
 	return nil
 }
