a3fb0fece173b5b21348f831c7b50f33c7358acc
Jiekun <jiekun@victoriametrics.com>
2025-07-03 00:01:52 +0800

    feature: [memory limit] replace memory usage

--- lib/memory/memory_linux.go
+++ lib/memory/memory_linux.go
@@ -1,6 +1,10 @@
 package memory
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/cgroup"
@@ -31,16 +35,12 @@ func sysTotalMemory() int {
 }
 
 func sysCurrentMemory() int {
-	var si syscall.Sysinfo_t
-	if err := syscall.Sysinfo(&si); err != nil {
-		logger.Panicf("FATAL: error in syscall.Sysinfo: %s", err)
-	}
-	usedMem := maxInt
-	if uint64(maxInt)/uint64(si.Totalram-si.Freeram) > uint64(si.Unit) {
-		usedMem = int(uint64(si.Totalram-si.Freeram) * uint64(si.Unit))
+	ms, err := getMemStats("/proc/self/status")
+	if err != nil {
+		return 0
 	}
 
-	return usedMem
+	return int(ms.rssAnon)
 	//mem := cgroup.GetMemoryUsage()
 	//if mem <= 0 || int64(int(mem)) != mem || int(mem) > usedMem {
 	//	mem = cgroup.GetHierarchicalMemoryUsage()
@@ -49,4 +49,55 @@ func sysCurrentMemory() int {
 	//	}
 	//}
 	//return int(mem)
+}
+
+func getMemStats(path string) (*memStats, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var ms memStats
+	lines := strings.Split(string(data), "\n")
+	for _, s := range lines {
+		if !strings.HasPrefix(s, "Vm") && !strings.HasPrefix(s, "Rss") {
+			continue
+		}
+		// Extract key value.
+		line := strings.Fields(s)
+		if len(line) != 3 {
+			return nil, fmt.Errorf("unexpected number of fields found in %q; got %d; want %d", s, len(line), 3)
+		}
+		memStatName := line[0]
+		memStatValue := line[1]
+		value, err := strconv.ParseUint(memStatValue, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse number from %q: %w", s, err)
+		}
+		if line[2] != "kB" {
+			return nil, fmt.Errorf("expecting kB value in %q; got %q", s, line[2])
+		}
+		value *= 1024
+		switch memStatName {
+		case "VmPeak:":
+			ms.vmPeak = value
+		case "VmHWM:":
+			ms.rssPeak = value
+		case "RssAnon:":
+			ms.rssAnon = value
+		case "RssFile:":
+			ms.rssFile = value
+		case "RssShmem:":
+			ms.rssShmem = value
+		}
+	}
+	return &ms, nil
+}
+
+// https://man7.org/linux/man-pages/man5/procfs.5.html
+type memStats struct {
+	vmPeak   uint64
+	rssPeak  uint64
+	rssAnon  uint64
+	rssFile  uint64
+	rssShmem uint64
 }
