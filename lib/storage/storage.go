d870e4ac19877bdb9055cb93b8faa1936a9e4d44
Zakhar Bessarab <z.bessarab@victoriametrics.com>
2025-07-09 15:54:39 +0400

    app/vminsert: implement metadata ingestion
    Implement ingesting and storing data to temporary implementation of storage.
    
    Signed-off-by: Zakhar Bessarab <z.bessarab@victoriametrics.com>

--- lib/storage/storage.go
+++ lib/storage/storage.go
@@ -177,6 +177,9 @@ type Storage struct {
 
 	// logNewSeriesUntil is the timestamp until which new series will be logged. We will log new series when logNewSeries is true or logNewSeriesUntil is greater than the current time.
 	logNewSeriesUntil atomic.Uint64
+
+	metricsMetadataStorage    map[string][]*MetricMetadataRow
+	metricMetadataStorageLock sync.RWMutex
 }
 
 type pendingHourMetricIDEntry struct {
@@ -1880,6 +1883,36 @@ func (s *Storage) AddRows(mrs []MetricRow, precisionBits uint8) {
 		s.rowsReceivedTotal.Add(uint64(len(mrsBlock)))
 	}
 	putMetricRowsInsertCtx(ic)
+}
+
+func (s *Storage) AddMetadataRows(metaRows []MetricMetadataRow) {
+	if len(metaRows) == 0 {
+		return
+	}
+	s.metricMetadataStorageLock.Lock()
+	defer s.metricMetadataStorageLock.Unlock()
+	if s.metricsMetadataStorage == nil {
+		s.metricsMetadataStorage = make(map[string][]*MetricMetadataRow, len(metaRows))
+
+	}
+	for _, mr := range metaRows {
+		if _, ok := s.metricsMetadataStorage[string(mr.MetricFamilyName)]; !ok {
+			s.metricsMetadataStorage[string(mr.MetricFamilyName)] = make([]*MetricMetadataRow, 0, 1)
+			s.metricsMetadataStorage[string(mr.MetricFamilyName)] = append(s.metricsMetadataStorage[string(mr.MetricFamilyName)], &mr)
+			continue
+		}
+		found := false
+		for _, v := range s.metricsMetadataStorage[string(mr.MetricFamilyName)] {
+			if v.Type == mr.Type && bytes.Equal(mr.Unit, v.Unit) && bytes.Equal(mr.Help, v.Help) {
+				found = true
+			}
+		}
+		if found {
+			continue
+		}
+		s.metricsMetadataStorage[string(mr.MetricFamilyName)] = append(s.metricsMetadataStorage[string(mr.MetricFamilyName)], &mr)
+	}
+	logger.Infof("Added %d metadata rows to storage", len(metaRows))
 }
 
 type metricRowsInsertCtx struct {
