4d0cdf1dc6be5b750df9471c4a12002fcfeb9d41
Andrei Baidarov <baidarov@nebius.com>
2025-07-18 09:00:45 +0200

    lib/storage: non-empty extDB in search tests/benchmarks (#9446)
    Current tests do not cover search from extDB

--- lib/storage/search_test.go
+++ lib/storage/search_test.go
@@ -169,7 +169,10 @@ func TestSearch_VariousTimeRanges(t *testing.T) {
 
 		s := MustOpenStorage(t.Name(), OpenOptions{})
 		defer s.MustClose()
-		s.AddRows(mrs, defaultPrecisionBits)
+		s.AddRows(mrs[:numMetrics/2], defaultPrecisionBits)
+		// Rotate the indexDB to ensure that the search operation covers both current and prev indexDBs.
+		s.mustRotateIndexDB(time.Now())
+		s.AddRows(mrs[numMetrics/2:], defaultPrecisionBits)
 		s.DebugFlush()
 
 		if err := testSearchInternal(s, tr, mrs); err != nil {
