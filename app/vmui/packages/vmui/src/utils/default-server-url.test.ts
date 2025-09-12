VictoriaMetrics/VictoriaMetrics

commit 483e00ffb90e1638b1c09b4481ba65e36416df76

vmui: replace VMAlert proxy with Alerting tab in VMUI (#8989)
### Describe Your Changes

Rules page header + content
<img width="1235" height="520" alt="image"
src="https://github.com/user-attachments/assets/bb0c5818-c44a-46e6-bc47-e6718be34016"
/>
Expanded rule without alert
<img width="1418" alt="image"
src="https://github.com/user-attachments/assets/ae0b265f-24fe-4549-8913-b1be8e7c2862"
/>
Expanded rule with alert
<img width="1418" alt="image"
src="https://github.com/user-attachments/assets/8a138403-0712-4de2-bfa5-467da3a979dd"
/>
Notifiers page
<img width="1419" alt="image"
src="https://github.com/user-attachments/assets/557c2831-e960-44ec-9b93-f1ebfeb1fbb0"
/>

fixes https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8330
fixes https://github.com/VictoriaMetrics/VictoriaMetrics/issues/6091
fixes https://github.com/VictoriaMetrics/VictoriaLogs/issues/90

VMUI:
- Added added `Alerting -> Rules` and `Alerting -> Notifiers` pages for
VictoriaMetrics
- Support includeAll option in Select component

VMAlert:
- added `/api/v1/group`useful to get information about certain group
- added `lastError` for `/api/v1/notifiers` for each target to see
information about failed notifiers

### Checklist

The following checks are **mandatory**:

- [ ] My change adheres to [VictoriaMetrics contributing
guidelines](https://docs.victoriametrics.com/victoriametrics/contributing/).

---------

Signed-off-by: hagen1778 <roman@victoriametrics.com>
Co-authored-by: hagen1778 <roman@victoriametrics.com>

app/vmui/packages/vmui/src/utils/default-server-url.ts
@@ -7,7 +7,8 @@ export const getDefaultServer = (tenantId?: string): string => {
   const { serverURL } = getAppModeParams();
   const storageURL = getFromStorage("SERVER_URL") as string;
   const anomalyURL = `${window.location.origin}${window.location.pathname.replace(/^\/vmui/, "")}`;
-  const defaultURL = window.location.href.replace(/(\/(?:prometheus\/)?(?:graph|vmui)\/.*|\/#\/.*)/, "/prometheus");
+  const baseURL = window.location.href.replace(/(\/(?:prometheus\/)?(?:graph|vmui)\/.*|\/#\/.*)/, "");
+  const defaultURL = baseURL.replace(/(\/select\/[\d:]+)$/, "$1/prometheus");
   const url = serverURL || storageURL || defaultURL;
 
   switch (APP_TYPE) {

commit 2a1b3866e18b819bc7bc9e00df6086fee2b343f1

app/vmui: fixed backend URL for multitenant endpoints (#9703)
### Describe Your Changes

vmui builds incorrect endpoint, while using multitenant API. bug was
introduced in PR #8989

### Checklist

The following checks are **mandatory**:

- [ ] My change adheres to [VictoriaMetrics contributing
guidelines](https://docs.victoriametrics.com/victoriametrics/contributing/#pull-request-checklist).
- [ ] My change adheres to [VictoriaMetrics development
goals](https://docs.victoriametrics.com/victoriametrics/goals/).

Co-authored-by: Max Kotliar <mkotlyar@victoriametrics.com>

app/vmui/packages/vmui/src/utils/default-server-url.ts
@@ -3,12 +3,15 @@ import { replaceTenantId } from "./tenants";
 import { APP_TYPE, AppType } from "../constants/appType";
 import { getFromStorage } from "./storage";
 
+export const getDefaultURL = (u: string) => {
+  return u.replace(/(\/(?:prometheus\/)?(?:graph|vmui)\/.*|\/#\/.*)/, "").replace(/(\/select\/[^/]+)$/, "$1/prometheus");
+};
+
 export const getDefaultServer = (tenantId?: string): string => {
   const { serverURL } = getAppModeParams();
   const storageURL = getFromStorage("SERVER_URL") as string;
   const anomalyURL = `${window.location.origin}${window.location.pathname.replace(/^\/vmui/, "")}`;
-  const baseURL = window.location.href.replace(/(\/(?:prometheus\/)?(?:graph|vmui)\/.*|\/#\/.*)/, "");
-  const defaultURL = baseURL.replace(/(\/select\/[\d:]+)$/, "$1/prometheus");
+  const defaultURL = getDefaultURL(window.location.href);
   const url = serverURL || storageURL || defaultURL;
 
   switch (APP_TYPE) {

app/vmui/packages/vmui/src/utils/tenants.ts
@@ -1,4 +1,4 @@
-const regexp = /(\/select\/)(\d+|\d.+)(\/)(.+)/;
+const regexp = /(\/select\/)([^/])(\/)(.+)/;
 
 export const replaceTenantId = (serverUrl: string, tenantId: string) => {
   return serverUrl.replace(regexp, `$1${tenantId}/$4`);

app/vmui/packages/vmui/src/utils/default-server-url.test.ts
@@ -1,0 +1,26 @@
+import { describe, it, expect } from "vitest";
+import { getDefaultURL } from "./default-server-url";
+
+describe("test server urls", () => {
+  describe("getDefaultURL()", () => {
+    it("/select/0/vmui/", () => {
+      const result = getDefaultURL("https://localhost:1111/select/0/vmui/");
+      expect(result).toBe("https://localhost:1111/select/0/prometheus");
+    });
+
+    it("/any/path/prefix/select/multitenant/vmui/#/rules?q=test", () => {
+      const result = getDefaultURL("http://test/any/path/prefix/select/multitenant/vmui/#/rules?q=test");
+      expect(result).toBe("http://test/any/path/prefix/select/multitenant/prometheus");
+    });
+
+    it("/test/select/1:1/prometheus/graph/", () => {
+      const result = getDefaultURL("https://domain.com/test/select/1:1/prometheus/graph/");
+      expect(result).toBe("https://domain.com/test/select/1:1/prometheus");
+    });
+
+    it("https://play.vm.com/#/rules?q=test", () => {
+      const result = getDefaultURL("https://play.vm.com/#/rules?q=test");
+      expect(result).toBe("https://play.vm.com");
+    });
+  });
+});