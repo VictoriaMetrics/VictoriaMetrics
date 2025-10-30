44d33c1570623dd6194aecc321b898b9ce8783c8
Aliaksandr Valialkin <valyala@victoriametrics.com>
2025-10-30 11:55:16 +0100

    lib/fs/fsutil: set the default value for -fs.maxConcurrency depending on the number of available CPU cores
    This should reduce the need to tune this flag on systems with different number of CPU cores.
    16 concurrent file operations per CPU should give quite low Go scheduling latency (~10ms)
    according to https://github.com/VictoriaMetrics/VictoriaLogs/issues/774#issuecomment-3456814064
    
    This is a follow-up for the commit 8a9a40dbdde06e252a23c3059367878ef0797d9c

--- lib/fs/fsutil/concurrency.go
+++ lib/fs/fsutil/concurrency.go
@@ -3,10 +3,20 @@ package fsutil
 import (
 	"flag"
 	"sync"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/cgroup"
 )
 
-var maxConcurrency = flag.Int("fs.maxConcurrency", 64, "The maximum number of concurrent goroutines to work with files; smaller values may help reducing Go scheduling latency "+
+var maxConcurrency = flag.Int("fs.maxConcurrency", getDefaultConcurrency(), "The maximum number of concurrent goroutines to work with files; smaller values may help reducing Go scheduling latency "+
 	"on systems with small number of CPU cores; higher values may help reducing data ingestion latency on systems with high-latency storage such as NFS or Ceph")
+
+func getDefaultConcurrency() int {
+	n := 16 * cgroup.AvailableCPUs()
+	if n > 265 {
+		n = 265
+	}
+	return n
+}
 
 // GetConcurrencyCh returns a channel for limiting the concurrency of operations with files.
 func GetConcurrencyCh() chan struct{} {
