cc5fa731513598c3e60a9117af7771d6adb5bfc2
Max Kotliar <mkotlyar@victoriametrics.com>
2025-10-31 16:06:01 +0200

    app/vminsert/netstorage: Reroute only from the slowest node
    Adjust slowness-based rerouting logic.
    
    Rerouting now occurs only from the slowest node, and only if the cluster
    as a whole has enough available capacity to handle the additional load.
    
    See the detailed proposal:
    https://github.com/VictoriaMetrics/VictoriaMetrics/issues/9890

--- app/vminsert/netstorage/netstorage_test.go
+++ app/vminsert/netstorage/netstorage_test.go
@@ -4,6 +4,11 @@ import (
 	"flag"
 	"runtime"
 	"testing"
+	"time"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/netutil"
+	"github.com/VictoriaMetrics/metrics"
+	"github.com/VividCortex/ewma"
 )
 
 func TestInitStopNodes(t *testing.T) {
@@ -29,4 +34,96 @@ func TestInitStopNodes(t *testing.T) {
 		runtime.Gosched()
 		MustStop()
 	}
+}
+
+func TestAllowRerouting(t *testing.T) {
+	originDisableRerouting := *disableRerouting
+	t.Cleanup(func() {
+		*disableRerouting = originDisableRerouting
+	})
+	// Enable rerouting for the test
+	*disableRerouting = false
+
+	newStorage := func(avgSendDuration float64, ready bool) *storageNode {
+		sn := &storageNode{
+			avgSendDuration: ewma.NewMovingAverage(),
+			dialer:          netutil.NewTCPDialer(metrics.NewSet(), "aName", "anAddr", time.Second, time.Second),
+		}
+		sn.isBroken.Store(!ready)
+		sn.avgSendDuration.Set(avgSendDuration)
+
+		return sn
+	}
+
+	f := func(sns []*storageNode, snSourceIdx int, expected bool) {
+		t.Helper()
+
+		snSource := sns[snSourceIdx]
+
+		actual := allowRerouting(snSource, sns)
+
+		if actual != expected {
+			t.Errorf("unexpected allowRerouting result for snSourceIdx=%d from %d storages; got %v; want %v", snSourceIdx, len(sns), actual, expected)
+		}
+	}
+
+	// rerouting triggered on the slowest node if cluster avg saturation less than 0.5
+	f([]*storageNode{
+		newStorage(0.801, true),
+		newStorage(0.5, true),
+		newStorage(0.5, true),
+	}, 0, true)
+
+	// rerouting not triggered because rerouting disabled by flag
+	*disableRerouting = true
+	f([]*storageNode{
+		newStorage(0.801, true),
+		newStorage(0.5, true),
+		newStorage(0.5, true),
+	}, 0, false)
+	*disableRerouting = false
+
+	// rerouting not triggered because cluster avg saturation more than 0.4
+	f([]*storageNode{
+		newStorage(0.801, true),
+		newStorage(0.501, true),
+		newStorage(0.501, true),
+	}, 0, false)
+
+	// rerouting not triggered because the snSource node not slow enough
+	f([]*storageNode{
+		newStorage(0.79, true),
+		newStorage(0.5, true),
+		newStorage(0.5, true),
+	}, 0, false)
+
+	// rerouting not triggered because snSource not the slowest
+	f([]*storageNode{
+		newStorage(0.81, true),
+		newStorage(0.805, true),
+		newStorage(0.01, true),
+		newStorage(0.01, true),
+	}, 1, false)
+
+	// rerouting not triggered if not enough nodes
+	f([]*storageNode{
+		newStorage(0.81, true),
+		newStorage(0.01, true),
+	}, 0, false)
+
+	// rerouting not triggered if not enough ready nodes
+	f([]*storageNode{
+		newStorage(0.81, true),
+		newStorage(0.01, true),
+		newStorage(0.01, false),
+		newStorage(0.01, false),
+	}, 0, false)
+
+	// rerouting triggered if enough ready nodes
+	f([]*storageNode{
+		newStorage(0.81, true),
+		newStorage(0.01, true),
+		newStorage(0.01, true),
+		newStorage(0.01, false),
+	}, 0, true)
 }
