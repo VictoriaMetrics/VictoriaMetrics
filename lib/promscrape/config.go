45b4d7c476dabc4ec27ebd49a716e20694963778
Zhu Jiekun <jiekun@victoriametrics.com>
2025-11-07 21:18:11 +0800

    lib/promscrape: prevent early exit when one of multiple service discovery configs fails
     When multiple service discovery configs of the same type exist (e.g.,
    `hetzner_sd_config`), vmagent currently behaves as follows:
    1. Attempts to request each config.
    2. Exits immediately if any config returns an error.
    3. Skips the rest configs and falls back to the previous service
    discovery result.
    
    The correct behavior—more compatible with Prometheus—should be:
    1. Attempt to request each config.
    2. Collect all valid results.
    3. Use the valid results if there's at least one. otherwise (all
    failed), fall back to the previous SD result.
    
    Scrape example:
    
    ```yaml
    scrape_configs:
      - job_name: hetzner-default
        hetzner_sd_configs:
          - role: "hcloud"
            authorization:
              credentials: "some_valid_value"
          - role: "hcloud"
            authorization:
              credentials: "some_wrong_value"
    ```
    
    Expected outcome: 
    - At least targets from `credentials: "some_valid_value"` should appear
    in the service discovery result.
    
    current outcome:
    - the error from `credentials: "some_wrong_value"` leads to an **empty**
    result.
    
    This issue should affect service discovery which using
    `getScrapeWorkGeneric` function:
    
    - `azure_sd_config`
    - `consul_sd_config`
    - `consulagent_sd_config`
    - `digitalocean_sd_config`
    - `dns_sd_config`
    - `docker_sd_config`
    - `dockerswarm_sd_config`
    - `ec2_sd_config`
    - `eureka_sd_config`
    - `gce_sd_config`
    - `hetzner_sd_config`
    - `http_sd_config`
    - `kuma_sd_config`
    - `marathon_sd_config`
    - `nomad_sd_config`
    - `openstack_sd_config`
    - `ovhcloud_sd_config`
    - `puppetdb_sd_config`
    - `vultr_sd_config`
    - `yandexcloud_sd_config`
    
    fixes https://github.com/VictoriaMetrics/VictoriaMetrics/issues/9375

--- lib/promscrape/config.go
+++ lib/promscrape/config.go
@@ -823,20 +823,23 @@ func (cfg *Config) getScrapeWorkGeneric(visitConfigs func(sc *ScrapeConfig, visi
 	dst := make([]*ScrapeWork, 0, len(prev))
 	for _, sc := range cfg.ScrapeConfigs {
 		dstLen := len(dst)
-		ok := true
+
+		// hasSuccess indicates that at least one xxxSDConfig in the []*xxxSDConfig list has returned a successful response.
+		// Therefore, the service discovery result should be updated based on the current round of results.
+		//
+		// If no successful response is received (i.e., hasSuccess is false), fall back to the result from the previous round.
+		hasSuccess := false
 		visitConfigs(sc, func(sdc targetLabelsGetter) {
-			if !ok {
-				return
-			}
 			targetLabels, err := sdc.GetLabels(cfg.baseDir)
 			if err != nil {
-				logger.Errorf("skipping %s targets for job_name=%s because of error: %s", discoveryType, sc.swc.jobName, err)
-				ok = false
+				logger.Errorf("skipping some %s targets for job_name=%s because of error: %s", discoveryType, sc.swc.jobName, err)
+				hasSuccess = hasSuccess || false
 				return
 			}
+			hasSuccess = true
 			dst = appendScrapeWorkForTargetLabels(dst, sc.swc, targetLabels, discoveryType)
 		})
-		if !ok {
+		if !hasSuccess {
 			dst = sc.appendPrevTargets(dst[:dstLen], swsPrevByJob, discoveryType)
 		}
 	}
