e228abf85cf1544a92d557fc0e100df5916988bc
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2025-11-14 13:36:45 +0200

    app/vmui: fixed ability to select multiple metrics in explore metrics tab

--- app/vmui/packages/vmui/src/components/Main/Select/Select.tsx
+++ app/vmui/packages/vmui/src/components/Main/Select/Select.tsx
@@ -150,7 +150,7 @@ const Select: FC<SelectProps> = ({
               onRemoveItem={handleSelected}
             />
           )}
-          {!hideInput && !selectedValues?.length && (
+          {!hideInput && (
             <input
               value={textFieldValue}
               type="text"
