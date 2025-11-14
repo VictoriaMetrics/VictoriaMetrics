0ba59757c9c62e70867f4354bc6e346609c7f6c3
f41gh7 <nik@victoriametrics.com>
2025-11-14 10:05:09 +0100

    apptest: add metrics metadata test for vmsingle
    related issue github.com/VictoriaMetrics/VictoriaMetrics/issues/2974

--- apptest/tests/metricsmetadata_test.go
+++ apptest/tests/metricsmetadata_test.go
@@ -11,6 +11,99 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/prompb"
 )
 
+func TestSingleMetricsMetadata(t *testing.T) {
+	fs.MustRemoveDir(t.Name())
+
+	tc := apptest.NewTestCase(t)
+	defer tc.Stop()
+	sut := tc.MustStartVmsingle("vmsingle", []string{
+		"-storageDataPath=" + tc.Dir(),
+		"-retentionPeriod=100y",
+		"-enableMetadata",
+	})
+	// verify empty stats
+	resp := sut.PrometheusAPIV1Metadata(t, "", 0, apptest.QueryOpts{})
+	if len(resp.Data) != 0 {
+		t.Fatalf("unexpected resp Records: %d, want: %d", len(resp.Data), 0)
+	}
+
+	const ingestTimestamp = 1707123456700
+	prometheusTextDataSet := []string{
+		`# HELP metric_name_1 some help message`,
+		`# TYPE metric_name_1 gauge`,
+		`metric_name_1{label="foo"} 10`,
+		`metric_name_1{label="bar"} 10`,
+		`metric_name_1{label="baz"} 10`,
+		`# HELP metric_name_2 some help message`,
+		`# TYPE metric_name_2 counter`,
+		`metric_name_2{label="baz"} 20`,
+		`# HELP metric_name_3 some help message`,
+		`# TYPE metric_name_3 gauge`,
+		`metric_name_3{label="baz"} 30`,
+	}
+	prometheusRemoteWriteDataSet := prompb.WriteRequest{
+		Timeseries: []prompb.TimeSeries{
+			{Labels: []prompb.Label{{Name: "__name__", Value: "metric_name_4"}}, Samples: []prompb.Sample{{Value: 40, Timestamp: ingestTimestamp}}},
+			{Labels: []prompb.Label{{Name: "__name__", Value: "metric_name_5"}}, Samples: []prompb.Sample{{Value: 40, Timestamp: ingestTimestamp}}},
+			{Labels: []prompb.Label{{Name: "__name__", Value: "metric_name_6"}}, Samples: []prompb.Sample{{Value: 40, Timestamp: ingestTimestamp}}},
+		},
+		Metadata: []prompb.MetricMetadata{
+			{MetricFamilyName: "metric_name_4", Help: "some help message", Type: uint32(prompb.MetricMetadataSUMMARY)},
+			{MetricFamilyName: "metric_name_5", Help: "some help message", Type: uint32(prompb.MetricMetadataSUMMARY)},
+			{MetricFamilyName: "metric_name_6", Help: "some help message", Type: uint32(prompb.MetricMetadataSTATESET)},
+		},
+	}
+
+	sut.PrometheusAPIV1ImportPrometheus(t, prometheusTextDataSet, apptest.QueryOpts{})
+	sut.PrometheusAPIV1Write(t, prometheusRemoteWriteDataSet, apptest.QueryOpts{})
+	sut.ForceFlush(t)
+	expected := &apptest.PrometheusAPIV1Metadata{
+		Status: "success",
+		Data: map[string][]apptest.MetadataEntry{
+			"metric_name_1": {{Help: "some help message", Type: "gauge"}},
+			"metric_name_2": {{Help: "some help message", Type: "counter"}},
+			"metric_name_3": {{Help: "some help message", Type: "gauge"}},
+			"metric_name_4": {{Help: "some help message", Type: "summary"}},
+			"metric_name_5": {{Help: "some help message", Type: "summary"}},
+			"metric_name_6": {{Help: "some help message", Type: "stateset"}},
+		},
+	}
+	gotStats := sut.PrometheusAPIV1Metadata(t, "", 0, apptest.QueryOpts{})
+	if diff := cmp.Diff(expected, gotStats); diff != "" {
+		t.Errorf("unexpected response (-want, +got):\n%s", diff)
+	}
+
+	// check query metric name filter
+	tc.Assert(&apptest.AssertOptions{
+		Msg: "unexpected /api/v1/metadata response",
+		Got: func() any {
+			return sut.PrometheusAPIV1Metadata(t, "metric_name_4", 0, apptest.QueryOpts{})
+		},
+		Want: &apptest.PrometheusAPIV1Metadata{
+			Status: "success",
+			Data: map[string][]apptest.MetadataEntry{
+				"metric_name_4": {{Help: "some help message", Type: "summary"}},
+			},
+		},
+	})
+
+	// check query limit filter
+	tc.Assert(&apptest.AssertOptions{
+		Msg: "unexpected /api/v1/metadata response",
+		Got: func() any {
+			return sut.PrometheusAPIV1Metadata(t, "", 3, apptest.QueryOpts{})
+		},
+		Want: &apptest.PrometheusAPIV1Metadata{
+			Status: "success",
+			Data: map[string][]apptest.MetadataEntry{
+				"metric_name_1": {{Help: "some help message", Type: "gauge"}},
+				"metric_name_2": {{Help: "some help message", Type: "counter"}},
+				"metric_name_3": {{Help: "some help message", Type: "gauge"}},
+			},
+		},
+	})
+}
+
 func TestClusterMetricsMetadata(t *testing.T) {
 	fs.MustRemoveDir(t.Name())
 
