36aefe0d6cc681c68fd7683090c0b3cb182c3e10
Artem Fetishev <rtm@victoriametrics.com>
2025-11-19 12:03:28 +0100

    update unit test to check the corresponding metric increased
    Signed-off-by: Artem Fetishev <rtm@victoriametrics.com>

--- lib/storage/storage_synctest_test.go
+++ lib/storage/storage_synctest_test.go
@@ -85,6 +85,13 @@ func TestStorageSearchTSIDs_CorruptedIndex(t *testing.T) {
 		if diff := cmp.Diff(wantMetricIDs, searchMetricIDs()); diff != "" {
 			t.Fatalf("unexpected metricIDs (-want, +got):\n%s", diff)
 		}
+		// Ensure the metric that counts metricIDs for which no TSIDs were found
+		// is not incremented yet.
+		var m Metrics
+		s.UpdateMetrics(&m)
+		if got, want := m.IndexDBMetrics.MissingTSIDsForMetricID, uint64(0); got != want {
+			t.Fatalf("unexpected MissingTSIDsForMetricID: got %d, want %d", got, want)
+		}
 
 		time.Sleep(61 * time.Second)
 		synctest.Wait()
@@ -97,6 +104,12 @@ func TestStorageSearchTSIDs_CorruptedIndex(t *testing.T) {
 		// As a result they cannot be searched anymore.
 		if diff := cmp.Diff([]uint64(nil), searchMetricIDs()); diff != "" {
 			t.Fatalf("unexpected metricIDs (-want, +got):\n%s", diff)
+		}
+		// Ensure the metric that counts metricIDs for which no TSIDs were found
+		// is incremented after the metricID deletion.
+		s.UpdateMetrics(&m)
+		if got, want := m.IndexDBMetrics.MissingTSIDsForMetricID, uint64(numMetrics); got != want {
+			t.Fatalf("unexpected MissingTSIDsForMetricID: got %d, want %d", got, want)
 		}
 	})
 }
@@ -173,6 +186,13 @@ func TestStorageSearchMetricNames_CorruptedIndex(t *testing.T) {
 		if diff := cmp.Diff(wantMetricIDs, searchMetricIDs()); diff != "" {
 			t.Fatalf("unexpected metricIDs (-want, +got):\n%s", diff)
 		}
+		// Ensure the metric that counts metricIDs for which no metric names
+		// were found is not incremented yet.
+		var m Metrics
+		s.UpdateMetrics(&m)
+		if got, want := m.IndexDBMetrics.MissingMetricNamesForMetricID, uint64(0); got != want {
+			t.Fatalf("unexpected MissingMetricNamesForMetricID: got %d, want %d", got, want)
+		}
 
 		time.Sleep(61 * time.Second)
 		synctest.Wait()
@@ -185,6 +205,12 @@ func TestStorageSearchMetricNames_CorruptedIndex(t *testing.T) {
 		// As a result they cannot be searched anymore.
 		if diff := cmp.Diff([]uint64(nil), searchMetricIDs()); diff != "" {
 			t.Fatalf("unexpected metricIDs (-want, +got):\n%s", diff)
+		}
+		// Ensure the metric that counts metricIDs for which no metric names
+		// were found is incremented after the metricID deletion.
+		s.UpdateMetrics(&m)
+		if got, want := m.IndexDBMetrics.MissingMetricNamesForMetricID, uint64(numMetrics); got != want {
+			t.Fatalf("unexpected MissingMetricNamesForMetricID: got %d, want %d", got, want)
 		}
 	})
 }
