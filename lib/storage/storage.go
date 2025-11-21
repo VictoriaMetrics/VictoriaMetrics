ed64c90e7ac8f231b2b4414e6247246658d1a2ee
Artem Fetishev <rtm@victoriametrics.com>
2025-11-21 13:31:20 +0100

    lib/storage: fix comments related to nextDayMetricIDs
    Follow-up for 49b0a4fb16
    
    Signed-off-by: Artem Fetishev <rtm@victoriametrics.com>

--- lib/storage/storage.go
+++ lib/storage/storage.go
@@ -1124,7 +1124,7 @@ func (s *Storage) mustSaveNextDayMetricIDs(e *nextDayMetricIDs) {
 	dst = encoding.MarshalUint64(dst, e.generation)
 	dst = encoding.MarshalUint64(dst, e.date)
 
-	// Marshal e.v
+	// Marshal metricIDs
 	dst = marshalUint64Set(dst, &e.metricIDs)
 
 	fs.MustWriteSync(path, dst)
@@ -2681,7 +2681,7 @@ func (s *Storage) updateNextDayMetricIDs(date uint64) {
 		return
 	}
 
-	// Slow path: union pendingMetricIDs with e.v
+	// Slow path: union pendingMetricIDs with e.metricIDs
 	if e.generation == generation && e.date == date {
 		pendingMetricIDs.Union(&e.metricIDs)
 	} else {
