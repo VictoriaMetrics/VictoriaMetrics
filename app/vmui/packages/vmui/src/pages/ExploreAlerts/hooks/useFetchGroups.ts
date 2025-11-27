d317d5b6da8c3167de883d291d23d597716b6266
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2025-11-27 15:56:19 +0200

    app/vmui: support pagination in alerting tab

--- app/vmui/packages/vmui/src/pages/ExploreAlerts/hooks/useFetchGroups.ts
+++ app/vmui/packages/vmui/src/pages/ExploreAlerts/hooks/useFetchGroups.ts
@@ -1,59 +1,79 @@
-import { useTimeState } from "../../../state/time/TimeStateContext";
-import { useEffect, useMemo, useState } from "preact/compat";
+import { useMemo, useCallback, useEffect, useState } from "preact/compat";
 import { getGroupsUrl } from "../../../api/explore-alerts";
 import { useAppState } from "../../../state/common/StateContext";
 import { ErrorTypes, Group } from "../../../types";
+import { useTimeState } from "../../../state/time/TimeStateContext";
 
 interface FetchGroupsReturn {
   groups: Group[];
   isLoading: boolean;
   error?: ErrorTypes | string;
+  hasMoreNext: boolean;
+  hasMoreBefore: boolean;
+  loadGroups: () => void;
 }
 
 interface FetchGroupsProps {
-  blockFetch: boolean
+  blockFetch: boolean;
+  search: string;
+  ruleType: string;
+  states: string[];
+  startToken: string;
 }
 
-export const useFetchGroups = ({ blockFetch }: FetchGroupsProps): FetchGroupsReturn => {
+const MAX_GROUPS = 100;
+
+export const useFetchGroups = ({ blockFetch, startToken, search, ruleType, states }: FetchGroupsProps): FetchGroupsReturn => {
   const { serverUrl } = useAppState();
   const { period } = useTimeState();
 
   const [groups, setGroups] = useState<Group[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [hasMoreNext, setHasMoreNext] = useState(true);
+  const [hasMoreBefore, setHasMoreBefore] = useState(!!startToken);
+  const [groupNextToken, setGroupNextToken] = useState(startToken || "");
   const [error, setError] = useState<ErrorTypes | string>();
 
   const fetchUrl = useMemo(
-    () => getGroupsUrl(serverUrl),
-    [serverUrl],
+    () => getGroupsUrl(serverUrl, search, ruleType, states, MAX_GROUPS),
+    [serverUrl, search, ruleType, states],
   );
 
-  const loaded = !!groups.length || !blockFetch;
+  const loadGroups = useCallback(async(nextToken = groupNextToken) => {
+    if (blockFetch) return;
+    setIsLoading(true);
+    let newGroups = groups;
+    if (nextToken !== groupNextToken || !nextToken) {
+      newGroups = [];
+      setHasMoreBefore(!!nextToken);
+    }
+    try {
+      const url = `${fetchUrl}&group_next_token=${nextToken}`;
+      const response = await fetch(url);
+      const resp = await response.json();
+      if (response.ok) {
+        const loadedGroups = (resp.data.groups || []) as Group[];
+        setGroups([...newGroups, ...loadedGroups]);
+        setHasMoreNext(!!resp.data.groupNextToken);
+        setGroupNextToken(resp.data.groupNextToken || "");
+        setError(undefined);
+      } else if (response.status === 400) {
+        setGroups([]);
+        setError(`${resp.errorType}\r\n${resp?.error}`);
+      } else {
+        setError(`${resp.errorType}\r\n${resp?.error}`);
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(`${e.name}: ${e.message}`);
+      }
+    }
+    setIsLoading(false);
+  }, [fetchUrl, blockFetch, groupNextToken]);
 
   useEffect(() => {
-    if (blockFetch) return;
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(fetchUrl);
-        const resp = await response.json();
+    loadGroups(startToken || "");
+  }, [period, fetchUrl, startToken]);
 
-        if (response.ok) {
-          const data = (resp.data.groups || []) as Group[];
-          setGroups(data.sort((a, b) => a.name.localeCompare(b.name)));
-          setError(undefined);
-        } else {
-          setError(`${resp.errorType}\r\n${resp?.error}`);
-        }
-      } catch (e) {
-        if (e instanceof Error) {
-          setError(`${e.name}: ${e.message}`);
-        }
-      }
-      setIsLoading(false);
-    };
-
-    fetchData().catch(console.error);
-  }, [fetchUrl, period, loaded]);
-
-  return { groups, isLoading, error };
+  return { groups, isLoading, error, hasMoreNext, hasMoreBefore, loadGroups };
 };
