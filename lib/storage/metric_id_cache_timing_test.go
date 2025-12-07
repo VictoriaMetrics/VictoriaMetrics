006705b163aa0d41d8fb58303cfc52e8d9d61774
Artem Fetishev <rtm@victoriametrics.com>
2025-12-07 13:54:44 +0100

    rewrite metric_id_cache benchmark
    Signed-off-by: Artem Fetishev <rtm@victoriametrics.com>

--- lib/storage/metric_id_cache_timing_test.go
+++ lib/storage/metric_id_cache_timing_test.go
@@ -7,34 +7,66 @@ import (
 	"time"
 )
 
-func BenchmarkMetricIDCache_Get(b *testing.B) {
-	f := func(b *testing.B, numMetricIDs, distance int64) {
+func BenchmarkMetricIDCache_Has(b *testing.B) {
+	f := func(b *testing.B, numMetricIDs, distance int64, hitsOnly, warmUp bool) {
 		b.Helper()
 		c := newMetricIDCache()
 		metricIDMin := time.Now().UnixNano()
-		metricID := metricIDMin
-		for range numMetricIDs {
+		metricIDMax := metricIDMin + numMetricIDs*distance
+		for metricID := metricIDMin; metricID <= metricIDMax; metricID += distance {
 			c.Set(uint64(metricID))
-			metricID += 1 + rand.Int63n(distance)
+			if warmUp && !c.Has(uint64(metricID)) {
+				b.Fatalf("metricID not in cache: %d", metricID)
+			}
 		}
-		metricIDMax := metricID
-
 		b.ResetTimer()
 		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				metricID := metricIDMin + rand.Int63n(metricIDMax-metricIDMin)
-				_ = c.Has(uint64(metricID))
+			if hitsOnly {
+				metricID := metricIDMin + rand.Int63n(numMetricIDs)*distance
+				for pb.Next() {
+					if !c.Has(uint64(metricID)) {
+						b.Fatalf("metricID not in cache: %d", metricID)
+					}
+					metricID += distance
+					if metricID > metricIDMax {
+						metricID = metricIDMin
+					}
+				}
+			} else {
+				// misses only
+				metricID := metricIDMax + distance
+				for pb.Next() {
+					if c.Has(uint64(metricID)) {
+						b.Fatalf("metricID is in cache: %d", metricID)
+					}
+					metricID += distance
+				}
 			}
 		})
 		b.ReportAllocs()
 	}
 
-	for _, numMetricIDs := range []int64{100_000, 1_000_000, 10_000_000} {
-		for _, distance := range []int64{1, 10, 100} {
-			name := fmt.Sprintf("numMetricID-%d/distance-%d", numMetricIDs, distance)
-			b.Run(name, func(b *testing.B) {
-				f(b, numMetricIDs, distance)
-			})
+	subB := func(numMetricIDs, distance int64, hitsOnly, warmUp bool) {
+		hitsOrMisses := "hitsss"
+		if !hitsOnly {
+			hitsOrMisses = "misses"
+		}
+		coldOrWarm := "cold"
+		if warmUp {
+			coldOrWarm = "warm"
+		}
+		name := fmt.Sprintf("%s/%s/n%d/d%d", hitsOrMisses, coldOrWarm, numMetricIDs, distance)
+		b.Run(name, func(b *testing.B) {
+			f(b, numMetricIDs, distance, hitsOnly, warmUp)
+		})
+	}
+	for _, hitsOnly := range []bool{true, false} {
+		for _, warmUp := range []bool{false, true} {
+			for _, numMetricIDs := range []int64{100_000, 1_000_000, 10_000_000} {
+				for _, distance := range []int64{1, 10, 100} {
+					subB(numMetricIDs, distance, hitsOnly, warmUp)
+				}
+			}
 		}
 	}
 }
