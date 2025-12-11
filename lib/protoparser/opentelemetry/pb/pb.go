VictoriaMetrics/VictoriaMetrics

commit 1dc71212f8e3712090567c86b6fbdc154cd8a8a4

lib/protoparser/opentelemetry/pb: reset the decoderContext.ls.Labels length to zero after clearing all the references to the original byte slice
This is a follow-up for 25f49e6f54426b98b0a5de388fbd13ab22de19b0

lib/protoparser/opentelemetry/pb/pb.go
@@ -1680,6 +1680,7 @@ func (dctx *decoderContext) reset() {
 	// Explicitly clear all the ls.Labels up to its' capacity in order to remove possible references
 	// to the original byte slices, so they could be cleared by Go GC.
 	clear(dctx.ls.Labels[:cap(dctx.ls.Labels)])
+	dctx.ls.Labels = lctx.ls.Labels[:0]
 
 	dctx.ls.Reset()
 	dctx.fb.reset()

commit 5a66a9d04000e2c3d1f79f71700252ca8980082b

lib/protoparser/opentelemetry: fix typo in code
#
github.com/VictoriaMetrics/VictoriaMetrics/lib/protoparser/opentelemetry/pb
lib/protoparser/opentelemetry/pb/pb.go:1683:19: undefined: lctx

Bug introduced in
https://github.com/VictoriaMetrics/VictoriaMetrics/commit/1dc71212f8e3712090567c86b6fbdc154cd8a8a4

lib/protoparser/opentelemetry/pb/pb.go
@@ -1680,7 +1680,7 @@ func (dctx *decoderContext) reset() {
 	// Explicitly clear all the ls.Labels up to its' capacity in order to remove possible references
 	// to the original byte slices, so they could be cleared by Go GC.
 	clear(dctx.ls.Labels[:cap(dctx.ls.Labels)])
-	dctx.ls.Labels = lctx.ls.Labels[:0]
+	dctx.ls.Labels = dctx.ls.Labels[:0]
 
 	dctx.ls.Reset()
 	dctx.fb.reset()