a05baab52b25edc26999d692acd4249fe26f8cc7
JAYICE <1185430411@qq.com>
2025-12-12 18:01:06 +0800

    lib/storage: include last sample when query at the last millisecond of the day
    One millisecond shouldn't be subtracted from the `tr.MaxTimestamp`, and
    related test cases will be added
    
    Fixes https://github.com/VictoriaMetrics/VictoriaMetrics/issues/9804

--- lib/storage/storage_test.go
+++ lib/storage/storage_test.go
@@ -3479,7 +3479,7 @@ func TestStorageAdjustTimeRange(t *testing.T) {
 	// Time range is smaller than 40 days. When the -disablePerDayIndex flag is
 	// unset, the time range will not be adjusted. When the flag is set, the
 	// adjusted time range will be globalIndexTimeRange.
-	tr = TimeRange{10 * msecPerDay, 50 * msecPerDay}
+	tr = TimeRange{10 * msecPerDay, 50*msecPerDay - 1}
 	f(false, tr, tr)
 	f(true, tr, globalIndexTimeRange)
 
@@ -3498,7 +3498,7 @@ func TestStorageAdjustTimeRange(t *testing.T) {
 	// When the -disablePerDayIndex flag is unset, the time range will not be
 	// adjusted. When the flag is set, the adjusted time range will be
 	// globalIndexTimeRange.
-	tr = TimeRange{10 * msecPerDay, 51 * msecPerDay}
+	tr = TimeRange{10 * msecPerDay, 50 * msecPerDay}
 	f(false, tr, tr)
 	f(true, tr, globalIndexTimeRange)
 
