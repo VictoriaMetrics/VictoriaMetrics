3050536485895450aea8bb49da7908fc423f2770
samuel <sbayo971@gmail.com>
2026-01-22 11:45:02 +0100

    app/vmselect: fix incorrect downsampling when start query arg is missing in /api/v1/export

--- app/vmselect/prometheus/prometheus.go
+++ app/vmselect/prometheus/prometheus.go
@@ -1181,6 +1181,19 @@ func getExportParams(r *http.Request, startTime time.Time) (*commonParams, error
 		return nil, err
 	}
 	cp.deadline = searchutil.GetDeadlineForExport(r, startTime)
+	if cp.IsDefaultTimeRange() {
+		// Adjust start time when it is missing, so it doesn't lead to
+		// fetching all the data from 1970 year.
+		// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1806
+		lookbackDelta, err := getMaxLookback(r)
+		if err != nil {
+			return nil, err
+		}
+		if lookbackDelta <= 0 {
+			lookbackDelta = defaultStep
+		}
+		cp.start = cp.end - lookbackDelta
+	}
 	return cp, nil
 }
 
