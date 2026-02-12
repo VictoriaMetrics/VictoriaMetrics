VictoriaMetrics/VictoriaMetrics

commit c705da74f68832b57080bdf168c0fb4104b87565

lib/storage: make pt and legacy idbs visible in golang profiles
Rewrite the searchAndMerge so that golang profiles could show exactly
how much resources is consumed by each idb type.

lib/storage/storage.go
@@ -1064,6 +1064,32 @@ func (s *Storage) putMetricNameToCache(metricID uint64, metricName []byte) {
 	s.metricNameCache.Set(key[:], metricName)
 }
 
+// indexDBWithType holds together the indexDB and its type.
+//
+// This type is used in searchAndMerge() to organize the code so that golang
+// profiles (cpu, mem, etc) could distinguish between searching pt-index, legacy
+// prev, and legacy curr indexDBs. Such profiles should significantly simplify
+// debugging index search performance issues.
+type indexDBWithType struct {
+	idb        *indexDB
+	legacyPrev bool
+	legacyCurr bool
+	pt         bool
+}
+
+func (idb indexDBWithType) name() string {
+	var idbType string
+	if idb.legacyPrev {
+		idbType = "legacy prev"
+	} else if idb.legacyCurr {
+		idbType = "legacy curr"
+	} else if idb.pt {
+		idbType = "pt"
+	}
+
+	return fmt.Sprintf("%s (%s)", idb.idb.name, idbType)
+}
+
 // searchAndMerge concurrently performs a search operation on all partition
 // IndexDBs that overlap with the given time range and optionally legacy current
 // and previous IndexDBs. The individual search results are then merged.
@@ -1075,12 +1101,16 @@ func searchAndMerge[T any](qt *querytracer.Tracer, s *Storage, tr TimeRange, sea
 	qt = qt.NewChild("search indexDBs: timeRange=%v", &tr)
 	defer qt.Done()
 
-	var idbs []*indexDB
+	var idbs []indexDBWithType
 
 	ptws := s.tb.GetPartitions(tr)
 	defer s.tb.PutPartitions(ptws)
 	for _, ptw := range ptws {
-		idbs = append(idbs, ptw.pt.idb)
+		idb := indexDBWithType{
+			idb: ptw.pt.idb,
+			pt:  true,
+		}
+		idbs = append(idbs, idb)
 	}
 
 	legacyIDBs := s.getLegacyIndexDBs()
@@ -1099,18 +1129,27 @@ func searchAndMerge[T any](qt *querytracer.Tracer, s *Storage, tr TimeRange, sea
 	if len(idbs) == 1 {
 		// It is faster to process one indexDB without spawning goroutines.
 		idb := idbs[0]
-		searchTR := s.adjustTimeRange(tr, idb.tr)
-		qtChild := qt.NewChild("search indexDB %s: timeRange=%v", idb.name, &searchTR)
-		data[0], errs[0] = search(qtChild, idb, searchTR)
+		searchTR := s.adjustTimeRange(tr, idb.idb.tr)
+		qtChild := qt.NewChild("search indexDB %s: timeRange=%v", idb.name(), &searchTR)
+		data[0], errs[0] = search(qtChild, idb.idb, searchTR)
 		qtChild.Done()
 	} else {
 		qtSearch := qt.NewChild("search %d indexDBs in parallel", len(idbs))
 		var wg sync.WaitGroup
 		for i, idb := range idbs {
-			searchTR := s.adjustTimeRange(tr, idb.tr)
-			qtChild := qtSearch.NewChild("search indexDB %s: timeRange=%v", idb.name, &searchTR)
+			searchTR := s.adjustTimeRange(tr, idb.idb.tr)
+			qtChild := qtSearch.NewChild("search indexDB %s: timeRange=%v", idb.name(), &searchTR)
 			wg.Go(func() {
-				data[i], errs[i] = search(qtChild, idb, searchTR)
+				// Intentionally repeat the same search code for each indexDB
+				// type so that profiles (cpu, mem, etc) could show how many
+				// resources have been consumed by each indexDB type.
+				if idb.pt {
+					data[i], errs[i] = search(qt, idb.idb, searchTR)
+				} else if idb.legacyPrev {
+					data[i], errs[i] = search(qt, idb.idb, searchTR)
+				} else if idb.legacyCurr {
+					data[i], errs[i] = search(qt, idb.idb, searchTR)
+				}
 				qtChild.Done()
 			})
 		}

lib/storage/storage_legacy.go
@@ -46,17 +46,25 @@ func (dbs *legacyIndexDBs) decRef() {
 	}
 }
 
-func (dbs *legacyIndexDBs) appendTo(dst []*indexDB) []*indexDB {
+func (dbs *legacyIndexDBs) appendTo(dst []indexDBWithType) []indexDBWithType {
 	if dbs == nil {
 		// No legacy indexDBs, nothing to append.
 		return dst
 	}
 
 	if dbs.idbPrev != nil {
-		dst = append(dst, dbs.idbPrev.idb)
+		idb := indexDBWithType{
+			idb:        dbs.idbPrev.idb,
+			legacyPrev: true,
+		}
+		dst = append(dst, idb)
 	}
 	if dbs.idbCurr != nil {
-		dst = append(dst, dbs.idbCurr.idb)
+		idb := indexDBWithType{
+			idb:        dbs.idbCurr.idb,
+			legacyCurr: true,
+		}
+		dst = append(dst, idb)
 	}
 	return dst
 }

commit 267f2ddb581dce0d7278441cf7463d00c68306b2

lib/storage: use child trace during index searches
This change only affects query trace. It correctly uses the branched
query trace in callback function, so in trace it is placed in the
right actions branch.

Bug was introduced in https://github.com/VictoriaMetrics/VictoriaMetrics/commit/c705da74f68832b57080bdf168c0fb4104b87565

Signed-off-by: hagen1778 <roman@victoriametrics.com>

lib/storage/storage.go
@@ -1146,11 +1146,11 @@ func searchAndMerge[T any](qt *querytracer.Tracer, s *Storage, tr TimeRange, sea
 				// resources have been consumed by each indexDB type.
 				switch idbt.t {
 				case indexDBTypePt:
-					data[i], errs[i] = search(qt, idbt.idb, searchTR)
+					data[i], errs[i] = search(qtChild, idbt.idb, searchTR)
 				case indexDBTypeLegacyPrev:
-					data[i], errs[i] = search(qt, idbt.idb, searchTR)
+					data[i], errs[i] = search(qtChild, idbt.idb, searchTR)
 				case indexDBTypeLegacyCurr:
-					data[i], errs[i] = search(qt, idbt.idb, searchTR)
+					data[i], errs[i] = search(qtChild, idbt.idb, searchTR)
 				}
 				qtChild.Done()
 			})