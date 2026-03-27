f6cef6e6be2e384d65642b42927610d6b6a74526
Nikolay <nik@victoriametrics.com>
2026-03-27 09:51:27 +0100

    lib/fs: restore async deletion of NFS folders
    Commit 83da33d8cfe8352fd0022d05a8b6346ebb48420d
     removed NFS directory delete retries. It was made on assumption, that
     only directory rename could cause such issues. However, both rename and
    unlink uses the same "silly rename" logic
    https://linux-nfs.org/wiki/index.php/Server-side_silly_rename
     and linux kernel - `fs/nfs/dir.c` `nfs_unlink` and  `nfs_rename`.
    
     And NFS client may treat file still open, even if it
    was properly closed by application. Most probably it could be triggered, because VictoriaMetrics may
    open the same file multiple times ( data read and background merges).
    
    There is no issue with VictoriaMetrics itself, it properly closes files. But NFS-client may have delays
    or cache metadata information for the files. So it could trigger silly rename behavior.
    
     This commit restores original behavior with deletion retries and brings
     back metrics for unsuccessful delete operations.
    
    Fixes https://github.com/VictoriaMetrics/VictoriaMetrics/issues/9842

--- app/vmstorage/main.go
+++ app/vmstorage/main.go
@@ -229,6 +229,7 @@ func main() {
 	strg.MustClose()
 	logger.Infof("successfully closed the storage in %.3f seconds", time.Since(startTime).Seconds())
 
+	fs.MustStopDirRemover()
 	logger.Infof("the vmstorage has been stopped")
 }
 
