a25c1ed7c21198ac307fd873a1156c7b02f5b135
Yury Moladau <yurymolodov@gmail.com>
2026-04-17 10:32:59 +0200

    app/vmui: fix Alerting Rules page query link and time display
    **"Run query" link params**  
    Added correct params to "Run query" link on Alerting Rules page:
    - `g0.step_input` - set to `group.interval` (in seconds)
    - `g0.end_time` - set to `rule.lastEvaluation` / `alert.activeAt`
    - `g0.relative_time=none` - to fix the time range
    
    **Time display timezone**  
    Changed `t.format(...)` to `t.tz().format(...)` to display time in the
    user-selected timezone.
    
    Related issue:
    https://github.com/VictoriaMetrics/VictoriaMetrics/issues/10366
    https://github.com/VictoriaMetrics/VictoriaMetrics/issues/10827

--- app/vmui/packages/vmui/src/components/ExploreAlerts/Rule/index.tsx
+++ app/vmui/packages/vmui/src/components/ExploreAlerts/Rule/index.tsx
@@ -2,15 +2,16 @@ import { FC } from "preact/compat";
 import ItemHeader from "../ItemHeader";
 import Accordion from "../../Main/Accordion/Accordion";
 import "./style.scss";
-import { Rule as APIRule } from "../../../types";
+import { Group, Rule as APIRule } from "../../../types";
 import BaseRule from "../BaseRule";
 
 interface RuleProps {
   states: Record<string, number>;
   rule: APIRule;
+  group: Group;
 }
 
-const Rule: FC<RuleProps> = ({ states, rule }) => {
+const Rule: FC<RuleProps> = ({ states, rule, group }) => {
   const state = Object.keys(states).length > 0 ? Object.keys(states)[0] : "ok";
   return (
     <div className={`vm-explore-alerts-rule vm-badge-item ${state.replace(" ", "-")}`}>
@@ -25,7 +26,10 @@ const Rule: FC<RuleProps> = ({ states, rule }) => {
           name={rule.name}
         />}
       >
-        <BaseRule item={rule} />
+        <BaseRule
+          item={rule}
+          group={group}
+        />
       </Accordion>
     </div>
   );
