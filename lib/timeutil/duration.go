533eee11c356a132cf210f8d192961a3570d6e25
Andrii Beskomornyi <andriibeee@gmail.com>
2026-04-26 20:19:32 +0300

    lib/timeutil: handle int64 overflow in relative duration parsing

--- lib/timeutil/duration.go
+++ lib/timeutil/duration.go
@@ -1,6 +1,8 @@
 package timeutil
 
 import (
+	"fmt"
+	"math"
 	"time"
 
 	"github.com/VictoriaMetrics/metricsql"
@@ -11,6 +13,11 @@ func ParseDuration(s string) (time.Duration, error) {
 	ms, err := metricsql.DurationValue(s, 0)
 	if err != nil {
 		return 0, err
+	}
+	const maxMs = math.MaxInt64 / int64(time.Millisecond)
+	if ms > maxMs || ms < -maxMs {
+		maxD := time.Duration(maxMs) * time.Millisecond
+		return 0, fmt.Errorf("duration %q must be in the range [%v, %v]", s, -maxD, maxD)
 	}
 	return time.Duration(ms) * time.Millisecond, nil
 }
