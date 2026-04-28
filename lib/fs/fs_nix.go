3669e1f236254e9e5dae082900e9a4857bf8275f
Jayice <1185430411@qq.com>
2026-04-29 00:07:25 +0800

    fix compile error in openbsd

--- lib/fs/fs_nix.go
+++ lib/fs/fs_nix.go
@@ -1,4 +1,4 @@
-//go:build linux || darwin || freebsd || openbsd
+//go:build linux || darwin || freebsd || netbsd
 
 package fs
 
--- lib/fs/fs_openbsd.go
+++ lib/fs/fs_openbsd.go
@@ -4,6 +4,8 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+type statfs_t = unix.Statfs_t
+
 func freeSpace(stat statfs_t) uint64 {
 	return uint64(stat.F_bavail) * uint64(stat.F_bsize)
 }
