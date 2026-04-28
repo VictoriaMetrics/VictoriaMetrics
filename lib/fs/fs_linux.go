6e0bee156586d6cecfd0f3574dd49dc13df37194
Jayice <1185430411@qq.com>
2026-04-28 23:46:49 +0800

    remove duplicate codes in lib/fs

--- lib/fs/fs_linux.go
+++ lib/fs/fs_linux.go
@@ -2,14 +2,6 @@
 
 package fs
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
-	"golang.org/x/sys/unix"
-)
-
 // https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/uapi/linux/magic.h
 var fsMagicNumberToName = map[int64]string{
 	0xEF53:     "ext2/ext3/ext4",
@@ -38,50 +30,6 @@ var fsMagicNumberToName = map[int64]string{
 	0xFF534D42: "cifs",
 	0x6c6f6f70: "binderfs",
 	0xBAD1DEA:  "futexfs",
-}
-
-func mmap(fd int, length int) (data []byte, err error) {
-	return unix.Mmap(fd, 0, length, unix.PROT_READ, unix.MAP_SHARED)
-
-}
-func mUnmap(data []byte) error {
-	return unix.Munmap(data)
-}
-
-func mustSyncPath(path string) {
-	d, err := os.Open(path)
-	if err != nil {
-		logger.Panicf("FATAL: cannot open file for fsync: %s", err)
-	}
-	if err := d.Sync(); err != nil {
-		_ = d.Close()
-		logger.Panicf("FATAL: cannot flush %q to storage: %s", path, err)
-	}
-	if err := d.Close(); err != nil {
-		logger.Panicf("FATAL: cannot close %q: %s", path, err)
-	}
-}
-
-func createFlockFile(flockFile string) (*os.File, error) {
-	flockF, err := os.Create(flockFile)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create lock file %q: %w", flockFile, err)
-	}
-	if err := unix.Flock(int(flockF.Fd()), unix.LOCK_EX|unix.LOCK_NB); err != nil {
-		return nil, fmt.Errorf("cannot acquire lock on file %q: %w", flockFile, err)
-	}
-	return flockF, nil
-}
-
-func mustGetDiskSpace(path string) (total, free uint64) {
-	var stat statfs_t
-	if err := statfs(path, &stat); err != nil {
-		logger.Panicf("FATAL: cannot determine free disk space on %q: %s", path, err)
-	}
-
-	total = totalSpace(stat)
-	free = freeSpace(stat)
-	return
 }
 
 func getFsType(path string) string {
