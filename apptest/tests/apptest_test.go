ea72cd1c595978a014671293c6648bcc3282d708
Zhu Jiekun <jiekun@victoriametrics.com>
2026-06-11 20:43:25 +0800

    Rename integration test environment check function
    Signed-off-by: Zhu Jiekun <jiekun@victoriametrics.com>

--- apptest/tests/apptest_test.go
+++ apptest/tests/apptest_test.go
@@ -10,13 +10,13 @@ import (
 
 func TestMain(m *testing.M) {
 	// check if the test is run via make command
-	checkIntegrationTestEnv()
+	checkApptestEnv()
 
 	// start the integration test.
 	os.Exit(m.Run())
 }
 
-func checkIntegrationTestEnv() {
+func checkApptestEnv() {
 	if os.Getenv("VM_APPTEST") == "" {
 		logger.Warnf("executing apptest with potential outdated binaries. it's recommended to execute via `make apptest` command. check this doc for more details: https://docs.victoriametrics.com/victoriametrics/contributing/#testing")
 	}
