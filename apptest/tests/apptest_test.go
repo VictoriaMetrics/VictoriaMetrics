c0d5bb21830dc86d71a1f96d0805e2d4a7793e99
Zhu Jiekun <jiekun@victoriametrics.com>
2026-06-11 20:42:22 +0800

    Refactor TestMain to remove binary checks
    Removed binary requirement checks from TestMain function.
    
    Signed-off-by: Zhu Jiekun <jiekun@victoriametrics.com>

--- apptest/tests/apptest_test.go
+++ apptest/tests/apptest_test.go
@@ -9,28 +9,11 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	// check if necessary binaries are there.
-	checkBinaryRequirement("../../bin/victoria-metrics-race")
-	checkBinaryRequirement("../../bin/vmagent-race")
-	checkBinaryRequirement("../../bin/vmauth-race")
-	checkBinaryRequirement("../../bin/vmctl-race")
-	checkBinaryRequirement("../../bin/vmbackup-race")
-	checkBinaryRequirement("../../bin/vmrestore-race")
-
 	// check if the test is run via make command
 	checkIntegrationTestEnv()
 
 	// start the integration test.
 	os.Exit(m.Run())
-}
-
-// checkBinaryRequirement panic if required binary not exist.
-func checkBinaryRequirement(path string) {
-	if _, err := os.Stat(path); err != nil {
-		if os.IsNotExist(err) {
-			panic(fmt.Sprintf("apptest failed: %s not found. please run `make apptest` to execute apptest. check how different tests are executed: https://docs.victoriametrics.com/victoriametrics/contributing/#testing", path))
-		}
-	}
 }
 
 func checkIntegrationTestEnv() {
