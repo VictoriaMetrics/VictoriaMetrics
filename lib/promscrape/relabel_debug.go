9d960bd6c5b76bb30b6c4e164c054f79093b89de
Jiekun <jiekun@victoriametrics.com>
2026-06-16 11:45:15 +0800

    relabel: display currenct selection of relabelconfig correctly

--- lib/promscrape/relabel_debug.go
+++ lib/promscrape/relabel_debug.go
@@ -17,16 +17,20 @@ func WriteMetricRelabelDebug(w http.ResponseWriter, r *http.Request, rwGlobalRel
 	relabelConfigs := r.FormValue("relabel_configs")
 	rwRelabelConfigs := r.FormValue("remote_write_relabel_configs") // global + per-URL configs.
 
-	rwURLRelabelConfigsIdx := r.FormValue("url_relabel_configs_index")     // only for per-URL configs and has to be set with reload_url_relabel_configs.
+	rwURLRelabelConfigsIdxStr := r.FormValue("url_relabel_configs_index")  // only for per-URL configs and has to be set with reload_url_relabel_configs.
 	reloadRWURLRelabelConfigs := r.FormValue("reload_url_relabel_configs") // if set, it will reset the whole remote_write_relabel_configs.
 
 	format := r.FormValue("format")
 	var err error
 
 	rwURLRelabelConfigsLength := len(rwURLRelabelConfigss)
+	rwURLRelabelConfigsIdx, idxErr := strconv.Atoi(rwURLRelabelConfigsIdxStr)
+	if idxErr != nil {
+		rwURLRelabelConfigsIdx = -1
+	}
 
 	// if everything is not set, we should load the initial data for user.
-	if metric == "" && relabelConfigs == "" && rwRelabelConfigs == "" && rwURLRelabelConfigsIdx == "" && reloadRWURLRelabelConfigs == "" && targetID != "" {
+	if metric == "" && relabelConfigs == "" && rwRelabelConfigs == "" && rwURLRelabelConfigsIdxStr == "" && reloadRWURLRelabelConfigs == "" && targetID != "" {
 		pcs, labels, ok := getMetricRelabelContextByTargetID(targetID)
 		if !ok {
 			err = fmt.Errorf("cannot find target for id=%s", targetID)
@@ -58,11 +62,10 @@ func WriteMetricRelabelDebug(w http.ResponseWriter, r *http.Request, rwGlobalRel
 		rwURLRelabelConfigs := ""
 		if len(rwURLRelabelConfigss) > 0 {
 			// ignore the error if the input is invalid or exceed the length, and fallback to 0.
-			idx, _ := strconv.Atoi(rwURLRelabelConfigsIdx)
-			if idx < 0 || idx >= len(rwURLRelabelConfigss) {
-				idx = 0
+			if rwURLRelabelConfigsIdx < 0 || rwURLRelabelConfigsIdx >= len(rwURLRelabelConfigss) {
+				rwURLRelabelConfigsIdx = 0
 			}
-			rwURLRelabelConfigs = rwURLRelabelConfigss[idx]
+			rwURLRelabelConfigs = rwURLRelabelConfigss[rwURLRelabelConfigsIdx]
 		}
 
 		// reload will remove the existing content
@@ -80,7 +83,7 @@ func WriteMetricRelabelDebug(w http.ResponseWriter, r *http.Request, rwGlobalRel
 		httpserver.EnableCORS(w, r)
 		w.Header().Set("Content-Type", "application/json")
 	}
-	promrelabel.WriteMetricRelabelDebug(w, targetID, metric, relabelConfigs, rwRelabelConfigs, rwURLRelabelConfigsLength, format, err)
+	promrelabel.WriteMetricRelabelDebug(w, targetID, metric, relabelConfigs, rwRelabelConfigs, rwURLRelabelConfigsLength, rwURLRelabelConfigsIdx, format, err)
 }
 
 // WriteTargetRelabelDebug generates response for /target-relabel-debug page
