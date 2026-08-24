bdb892733322858540f5701a5d4af85e641b68b0
hagen1778 <roman@victoriametrics.com>
2026-08-24 22:47:21 +0200

    app/vmctl: properly close export/import requests on errors
    Before, with an error of export or import requests, its counterpart
    might have keep hanging as no one was reading/writing into the pipe.
    Properly closing requests on connection should help with hanging requests
    that could pile up during long migrations.
    
    Signed-off-by: hagen1778 <roman@victoriametrics.com>

--- app/vmctl/vm_native.go
+++ app/vmctl/vm_native.go
@@ -124,6 +124,11 @@ func (p *vmNativeProcessor) runSingle(ctx context.Context, f native.Filter, srcU
 	if err != nil {
 		return fmt.Errorf("failed to init export pipe: %w", err)
 	}
+	defer func() {
+		// close the export reader on exit, so it doesn't hang at the source
+		// until server-side timeout.
+		_ = reader.Close()
+	}()
 
 	if p.disablePerMetricRequests {
 		pr := bar.NewProxyReader(reader)
@@ -134,10 +139,10 @@ func (p *vmNativeProcessor) runSingle(ctx context.Context, f native.Filter, srcU
 	}
 
 	pr, pw := io.Pipe()
-	importCh := make(chan error)
+	// make importCh buffered so goroutine won't get stuck if nothing reads from chan
+	importCh := make(chan error, 1)
 	go func() {
 		importCh <- p.dst.ImportPipe(ctx, dstURL, pr)
-		close(importCh)
 	}()
 
 	w := io.Writer(pw)
@@ -148,13 +153,17 @@ func (p *vmNativeProcessor) runSingle(ctx context.Context, f native.Filter, srcU
 
 	written, err := io.Copy(w, reader)
 	if err != nil {
-		// io.Copy could fail if ImportPipe will fail before and close the pr
-		// so we check if that's the case and to not ignore importErr if it exists.
+		// close the import writer, so the destination doesn't hang until server-side timeout
+		_ = pw.CloseWithError(err)
 		select {
+		// check if the error happened in the ImportPipe
 		case importErr := <-importCh:
 			if importErr != nil {
 				return fmt.Errorf("failed to import %s: %w", p.dst.Addr, importErr)
 			}
+		// or because vmctl has been stopped
+		case <-ctx.Done():
+			return ctx.Err()
 		default:
 		}
 		return fmt.Errorf("failed to write into %q: %w", p.dst.Addr, err)
