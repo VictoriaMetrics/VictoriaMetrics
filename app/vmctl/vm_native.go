75f5320d516344adab26a69791e8dc33c6a0067d
hagen1778 <roman@victoriametrics.com>
2026-08-25 10:26:59 +0200

    address review comments
    Signed-off-by: hagen1778 <roman@victoriametrics.com>

--- app/vmctl/vm_native.go
+++ app/vmctl/vm_native.go
@@ -120,16 +120,17 @@ func (p *vmNativeProcessor) do(ctx context.Context, f native.Filter, srcURL, dst
 }
 
 func (p *vmNativeProcessor) runSingle(ctx context.Context, f native.Filter, srcURL, dstURL string, bar barpool.Bar) error {
-	reader, err := p.src.ExportPipe(ctx, srcURL, f)
+	exportReader, err := p.src.ExportPipe(ctx, srcURL, f)
 	if err != nil {
 		return fmt.Errorf("failed to init export pipe: %w", err)
 	}
 	defer func() {
 		// close the export reader on exit, so it doesn't hang at the source
 		// until server-side timeout.
-		_ = reader.Close()
+		_ = exportReader.Close()
 	}()
 
+	reader := io.Reader(exportReader)
 	if p.disablePerMetricRequests {
 		pr := bar.NewProxyReader(reader)
 		if pr != nil {
@@ -143,6 +144,7 @@ func (p *vmNativeProcessor) runSingle(ctx context.Context, f native.Filter, srcU
 	importCh := make(chan error, 1)
 	go func() {
 		importCh <- p.dst.ImportPipe(ctx, dstURL, pr)
+		_ = pr.Close()
 	}()
 
 	w := io.Writer(pw)
