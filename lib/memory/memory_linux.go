2e8c19244e47240cf6cef47e176ba4f337bb0936
Zasda Mikail <zmikail@victoriametrics.com>
2026-08-25 07:20:10 +0700

    lib/cgroup: expose cpu and memory host and cgroup limit metrics
    Signed-off-by: Zasda Mikail <zmikail@victoriametrics.com>

--- lib/memory/memory_linux.go
+++ lib/memory/memory_linux.go
@@ -1,6 +1,8 @@
 package memory
 
 import (
+	"math"
+	"os"
 	"syscall"
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/cgroup"
@@ -18,14 +20,28 @@ func sysTotalMemory() int {
 	if uint64(maxInt)/uint64(si.Totalram) > uint64(si.Unit) {
 		totalMem = int(uint64(si.Totalram) * uint64(si.Unit))
 	}
+	memoryHostBytes = float64(totalMem)
 	mem := cgroup.GetMemoryLimit()
 	if mem <= 0 || int64(int(mem)) != mem || int(mem) > totalMem {
 		// Try reading hierarchical memory limit.
 		// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/699
-		mem = cgroup.GetHierarchicalMemoryLimit()
-		if mem <= 0 || int64(int(mem)) != mem || int(mem) > totalMem {
-			return totalMem
+		if hmem := cgroup.GetHierarchicalMemoryLimit(); isCgroupMemoryLimitSet(hmem) {
+			mem = hmem
 		}
 	}
+	if isCgroupMemoryLimitSet(mem) {
+		memoryCgroupBytes = float64(mem)
+	}
+	if mem <= 0 || int64(int(mem)) != mem || int(mem) > totalMem {
+		return totalMem
+	}
 	return int(mem)
+}
+
+// isCgroupMemoryLimitSet returns whether mem is a real cgroup memory limit.
+// Cgroup v1 reports math.MaxInt64 rounded down to the page size when no limit is set.
+func isCgroupMemoryLimitSet(mem int64) bool {
+	pageSize := int64(os.Getpagesize())
+	noLimit := math.MaxInt64 / pageSize * pageSize
+	return mem > 0 && mem < noLimit
 }
