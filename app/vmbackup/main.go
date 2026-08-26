1cb4c4188cec15667f4e93d8c147512f0eabc5b6
f41gh7 <nik@victoriametrics.com>
2026-08-26 10:05:30 +0200

    lib/httpserver: add support for listening on Unix domain socket
    The syntax is `-httpListenAddr=unix:/path/to/file`. This allows
    restricting access to HTTP API via filesystem permissions and avoid TCP
    overhead.
    
    `-tls` and `-httpListenAddr.useProxyProtocol` flags cannot be used with
    Unix domain sockets.
    
    Fixes: https://github.com/VictoriaMetrics/VictoriaLogs/issues/1618
    Updates: https://github.com/VictoriaMetrics/VictoriaMetrics/pull/10331

--- app/vmbackup/main.go
+++ app/vmbackup/main.go
@@ -27,7 +27,8 @@ import (
 )
 
 var (
-	httpListenAddr    = flag.String("httpListenAddr", ":8420", "TCP address for exporting metrics at /metrics page")
+	httpListenAddr = flag.String("httpListenAddr", ":8420", "Address for exporting metrics at /metrics page. "+
+		"Use unix:/path/to/socket to listen on Unix domain socket")
 	storageDataPath   = flag.String("storageDataPath", "victoria-metrics-data", "Path to VictoriaMetrics data. Must match -storageDataPath from VictoriaMetrics or vmstorage")
 	snapshotName      = flag.String("snapshotName", "", "Name for the snapshot to backup. See https://docs.victoriametrics.com/victoriametrics/single-server-victoriametrics/#how-to-work-with-snapshots. There is no need in setting -snapshotName if -snapshot.createURL is set")
 	snapshotCreateURL = flag.String("snapshot.createURL", "", "VictoriaMetrics create snapshot url. When this is given a snapshot will automatically be created during backup. "+
--- app/vmstorage/main.go
+++ app/vmstorage/main.go
@@ -43,7 +43,9 @@ var (
 		"By default, or when set to 0, -maxBackfillAge equals to -retentionPeriod, e.g. it is unlimited within the configured retention. "+
 		"This can be useful for limiting ingestion of historical samples, for example, when older data has been moved to another storage tier. "+
 		"See https://docs.victoriametrics.com/victoriametrics/single-server-victoriametrics/#retention")
-	httpListenAddrs  = flagutil.NewArrayString("httpListenAddr", "Address to listen for incoming http requests. See also -httpListenAddr.useProxyProtocol")
+	httpListenAddrs = flagutil.NewArrayString("httpListenAddr", "Address to listen for incoming http requests. "+
+		"See also -httpListenAddr.useProxyProtocol. "+
+		"Use unix:/path/to/socket to listen on Unix domain socket. Note that -tls and -httpListenAddr.useProxyProtocol cannot be used with Unix sockets")
 	useProxyProtocol = flagutil.NewArrayBool("httpListenAddr.useProxyProtocol", "Whether to use proxy protocol for connections accepted at the given -httpListenAddr . "+
 		"See https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt . "+
 		"With enabled proxy protocol http server cannot serve regular /metrics endpoint. Use -pushmetrics.url for metrics pushing")
