551bee46973fbc1568cd109e1533bda21ea4030c
Zasda Mikail <zmikail@victoriametrics.com>
2026-09-22 15:07:08 +0700

    lib/fs: clarify hints

--- lib/fs/parallel.go
+++ lib/fs/parallel.go
@@ -18,17 +18,17 @@ type parallelReaderAtOpenerTask struct {
 	path                  string
 	rc                    *MustReadAtCloser
 	fileSize              *uint64
-	disableRandomReadHint bool
+	disableAdviseRandomRead bool
 }
 
 // Add adds a task for opening the file at the given path and storing it to *rc, while storing the file size into *fileSize.
 // Call Run() for running all the registered tasks in parallel.
-func (pro *ParallelReaderAtOpener) Add(path string, rc *MustReadAtCloser, fileSize *uint64, disableRandomReadHint bool) {
+func (pro *ParallelReaderAtOpener) Add(path string, rc *MustReadAtCloser, fileSize *uint64, disableAdviseRandomRead bool) {
 	pro.tasks = append(pro.tasks, parallelReaderAtOpenerTask{
 		path:                  path,
 		rc:                    rc,
 		fileSize:              fileSize,
-		disableRandomReadHint: disableRandomReadHint,
+		disableAdviseRandomRead: disableAdviseRandomRead,
 	})
 }
 
@@ -41,7 +41,7 @@ func (pro *ParallelReaderAtOpener) Run() {
 
 		wg.Go(func() {
 			r := MustOpenReaderAt(task.path)
-			r.disableRandomReadHint = task.disableRandomReadHint
+			r.disableRandomReadHint = task.disableAdviseRandomRead
 			*task.rc = r
 			*task.fileSize = MustFileSize(task.path)
 
--- lib/fs/reader_at.go
+++ lib/fs/reader_at.go
@@ -17,9 +17,9 @@ var disableMmap = flag.Bool("fs.disableMmap", is32BitPtr, "Whether to use pread(
 	"By default, mmap() is used for 64-bit arches and pread() is used for 32-bit arches, since they cannot read data files bigger than 2^32 bytes in memory. "+
 	"mmap() is usually faster for reading small data chunks than pread()")
 
-var disableAdviseRandomRead = flag.Bool("fs.disableAdviseRandomRead", true, "Whether to disable FADV_RANDOM and MADV_RANDOM hints for data files. "+
-	"These hints reduce the amount of unneeded data read from disk during queries via pread() and mmap(). "+
-	"Disabling these hints may improve performance for heavy queries that read most of the data in large data files.")
+var disableAdviseRandomRead = flag.Bool("fs.disableAdviseRandomRead", true, "Whether to disable FADV_RANDOM and MADV_RANDOM hints for data part files. "+
+	"Typical queries with selectors access data part files at random offsets, so FADV_RANDOM is enforced to reduce excessive disk read of readahead."+
+	"Use this flag if your query read pattern benefits from sequential read (e.g. query by metric name without selectors) to prefer FADV_NORMAL.")
 
 var disableMincore = flag.Bool("fs.disableMincore", false, "Whether to disable the mincore() syscall for checking mmap()ed files. "+
 	"By default, mincore() is used to detect whether mmap()ed file pages are resident in memory. "+
